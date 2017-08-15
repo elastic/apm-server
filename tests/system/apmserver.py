d71d162ceddfcf50543518ddb618090aa8420d86
simitt <mitter.silvia@gmail.com>
2017-08-15 15:30:17 +0200

    Remove python tests for fields documentation.
    Duplicate test, since this was moved over to go.

--- tests/system/apmserver.py
+++ tests/system/apmserver.py
@@ -14,22 +14,6 @@ class BaseTest(TestCase):
         cls.beat_path = "../../"
         super(BaseTest, cls).setUpClass()
 
-    # TODO: rewrite system test in go
-    def assert_fields_are_documented(self, evt):
-        """
-        Assert that all keys present in evt are documented in fields.yml.
-        This reads from the global fields.yml,
-        means `make collect` has to be run before the check.
-        """
-        expected_fields, dict_fields = self.load_fields()
-        flat = self.flatten_object(evt, dict_fields)
-
-        for key in flat.keys():
-            # Allow any keys for "tags" subgroup.
-            if key not in expected_fields and not key.startswith("transaction.tags."):
-                raise Exception("Key '{}' found in event is not documented!"
-                                .format(key))
-
 
 class ServerBaseTest(BaseTest):
 
