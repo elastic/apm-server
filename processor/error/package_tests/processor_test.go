b8424e0238090a82daed9ff1311613746213cff0
simitt <mitter.silvia@gmail.com>
2017-08-16 13:48:07 +0200

    Properly test every json schema on its own.
    Raise error if invalid json example file is not added to tests.
    
    closes #48.

--- processor/error/package_tests/processor_test.go
+++ processor/error/package_tests/processor_test.go
@@ -2,11 +2,7 @@ package package_tests
 
 import (
 	"bytes"
-	"path/filepath"
-	"strings"
 	"testing"
-
-	"fmt"
 
 	"github.com/stretchr/testify/assert"
 
@@ -24,32 +20,10 @@ func TestProcessorOK(t *testing.T) {
 	tests.TestProcessRequests(t, er.NewProcessor, requestInfo)
 }
 
-// ensure all invalid documents fail the json schema validation already
+// ensure invalid documents fail the json schema validation already
 func TestProcessorFailedValidation(t *testing.T) {
-	invalidRequests := [][]string{
-		{"invalid_app_name.json", "[#/properties/app/properties/name/pattern] does not match pattern"},
-		{"invalid_id.json", "[#/properties/errors/items/properties/id/pattern] does not match pattern"},
-		{"invalid_timestamp.json", "[#/properties/errors/items/properties/timestamp/pattern] does not match pattern"},
-		{"invalid_timestamp2.json", "[#/properties/errors/items/properties/timestamp/pattern] does not match pattern"},
-		{"invalid_timestamp3.json", "[#/properties/errors/items/properties/timestamp/pattern] does not match pattern"},
-		{"invalid_timestamp4.json", "[#/properties/errors/items/properties/timestamp/pattern] does not match pattern"},
-		{"no_log_message.json", "missing properties: \"message\""},
-		{"no_exception_message.json", "missing properties: \"message\""},
-		{"no_log_or_exception.json", "missing properties: \"exception\""},
-		{"no_log_or_exception.json", "missing properties: \"log\""},
-		{"no_timestamp.json", "missing properties: \"timestamp\""},
-		{"no_app.json", "missing properties: \"app\""},
-		{"no_http_url.json", "missing properties: \"url\""},
-		{"no_http_method.json", "missing properties: \"method\""},
-		{"no_stacktrace_filename.json", "missing properties: \"filename\""},
-		{"no_stacktrace_lineno.json", "missing properties: \"lineno\""},
-		{"non_string_tags.json", "expected string, but got object"},
-	}
-	for _, dataRow := range invalidRequests {
-		data, err := tests.LoadData(filepath.Join("tests/data/invalid/error/", dataRow[0]))
-		assert.Nil(t, err)
-		err = er.NewProcessor().Validate(bytes.NewReader(data))
-		assert.NotNil(t, err)
-		assert.True(t, strings.Contains(err.Error(), dataRow[1]), fmt.Sprintf("'%v' not found in '%v'", dataRow[1], err.Error()))
-	}
+	data, err := tests.LoadInvalidData("error")
+	assert.Nil(t, err)
+	err = er.NewProcessor().Validate(bytes.NewReader(data))
+	assert.NotNil(t, err)
 }
