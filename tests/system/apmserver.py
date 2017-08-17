95366f58f282711b9a3c485fbc31b281b084496d
Juan Álvarez <juan.alvarez@elastic.co>
2017-08-17 20:24:48 +0200

    SSL system tests (#66)
    * Private key and certificate for SSL tests, signed for localhost and not verified by CA
    * Add SSL system tests
    * method name more explicit
    
     Minor refactor:
    - Show returned status code as assertion error message.
    - Create test classes so to reuse configuration for different scenarios.

--- tests/system/apmserver.py
+++ tests/system/apmserver.py
@@ -1,10 +1,14 @@
 import sys
 import os
+import json
+import shutil
 sys.path.append('../../_beats/libbeat/tests/system')
 from beat.beat import TestCase
-
+from elasticsearch import Elasticsearch
 
 # TODO: rework system tests in go
+
+
 class BaseTest(TestCase):
 
     @classmethod
@@ -14,17 +18,99 @@ class BaseTest(TestCase):
         cls.beat_path = "../../"
         super(BaseTest, cls).setUpClass()
 
+    def get_transaction_payload(self):
+        path = os.path.abspath(os.path.join(self.beat_path,
+                                            'tests',
+                                            'data',
+                                            'valid',
+                                            'transaction',
+                                            'payload.json'))
+        transactions = json.loads(open(path).read())
+        return json.dumps(transactions)
+
 
 class ServerBaseTest(BaseTest):
 
+    def config(self):
+        return {
+            "ssl_enabled": "false",
+            "path": os.path.abspath(self.working_dir) + "/log/*"
+        }
+
     def setUp(self):
         super(ServerBaseTest, self).setUp()
-        self.render_config_template(
-            path=os.path.abspath(self.working_dir) + "/log/*"
-        )
+        self.render_config_template(**self.config())
         self.apmserver_proc = self.start_beat()
         self.wait_until(lambda: self.log_contains("apm-server is running"))
 
     def tearDown(self):
         super(ServerBaseTest, self).tearDown()
-        self.apmserver_proc.kill_and_wait()
+        self.apmserver_proc.check_kill_and_wait()
+
+
+class SecureServerBaseTest(ServerBaseTest):
+
+    def config(self):
+        cfg = super(SecureServerBaseTest, self).config()
+        cfg.update({
+            "ssl_enabled": "true",
+            "ssl_cert": "config/certs/cert.pem",
+            "ssl_key": "config/certs/key.pem",
+        })
+        return cfg
+
+
+class AccessTest(ServerBaseTest):
+
+    def config(self):
+        cfg = super(AccessTest, self).config()
+        cfg.update({"secret_token": "1234"})
+        return cfg
+
+
+class ElasticTest(ServerBaseTest):
+
+    @classmethod
+    def setUpClass(cls):
+        super(ElasticTest, cls).setUpClass()
+        cls.base_name = "apm-server-tests"
+        cls.beat_version = "0.1.1"
+        cls.index_name = cls.base_name + "-" + cls.beat_version + "-1"
+
+    def config(self):
+        cfg = super(ElasticTest, self).config()
+        cfg.update({"elasticsearch_host": self.get_elasticsearch_url(),
+                    "file_enabled": "false",
+                    "index_name": self.index_name,
+                    "template_base_name": self.base_name})
+        return cfg
+
+    def setUp(self):
+        super(ElasticTest, self).setUp()
+
+        self.es = Elasticsearch([self.get_elasticsearch_url()])
+
+        # Cleanup index and template first
+        try:
+            self.es.indices.delete(index=self.index_name)
+        except:
+            pass
+        self.wait_until(lambda: not self.es.indices.exists(self.base_name))
+
+        try:
+            self.es.indices.delete_template(
+                name=self.base_name + "-" + self.beat_version)
+        except:
+            pass
+
+        shutil.copy(self.beat_path + "/fields.yml", self.working_dir)
+
+    def get_elasticsearch_url(self):
+        """
+        Returns an elasticsearch.Elasticsearch instance built from the
+        env variables like the integration tests.
+        """
+        return "http://{host}:{port}".format(
+            host=os.getenv("ES_HOST", "localhost"),
+            port=os.getenv("ES_PORT", "9200"),
+        )
--- tests/system/test_requests.py
+++ tests/system/test_requests.py
@@ -1,8 +1,8 @@
-import json
+from nose.tools import raises
 
-from apmserver import ServerBaseTest
+from apmserver import ServerBaseTest, SecureServerBaseTest
+from requests.exceptions import SSLError
 import requests
-import os
 import zlib
 import gzip
 try:
@@ -15,37 +15,37 @@ class Test(ServerBaseTest):
     transactions_url = 'http://localhost:8080/v1/transactions'
 
     def test_ok(self):
-        transactions = self.get_payload()
+        transactions = self.get_transaction_payload()
         r = requests.post(self.transactions_url, data=transactions,
                           headers={'Content-Type': 'application/json'})
-        assert r.status_code == 202
+        assert r.status_code == 202, r.status_code
 
     def test_empty(self):
         transactions = {}
         r = requests.post(self.transactions_url, json=transactions)
-        assert r.status_code == 400
+        assert r.status_code == 400, r.status_code
 
     def test_not_existent(self):
         transactions = {}
         invalid_url = 'http://localhost:8080/transactionX'
         r = requests.post(invalid_url, json=transactions)
-        assert r.status_code == 404
+        assert r.status_code == 404, r.status_code
 
     def test_method_not_allowed(self):
         r = requests.get(self.transactions_url)
-        assert r.status_code == 405
+        assert r.status_code == 405, r.status_code
 
     def test_bad_json(self):
         r = requests.post(self.transactions_url, json="not json")
-        assert r.status_code == 400
+        assert r.status_code == 400, r.status_code
 
     def test_healthcheck(self):
         healtcheck_url = 'http://localhost:8080/healthcheck'
         r = requests.get(healtcheck_url)
-        assert r.status_code == 200
+        assert r.status_code == 200, r.status_code
 
     def test_gzip(self):
-        transactions = self.get_payload()
+        transactions = self.get_transaction_payload()
 
         out = ""
         try:
@@ -61,10 +61,10 @@ class Test(ServerBaseTest):
 
         r = requests.post(self.transactions_url, data=out.getvalue(),
                           headers={'Content-Encoding': 'gzip', 'Content-Type': 'application/json'})
-        assert r.status_code == 202
+        assert r.status_code == 202, r.status_code
 
     def test_deflat(self):
-        transactions = self.get_payload()
+        transactions = self.get_transaction_payload()
         compressed_data = None
         try:
             compressed_data = zlib.compress(transactions)
@@ -73,28 +73,36 @@ class Test(ServerBaseTest):
 
         r = requests.post(self.transactions_url, data=compressed_data,
                           headers={'Content-Encoding': 'deflate', 'Content-Type': 'application/json'})
-        assert r.status_code == 202
+        assert r.status_code == 202, r.status_code
 
     def test_gzip_error(self):
-        data = self.get_payload()
+        data = self.get_transaction_payload()
 
         r = requests.post(self.transactions_url, data=data,
                           headers={'Content-Encoding': 'gzip', 'Content-Type': 'application/json'})
-        assert r.status_code == 400
+        assert r.status_code == 400, r.status_code
 
     def test_deflate_error(self):
-        data = self.get_payload()
+        data = self.get_transaction_payload()
 
         r = requests.post(self.transactions_url, data=data,
                           headers={'Content-Encoding': 'deflate', 'Content-Type': 'application/json'})
-        assert r.status_code == 400
+        assert r.status_code == 400, r.status_code
 
-    def get_payload(self):
-        path = os.path.abspath(os.path.join(self.beat_path,
-                                            'tests',
-                                            'data',
-                                            'valid',
-                                            'transaction',
-                                            'payload.json'))
-        transactions = json.loads(open(path).read())
-        return json.dumps(transactions)
+
+class SecureTest(SecureServerBaseTest):
+
+    def test_https_ok(self):
+        transactions = self.get_transaction_payload()
+        r = requests.post("https://localhost:8080/v1/transactions",
+                          data=transactions,
+                          headers={'Content-Type': 'application/json'},
+                          verify=False)
+        assert r.status_code == 202, r.status_code
+
+    @raises(SSLError)
+    def test_https_verify(self):
+        transactions = self.get_transaction_payload()
+        requests.post("https://localhost:8080/v1/transactions",
+                      data=transactions,
+                      headers={'Content-Type': 'application/json'})
