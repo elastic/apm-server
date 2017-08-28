d29ffbb1ee829af31ebf426189a4823799db1f16
Nicolas Ruflin <spam@ruflin.com>
2017-08-28 10:44:12 +0200

    Introduce metrics to apm-server (#104)
    This is a first take at adding metrics to the APM server. This is a basic set of metrics to get stats for requests, response and processor internals. We should adjust and add and modify these metrics over time when we see which ones are valuable in production, which ones are not and which ones are missing. This only sets the foundation.
    
    These metrics can be later used to be shipped for monitoring. To access the metrics right now start apm-server as following:
    
    ```
    ./apm-server -e -E http.enabled=true
    ```
    
    Then you can access the metrics under http://localhost:5066/stats?pretty
    
    Closes https://github.com/elastic/apm-server/issues/99

--- processor/transaction/processor.go
+++ processor/transaction/processor.go
@@ -5,6 +5,7 @@ import (
 
 	pr "github.com/elastic/apm-server/processor"
 	"github.com/elastic/beats/libbeat/beat"
+	"github.com/elastic/beats/libbeat/monitoring"
 
 	"github.com/santhosh-tekuri/jsonschema"
 )
@@ -12,6 +13,13 @@ import (
 func init() {
 	pr.Registry.AddProcessor(Endpoint, NewProcessor())
 }
+
+var (
+	transactionMetrics = monitoring.Default.NewRegistry("apm-server.processor.transaction")
+	transformations    = monitoring.NewInt(transactionMetrics, "transformations")
+	validationCount    = monitoring.NewInt(transactionMetrics, "validation.count")
+	validationError    = monitoring.NewInt(transactionMetrics, "validation.errors")
+)
 
 const (
 	Endpoint      = "/v1/transactions"
@@ -29,11 +37,17 @@ type processor struct {
 }
 
 func (p *processor) Validate(buf []byte) error {
-	return pr.Validate(buf, p.schema)
+	validationCount.Inc()
+	err := pr.Validate(buf, p.schema)
+	if err != nil {
+		validationError.Inc()
+	}
+	return err
 }
 
 func (p *processor) Transform(buf []byte) ([]beat.Event, error) {
 	var pa payload
+	transformations.Inc()
 	err := json.Unmarshal(buf, &pa)
 	if err != nil {
 		return nil, err
