4c535d181ea2fe9a5761bae1ef1cc0ff60e6565a
ruflin <spam@ruflin.com>
2017-09-01 13:27:10 +0200

    Remove apm-server specific version
    This removes the version in apm-server which we used previously for internal release tracking. apm-server from now on automatically follows the version of the elastic stack given by libbeat.

--- cmd/root.go
+++ cmd/root.go
@@ -18,5 +18,5 @@ var RootCmd *cmd.BeatsRootCmd
 
 func init() {
 	var runFlags = pflag.NewFlagSet(Name, pflag.ExitOnError)
-	RootCmd = cmd.GenRootCmdWithRunFlags(Name, "0.2.0", beater.New, runFlags)
+	RootCmd = cmd.GenRootCmdWithRunFlags(Name, "", beater.New, runFlags)
 }
