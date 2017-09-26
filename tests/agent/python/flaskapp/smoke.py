cc5aeee11c1a5626162a41fc5e23dbdab72643f1
Juan A <juan@opbeat.com>
2017-09-26 14:06:01 +0200

    incorporate changes suggested by Beni, which mostly kills gunicorn workers in a more reliable way

--- tests/agent/python/flaskapp/smoke.py
+++ tests/agent/python/flaskapp/smoke.py
@@ -2,14 +2,13 @@ import time
 from tornado import ioloop, httpclient
 from elasticsearch import Elasticsearch
 from datetime import datetime, timedelta
-import os
-import signal
 import subprocess
 import logging
-import random
 import json
 import copy
+import uuid
 
+p_uid = uuid.uuid4().hex
 
 num_reqs = 0    # use with care
 
@@ -43,39 +42,38 @@ def handle(r):
 def start_gunicorn(nworkers=4):
     cmd = ['gunicorn', '-w', str(nworkers), '-b', '0.0.0.0:5000', 'app:app']
     p = subprocess.Popen(cmd, shell=False)
-    return p.pid
+    return p
 
 
 def send_batch(nreqs):
     global num_reqs
+    global p_uid
+
     http_client = httpclient.AsyncHTTPClient(max_clients=4)
     for _ in range(nreqs):
         num_reqs += 1
         endpoint = "foo" if num_reqs % 2 == 0 else "bar"
-        url = "http://localhost:5000/" + endpoint
+        url = "http://localhost:5000/" + endpoint + '?' + p_uid
         http_client.fetch(url, handle, method='GET', connect_timeout=90, request_timeout=120)
-        time.sleep(random.random() / 10)
+
     logger.info("Starting tornado I/O loop")
     ioloop.IOLoop.instance().start()
 
 
 class Gunicorn:
-    # todo make this nicer / more resilient
-    SLEEP = 3
 
     def __init__(self, nworkers):
         self.w = nworkers
 
     def __enter__(self):
-        self.pid = start_gunicorn(self.w)
-        time.sleep(self.SLEEP)
-        logger.info("Gunicorn ready {}".format(self.pid))
+        self.p = start_gunicorn(self.w)
+        time.sleep(3)
+        logger.info("Gunicorn ready {}".format(self.p.pid))
 
     def __exit__(self, *args):
         logger.info("Flushing queues")
-        time.sleep(self.SLEEP)
-        os.kill(self.pid, signal.SIGKILL)
-        time.sleep(self.SLEEP)
+        self.p.terminate()
+        time.sleep(10)
         logger.info("Gunicorn terminated")
 
 
@@ -84,26 +82,46 @@ def load_test(nworkers, nreqs):
         send_batch(nreqs)
 
 
-def check(index_name, count):
+def check(index_name, size, it):
+    count = size * it
+    es.indices.refresh(index_name)
 
     def anomaly(x): return x > 100000 or x < 1  # 100000 = 0.1 sec
+
+    err = "queried for {}, expected {}, got {}"
+    for (doc_type, c) in [('transaction', count), ('trace', count * 2)]:
+        rs = es.count(index=index_name, body=es_query("processor.event", doc_type))
+        assert rs['count'] == c, err.format(doc_type, c, rs)
+
+    for (trace_name, c) in [('app.foo', count / 2), ('app.bar', count / 2), ('transaction', count)]:
+        rs = es.count(index=index_name, body=es_query("trace.name", trace_name))
+        assert rs['count'] == c, err.format(trace_name, c, rs)
+
+    for transaction_name in ['GET /foo', 'GET /bar']:
+        rs = es.count(index=index_name, body=es_query("transaction.name.keyword", transaction_name))
+        assert rs['count'] == count / 2, err.format(transaction_name, count / 2, rs)
 
     transactions_query = es_query("processor.event", "transaction")
     transaction_dict = {}
     for hit in lookup(es.search(index, body=transactions_query), 'hits', 'hits'):
 
-        timestamp = datetime.strptime(lookup(hit, '_source', '@timestamp'), '%Y-%m-%dT%H:%M:%S.%fZ')
-        assert datetime.utcnow() - timedelta(minutes=2) < timestamp < datetime.utcnow(), \
-            "{} is too far of {} ".format(timestamp, datetime.utcnow())
-
         transaction = lookup(hit, '_source', 'transaction')
         duration = lookup(transaction, 'duration', 'us')
+
+        transaction_dict[transaction['id']] = (transaction['name'], duration)
+
         assert not anomaly(duration), duration
+
+        timestamp = datetime.strptime(lookup(hit, '_source', '@timestamp'), '%Y-%m-%dT%H:%M:%S.%fZ')
+        assert datetime.utcnow() - timedelta(minutes=it) < timestamp < datetime.utcnow(), \
+            "{} is too far of {} ".format(timestamp, datetime.utcnow())
 
         assert transaction['result'] == '200', transaction['result']
         assert transaction['type'] == 'web.flask', transaction['type']
 
         context = lookup(hit, '_source', 'context')
+        assert context['request']['url']['search'] == p_uid, \
+            "{} not in context {}".format(p_uid, context)
         assert lookup(context, 'app', 'language', 'name') == 'python', context
         assert lookup(context, 'app', 'name') == 'test-app', context
         agent_version = __import__('pkg_resources').get_distribution('elastic-apm').version
@@ -116,8 +134,6 @@ def check(index_name, count):
         assert context['tags'] == {}, context
 
         assert hit['_source']['processor'] == {'name': 'transaction', 'event': 'transaction'}
-
-        transaction_dict[transaction['id']] = (transaction['name'], duration)
 
     traces_query = es_query("processor.event", "trace")
     for hit in lookup(es.search(index, body=traces_query), 'hits', 'hits'):
@@ -161,19 +177,6 @@ def check(index_name, count):
             assert trace['id'] == 0
             assert False, "trace name not expected {}".format(trace['name'])
 
-    err = "queried for {}, expected {}, got {}"
-    for (doc_type, c) in [('transaction', count), ('trace', count * 2)]:
-        rs = es.count(index=index_name, body=es_query("processor.event", doc_type))
-        assert rs['count'] == c, err.format(doc_type, c, rs)
-
-    for (trace_name, c) in [('app.foo', count / 2), ('app.bar', count / 2), ('transaction', count)]:
-        rs = es.count(index=index_name, body=es_query("trace.name", trace_name))
-        assert rs['count'] == c, err.format(trace_name, c, rs)
-
-    for transaction_name in ['GET /foo', 'GET /bar']:
-        rs = es.count(index=index_name, body=es_query("transaction.name.keyword", transaction_name))
-        assert rs['count'] == count / 2, err.format(transaction_name, count / 2, rs)
-
 
 def lookup(d, *keys):
     d1 = copy.deepcopy(d)
@@ -200,8 +203,8 @@ def reset():
 
 if __name__ == '__main__':
     # todo make these input-able
-    iters = 3
-    size = 100
+    iters = 10
+    size = 1000
     workers = 4
 
     index = reset()
@@ -209,7 +212,7 @@ if __name__ == '__main__':
     for it in range(1, iters + 1):
         logger.info("Sending batch {} / {}".format(it, iters))
         load_test(workers, size)
-        check(index, size * it)
+        check(index, size, it)
         logger.info("So far so good...")
 
     logger.info("ALL DONE")
