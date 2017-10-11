e6ae706a640540fc453b14b78a23edfa72664bf1
Silvia Mitter <silvia.mitter@elastic.co>
2017-10-11 15:55:28 +0200

    Update to latest beats master functionality. (#228)
    * Update Fields.yml entries according to beats changes.
    * Change url_template string to versionized string format.

--- vendor/github.com/elastic/beats/libbeat/cmd/instance/beat.go
+++ vendor/github.com/elastic/beats/libbeat/cmd/instance/beat.go
@@ -537,7 +537,7 @@ func (b *Beat) loadDashboards(force bool) error {
 		if b.Config.Output.Name() == "elasticsearch" {
 			esConfig = b.Config.Output.Config()
 		}
-		err := dashboards.ImportDashboards(b.Info.Beat, b.Info.Version, paths.Resolve(paths.Home, ""),
+		err := dashboards.ImportDashboards(b.Info.Beat, b.Info.Name, paths.Resolve(paths.Home, ""),
 			b.Config.Kibana, esConfig, b.Config.Dashboards, nil)
 		if err != nil {
 			return fmt.Errorf("Error importing Kibana dashboards: %v", err)
@@ -575,7 +575,7 @@ func (b *Beat) registerTemplateLoading() error {
 			return err
 		}
 
-		if esCfg.Index != "" && (cfg.Name == "" || cfg.Pattern == "") {
+		if esCfg.Index != "" && (cfg.Name == "" || cfg.Pattern == "") && (b.Config.Template == nil || b.Config.Template.Enabled()) {
 			return fmt.Errorf("setup.template.name and setup.template.pattern have to be set if index name is modified.")
 		}
 
