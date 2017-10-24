67e395f22cf3f6a204752134d56ddc5991dd2a90
Juan A <juan@opbeat.com>
2017-10-24 10:18:49 +0200

    rename as per code review suggestion

--- tests/system/test_requests.py
+++ tests/system/test_requests.py
@@ -54,7 +54,7 @@ class Test(ServerBaseTest):
         assert r.status_code == 400, r.status_code
         assert "Problem validating JSON document against schema" in r.content, r.content
 
-    def test_frontend_disabled(self):
+    def test_frontend_default_disabled(self):
         transactions = self.get_transaction_payload()
         r = requests.post('http://localhost:8200/v1/client-side/transactions', json=transactions)
         assert r.status_code == 403, r.status_code
