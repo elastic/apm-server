11bcfe3d12aef1b2604f8bbd30f2a82cf9347201
Jim Kalafut <jim@kalafut.net>
2017-10-26 02:27:16 -0700

    Add isAuthorized unit tests (#254)

--- processor/processor.go
+++ processor/processor.go
@@ -11,9 +11,9 @@ import (
 type NewProcessor func() Processor
 
 const (
-	Backend     = iota
-	Frontend    = iota
-	HealthCheck = iota
+	Backend = iota
+	Frontend
+	HealthCheck
 )
 
 type Processor interface {
