b2cbc203dc122cdbf76892ca25cb067128cc5b8e
Juan A <juan@opbeat.com>
2017-11-03 10:54:51 +0100

    disable urllib warnings so that it doesn't break tests with unexpected exit code 2

--- tests/system/test_requests.py
+++ tests/system/test_requests.py
@@ -10,6 +10,7 @@ import time
 from datetime import datetime
 from collections import defaultdict
 import threading
+import urllib3
 
 try:
     from StringIO import StringIO
@@ -117,6 +118,7 @@ class Test(ServerBaseTest):
 class SecureTest(SecureServerBaseTest):
 
     def test_https_ok(self):
+        urllib3.disable_warnings()
         transactions = self.get_transaction_payload()
         r = requests.post("https://localhost:8200/v1/transactions",
                           json=transactions, verify=False)
