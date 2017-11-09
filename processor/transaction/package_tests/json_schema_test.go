473fe9fa565cb2d51bb6190261ad2248d9b7b607
Juan A <juan@opbeat.com>
2017-11-09 16:48:41 +0100

    Split app spec in 2 files so the core attributes can be reused by the upcoming sourcemap data type.
    Many packages tests required some rework, as this introduces new semantics that have never been used before, and the tests didn't know how to walk the schemas with the new semantics.
    KeywordLengthValidation tests have been removed due to complexity.

--- processor/transaction/package_tests/json_schema_test.go
+++ processor/transaction/package_tests/json_schema_test.go
@@ -5,7 +5,6 @@ import (
 
 	"github.com/fatih/set"
 
-	"github.com/elastic/apm-server/processor/transaction"
 	"github.com/elastic/apm-server/tests"
 )
 
@@ -19,25 +18,13 @@ func TestPayloadAttributesInSchema(t *testing.T) {
 		"transactions.context.request.headers.array",
 		"transactions.context.request.env.SERVER_SOFTWARE",
 		"transactions.context.request.env.GATEWAY_INTERFACE",
-		"transactions.context.request.body",
 		"transactions.context.request.cookies.c1",
 		"transactions.context.request.cookies.c2",
-		"transactions.context.custom",
 		"transactions.context.custom.my_key",
 		"transactions.context.custom.some_other_value",
 		"transactions.context.custom.and_objects",
 		"transactions.context.custom.and_objects.foo",
-		"transactions.context.tags",
 		"transactions.context.tags.organization_uuid",
 	)
-	tests.TestPayloadAttributesInSchema(t, "transaction", undocumented, transaction.Schema())
-}
-
-func TestJsonSchemaKeywordLimitation(t *testing.T) {
-	fieldsPaths := []string{
-		"./../../../_meta/fields.common.yml",
-		"./../_meta/fields.yml",
-	}
-	exceptions := set.New("processor.event", "processor.name", "context.app.name", "transaction.id", "trace.transaction_id", "listening")
-	tests.TestJsonSchemaKeywordLimitation(t, fieldsPaths, transaction.Schema(), exceptions)
+	tests.TestPayloadAttributesInSchema(t, "transaction/payload.json", undocumented, "transactions/payload.json")
 }
