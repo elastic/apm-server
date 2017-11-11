1dccbe318fccf24784b39da23a57e766a96366af
Ron Cohen <ron@elastic.co>
2017-11-11 15:32:56 +0100

    Separate core app attributes into own JSON Spec
    This is done in preperation for sourcemap support.
    Based on @jalvz changes.

--- tests/json_schema.go
+++ tests/json_schema.go
@@ -3,13 +3,9 @@ package tests
 import (
 	"encoding/json"
 	"fmt"
-
+	"io"
+	"strings"
 	"testing"
-
-	"io/ioutil"
-
-	"path/filepath"
-	"runtime"
 
 	"github.com/fatih/set"
 	"github.com/stretchr/testify/assert"
@@ -22,31 +18,25 @@ type Schema struct {
 	PatternProperties    map[string]interface{}
 	Items                *Schema
 	MaxLength            int
+	AllOf                []Schema
 }
 type Mapping struct {
 	from string
 	to   string
 }
 
-// not exhaustive list, just the words currently used in our spec
-var jsonSchemaReservedWords = []string{"$schema", "$id", "type", "description", "anyOf", "required", "title", "items",
-	"minItems", "allOf", "properties", "maxLength", "pattern", "enum", "default", "format", "additionalProperties",
-	"regexProperties", "dependencies"}
-
-func TestPayloadAttributesInSchema(t *testing.T, name string, undocumented *set.Set, specDir string) {
-
-	schema, specDir := loadSchema(specDir, "")
-	schemaNames := set.New()
-	flattenJsonKeys(schemaNames, schema, specDir, "")
-
-	var payload map[string]interface{}
+func TestPayloadAttributesInSchema(t *testing.T, name string, undocumentedAttrs *set.Set, schema string) {
+	var payload interface{}
 	UnmarshalValidData(name, &payload)
 	jsonNames := set.New()
-	flattenJsonKeys(jsonNames, payload, "", "")
-	jsonNames = set.Difference(jsonNames, undocumented).(*set.Set)
+	flattenJsonKeys(payload, "", jsonNames)
+	jsonNamesDoc := set.Difference(jsonNames, undocumentedAttrs).(*set.Set)
 
-	missing := set.Difference(jsonNames, schemaNames).(*set.Set)
+	schemaStruct, _ := schemaStruct(strings.NewReader(schema))
+	schemaNames := set.New()
+	flattenSchemaNames(schemaStruct, "", addAllPropNames, schemaNames)
 
+	missing := set.Difference(jsonNamesDoc, schemaNames).(*set.Set)
 	if missing.Size() > 0 {
 		msg := fmt.Sprintf("Json Transaction Payload fields missing in Schema %v", missing)
 		assert.Fail(t, msg)
@@ -59,48 +49,102 @@ func TestPayloadAttributesInSchema(t *testing.T, name string, undocumented *set.
 	}
 }
 
-// loads jsonSchema file (.json) given a current directory, and returns its contents and directory
-// current directories need to be propagate across calls to loadSchema because $ref paths are relative
-func loadSchema(spec string, currentDir string) (map[string]interface{}, string) {
-	if currentDir == "" {
-		_, current, _, _ := runtime.Caller(0)
-		currentDir = filepath.Join(filepath.Dir(current), "../docs/spec/")
+func TestJsonSchemaKeywordLimitation(t *testing.T, fieldPaths []string, schema string, exceptions *set.Set) {
+	fieldNames, err := fetchFlattenedFieldNames(fieldPaths, addKeywordFields)
+	assert.NoError(t, err)
+
+	schemaStruct, _ := schemaStruct(strings.NewReader(schema))
+	schemaNames := set.New()
+	flattenSchemaNames(schemaStruct, "", addLengthRestrictedPropNames, schemaNames)
+
+	mapping := []Mapping{
+		{"errors.context", "context"},
+		{"transactions.context", "context"},
+		{"errors", "error"},
+		{"transactions.traces", "trace"},
+		{"transactions", "transaction"},
+		{"app", "context.app"},
+		{"system", "context.system"},
 	}
-	specFile := filepath.Join(currentDir, spec)
-	schemaData, _ := ioutil.ReadFile(specFile)
-	var schema map[string]interface{}
-	json.Unmarshal(schemaData, &schema)
-	return schema, filepath.Dir(specFile)
+
+	mappedSchemaNames := set.New()
+	for _, k := range schemaNames.List() {
+		name := k.(string)
+		for _, m := range mapping {
+			if strings.HasPrefix(name, m.from) {
+				k = strings.Replace(name, m.from, m.to, 1)
+				break
+			}
+		}
+		mappedSchemaNames.Add(k)
+	}
+	found := set.Union(mappedSchemaNames, exceptions).(*set.Set)
+	diff := set.SymmetricDifference(fieldNames, found).(*set.Set)
+
+	errMsg := fmt.Sprintf("Missing json schema length limit 1024 for: %v", diff)
+	assert.Equal(t, 0, diff.Size(), errMsg)
 }
 
-// walks a json collecting nested keys separated by dots, ie. {a:{b:c:{d:1}} ==> a.b.c.d
-// accumulates the result in the first argument, which is mutated recursively
-// it has some rudimentary knowledge of jsonSchema.
-// jsonSchema keys are traversed, but not accumulated in the result
-func flattenJsonKeys(flattened *set.Set, data interface{}, currentSpecDir string, keyPrefix string) {
+func schemaStruct(reader io.Reader) (*Schema, error) {
+	decoder := json.NewDecoder(reader)
+	var schema Schema
+	err := decoder.Decode(&schema)
+	return &schema, err
+}
+
+func flattenSchemaNames(s *Schema, prefix string, addFn addProperty, flattened *set.Set) {
+	if len(s.Properties) > 0 {
+		for k, v := range s.Properties {
+			flattenedKey := StrConcat(prefix, k, ".")
+			if addFn(v) {
+				flattened.Add(flattenedKey)
+			}
+			flattenSchemaNames(v, flattenedKey, addFn, flattened)
+		}
+	} else if s.Items != nil {
+		flattenSchemaNames(s.Items, prefix, addFn, flattened)
+	} else if len(s.AllOf) > 0 {
+		for _, v := range s.AllOf {
+			flattenSchemaNames(&v, prefix, addFn, flattened)
+		}
+	}
+}
+
+func flattenJsonKeys(data interface{}, prefix string, flattened *set.Set) {
 	if d, ok := data.(map[string]interface{}); ok {
 		for k, v := range d {
-			key := StrConcat(keyPrefix, k, ".")
-			if Contains(jsonSchemaReservedWords, k) {
-				// if we find a jsonSchema key, we walk it down without "capturing" the key
-				// eg: {properties: { app {properties: name: {...}}}} becomes just app.name
-				flattenJsonKeys(flattened, v, currentSpecDir, keyPrefix)
-			} else if k == "$ref" {
-				// if we find a reference to another file, we load it and continue from there
-				schema, newCurrentSpecDir := loadSchema(v.(string), currentSpecDir)
-				flattenJsonKeys(flattened, schema, newCurrentSpecDir, keyPrefix)
-			} else if k == "patternProperties" {
-				// jsonSchema allows values as keys under patternProperties key
-				// we need to stop here so to not confuse arbitrary regex values with actual keys
-				continue
-			} else {
-				flattened.Add(key)
-				flattenJsonKeys(flattened, v, currentSpecDir, key)
-			}
+			key := StrConcat(prefix, k, ".")
+			flattened.Add(key)
+			flattenJsonKeys(v, key, flattened)
 		}
 	} else if d, ok := data.([]interface{}); ok {
 		for _, v := range d {
-			flattenJsonKeys(flattened, v, currentSpecDir, keyPrefix)
+			flattenJsonKeys(v, prefix, flattened)
 		}
 	}
+}
+
+type addProperty func(s *Schema) bool
+
+func addAllPropNames(s *Schema) bool { return true }
+
+func addLengthRestrictedPropNames(s *Schema) bool {
+	if s.MaxLength == 1024 {
+		return true
+	} else if val, ok := s.AdditionalProperties["maxLength"]; ok {
+		if valF, okF := val.(float64); okF && valF == 1024 {
+			return true
+		}
+	} else if len(s.PatternProperties) > 0 {
+		for _, v := range s.PatternProperties {
+			val, ok := v.(map[string]interface{})["maxLength"]
+			if ok && val.(float64) == 1024 {
+				continue
+			} else {
+				return false
+			}
+		}
+		return true
+	}
+	return false
 }
