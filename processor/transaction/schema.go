c35eb01e7c190bc65d78d9eb4bfc531df0427814
Silvia Mitter <silvia.mitter@elastic.co>
2017-11-13 13:08:57 +0100

    Revert "Make transaction.result a mandatory field. (#250)" (#292)
    This reverts commit 11af2e8f9bc936fbd26d5bf444cdd77c6c8ab48d.

--- processor/transaction/schema.go
+++ processor/transaction/schema.go
@@ -505,7 +505,7 @@ var transactionSchema = `{
             "maxLength": 1024
         }
     },
-    "required": ["id", "name", "duration", "type", "timestamp", "result"]
+    "required": ["id", "name", "duration", "type", "timestamp"]
             },
             "minItems": 1
         }
