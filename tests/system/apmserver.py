71d8a69996fafdc944fc4a3b9664666c0fcd22a0
Silvia Mitter <silvia.mitter@elastic.co>
2017-11-17 15:26:06 +0100

    Change teardown handling for server test. (#312)

--- tests/system/apmserver.py
+++ tests/system/apmserver.py
@@ -26,7 +26,7 @@ class BaseTest(TestCase):
         return json.loads(open(path).read())
 
 
-class ServerBaseTest(BaseTest):
+class ServerSetUpBaseTest(BaseTest):
 
     transactions_url = 'http://localhost:8200/v1/transactions'
 
@@ -37,19 +37,21 @@ class ServerBaseTest(BaseTest):
         }
 
     def setUp(self):
-        super(ServerBaseTest, self).setUp()
+        super(ServerSetUpBaseTest, self).setUp()
         shutil.copy(self.beat_path + "/fields.yml", self.working_dir)
 
         self.render_config_template(**self.config())
         self.apmserver_proc = self.start_beat()
         self.wait_until(lambda: self.log_contains("Starting apm-server"))
 
+
+class ServerBaseTest(ServerSetUpBaseTest):
     def tearDown(self):
         super(ServerBaseTest, self).tearDown()
         self.apmserver_proc.check_kill_and_wait()
 
 
-class SecureServerBaseTest(ServerBaseTest):
+class SecureServerBaseTest(ServerSetUpBaseTest):
 
     @classmethod
     def setUpClass(cls):
@@ -75,6 +77,10 @@ class SecureServerBaseTest(ServerBaseTest):
             "ssl_key": "config/certs/key.pem",
         })
         return cfg
+
+    def tearDown(self):
+        super(SecureServerBaseTest, self).tearDown()
+        self.apmserver_proc.kill_and_wait()
 
 
 class AccessTest(ServerBaseTest):
