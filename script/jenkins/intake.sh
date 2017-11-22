98f439dbe7df930cf96199b77ea1570dc9eec6db
Ron Cohen <cohen1@gmail.com>
2017-11-22 12:29:07 +0100

    Use Go 1.9.2 everywhere (#336)

--- script/jenkins/intake.sh
+++ script/jenkins/intake.sh
@@ -11,7 +11,7 @@ go_file="_beats/.go-version"
 if [ -f "$go_file" ]; then
   eval "$(gvm $(cat $go_file))"
 else
-  eval "$(gvm 1.8.3)"
+  eval "$(gvm 1.9.2)"
 fi
 
 # Workaround for Python virtualenv path being too long.
