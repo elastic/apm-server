263033a3d2cc8849fa9af47b4b184bb6160af91f
Gil Raphaelli <gil@elastic.co>
2017-12-15 17:41:13 -0500

    do not change version globally, just print it on startup
    #396 introduced a bug where the index name contained invalid characters
    that were added to the global version.

--- cmd/root.go
+++ cmd/root.go
@@ -1,14 +1,10 @@
 package cmd
 
 import (
-	"fmt"
-
 	"github.com/spf13/pflag"
 
 	"github.com/elastic/apm-server/beater"
-	"github.com/elastic/apm-server/version"
 	"github.com/elastic/beats/libbeat/cmd"
-	libbeat "github.com/elastic/beats/libbeat/version"
 )
 
 // Name of the beat (apm-server).
@@ -22,6 +18,5 @@ var RootCmd *cmd.BeatsRootCmd
 
 func init() {
 	var runFlags = pflag.NewFlagSet(Name, pflag.ExitOnError)
-	version := fmt.Sprintf("%s [%s]", libbeat.GetDefaultVersion(), version.String())
-	RootCmd = cmd.GenRootCmdWithIndexPrefixWithRunFlags(Name, IdxPattern, version, beater.New, runFlags)
+	RootCmd = cmd.GenRootCmdWithIndexPrefixWithRunFlags(Name, IdxPattern, "", beater.New, runFlags)
 }
