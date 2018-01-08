5869b6524baeacaaad2858538e7b5b6394e6aa3d
Gil Raphaelli <gil@elastic.co>
2018-01-08 13:13:31 -0500

    handle early exit before single popd

--- script/update_beats.sh
+++ script/update_beats.sh
@@ -12,7 +12,7 @@ pushd $BASEDIR
 
 # Check out beats repo for updating
 GIT_CLONE=repo
-trap "{ rm -rf ${BASEDIR}/${GIT_CLONE}; }" EXIT
+trap "{ set +e;popd >/dev/null;set -e;rm -rf ${BASEDIR}/${GIT_CLONE}; }" EXIT
 
 git clone https://github.com/elastic/beats.git ${GIT_CLONE}
 (
