89ce54609812feb3ef04cf87cd98f238e9d48985
jalvz <juan.alvarez@elastic.co>
2018-02-19 10:32:43 +0100

    update comment

--- utility/common.go
+++ utility/common.go
@@ -14,9 +14,9 @@ func CleanUrlPath(p string) string {
 	return url.String()
 }
 
-// InsertInMap modifies `data` *in place*, inserting `values` at the given `key` or at the top level if the `key`is "".
-// If `key` doesn't exist in data, it gets created.
-// If `key` exists in data and its value is not a map, InsertInMap does nothing.
+// InsertInMap modifies `data` *in place*, inserting `values` at the given `key`.
+// If `key` doesn't exist in data (at the top level), it gets created.
+// If the value under `key` is not a map, InsertInMap does nothing.
 func InsertInMap(data map[string]interface{}, key string, values map[string]interface{}) {
 	if data == nil || values == nil || key == "" {
 		return
