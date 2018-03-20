be5909f6c934af4faa0795316f775a90b6eaf1af
Ron cohen <ron@elastic.co>
2018-03-20 13:39:25 +0100

    Update beats framework to 768e58e

--- vendor/github.com/elastic/beats/libbeat/autodiscover/providers/docker/config.go
+++ vendor/github.com/elastic/beats/libbeat/autodiscover/providers/docker/config.go
@@ -12,20 +12,21 @@ type Config struct {
 	TLS       *docker.TLSConfig       `config:"ssl"`
 	Prefix    string                  `config:"string"`
 	Builders  []*common.Config        `config:"builders"`
+	Appenders []*common.Config        `config:"appenders"`
 	Templates template.MapperSettings `config:"templates"`
 }
 
 func defaultConfig() *Config {
 	return &Config{
 		Host:   "unix:///var/run/docker.sock",
-		Prefix: "co.elastic.",
+		Prefix: "co.elastic",
 	}
 }
 
 // Validate ensures correctness of config
 func (c *Config) Validate() {
-	// Make sure that prefix ends with a '.'
-	if c.Prefix[len(c.Prefix)-1] != '.' {
-		c.Prefix = c.Prefix + "."
+	// Make sure that prefix doesn't ends with a '.'
+	if c.Prefix[len(c.Prefix)-1] == '.' && c.Prefix != "." {
+		c.Prefix = c.Prefix[:len(c.Prefix)-2]
 	}
 }
