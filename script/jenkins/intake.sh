dd9c8622ee7c0893b25a67742ebb3534e7183b56
Juan Álvarez <juan.alvarez@elastic.co>
2018-08-02 14:26:22 +0200

    Reuse Beats ci scripts (#1247)

--- script/jenkins/intake.sh
+++ script/jenkins/intake.sh
@@ -1,22 +1,9 @@
 #!/usr/bin/env bash
 set -euox pipefail
 
-: "${HOME:?Need to set HOME to a non-empty value.}"
-: "${WORKSPACE:?Need to set WORKSPACE to a non-empty value.}"
+source ./_beats/dev-tools/common.bash
 
-# Setup Go.
-export GOPATH=${WORKSPACE}
-export PATH=${GOPATH}/bin:${PATH}
-go_file="_beats/.go-version"
-if [ -f "$go_file" ]; then
-  eval "$(gvm $(cat $go_file))"
-else
-  eval "$(gvm 1.10.3)"
-fi
-
-# Workaround for Python virtualenv path being too long.
-TEMP_PYTHON_ENV=$(mktemp -d)
-export PYTHON_ENV="${TEMP_PYTHON_ENV}/python-env"
+jenkins_setup
 
 cleanup() {
   rm -rf $TEMP_PYTHON_ENV
