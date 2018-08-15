ef05283ed8493011612248deb02633d6519177e2
Juan Álvarez <juan.alvarez@elastic.co>
2018-08-15 17:32:34 +0200

    Own make target to build APM docs (#1279)
    - Pulls elastic/docs instead of remove & close each time
    - Drops maxdepth = 1 so to build guide/index too

---
+++ script/build_apm_docs.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+set -e
+
+name=$1
+path=$2
+build_dir=$3
+
+docs_dir=docs/elastic
+html_dir=$build_dir/html_docs
+
+# Checks if docs clone already exists
+if [ ! -d $docs_dir ]; then
+    # Only head is cloned
+    git clone --depth=1 https://github.com/elastic/docs.git $docs_dir
+else
+    git -C $docs_dir pull https://github.com/elastic/docs.git
+fi
+
+index_list="$(find ${GOPATH%%:*}/src/$path -name 'index.asciidoc')"
+for index in $index_list
+do
+  echo "Building docs for ${name}..."
+  echo "Index document: ${index}"
+  index_path=$(basename $(dirname $index))
+  echo "Index path: $index_path"
+
+  dest_dir="$html_dir/${name}/${index_path}"
+  mkdir -p "$dest_dir"
+  params="--chunk=1"
+  $docs_dir/build_docs.pl $params --doc "$index" -out "$dest_dir"
+done
