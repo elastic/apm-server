513f432ea9296139c7c5b4b409bc7f70161fc513
Ron cohen <ron@elastic.co>
2018-09-20 16:53:31 +0200

    Added more DataValidation tests

--- processor/stream/package_tests/span_attrs_test.go
+++ processor/stream/package_tests/span_attrs_test.go
@@ -145,28 +145,24 @@ func TestKeywordLimitationOnSpanAttrs(t *testing.T) {
 	)
 }
 
-// func TestPayloadDataForTransaction(t *testing.T) {
-// 	// add test data for testing
-// 	// * specific edge cases
-// 	// * multiple allowed dataypes
-// 	// * regex pattern, time formats
-// 	// * length restrictions, other than keyword length restrictions
+func TestPayloadDataForSpans(t *testing.T) {
+	// add test data for testing
+	// * specific edge cases
+	// * multiple allowed dataypes
+	// * regex pattern, time formats
+	// * length restrictions, other than keyword length restrictions
 
-// 	spanProcSetup().DataValidation(t, schemaTestData(
-// 		[]tests.SchemaTestData{
-// 			{Key: "transactions.id",
-// 				Valid:   []interface{}{"85925e55-B43f-4340-a8e0-df1906ecbf7a"},
-// 				Invalid: []tests.Invalid{{Msg: `id/pattern`, Values: val{"123", "z5925e55-b43f-4340-a8e0-df1906ecbf7a", "85925e55-b43f-4340-a8e0-df1906ecbf7"}}}},
-// 			{Key: "transactions.spans", Valid: []interface{}{[]interface{}{}}},
-// 			{Key: "transactions.spans.stacktrace.pre_context",
-// 				Valid: val{[]interface{}{}, []interface{}{"context"}},
-// 				Invalid: []tests.Invalid{
-// 					{Msg: `/stacktrace/items/properties/pre_context/items/type`, Values: val{[]interface{}{123}}},
-// 					{Msg: `stacktrace/items/properties/pre_context/type`, Values: val{"test"}}}},
-// 			{Key: "transactions.spans.stacktrace.post_context",
-// 				Valid: val{[]interface{}{}, []interface{}{"context"}},
-// 				Invalid: []tests.Invalid{
-// 					{Msg: `/stacktrace/items/properties/post_context/items/type`, Values: val{[]interface{}{123}}},
-// 					{Msg: `stacktrace/items/properties/post_context/type`, Values: val{"test"}}}},
-// 		}))
-// }
+	spanProcSetup().DataValidation(t,
+		[]tests.SchemaTestData{
+			{Key: "span.stacktrace.pre_context",
+				Valid: val{[]interface{}{}, []interface{}{"context"}},
+				Invalid: []tests.Invalid{
+					{Msg: `/stacktrace/items/properties/pre_context/items/type`, Values: val{[]interface{}{123}}},
+					{Msg: `stacktrace/items/properties/pre_context/type`, Values: val{"test"}}}},
+			{Key: "span.stacktrace.post_context",
+				Valid: val{[]interface{}{}, []interface{}{"context"}},
+				Invalid: []tests.Invalid{
+					{Msg: `/stacktrace/items/properties/post_context/items/type`, Values: val{[]interface{}{123}}},
+					{Msg: `stacktrace/items/properties/post_context/type`, Values: val{"test"}}}},
+		})
+}
--- processor/stream/package_tests/transaction_attrs_test.go
+++ processor/stream/package_tests/transaction_attrs_test.go
@@ -131,28 +131,62 @@ func TestKeywordLimitationOnTransactionAttrs(t *testing.T) {
 	)
 }
 
-// func TestPayloadDataForTransaction(t *testing.T) {
-// 	// add test data for testing
-// 	// * specific edge cases
-// 	// * multiple allowed dataypes
-// 	// * regex pattern, time formats
-// 	// * length restrictions, other than keyword length restrictions
+func TestPayloadDataForTransaction(t *testing.T) {
+	// add test data for testing
+	// * specific edge cases
+	// * multiple allowed dataypes
+	// * regex pattern, time formats
+	// * length restrictions, other than keyword length restrictions
 
-// 	b.DataValidation(t, schemaTestData(
-// 		[]tests.SchemaTestData{
-// 			{Key: "transactions.id",
-// 				Valid:   []interface{}{"85925e55-B43f-4340-a8e0-df1906ecbf7a"},
-// 				Invalid: []tests.Invalid{{Msg: `id/pattern`, Values: val{"123", "z5925e55-b43f-4340-a8e0-df1906ecbf7a", "85925e55-b43f-4340-a8e0-df1906ecbf7"}}}},
-// 			{Key: "transactions.spans", Valid: []interface{}{[]interface{}{}}},
-// 			{Key: "transactions.spans.stacktrace.pre_context",
-// 				Valid: val{[]interface{}{}, []interface{}{"context"}},
-// 				Invalid: []tests.Invalid{
-// 					{Msg: `/stacktrace/items/properties/pre_context/items/type`, Values: val{[]interface{}{123}}},
-// 					{Msg: `stacktrace/items/properties/pre_context/type`, Values: val{"test"}}}},
-// 			{Key: "transactions.spans.stacktrace.post_context",
-// 				Valid: val{[]interface{}{}, []interface{}{"context"}},
-// 				Invalid: []tests.Invalid{
-// 					{Msg: `/stacktrace/items/properties/post_context/items/type`, Values: val{[]interface{}{123}}},
-// 					{Msg: `stacktrace/items/properties/post_context/type`, Values: val{"test"}}}},
-// 		}))
-// }
+	transactionProcSetup().DataValidation(t,
+		[]tests.SchemaTestData{
+			{Key: "transaction.duration",
+				Valid:   []interface{}{12.4},
+				Invalid: []tests.Invalid{{Msg: `duration/type`, Values: val{"123"}}}},
+			{Key: "transaction.timestamp",
+				Valid: val{"2017-05-30T18:53:42.281Z"},
+				Invalid: []tests.Invalid{
+					{Msg: `timestamp/format`, Values: val{"2017-05-30T18:53Z", "2017-05-30T18:53:27.Z", "2017-05-30T18:53:27a123Z"}},
+					{Msg: `timestamp/pattern`, Values: val{"2017-05-30T18:53:27.000+00:20", "2017-05-30T18:53:27ZNOTCORRECT"}}}},
+			{Key: "transaction.marks",
+				Valid: []interface{}{obj{}, obj{tests.Str1024: obj{tests.Str1024: 21.0, "end": -45}}},
+				Invalid: []tests.Invalid{
+					{Msg: `marks/type`, Values: val{"marks"}},
+					{Msg: `marks/patternproperties`, Values: val{
+						obj{"timing": obj{"start": "start"}},
+						obj{"timing": obj{"start": obj{}}},
+						obj{"timing": obj{"m*e": -45}},
+						obj{"timing": obj{"m\"": -45}},
+						obj{"timing": obj{"m.": -45}}}},
+					{Msg: `marks/additionalproperties`, Values: val{
+						obj{"tim*ing": obj{"start": -45}},
+						obj{"tim\"ing": obj{"start": -45}},
+						obj{"tim.ing": obj{"start": -45}}}}}},
+			{Key: "transaction.context.custom",
+				Valid: val{obj{"whatever": obj{"comes": obj{"end": -45}}},
+					obj{"whatever": 123}},
+				Invalid: []tests.Invalid{
+					{Msg: `context/properties/custom/additionalproperties`, Values: val{obj{"what.ever": 123}, obj{"what*ever": 123}, obj{"what\"ever": 123}}},
+					{Msg: `context/properties/custom/type`, Values: val{"context"}}}},
+			{Key: "transaction.context.request.body",
+				Valid:   []interface{}{obj{}, tests.Str1025},
+				Invalid: []tests.Invalid{{Msg: `context/properties/request/properties/body/type`, Values: val{102}}}},
+			{Key: "transaction.context.request.env",
+				Valid:   []interface{}{obj{}},
+				Invalid: []tests.Invalid{{Msg: `context/properties/request/properties/env/type`, Values: val{102, "a"}}}},
+			{Key: "transaction.context.request.cookies",
+				Valid:   []interface{}{obj{}},
+				Invalid: []tests.Invalid{{Msg: `context/properties/request/properties/cookies/type`, Values: val{123, ""}}}},
+			{Key: "transaction.context.tags",
+				Valid: val{obj{tests.Str1024Special: tests.Str1024Special}},
+				Invalid: []tests.Invalid{
+					{Msg: `tags/type`, Values: val{"tags"}},
+					{Msg: `tags/patternproperties`, Values: val{obj{"invalid": tests.Str1025}, obj{tests.Str1024: 123}, obj{tests.Str1024: obj{}}}},
+					{Msg: `tags/additionalproperties`, Values: val{obj{"invali*d": "hello"}, obj{"invali\"d": "hello"}, obj{"invali.d": "hello"}}}}},
+			{Key: "transaction.context.user.id",
+				Valid: val{123, tests.Str1024Special},
+				Invalid: []tests.Invalid{
+					{Msg: `context/properties/user/properties/id/type`, Values: val{obj{}}},
+					{Msg: `context/properties/user/properties/id/maxlength`, Values: val{tests.Str1025}}}},
+		})
+}
