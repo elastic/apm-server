67c4429bfbd7ce26ce754d21058e7d660aedc876
Ron cohen <ron@elastic.co>
2018-09-21 14:58:56 +0200

    add initial metadata test

---
+++ processor/stream/package_tests/metadata_attrs_test.go
@@ -0,0 +1,53 @@
+// Licensed to Elasticsearch B.V. under one or more contributor
+// license agreements. See the NOTICE file distributed with
+// this work for additional information regarding copyright
+// ownership. Elasticsearch B.V. licenses this file to you under
+// the Apache License, Version 2.0 (the "License"); you may
+// not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+package package_tests
+
+import (
+	"testing"
+
+	"github.com/elastic/apm-server/model/metadata/generated/schema"
+	"github.com/elastic/apm-server/processor/stream"
+	"github.com/elastic/apm-server/tests"
+)
+
+func metadataProcSetup() *tests.ProcessorSetup {
+	return &tests.ProcessorSetup{
+		Proc:            &V2TestProcessor{StreamProcessor: stream.StreamProcessor{}},
+		FullPayloadPath: "../testdata/intake-v2/spans.ndjson",
+		Schema:          schema.ModelSchema,
+		TemplatePaths: []string{
+			"../../../_meta/fields.common.yml",
+		},
+	}
+}
+
+func TestKeywordLimitationOnMetadataAttrs(t *testing.T) {
+	metadataProcSetup().KeywordLimitation(
+		t,
+		tests.NewSet("processor.event", "processor.name", "listening",
+			tests.Group("context.request"),
+			tests.Group("context.tags"),
+			tests.Group("transaction"),
+			tests.Group("parent"),
+			tests.Group("trace"),
+		),
+		map[string]string{
+			"context.": "",
+		},
+	)
+}
