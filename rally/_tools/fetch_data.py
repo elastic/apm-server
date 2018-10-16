8d15ffeefe7fa3deab26f7ce7db4ab6d0ce8f10c
Silvia Mitter <silvia.mitter@elastic.co>
2018-10-16 16:39:32 +0200

    Support daily indices for queries in rally. (#1423)
    * Support daily indices for queries in rally.
    
    Add support for indexing events into daily indices to allow running APM
    queries on them.
    
    implements #1404

--- rally/_tools/fetch_data.py
+++ rally/_tools/fetch_data.py
@@ -4,24 +4,24 @@ import json
 import os
 
 
-def fetch(q, service):
+def fetch(path, q, service):
     es = Elasticsearch(["http://localhost:9200"])
-    events = ["error", "transaction", "span"]
-    doc_ct = 1000000
+    events = {"error": 1000000,
+              "transaction": 5000000,
+              "span": 20000000}
     batch_size = 1000
-    for et in events:
-        idx = "apm*{}*".format(et)
+    for ev, doc_ct in events.items():
+        print("fetching up to {} data for {}".format(doc_ct, ev))
+        idx = "apm*{}*".format(ev)
         ct = 0
-        f = os.path.join("rally", "documents")
         name = ""
         if service != "":
             name = "{}_".format(service)
-        f = os.path.join(f, "{}{}.json".format(name, et))
-        with open(f, 'a+') as out:
+        f = os.path.join(path, "{}{}_base.json".format(name, ev))
+        with open(f, 'w') as out:
             for doc in helpers.scan(es, query=q, index=idx, size=batch_size):
                 out.write(json.dumps(doc["_source"]))
                 out.write("\n")
-
                 ct += 1
                 if ct >= doc_ct:
                     break
@@ -30,7 +30,7 @@ def fetch(q, service):
         os.system("stat -f '%z' {}".format(f))
 
 
-def main():
+def fetch_per_service(path):
     services = [
         "opbeans-node",
         "opbeans-python",
@@ -42,11 +42,22 @@ def main():
     # fetch data per event type per agent
     for service in services:
         q = {"query": {"match": {"context.service.name": service}}}
-        fetch(q, service)
+        fetch(path, q, service)
 
+
+def fetch_all(path):
     # fetch data per event type
     q = {"query": {"match_all": {}}}
-    fetch(q, "")
+    fetch(path, q, "")
+
+
+def main():
+    cwd = os.path.dirname(os.path.realpath(__file__))
+    path = os.path.join(cwd, "tmp")
+    if not os.path.exists(path):
+        os.makedirs(path)
+    # fetch_per_service(path)
+    fetch_all(path)
 
 
 if __name__ == '__main__':
