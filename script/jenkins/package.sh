2468457d720829a59369e05178427ee41018b702
Gil Raphaelli <gil@elastic.co>
2018-10-16 16:13:50 -0400

    test

--- script/jenkins/package.sh
+++ script/jenkins/package.sh
@@ -3,4 +3,5 @@ set -euox pipefail
 
 source ./_beats/dev-tools/jenkins_release.sh
 
+go test -v tests/packaging/package_test.go -files /Users/gil/.local/go/src/github.com/elastic/apm-server/build/distributions/\* -tags=package
 make package-tests
