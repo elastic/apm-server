74bba84e5ecf8c8e5179bb2dc8e3c1f42859cf2b
Gil Raphaelli <g@raphaelli.com>
2018-10-31 11:30:08 -0400

    [6.5] set more config defaults in code (#1494) (#1502)
    * set more config defaults in code (#1494)
    
    * adapt test for 6.5

--- tests/system/test_export.py
+++ tests/system/test_export.py
@@ -1,5 +1,4 @@
 import json
-import os
 import yaml
 
 from apmserver import ServerSetUpBaseTest
@@ -21,7 +20,7 @@ class SubCommandTest(ServerSetUpBaseTest):
             assert trimmed.split(None, 1)[0] in ("PASS", "coverage:"), trimmed
 
 
-class ExportConfigTest(SubCommandTest):
+class ExportConfigDefaultTest(SubCommandTest):
     """
     Test export config subcommand.
     """
@@ -34,14 +33,73 @@ class ExportConfigTest(SubCommandTest):
 
     def test_export_config(self):
         """
-        Test export config works
+        Test export default config
         """
         config = yaml.load(self.command_output)
-        total_fields_limit = config['setup']['template']['settings']['index']['mapping']['total_fields']['limit']
-        assert total_fields_limit == 2000, total_fields_limit
+        # logging settings
+        self.assertDictEqual(
+            {"metrics": {"enabled": False}}, config["logging"]
+        )
+
+        # template settings
+        self.assertDictEqual(
+            {
+                "template": {
+                    "settings": {
+                        "index": {
+                            "codec": "best_compression",
+                            "mapping": {
+                                "total_fields": {"limit": 2000}
+                            },
+                            "number_of_shards": 1,
+                        },
+                    },
+                },
+            }, config["setup"])
 
 
-class ExportTemplateTest(SubCommandTest):
+class ExportConfigTest(SubCommandTest):
+    """
+    Test export config subcommand.
+    """
+
+    def start_args(self):
+        return {
+            "extra_args": ["export", "config",
+                           "-E", "logging.metrics.enabled=true",
+                           "-E", "setup.template.settings.index.mapping.total_fields.limit=5",
+                           ],
+            "logging_args": None,
+        }
+
+    def test_export_config(self):
+        """
+        Test export customized config
+        """
+        config = yaml.load(self.command_output)
+        # logging settings
+        self.assertDictEqual(
+            {"metrics": {"enabled": True}}, config["logging"]
+        )
+
+        # template settings
+        self.assertDictEqual(
+            {
+                "template": {
+                    "settings": {
+                        "index": {
+                            "codec": "best_compression",
+                            "mapping": {
+                                "total_fields": {"limit": 5}
+                            },
+                            "number_of_shards": 1,
+                        },
+                    },
+                },
+            }, config["setup"])
+
+
+class ExportTemplateDefaultTest(SubCommandTest):
     """
     Test export template subcommand.
     """
@@ -54,8 +112,52 @@ class ExportTemplateTest(SubCommandTest):
 
     def test_export_template(self):
         """
-        Test export template works
+        Test export default template
         """
-        template = json.loads(self.command_output)
-        total_fields_limit = template['settings']['index']['mapping']['total_fields']['limit']
-        assert total_fields_limit == 2000, total_fields_limit
+        template = yaml.load(self.command_output)
+        settings = template["settings"]
+        self.assertDictEqual(
+            {
+                "index": {
+                    "codec": "best_compression",
+                    "mapping": {
+                        "total_fields": {"limit": 2000}
+                    },
+                    "number_of_routing_shards": 30,
+                    "number_of_shards": 1,
+                    "refresh_interval": "5s",
+                },
+            }, settings)
+
+
+class ExportTemplateTest(SubCommandTest):
+    """
+    Test export template subcommand.
+    """
+
+    def start_args(self):
+        return {
+            "extra_args": ["export", "template",
+                           "-E", "setup.template.settings.index.mapping.total_fields.limit=5",
+                           ],
+            "logging_args": None,
+        }
+
+    def test_export_template(self):
+        """
+        Test export customized template
+        """
+        template = yaml.load(self.command_output)
+        settings = template["settings"]
+        self.assertDictEqual(
+            {
+                "index": {
+                    "codec": "best_compression",
+                    "mapping": {
+                        "total_fields": {"limit": 5}
+                    },
+                    "number_of_routing_shards": 30,
+                    "number_of_shards": 1,
+                    "refresh_interval": "5s",
+                },
+            }, settings)
