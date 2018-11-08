a843f5608a8c02513d5e893adf3c5b02233bf53b
Gil Raphaelli <gil@elastic.co>
2018-11-08 16:11:52 -0500

    map ECS fields

---
+++ tests/system/test_ecs_mappings.py
@@ -0,0 +1,77 @@
+import json
+
+import yaml
+
+from apmserver import SubCommandTest
+
+
+def flatmap(fields, pfx=None):
+    if pfx is None:
+        pfx = []
+    for field, attribs in sorted(fields.items()):
+        if 'properties' in attribs:
+            for f in flatmap(attribs['properties'], pfx + [field]):
+                yield f
+        else:
+            yield ".".join(pfx + [field]), attribs
+
+
+class ECSTest(SubCommandTest):
+    """
+    Test export template subcommand.
+    """
+
+    def start_args(self):
+        return {
+            "extra_args": ["export", "template"],
+            "logging_args": None,
+        }
+
+    def test_ecs_migration(self):
+        """
+        Test that all fields are aliased or otherwise accounted for in ECS migration.
+        """
+        all_fields = set()
+        alias_fields = set()
+        for f, a in flatmap(yaml.load(self.command_output)["mappings"]["doc"]["properties"]):
+            if a.get("type") == "alias":
+                alias_fields.add(a["path"])
+            else:
+                all_fields.add(f)
+
+        # fields with special exception, due to mapping type changes, etc
+        # no comment means unchanged
+        exception_fields = {
+            "@timestamp",
+            "context.request.url.port",  # field copy to url.port, keyword -> int
+            "context.request.url.protocol",  # field copy to url.scheme, drop trailing ":"
+            "context.tags",  # field copy, can't alias objects
+            "labels",  # target for context.tags copy
+            "parent.id",
+            "processor.event", "processor.name",
+            "sourcemap.bundle_filepath", "sourcemap.service.name", "sourcemap.service.version",
+            "span.duration.us", "span.hex_id", "span.id", "span.name", "span.parent", "span.start.us", "span.type",
+            "tags",
+            "timestamp.us",
+            "trace.id",
+            "transaction.duration.us", "transaction.id", "transaction.marks.navigationTiming", "transaction.name",
+            "transaction.result", "transaction.sampled", "transaction.span_count.dropped.total", "transaction.type",
+            "url.port",  # field copy from context.request.url.port
+            "url.scheme",  # field copy from context.request.url.protocol
+
+            # scripted fields
+            "error id icon",
+            "view errors",
+            "view spans",
+        }
+
+        should_not_be_aliased = alias_fields - all_fields
+        self.assertFalse(should_not_be_aliased, json.dumps(sorted(should_not_be_aliased)))
+
+        not_aliased = all_fields - alias_fields - exception_fields
+        self.assertFalse(not_aliased,
+                         "\nall fields ({:d}):\n{}\n\naliased ({:d}):\n{}\n\nunaccounted for ({:d}):\n{}".format(
+                             len(all_fields), json.dumps(sorted(all_fields)),
+                             len(alias_fields), json.dumps(sorted(alias_fields)),
+                             len(not_aliased), json.dumps(sorted(not_aliased)),
+                         ))
