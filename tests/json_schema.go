2c64f0c90d46a6c9db6b1e252ad1d70e5007c431
Silvia Mitter <silvia.mitter@elastic.co>
2018-11-19 19:58:41 +0100

    spec: Allow framework.name/version to be null (#1546)
    * spec: Allow `service.framework.*` to be null
    
    implements #1515

--- tests/json_schema.go
+++ tests/json_schema.go
@@ -133,8 +133,6 @@ func (ps *ProcessorSetup) AttrsPresence(t *testing.T, requiredKeys *Set, condReq
 		"service.language.name",
 		"service.runtime.name",
 		"service.runtime.version",
-		"service.framework.name",
-		"service.framework.version",
 		"process.pid",
 	))
 
