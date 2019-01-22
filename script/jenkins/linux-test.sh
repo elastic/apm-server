0cda1d1e97512488b9b7243469521fe75ee25705
Ivan Fernandez Calvo <kuisathaverat@yahoo.com>
2019-01-22 11:21:23 +0100

    scripts
    Signed-off-by: Ivan Fernandez Calvo <kuisathaverat@yahoo.com>

---
+++ script/jenkins/linux-test.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+set -exuo pipefail
+
+source ./_beats/dev-tools/common.bash
+
+jenkins_setup
+
+#cleanup() {
+#  rm -rf $TEMP_PYTHON_ENV
+#  make stop-environment fix-permissions
+#}
+#trap cleanup EXIT
+
+make testsuite
+
+go get -v -u github.com/t-yuki/gocover-cobertura
+
+export COV_DIR="build/coverage"
+
+for i in "full.cov" "integration.cov" "system.cov" "unit.cov"
+do
+  name=$(basename ${i} .cov)
+  if [ -f "${COV_DIR}/${i}" ]; then 
+    go tool cover -html="${COV_DIR}/${i}" -o "${COV_DIR}/coverage-${name}-report.html"
+    gocover-cobertura < "${COV_DIR}/${i}" > "${COV_DIR}/coverage-${name}-report.xml"
+  fi
+done
+exit 0
