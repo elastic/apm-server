89080090b6a135e18558971fb1c28e1b9f7aad03
Gil Raphaelli <g@raphaelli.com>
2019-02-13 11:46:13 -0500

    index pattern sync check symmetry (#1920)

--- script/are_kibana_saved_objects_updated.py
+++ script/are_kibana_saved_objects_updated.py
@@ -9,23 +9,17 @@ import os
 import json
 import jsondiff
 import sys
-
-
-def fetch_kibana_file(repo_path, ref, file_name):
-    path = "{}/{}/{}".format(repo_path, ref, file_name)
-    print("---- Comparing Index Pattern with " + path)
-    rsp = requests.get(path)
-    if rsp.status_code != 200:
-        print("failed to query '{}'".format(path))
-        return 1
-    return rsp.json()
+try:
+    from urlparse import urljoin, urlparse
+except ImportError:
+    from urllib.parse import urljoin, urlparse
 
 
 def json_val(v1, v2):
     try:
-        return (json.loads(v1), json.loads(v2))
+        return json.loads(v1), json.loads(v2)
     except:
-        return (v1, v2)
+        return v1, v2
 
 
 def find_key(item):
@@ -60,11 +54,16 @@ def build_key(k1, k2):
     return "{}.{}".format(k1, k2)
 
 
-def iterate(val_id, key, v1, v2):
+def iterate(val_id, key, v1, v2, apm_v1=True):
     ret_val = 0
     if isinstance(v1, dict) and isinstance(v2, dict):
         for k, v in v1.items():
-            ret_val = max(ret_val, iterate(val_id, build_key(key, k), *json_val(v, v2[k] if k in v2 else "")))
+            ret_val = max(ret_val, iterate(val_id, build_key(key, k), *json_val(v, v2[k] if k in v2 else ""),
+                                           apm_v1=apm_v1))
+        if ret_val == 0:
+            for k, v in v2.items():
+                ret_val = max(ret_val, iterate(val_id, build_key(key, k), *json_val(v, v1[k] if k in v1 else ""),
+                                               apm_v1=not apm_v1))
     elif isinstance(v1, list) and isinstance(v2, list):
         v1, v2 = json_val(v1, v2)
         # assumption: an array only contains items of same type
@@ -76,11 +75,12 @@ def iterate(val_id, key, v1, v2):
                     return 3
 
                 item2 = find_item(v2, qkey, item[qkey])
-                ret_val = max(ret_val, iterate(val_id, build_key(key, "{}={}".format(qkey, item[qkey])), item, item2))
+                ret_val = max(ret_val, iterate(val_id, build_key(key, "{}={}".format(qkey, item[qkey])), item, item2,
+                                               apm_v1=apm_v1))
         else:
             v1, v2 = sorted(v1), sorted(v2)
             for item1, item2 in zip(v1, v2):
-                ret_val = max(ret_val, iterate(val_id, key, *json_val(item1, item2)))
+                ret_val = max(ret_val, iterate(val_id, key, *json_val(item1, item2), apm_v1=apm_v1))
     else:
         d = jsondiff.JsonDiffer(syntax='symmetric').diff(*json_val(v1, v2))
         if d:
@@ -92,8 +92,11 @@ def iterate(val_id, key, v1, v2):
                 print(json.dumps(d, indent=4))
             except:
                 print(d)
-            print("Value in APM Server: {}".format(v1))
-            print("Value in Kibana: {}".format(v2))
+            v1_label, v2_label = "APM Server", "Kibana"
+            if not apm_v1:
+                v1_label, v2_label = v2_label, v1_label
+            print("Value in {}: {!r}".format(v1_label, v1))
+            print("Value in {}: {!r}".format(v2_label, v2))
             print("---")
     return ret_val
 
@@ -118,38 +121,66 @@ def get_kibana_commit(branch):
     return None
 
 
+def load_kibana_index_pattern_file(p):
+    with open(p) as f:
+        return json.load(f)
+
+
+def load_kibana_index_pattern_url(p):
+    rsp = requests.get(p)
+    rsp.raise_for_status()
+    return rsp.json()
+
+
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('branch')
-    parser.add_argument('-I', '--index-pattern', type=str,
+    parser.add_argument('--branch', default='master')
+    parser.add_argument('-I', '--index-pattern',
                         default='src/legacy/core_plugins/kibana/server/tutorials/apm/saved_objects/index_pattern.json',
                         help='index-pattern file path')
-    parser.add_argument('-P', '--repo-path', type=str,
-                        default='https://raw.githubusercontent.com/elastic/kibana',
+    parser.add_argument('-P', '--repo-path',
+                        default='https://raw.githubusercontent.com/elastic/kibana/',
                         help='base repository path')
-    parser.add_argument('-C', '--commit', type=str, help='Commit sha to get the index-pattern from')
+    parser.add_argument('-C', '--commit', help='Commit sha to get the index-pattern from')
+    parser.add_argument("gen_index_pattern", type=argparse.FileType(mode="r"), help="expected index pattern")
     args = parser.parse_args()
+
+    # load expected kibana index pattern from url or local file
+    if args.repo_path.startswith("file://"):
+        parsed = urlparse(args.repo_path)
+        path = os.path.join(parsed.path, args.index_pattern)
+        load_kibana_index_pattern = load_kibana_index_pattern_file
+    else:
+        ref = args.commit
+        if ref is None:
+            ref = get_kibana_commit(args.branch)
+        if ref is None:
+            ref = args.branch
+        path = urljoin(args.repo_path, "/".join([ref, args.index_pattern]))
+        load_kibana_index_pattern = load_kibana_index_pattern_url
+
+    # load index pattern sync'd to kibana
+    print("---- Comparing Generated Index Pattern with " + path)
+    sync_index_pattern = load_kibana_index_pattern(path)
+
+    # load generated index pattern
+    gen_index_pattern = json.load(args.gen_index_pattern)["objects"][0]
+    gen_index_pattern["attributes"].pop("title")  # detects title set in sync'd pattern
+
+    # decode fields, they are json
+    sync_index_pattern["attributes"]["fields"] = json.loads(sync_index_pattern["attributes"].pop("fields"))
+    gen_index_pattern["attributes"]["fields"] = json.loads(gen_index_pattern["attributes"].pop("fields"))
+
     exit_val = 0
+    exit_val = max(exit_val, iterate(sync_index_pattern["id"], "", gen_index_pattern, sync_index_pattern))
 
-    ref = args.commit
-    if ref is None:
-        ref = get_kibana_commit(args.branch)
-    if ref is None:
-        ref = args.branch
-
-    k = fetch_kibana_file(args.repo_path, ref, args.index_pattern)
-    if k == 1:
-        print("Kibana file containing index pattern not found")
-        return 1
-
-    with open(os.path.abspath(os.path.join('index-pattern.json'))) as f:
-        s = json.load(f)["objects"][0]
-    exit_val = max(exit_val, iterate(k["id"], "", s, k))
+    # double check that there is no difference
     if exit_val == 0:
+        d = jsondiff.JsonDiffer(syntax='symmetric').diff(sync_index_pattern, gen_index_pattern)
+        if d:
+            print("index patterns differ: ", d)
+            return 5
         print("up-to-date")
-    if "title" in k["attributes"]:
-        print("`title` need to be set dynamically, remove it from the index-pattern!")
-        exit_val = 3
 
     return exit_val
 
