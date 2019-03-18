46f3cef47ba621a59599d945e1de620ae733df23
Gil Raphaelli <gil@elastic.co>
2019-03-18 17:19:43 -0400

    enable pipeline registration for setup --pipelines

--- tests/system/test_setup.py
+++ tests/system/test_setup.py
@@ -1,9 +1,64 @@
 import unittest
 
-from apmserver import SubCommandTest, get_elasticsearch_url
+from apmserver import ElasticTest, SubCommandTest, get_elasticsearch_url
 from beat.beat import INTEGRATION_TESTS
-from elasticsearch import Elasticsearch
+from elasticsearch import Elasticsearch, NotFoundError
+from elasticsearch.client import IngestClient
 
+
+class SetupPipelinesDefaultTest(SubCommandTest):
+    pipeline_name = "apm_user_agent"
+
+    def config(self):
+        cfg = super(SubCommandTest, self).config()
+        cfg.update({
+            "elasticsearch_host": get_elasticsearch_url(),
+            "file_enabled": "false",
+        })
+        return cfg
+
+    def start_args(self):
+        return {
+            "logging_args": ["-v", "-d", "*"],
+            "extra_args":   ["-e",
+                             "setup",
+                             "--pipelines"]
+        }
+
+    def setUp(self):
+        # TODO (gr): consolidate with ElasticTest
+        self.es = Elasticsearch([get_elasticsearch_url()])
+        self.es.ingest.delete_pipeline(id="*")
+        super(SetupPipelinesDefaultTest, self).setUp()
+
+    def assert_pipeline_presence(self, should_exist=False):
+        try:
+            self.es.ingest.get_pipeline(self.pipeline_name)
+            present = True
+        except NotFoundError:
+            present = False
+
+        assert should_exist == present, "expected {}pipelines".format("" if should_exist else "no ")
+
+    @unittest.skipUnless(INTEGRATION_TESTS, "integration test")
+    def test_setup_pipelines(self):
+        self.assert_pipeline_presence(True)
+        assert self.log_contains("Pipeline successfully registered: apm_user_agent")
+        assert self.log_contains("Registered Ingest Pipelines successfully.")
+
+
+class SetupPipelinesDisabledTest(SetupPipelinesDefaultTest):
+    def config(self):
+        cfg = super(SetupPipelinesDisabledTest, self).config()
+        cfg.update({
+            "register_pipeline_enabled": "false",
+        })
+        return cfg
+
+    @unittest.skipUnless(INTEGRATION_TESTS, "integration test")
+    def test_setup_pipelines(self):
+        self.assert_pipeline_presence(False)
+        assert self.log_contains("No pipeline callback registered")
 
 class SetupTemplateDefaultTest(SubCommandTest):
     """
