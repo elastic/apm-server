b953715c90136e7ffea1fd48f9bc4e134a73eee2
Silvia Mitter <silvia.mitter@elastic.co>
2019-04-23 15:21:18 +0200

    Fix test for syncing Kibana index pattern. (#2138)

--- script/are_kibana_saved_objects_updated.py
+++ script/are_kibana_saved_objects_updated.py
@@ -136,7 +136,7 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--branch', default='master')
     parser.add_argument('-I', '--index-pattern',
-                        default='src/legacy/core_plugins/kibana/server/tutorials/apm/saved_objects/index_pattern.json',
+                        default='src/legacy/core_plugins/kibana/server/tutorials/apm/index_pattern.json',
                         help='index-pattern file path')
     parser.add_argument('-P', '--repo-path',
                         default='https://raw.githubusercontent.com/elastic/kibana/',
