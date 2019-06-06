b496a59b5c03089d7adb0a2e9050267241ed461f
simitt <silvia.mitter@elastic.co>
2019-06-06 16:27:12 +0200

    some fixes

--- tests/system/test_access.py
+++ tests/system/test_access.py
@@ -54,6 +54,7 @@ class TestAccessWithCredentials(ServerBaseTest):
         assert r.status_code == 401, r.status_code
 
 
+@unittest.skipUnless(INTEGRATION_TESTS, "integration test")
 class SecureServerBaseTest(ServerSetUpBaseTest):
     @classmethod
     def setUpClass(cls):
@@ -80,6 +81,7 @@ class SecureServerBaseTest(ServerSetUpBaseTest):
         self.client_key = os.path.join(self.cert_path, "client.key.pem")
         self.server_cert = os.path.join(self.cert_path, "server.crt.pem")
         self.server_key = os.path.join(self.cert_path, "server.key.pem")
+        self.password = "foobar"
         super(SecureServerBaseTest, self).setUp()
 
     def tearDown(self):
@@ -92,7 +94,7 @@ class SecureServerBaseTest(ServerSetUpBaseTest):
             "ssl_certificate_authorities": self.ca_cert,
             "ssl_certificate": self.server_cert,
             "ssl_key": self.server_key,
-            "ssl_key_passphrase": "foobar"
+            "ssl_key_passphrase": self.password
         }
         cfg.update(self.ssl_overrides())
         return cfg
@@ -119,10 +121,10 @@ class SecureServerBaseTest(ServerSetUpBaseTest):
         context = ssl.SSLContext(protocol)
         if ciphers:
             context.set_ciphers(ciphers)
-        socket = ssl.socket()
-        s = context.wrap_socket(socket)
+        context.load_verify_locations(self.ca_cert)
+        context.load_cert_chain(certfile=self.server_cert, keyfile=self.server_key, password=self.password)
+        s = context.wrap_socket(ssl.socket())
         s.connect((self.host, self.port))
-        print s.cipher()
 
 
 class TestSSLBadPassphrase(SecureServerBaseTest):
@@ -138,17 +140,14 @@ class TestSSLEnabledNoClientVerification(SecureServerBaseTest):
     def ssl_overrides(self):
         return {"ssl_client_authentication": "none"}
 
-    @unittest.skipUnless(INTEGRATION_TESTS, "integration test")
     def test_https_no_cert_ok(self):
         r = self.send_http_request(verify=self.ca_cert)
         assert r.status_code == 202, r.status_code
 
-    @unittest.skipUnless(INTEGRATION_TESTS, "integration test")
     @raises(ChunkedEncodingError)
     def test_http_fails(self):
         self.send_http_request(protocol='http')
 
-    @unittest.skipUnless(INTEGRATION_TESTS, "integration test")
     @raises(SSLError)
     def test_https_server_validation_fails(self):
         r = self.send_http_request(verify=True)
@@ -159,18 +158,15 @@ class TestSSLEnabledOptionalClientVerification(SecureServerBaseTest):
     def ssl_overrides(self):
         return {"ssl_client_authentication": "optional"}
 
-    @unittest.skipUnless(INTEGRATION_TESTS, "integration test")
     def test_https_no_certificate_ok(self):
         r = self.send_http_request(verify=self.ca_cert)
         assert r.status_code == 202, r.status_code
 
-    @unittest.skipUnless(INTEGRATION_TESTS, "integration test")
     @raises(SSLError)
     def test_https_verify_cert_if_given(self):
         self.send_http_request(verify=self.ca_cert,
                                cert=(self.simple_cert, self.simple_key))
 
-    @unittest.skipUnless(INTEGRATION_TESTS, "integration test")
     def test_https_auth_cert_ok(self):
         r = self.send_http_request(verify=self.ca_cert,
                                    cert=(self.client_cert, self.client_key))
@@ -180,18 +176,15 @@ class TestSSLEnabledOptionalClientVerification(SecureServerBaseTest):
 class TestSSLEnabledRequiredClientVerification(SecureServerBaseTest):
     # no ssl_overrides necessary as `required` is default
 
-    @unittest.skipUnless(INTEGRATION_TESTS, "integration test")
     @raises(SSLError)
     def test_https_no_cert_fails(self):
         self.send_http_request(verify=self.ca_cert)
 
-    @unittest.skipUnless(INTEGRATION_TESTS, "integration test")
     @raises(SSLError)
     def test_https_invalid_cert_fails(self):
         self.send_http_request(verify=self.ca_cert,
                                cert=(self.simple_cert, self.simple_key))
 
-    @unittest.skipUnless(INTEGRATION_TESTS, "integration test")
     def test_https_auth_cert_ok(self):
         r = self.send_http_request(verify=self.ca_cert,
                                    cert=(self.client_cert, self.client_key))
@@ -199,62 +192,49 @@ class TestSSLEnabledRequiredClientVerification(SecureServerBaseTest):
 
 
 class TestSSLDefaultSupportedProcotols(SecureServerBaseTest):
-
     def ssl_overrides(self):
         return {"ssl_client_authentication": "none"}
 
-    @unittest.skipUnless(INTEGRATION_TESTS, "integration test")
     @raises(ssl.SSLError)
     def test_tls_v1_0(self):
         self.ssl_connect(protocol=ssl.PROTOCOL_TLSv1)
 
-    @unittest.skipUnless(INTEGRATION_TESTS, "integration test")
     def test_tls_v1_1(self):
         self.ssl_connect(protocol=ssl.PROTOCOL_TLSv1_1)
 
-    @unittest.skipUnless(INTEGRATION_TESTS, "integration test")
     def test_tls_v1_2(self):
         self.ssl_connect()
 
 
 class TestSSLSupportedProcotols(SecureServerBaseTest):
-
     def ssl_overrides(self):
         return {"ssl_client_authentication": "none",
                 "ssl_supported_protocols": ["TLSv1.2"]}
 
-    @unittest.skipUnless(INTEGRATION_TESTS, "integration test")
     @raises(ssl.SSLError)
     def test_tls_v1_1(self):
         self.ssl_connect(protocol=ssl.PROTOCOL_TLSv1_1)
 
-    @unittest.skipUnless(INTEGRATION_TESTS, "integration test")
     def test_tls_v1_2(self):
         self.ssl_connect()
 
 
 class TestSSLSupportedCiphers(SecureServerBaseTest):
-
     def ssl_overrides(self):
-        return {"ssl_client_authentication": "none",
-                "ssl_cipher_suites": ['ECDHE-RSA-AES128-GCM-SHA256']}
+        return {"ssl_cipher_suites": ['ECDHE-RSA-AES128-GCM-SHA256']}
 
-    @unittest.skipUnless(INTEGRATION_TESTS, "integration test")
     def test_https_no_cipher_set(self):
         self.ssl_connect()
 
-    @unittest.skipUnless(INTEGRATION_TESTS, "integration test")
     def test_https_supports_cipher(self):
         # set the same cipher in the client as set in the server
         self.ssl_connect(ciphers='ECDHE-RSA-AES128-GCM-SHA256')
 
-    @unittest.skipUnless(INTEGRATION_TESTS, "integration test")
     def test_https_unsupported_cipher(self):
         # client only offers unsupported cipher
         with self.assertRaisesRegexp(ssl.SSLError, 'SSLV3_ALERT_HANDSHAKE_FAILURE'):
             self.ssl_connect(ciphers='ECDHE-RSA-AES256-SHA384')
 
-    @unittest.skipUnless(INTEGRATION_TESTS, "integration test")
     def test_https_no_cipher_selected(self):
         # client provides invalid cipher
         with self.assertRaisesRegexp(ssl.SSLError, 'No cipher can be selected'):
