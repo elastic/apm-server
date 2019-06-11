b6c76ced072f7d8e6d9de893e951111984fb8785
Ivan Fernandez Calvo <kuisathaverat@yahoo.com>
2019-06-11 16:21:39 +0200

    fix: install go

---
+++ script/jenkins/test-install-packages.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+source ./_beats/dev-tools/common.bash
+
+jenkins_setup
+
+cleanup() {
+  rm -rf $TEMP_PYTHON_ENV
+  make stop-environment fix-permissions
+}
+trap cleanup EXIT
+
+mage -v testPackagesInstall
