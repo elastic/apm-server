507bf471c0bf2572221533643302fa3d34043b6f
Juan Álvarez <juan.alvarez@elastic.co>
2019-06-28 14:14:55 +0200

    Adds initial support for agent remote configuration (#2289) (#2349)
    This introduces a new requirement (Kibana) and setting for it, in the apm-server namespace (disabled by default).
    It also adds an endpoint /v1/agent/configs for agents to poll configuration, requiring them to be authenticated (except RUM).
    
    Supersedes https://github.com/elastic/apm-server/pull/2095/
    
    More info in https://github.com/elastic/apm/issues/4

---
+++ agentcfg/model.go
@@ -0,0 +1,72 @@
+// Licensed to Elasticsearch B.V. under one or more contributor
+// license agreements. See the NOTICE file distributed with
+// this work for additional information regarding copyright
+// ownership. Elasticsearch B.V. licenses this file to you under
+// the Apache License, Version 2.0 (the "License"); you may
+// not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+package agentcfg
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+const (
+	// ServiceName keyword
+	ServiceName = "service.name"
+	// ServiceEnv keyword
+	ServiceEnv = "service.environment"
+)
+
+// Doc represents an elasticsearch document
+type Doc struct {
+	ID     string `json:"_id"`
+	Source Source `json:"_source"`
+}
+
+// Source represents the elasticsearch _source field of a document
+type Source struct {
+	Settings Settings `json:"settings"`
+}
+
+// Settings hold agent configuration
+type Settings map[string]string
+
+// UnmarshalJSON overrides default method to convert any JSON type to string
+func (s *Settings) UnmarshalJSON(b []byte) error {
+	in := make(map[string]interface{})
+	out := make(map[string]string)
+	err := json.Unmarshal(b, &in)
+	for k, v := range in {
+		out[k] = fmt.Sprintf("%v", v)
+	}
+	*s = out
+	return err
+}
+
+// NewQuery creates a Query struct
+func NewQuery(name, env string) Query {
+	return Query{Service{name, env}}
+}
+
+// Query represents an URL body or query params for agent configuration
+type Query struct {
+	Service Service `json:"service"`
+}
+
+// Service holds supported attributes for querying configuration
+type Service struct {
+	Name        string `json:"name"`
+	Environment string `json:"environment,omitempty"`
+}
---
+++ beater/agent_config_handler.go
@@ -0,0 +1,94 @@
+// Licensed to Elasticsearch B.V. under one or more contributor
+// license agreements. See the NOTICE file distributed with
+// this work for additional information regarding copyright
+// ownership. Elasticsearch B.V. licenses this file to you under
+// the Apache License, Version 2.0 (the "License"); you may
+// not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+package beater
+
+import (
+	"net/http"
+
+	"github.com/elastic/apm-server/convert"
+
+	"github.com/pkg/errors"
+
+	"github.com/elastic/beats/libbeat/kibana"
+
+	"github.com/elastic/apm-server/agentcfg"
+)
+
+func agentConfigHandler(kbClient *kibana.Client, secretToken string) http.Handler {
+
+	var handler http.Handler = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		send := wrap(w, r)
+		clientEtag := r.Header.Get("If-None-Match")
+
+		query, requestErr := buildQuery(r)
+		cfg, upstreamEtag, internalErr := agentcfg.Fetch(kbClient, query, requestErr)
+
+		switch {
+		case requestErr != nil:
+			send(requestErr.Error(), http.StatusBadRequest)
+		case query == agentcfg.Query{}:
+			send(nil, http.StatusMethodNotAllowed)
+		case internalErr != nil:
+			send(internalErr.Error(), http.StatusInternalServerError)
+		case len(cfg) == 0:
+			send(nil, http.StatusNotFound)
+		case clientEtag != "" && clientEtag == upstreamEtag:
+			w.Header().Set("Cache-Control", "max-age=0")
+			send(nil, http.StatusNotModified)
+		case upstreamEtag != "":
+			w.Header().Set("Cache-Control", "max-age=0")
+			w.Header().Set("Etag", upstreamEtag)
+			fallthrough
+		default:
+			send(cfg, http.StatusOK)
+		}
+	})
+	return authHandler(secretToken, logHandler(handler))
+}
+
+// Returns (zero, error) if request body can't be unmarshalled or service.name is missing
+// Returns (zero, zero) if request method is not GET or POST
+func buildQuery(r *http.Request) (query agentcfg.Query, err error) {
+	switch r.Method {
+	case http.MethodPost:
+		err = convert.FromReader(r.Body, &query)
+	case http.MethodGet:
+		params := r.URL.Query()
+		query = agentcfg.NewQuery(
+			params.Get(agentcfg.ServiceName),
+			params.Get(agentcfg.ServiceEnv),
+		)
+	default:
+		return
+	}
+
+	if err == nil && query.Service.Name == "" {
+		err = errors.New(agentcfg.ServiceName + " is required")
+	}
+	return
+}
+
+func wrap(w http.ResponseWriter, r *http.Request) func(interface{}, int) {
+	return func(body interface{}, code int) {
+		if body == nil {
+			w.WriteHeader(code)
+		} else {
+			send(w, r, body, code)
+		}
+	}
+}
