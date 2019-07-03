90c37d11aad33f2c834fd4a17bd43fac56882759
Ivan Fernandez Calvo <kuisathaverat@yahoo.com>
2019-07-03 14:09:10 +0200

    fix: build inside a Golang Docker container

--- script/jenkins/build.sh
+++ script/jenkins/build.sh
@@ -1,15 +1,4 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-source ./_beats/dev-tools/common.bash
-
-jenkins_setup
-
-cleanup() {
-  rm -rf $TEMP_PYTHON_ENV
-  make stop-environment fix-permissions
-}
-trap cleanup EXIT
-
-make clean
-make
+mage -debug build
