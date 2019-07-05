89bb6af50599565f9f742c6f929d89775997c572
Victor Martinez <VictorMartinezRubio@gmail.com>
2019-07-05 15:24:32 +0100

    support 3.6 version in the CI

--- script/jenkins/check-changelogs.sh
+++ script/jenkins/check-changelogs.sh
@@ -5,4 +5,12 @@ source ./_beats/dev-tools/common.bash
 
 jenkins_setup
 
+## Instal CI dependencies to run the script in python 3.6
+git clone https://github.com/pyenv/pyenv-virtualenv.git $(pyenv root)/plugins/pyenv-virtualenv
+eval "$(pyenv virtualenv-init -)"
+pyenv install 3.6.8
+pyenv virtualenv 3.6.8 my-virtual-env-3.6.8
+pip install requests
+
+## Run the goal
 make check-changelogs
