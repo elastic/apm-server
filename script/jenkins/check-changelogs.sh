0a15ab1290031139ec9ef3327fb366c83ef6e640
Victor Martinez <VictorMartinezRubio@gmail.com>
2019-07-05 15:32:29 +0100

    Install pyenv

--- script/jenkins/check-changelogs.sh
+++ script/jenkins/check-changelogs.sh
@@ -6,10 +6,19 @@ source ./_beats/dev-tools/common.bash
 jenkins_setup
 
 ## Instal CI dependencies to run the script in python 3.6
+VERSION="3.6.8"
+### Install pyenv
+git clone https://github.com/pyenv/pyenv.git ~/.pyenv
+export PYENV_ROOT="$HOME/.pyenv"
+export PATH="$PYENV_ROOT/bin:$PATH"
+
+### Install pyenv-virtualenv plugin
 git clone https://github.com/pyenv/pyenv-virtualenv.git $(pyenv root)/plugins/pyenv-virtualenv
 eval "$(pyenv virtualenv-init -)"
-pyenv install 3.6.8
-pyenv virtualenv 3.6.8 my-virtual-env-3.6.8
+
+### Install the python ersion and setup environment
+pyenv install ${VERSION}
+pyenv virtualenv ${VERSION} my-virtual-env-${VERSION}
 pip install requests
 
 ## Run the goal
