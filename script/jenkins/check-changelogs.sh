f256f093cac3f7a5207e411f2df617a538a4af66
Victor Martinez <VictorMartinezRubio@gmail.com>
2019-07-05 16:00:08 +0100

    avoid interactive execution as TTY is required

--- script/jenkins/check-changelogs.sh
+++ script/jenkins/check-changelogs.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -exuo pipefail
 
-docker run --rm -ti \
+docker run --rm \
           -v "$PWD":/usr/src/myapp \
           -w /usr/src/myapp python:3.6.8-jessie \
           bash -c 'pip install requests; make check-changelogs'
