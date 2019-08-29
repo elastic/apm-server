6f14a43a44fe71987ab348a8ccf5fa3d8fd315e1
Juan Álvarez <juan.alvarez@elastic.co>
2019-08-29 14:56:24 +0200

    Update beats framework to c2803f9

--- vendor/github.com/elastic/beats/x-pack/libbeat/licenser/es_callback.go
+++ vendor/github.com/elastic/beats/x-pack/libbeat/licenser/es_callback.go
@@ -1,6 +1,19 @@
-// Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
-// or more contributor license agreements. Licensed under the Elastic License;
-// you may not use this file except in compliance with the Elastic License.
+// Licensed to Elasticsearch B.V. under one or more contributor
+// license agreements. See the NOTICE file distributed with
+// this work for additional information regarding copyright
+// ownership. Elasticsearch B.V. licenses this file to you under
+// the Apache License, Version 2.0 (the "License"); you may
+// not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
 
 package licenser
 
@@ -15,7 +28,7 @@ import (
 
 // Enforce setups the corresponding callbacks in libbeat to verify the license on the
 // remote elasticsearch cluster.
-func Enforce(log *logp.Logger, checks ...CheckFunc) {
+func Enforce(log *logp.Logger, name string, checks ...CheckFunc) {
 	cb := func(client *elasticsearch.Client) error {
 		fetcher := NewElasticFetcher(client)
 		license, err := fetcher.Fetch()
@@ -25,9 +38,9 @@ func Enforce(log *logp.Logger, checks ...CheckFunc) {
 		}
 
 		if license == OSSLicense {
-			return errors.New("This Beat requires the default distribution of Elasticsearch. Please " +
-				"install the default distribution of Elasticsearch from elastic.co, or install " +
-				"the oss-only distribution of beats")
+			return errors.Errorf("%s requires the default distribution of Elasticsearch. Please "+
+				"update to the default distribution of Elasticsearch for full access to all "+
+				"free features, or switch to the OSS distribution of %s.", name, name)
 		}
 
 		if !Validate(log, *license, checks...) {
