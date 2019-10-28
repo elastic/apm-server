5a88c3c0cea246e9fee411cba31555407cda7605
Ivan Fernandez Calvo <kuisathaverat@yahoo.com>
2019-10-28 18:08:50 +0100

    fix: trap errors on windows builds

--- script/jenkins/windows-build.ps1
+++ script/jenkins/windows-build.ps1
@@ -1,25 +1,9 @@
-function Exec {
-    [CmdletBinding()]
-    param(
-        [Parameter(Mandatory = $true)]
-        [scriptblock]$cmd,
-        [string]$errorMessage = ($msgs.error_bad_command -f $cmd)
-    )
-
-    try {
-        $global:lastexitcode = 0
-        & $cmd
-        if ($lastexitcode -ne 0) {
-            throw $errorMessage
-        }
-    }
-    catch [Exception] {
-        throw $_
-    }
-}
+$ErrorActionPreference = "Stop"
+trap { "Error trapped: $_"; break }
 
 # Setup Go.
 $env:GOPATH = $env:WORKSPACE
+$env:GO111MODULE = "off"
 $env:PATH = "$env:GOPATH\bin;C:\tools\mingw64\bin;$env:PATH"
 & gvm --format=powershell $(Get-Content .go-version) | Invoke-Expression
 
@@ -32,11 +16,11 @@ $env:TEST_COVERAGE = "true"
 $env:RACE_DETECTOR = "true"
 
 # Install mage from vendor.
-exec { go install github.com/elastic/apm-server/vendor/github.com/magefile/mage }
+& go install github.com/elastic/apm-server/vendor/github.com/magefile/mage
 
 echo "Fetching testing dependencies"
 # TODO (elastic/beats#5050): Use a vendored copy of this.
-exec { go get github.com/docker/libcompose }
+& go get github.com/docker/libcompose
 
 if (Test-Path "build") { Remove-Item -Recurse -Force build }
 New-Item -ItemType directory -Path build\coverage | Out-Null
@@ -44,7 +28,7 @@ New-Item -ItemType directory -Path build\system-tests | Out-Null
 New-Item -ItemType directory -Path build\system-tests\run | Out-Null
 
 echo "Building fields.yml"
-exec { mage fields }
+& mage fields
 
 echo "Building $env:beat"
-exec { mage build } "Build FAILURE"
+& mage build
--- script/jenkins/windows-test.ps1
+++ script/jenkins/windows-test.ps1
@@ -1,25 +1,9 @@
-function Exec {
-    [CmdletBinding()]
-    param(
-        [Parameter(Mandatory = $true)]
-        [scriptblock]$cmd,
-        [string]$errorMessage = ($msgs.error_bad_command -f $cmd)
-    )
-
-    try {
-        $global:lastexitcode = 0
-        & $cmd
-        if ($lastexitcode -ne 0) {
-            throw $errorMessage
-        }
-    }
-    catch [Exception] {
-        throw $_
-    }
-}
+$ErrorActionPreference = "Stop"
+trap { "Error trapped: $_"; break }
 
 # Setup Go.
 $env:GOPATH = $env:WORKSPACE
+$env:GO111MODULE = "off"
 $env:PATH = "$env:GOPATH\bin;C:\tools\mingw64\bin;$env:PATH"
 & gvm --format=powershell $(Get-Content .go-version) | Invoke-Expression
 
@@ -32,11 +16,11 @@ $env:TEST_COVERAGE = "true"
 $env:RACE_DETECTOR = "true"
 
 # Install mage from vendor.
-exec { go install github.com/elastic/apm-server/vendor/github.com/magefile/mage }
+& go install github.com/elastic/apm-server/vendor/github.com/magefile/mage
 
 echo "Fetching testing dependencies"
 # TODO (elastic/beats#5050): Use a vendored copy of this.
-exec { go get github.com/docker/libcompose }
+& go get github.com/docker/libcompose
 
 if (Test-Path "build") { Remove-Item -Recurse -Force build }
 New-Item -ItemType directory -Path build\coverage | Out-Null
@@ -44,18 +28,18 @@ New-Item -ItemType directory -Path build\system-tests | Out-Null
 New-Item -ItemType directory -Path build\system-tests\run | Out-Null
 
 echo "Building fields.yml"
-exec { mage fields }
+& mage fields
 
 echo "Building $env:beat"
-exec { mage build } "Build FAILURE"
+& mage build
 
 echo "Unit testing $env:beat"
-exec { mage goTestUnit }
+& mage goTestUnit
 
 echo "System testing $env:beat"
 # Get a CSV list of package names.
 $packages = $(go list ./... | select-string -Pattern "/vendor/" -NotMatch | select-string -Pattern "/scripts/cmd/" -NotMatch)
 $packages = ($packages|group|Select -ExpandProperty Name) -join ","
-exec { go test -race -c -cover -covermode=atomic -coverpkg $packages } "go test FAILURE"
+& go test -race -c -cover -covermode=atomic -coverpkg $packages
 Set-Location -Path tests/system
-exec { nosetests --with-timer --with-xunit --xunit-file=../../build/TEST-system.xml } "System test FAILURE"
+& nosetests --with-timer --with-xunit --xunit-file=../../build/TEST-system.xml
