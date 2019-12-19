c27775d52b33be613df5a21c84c1b58d5b05d3c9
Ivan Fernandez Calvo <kuisathaverat@users.noreply.github.com>
2019-12-19 11:03:22 +0100

    feat: grab docker container logs after run tests (#2948) (#3082)
    * feat: grab docker container logs after run tests
    
    * fix: set execution permission on .ci/scripts/docker-get-logs.sh
    
    * chore: make docker-info path shorter
    
    * feat: grab system test junit results

--- script/jenkins/linux-test.sh
+++ script/jenkins/linux-test.sh
@@ -5,12 +5,12 @@ source ./_beats/dev-tools/common.bash
 
 jenkins_setup
 
-#cleanup() {
-#  rm -rf $TEMP_PYTHON_ENV
-#  make stop-environment fix-permissions
-#}
-#trap cleanup EXIT
+cleanup() {
+  rm -rf $TEMP_PYTHON_ENV
+  .ci/scripts/docker-get-logs.sh
+  make stop-environment fix-permissions
+}
+trap cleanup EXIT
 
 make update
 make system-tests-environment
-make stop-environment
