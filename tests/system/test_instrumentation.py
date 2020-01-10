a1d7ed8cb543c97e8dd5970fbc47f06e37e36e0d
Andrew Wilkins <axwalk@gmail.com>
2020-01-10 15:00:09 +0800

    tests/system: test tracing to external host + auth

--- tests/system/test_instrumentation.py
+++ tests/system/test_instrumentation.py
@@ -1,14 +1,87 @@
 from datetime import datetime, timedelta
+import os
 import time
 
 import requests
 
 from apmserver import integration_test
 from apmserver import ElasticTest
+from test_access import BaseAPIKeySetup
+
+
+# Set ELASTIC_APM_API_REQUEST_TIME to a short duration
+# to speed up the time taken for self-tracing events
+# to be ingested.
+os.environ["ELASTIC_APM_API_REQUEST_TIME"] = "1s"
 
 
 @integration_test
-class ProfileIntegrationTest(ElasticTest):
+class TestExternalTracingAPIKey(BaseAPIKeySetup):
+    def config(self):
+        cfg = super(TestExternalTracingAPIKey, self).config()
+        api_key = self.create_api_key([self.privilege_intake], self.resource_any).lstrip("ApiKey ")
+        cfg.update({
+            "api_key_enabled": True,
+            "instrumentation_enabled": "true",
+            "instrumentation_api_key": api_key,
+            # Set instrumentation.hosts to the same APM Server.
+            #
+            # Explicitly specifying hosts configures the tracer to
+            # behave as if it's sending to an external server, rather
+            # than using the in-memory transport that bypasses auth.
+            "instrumentation_host": BaseAPIKeySetup.host,
+        })
+        return cfg
+
+    def test_api_key_auth(self):
+        # Send a POST request to the intake API URL. Doesn't matter what the
+        # request body contents are, as the request will fail due to lack of
+        # authorization. We just want to trigger the server's tracing.
+        r = requests.post(self.intake_url, data="invalid")
+        self.assertEqual(401, r.status_code)
+
+        query = {"term": {"processor.name": "transaction"}}
+        index = self.index_transaction
+        def get_transactions():
+            self.es.indices.refresh(index=index)
+            return self.es.count(index=index, body={"query": query})['count'] > 0
+        self.wait_until(get_transactions, name='have transaction documents')
+
+
+@integration_test
+class TestExternalTracingSecretToken(ElasticTest):
+    def config(self):
+        cfg = super(TestExternalTracingSecretToken, self).config()
+        secret_token = "abc123"
+        cfg.update({
+            "secret_token": secret_token,
+            "instrumentation_enabled": "true",
+            "instrumentation_secret_token": secret_token,
+            # Set instrumentation.hosts to the same APM Server.
+            #
+            # Explicitly specifying hosts configures the tracer to
+            # behave as if it's sending to an external server, rather
+            # than using the in-memory transport that bypasses auth.
+            "instrumentation_host": ElasticTest.host,
+        })
+        return cfg
+
+    def test_secret_token_auth(self):
+        # Send a POST request to the intake API URL. Doesn't matter what the
+        # request body contents are, as the request will fail due to lack of
+        # authorization. We just want to trigger the server's tracing.
+        r = requests.post(self.intake_url, data="invalid")
+        self.assertEqual(401, r.status_code)
+
+        query = {"term": {"processor.name": "transaction"}}
+        index = self.index_transaction
+        def get_transactions():
+            self.es.indices.refresh(index=index)
+            return self.es.count(index=index, body={"query": query})['count'] > 0
+        self.wait_until(get_transactions, name='have transaction documents')
+
+
+class ProfilingTest(ElasticTest):
     def metric_fields(self):
         metric_fields = set()
         rs = self.es.search(index=self.index_profile)
@@ -26,7 +99,7 @@ class ProfileIntegrationTest(ElasticTest):
 
 
 @integration_test
-class CPUProfileIntegrationTest(ProfileIntegrationTest):
+class TestCPUProfiling(ProfilingTest):
     config_overrides = {
         "instrumentation_enabled": "true",
         "profiling_cpu_enabled": "true",
@@ -57,7 +130,7 @@ class CPUProfileIntegrationTest(ProfileIntegrationTest):
 
 
 @integration_test
-class HeapProfileIntegrationTest(ProfileIntegrationTest):
+class TestHeapProfiling(ProfilingTest):
     config_overrides = {
         "instrumentation_enabled": "true",
         "profiling_heap_enabled": "true",
