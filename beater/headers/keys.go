f135da3f5177da66b4e63d3c614779026720337f
Juan Álvarez <juan.alvarez@elastic.co>
2020-01-10 14:49:46 +0100

    fix another golint refactor screwup

--- beater/headers/keys.go
+++ beater/headers/keys.go
@@ -26,7 +26,7 @@ const (
 	AccessControlExposeHeaders = "Access-Control-Expose-Headers"
 	AccessControlMaxAge        = "Access-Control-Max-Age"
 	Authorization              = "Authorization"
-	APIKey                     = "APIKey"
+	APIKey                     = "ApiKey"
 	Bearer                     = "Bearer"
 	CacheControl               = "Cache-Control"
 	Connection                 = "Connection"
