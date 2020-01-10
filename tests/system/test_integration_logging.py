f078975ec0018f075304adff482ce5f5a4ba106d
Andrew Wilkins <axwalk@gmail.com>
2020-01-10 16:19:21 +0800

    beater/middleware: add trace/log correlation

--- tests/system/test_integration_logging.py
+++ tests/system/test_integration_logging.py
@@ -68,3 +68,24 @@ class LoggingIntegrationEventSizeTest(ElasticTest):
             }, req)
             error = req.get("error")
             assert error.startswith("event exceeded the permitted size."), json.dumps(req)
+
+
+@integration_test
+class LoggingIntegrationTraceCorrelationTest(ElasticTest):
+    config_overrides = {
+        "logging_json": "true",
+        "instrumentation_enabled": "true",
+    }
+
+    def test_trace_ids(self):
+        with open(self.get_transaction_payload_path()) as f:
+            r = requests.post(self.intake_url,
+                              data=f,
+                              headers={'content-type': 'application/x-ndjson'})
+            assert r.status_code == 202, r.status_code
+            intake_request_logs = list(self.logged_requests())
+            assert len(intake_request_logs) == 1, "multiple requests found"
+            req = intake_request_logs[0]
+            self.assertIn("trace.id", req)
+            self.assertIn("transaction.id", req)
+            self.assertEqual(req["transaction.id"], req["request_id"])
