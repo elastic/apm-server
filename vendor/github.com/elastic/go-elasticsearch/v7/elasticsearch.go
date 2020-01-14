4b0401a16a7d38e1fcb6848c8859a81418e4b674
simitt <silvia.mitter@elastic.co>
2020-01-14 14:08:59 +0100

    Update go-elasticsearch-client v7 to 7.x branch

--- vendor/github.com/elastic/go-elasticsearch/v7/elasticsearch.go
+++ vendor/github.com/elastic/go-elasticsearch/v7/elasticsearch.go
@@ -12,6 +12,7 @@ import (
 	"net/url"
 	"os"
 	"strings"
+	"time"
 
 	"github.com/elastic/go-elasticsearch/v7/esapi"
 	"github.com/elastic/go-elasticsearch/v7/estransport"
@@ -36,8 +37,25 @@ type Config struct {
 	CloudID string // Endpoint for the Elastic Service (https://elastic.co/cloud).
 	APIKey  string // Base64-encoded token for authorization; if set, overrides username and password.
 
-	Transport http.RoundTripper  // The HTTP transport object.
-	Logger    estransport.Logger // The logger object.
+	RetryOnStatus        []int // List of status codes for retry. Default: 502, 503, 504.
+	DisableRetry         bool  // Default: false.
+	EnableRetryOnTimeout bool  // Default: false.
+	MaxRetries           int   // Default: 3.
+
+	DiscoverNodesOnStart  bool          // Discover nodes when initializing the client. Default: false.
+	DiscoverNodesInterval time.Duration // Discover nodes periodically. Default: disabled.
+
+	EnableMetrics     bool // Enable the metrics collection.
+	EnableDebugLogger bool // Enable the debug logging.
+
+	RetryBackoff func(attempt int) time.Duration // Optional backoff duration. Default: nil.
+
+	Transport http.RoundTripper    // The HTTP transport object.
+	Logger    estransport.Logger   // The logger object.
+	Selector  estransport.Selector // The selector object.
+
+	// Optional constructor function for a custom ConnectionPool. Default: nil.
+	ConnectionPoolFunc func([]*estransport.Connection, estransport.Selector) estransport.ConnectionPool
 }
 
 // Client represents the Elasticsearch client.
@@ -65,38 +83,31 @@ func NewDefaultClient() (*Client, error) {
 // It will use the ELASTICSEARCH_URL environment variable, if set,
 // to configure the addresses; use a comma to separate multiple URLs.
 //
-// It's an error to set both cfg.Addresses and the ELASTICSEARCH_URL
-// environment variable.
+// If either cfg.Addresses or cfg.CloudID is set, the ELASTICSEARCH_URL
+// environment variable is ignored.
+//
+// It's an error to set both cfg.Addresses and cfg.CloudID.
 //
 func NewClient(cfg Config) (*Client, error) {
 	var addrs []string
 
-	envAddrs := addrsFromEnvironment()
-
-	if len(envAddrs) > 0 && len(cfg.Addresses) > 0 {
-		return nil, errors.New("cannot create client: both ELASTICSEARCH_URL and Addresses are set")
-	}
-
-	if len(envAddrs) > 0 && cfg.CloudID != "" {
-		return nil, errors.New("cannot create client: both ELASTICSEARCH_URL and CloudID are set")
-	}
-
-	if len(cfg.Addresses) > 0 && cfg.CloudID != "" {
-		return nil, errors.New("cannot create client: both Adresses and CloudID are set")
-	}
-
-	if cfg.CloudID != "" {
-		cloudAddrs, err := addrFromCloudID(cfg.CloudID)
-		if err != nil {
-			return nil, fmt.Errorf("cannot create client: cannot parse CloudID: %s", err)
-		}
-		addrs = append(addrs, cloudAddrs)
+	if len(cfg.Addresses) == 0 && cfg.CloudID == "" {
+		addrs = addrsFromEnvironment()
 	} else {
-		if len(envAddrs) > 0 {
-			addrs = append(envAddrs, envAddrs...)
+		if len(cfg.Addresses) > 0 && cfg.CloudID != "" {
+			return nil, errors.New("cannot create client: both Addresses and CloudID are set")
 		}
+
+		if cfg.CloudID != "" {
+			cloudAddr, err := addrFromCloudID(cfg.CloudID)
+			if err != nil {
+				return nil, fmt.Errorf("cannot create client: cannot parse CloudID: %s", err)
+			}
+			addrs = append(addrs, cloudAddr)
+		}
+
 		if len(cfg.Addresses) > 0 {
-			addrs = append(envAddrs, cfg.Addresses...)
+			addrs = append(addrs, cfg.Addresses...)
 		}
 	}
 
@@ -110,23 +121,67 @@ func NewClient(cfg Config) (*Client, error) {
 		urls = append(urls, u)
 	}
 
+	// TODO(karmi): Refactor
+	if urls[0].User != nil {
+		cfg.Username = urls[0].User.Username()
+		pw, _ := urls[0].User.Password()
+		cfg.Password = pw
+	}
+
 	tp := estransport.New(estransport.Config{
 		URLs:     urls,
 		Username: cfg.Username,
 		Password: cfg.Password,
 		APIKey:   cfg.APIKey,
 
-		Transport: cfg.Transport,
-		Logger:    cfg.Logger,
+		RetryOnStatus:        cfg.RetryOnStatus,
+		DisableRetry:         cfg.DisableRetry,
+		EnableRetryOnTimeout: cfg.EnableRetryOnTimeout,
+		MaxRetries:           cfg.MaxRetries,
+		RetryBackoff:         cfg.RetryBackoff,
+
+		EnableMetrics:     cfg.EnableMetrics,
+		EnableDebugLogger: cfg.EnableDebugLogger,
+
+		DiscoverNodesInterval: cfg.DiscoverNodesInterval,
+
+		Transport:          cfg.Transport,
+		Logger:             cfg.Logger,
+		Selector:           cfg.Selector,
+		ConnectionPoolFunc: cfg.ConnectionPoolFunc,
 	})
 
-	return &Client{Transport: tp, API: esapi.New(tp)}, nil
+	client := &Client{Transport: tp, API: esapi.New(tp)}
+
+	if cfg.DiscoverNodesOnStart {
+		go client.DiscoverNodes()
+	}
+
+	return client, nil
 }
 
 // Perform delegates to Transport to execute a request and return a response.
 //
 func (c *Client) Perform(req *http.Request) (*http.Response, error) {
 	return c.Transport.Perform(req)
+}
+
+// Metrics returns the client metrics.
+//
+func (c *Client) Metrics() (estransport.Metrics, error) {
+	if mt, ok := c.Transport.(estransport.Measurable); ok {
+		return mt.Metrics()
+	}
+	return estransport.Metrics{}, errors.New("transport is missing method Metrics()")
+}
+
+// DiscoverNodes reloads the client connections by fetching information from the cluster.
+//
+func (c *Client) DiscoverNodes() error {
+	if dt, ok := c.Transport.(estransport.Discoverable); ok {
+		return dt.DiscoverNodes()
+	}
+	return errors.New("transport is missing method DiscoverNodes()")
 }
 
 // addrsFromEnvironment returns a list of addresses by splitting
@@ -164,10 +219,7 @@ func addrsToURLs(addrs []string) ([]*url.URL, error) {
 // See: https://www.elastic.co/guide/en/cloud/current/ec-cloud-id.html
 //
 func addrFromCloudID(input string) (string, error) {
-	var (
-		port   = 9243
-		scheme = "https://"
-	)
+	var scheme = "https://"
 
 	values := strings.Split(input, ":")
 	if len(values) != 2 {
@@ -178,5 +230,10 @@ func addrFromCloudID(input string) (string, error) {
 		return "", err
 	}
 	parts := strings.Split(string(data), "$")
-	return fmt.Sprintf("%s%s.%s:%d", scheme, parts[1], parts[0], port), nil
+
+	if len(parts) < 2 {
+		return "", fmt.Errorf("invalid encoded value: %s", parts)
+	}
+
+	return fmt.Sprintf("%s%s.%s", scheme, parts[1], parts[0]), nil
 }
--- vendor/github.com/elastic/go-elasticsearch/v7/esapi/api.xpack.security.get_api_key.go
+++ vendor/github.com/elastic/go-elasticsearch/v7/esapi/api.xpack.security.get_api_key.go
@@ -1,14 +1,15 @@
-// Licensed to Elasticsearch B.V. under one or more agreements.
+// Licensed to Elasticsearch B.V under one or more agreements.
 // Elasticsearch B.V. licenses this file to you under the Apache 2.0 License.
 // See the LICENSE file in the project root for more information.
-
-// Code generated from specification version 7.4.1: DO NOT EDIT
+//
+// Code generated from specification version 7.5.0: DO NOT EDIT
 
 package esapi
 
 import (
 	"context"
 	"net/http"
+	"strconv"
 	"strings"
 )
 
@@ -35,6 +36,7 @@ type SecurityGetAPIKey func(o ...func(*SecurityGetAPIKeyRequest)) (*Response, er
 type SecurityGetAPIKeyRequest struct {
 	ID        string
 	Name      string
+	Owner     *bool
 	RealmName string
 	Username  string
 
@@ -72,6 +74,10 @@ func (r SecurityGetAPIKeyRequest) Do(ctx context.Context, transport Transport) (
 		params["name"] = r.Name
 	}
 
+	if r.Owner != nil {
+		params["owner"] = strconv.FormatBool(*r.Owner)
+	}
+
 	if r.RealmName != "" {
 		params["realm_name"] = r.RealmName
 	}
@@ -96,7 +102,10 @@ func (r SecurityGetAPIKeyRequest) Do(ctx context.Context, transport Transport) (
 		params["filter_path"] = strings.Join(r.FilterPath, ",")
 	}
 
-	req, _ := newRequest(method, path.String(), nil)
+	req, err := newRequest(method, path.String(), nil)
+	if err != nil {
+		return nil, err
+	}
 
 	if len(params) > 0 {
 		q := req.URL.Query()
@@ -160,6 +169,14 @@ func (f SecurityGetAPIKey) WithName(v string) func(*SecurityGetAPIKeyRequest) {
 	}
 }
 
+// WithOwner - flag to query api keys owned by the currently authenticated user.
+//
+func (f SecurityGetAPIKey) WithOwner(v bool) func(*SecurityGetAPIKeyRequest) {
+	return func(r *SecurityGetAPIKeyRequest) {
+		r.Owner = &v
+	}
+}
+
 // WithRealmName - realm name of the user who created this api key to be retrieved.
 //
 func (f SecurityGetAPIKey) WithRealmName(v string) func(*SecurityGetAPIKeyRequest) {
@@ -218,5 +235,16 @@ func (f SecurityGetAPIKey) WithHeader(h map[string]string) func(*SecurityGetAPIK
 		for k, v := range h {
 			r.Header.Add(k, v)
 		}
+	}
+}
+
+// WithOpaqueID adds the X-Opaque-Id header to the HTTP request.
+//
+func (f SecurityGetAPIKey) WithOpaqueID(s string) func(*SecurityGetAPIKeyRequest) {
+	return func(r *SecurityGetAPIKeyRequest) {
+		if r.Header == nil {
+			r.Header = make(http.Header)
+		}
+		r.Header.Set("X-Opaque-Id", s)
 	}
 }
