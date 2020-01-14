189cd906fbb45ccaff4750c64e4cc587d8aeaec6
simitt <silvia.mitter@elastic.co>
2020-01-14 14:19:54 +0100

    Update go-elasticsearch client to 7.6
    fixes #3131

--- vendor/github.com/elastic/go-elasticsearch/v7/esapi/api.index.go
+++ vendor/github.com/elastic/go-elasticsearch/v7/esapi/api.index.go
@@ -1,8 +1,8 @@
-// Licensed to Elasticsearch B.V. under one or more agreements.
+// Licensed to Elasticsearch B.V under one or more agreements.
 // Elasticsearch B.V. licenses this file to you under the Apache 2.0 License.
 // See the LICENSE file in the project root for more information.
-
-// Code generated from specification version 7.4.1: DO NOT EDIT
+//
+// Code generated from specification version 7.5.0: DO NOT EDIT
 
 package esapi
 
@@ -152,7 +152,10 @@ func (r IndexRequest) Do(ctx context.Context, transport Transport) (*Response, e
 		params["filter_path"] = strings.Join(r.FilterPath, ",")
 	}
 
-	req, _ := newRequest(method, path.String(), r.Body)
+	req, err := newRequest(method, path.String(), r.Body)
+	if err != nil {
+		return nil, err
+	}
 
 	if len(params) > 0 {
 		q := req.URL.Query()
@@ -236,7 +239,7 @@ func (f Index) WithIfSeqNo(v int) func(*IndexRequest) {
 	}
 }
 
-// WithOpType - explicit operation type.
+// WithOpType - explicit operation type. defaults to `index` for requests with an explicit document ID, and to `create`for requests without an explicit document ID.
 //
 func (f Index) WithOpType(v string) func(*IndexRequest) {
 	return func(r *IndexRequest) {
@@ -342,5 +345,16 @@ func (f Index) WithHeader(h map[string]string) func(*IndexRequest) {
 		for k, v := range h {
 			r.Header.Add(k, v)
 		}
+	}
+}
+
+// WithOpaqueID adds the X-Opaque-Id header to the HTTP request.
+//
+func (f Index) WithOpaqueID(s string) func(*IndexRequest) {
+	return func(r *IndexRequest) {
+		if r.Header == nil {
+			r.Header = make(http.Header)
+		}
+		r.Header.Set("X-Opaque-Id", s)
 	}
 }
