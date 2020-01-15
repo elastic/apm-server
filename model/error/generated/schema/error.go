edb7683da01fb188cbb08ee85b32338f41a780d7
Silvia Mitter <silvia.mitter@elastic.co>
2020-01-15 07:42:40 +0100

    Add description to Intake API for remote_address (#2809) (#3164)
    Help defining how the `context.request.socket.remote_address` should be
    populated.
    
    Co-Authored-By: eyalkoren <41850454+eyalkoren@users.noreply.github.com>
    
    Co-authored-by: eyalkoren <41850454+eyalkoren@users.noreply.github.com>
    Co-authored-by: Andrew Wilkins <axwalk@gmail.com>

--- model/error/generated/schema/error.go
+++ model/error/generated/schema/error.go
@@ -156,6 +156,7 @@ const ModelSchema = `{
                     "type": ["boolean", "null"]
                 },
                 "remote_address": {
+                    "description": "The network address sending the request. Should be obtained through standard APIs and not parsed from any headers like 'Forwarded'.",
                     "type": ["string", "null"]
                 }
             }
