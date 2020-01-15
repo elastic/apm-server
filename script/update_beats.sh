61d54770c29db1cbde5c9727f72398f1e89b92f3
simitt <silvia.mitter@elastic.co>
2020-01-15 22:44:48 +0100

    Update beats framework to 3ed9a54

--- script/update_beats.sh
+++ script/update_beats.sh
@@ -75,8 +75,11 @@ rsync -crpv --delete \
 popd
 
 # temporary for separate beats code vendoring and packaging
+mkdir -p ${BASEDIR}/dev-tools/vendor/
+touch  ${BASEDIR}/dev-tools/vendor/.keep
+mkdir -p ${BASEDIR}/vendor/github.com/tsg/
 rsync -crpv --delete \
-  ${BASEDIR}/${GIT_CLONE}/vendor/github.com/tsg/go-daemon ${BASEDIR}/vendor/
+  ${BASEDIR}/${GIT_CLONE}/vendor/github.com/tsg/go-daemon ${BASEDIR}/vendor/github.com/tsg/
 
 # use exactly the same beats revision rather than $BEATS_VERSION
 BEATS_REVISION=$(GIT_DIR=${BASEDIR}/${GIT_CLONE}/.git git rev-parse HEAD)
