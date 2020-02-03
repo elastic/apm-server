6907417e5c53e131f0e8e128c40afb3e92e5e1b0
Gil Raphaelli <gil@elastic.co>
2020-02-03 15:53:16 -0500

    sort test imports

--- beater/config/config_test.go
+++ beater/config/config_test.go
@@ -25,11 +25,11 @@ import (
 	"testing"
 	"time"
 
-	"github.com/elastic/beats/libbeat/common/transport/tlscommon"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 
 	"github.com/elastic/beats/libbeat/common"
+	"github.com/elastic/beats/libbeat/common/transport/tlscommon"
 	"github.com/elastic/beats/libbeat/outputs"
 
 	"github.com/elastic/apm-server/elasticsearch"
