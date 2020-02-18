44a39f8fbcf1bc907f6f3fadfec66e77355ebffa
simitt <silvia.mitter@elastic.co>
2020-02-18 19:56:16 +0100

    Update to python3
    add python-env to make targets

--- script/jenkins/check-changelogs.sh
+++ script/jenkins/check-changelogs.sh
@@ -4,4 +4,4 @@ set -exuo pipefail
 docker run --rm \
           -v "$PWD":/usr/src/myapp \
           -w /usr/src/myapp python:3.6.8-alpine \
-          sh -c 'pip install requests; python script/check_changelogs.py'
+          sh -c 'pip3 install requests; python3 script/check_changelogs.py'
