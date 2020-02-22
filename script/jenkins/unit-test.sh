d1e1f8f900c24041d01c7c981bfa601c9a9b9841
Andrew Wilkins <axwalk@gmail.com>
2020-02-22 13:56:06 +0800

    script/jenkins: fix path to build tools

--- script/jenkins/unit-test.sh
+++ script/jenkins/unit-test.sh
@@ -13,6 +13,6 @@ make update test-deps
 mkdir -p ${COV_DIR}
 (go test -race -covermode=atomic -coverprofile=${COV_DIR}/unit.cov -v ./... 2>&1 | tee ${OUT_FILE}) || echo -e "\033[31;49mTests FAILED\033[0m"
 
-cat ${OUT_FILE} | go-junit-report > build/junit-apm-server-report.xml
+cat ${OUT_FILE} | bin/go-junit-report > build/junit-apm-server-report.xml
 go tool cover -html="${COV_DIR}/unit.cov" -o "${COV_DIR}/coverage-unit-report.html"
-gocover-cobertura < "${COV_DIR}/unit.cov" > "${COV_DIR}/coverage-unit-report.xml"
+bin/gocover-cobertura < "${COV_DIR}/unit.cov" > "${COV_DIR}/coverage-unit-report.xml"
