3e5ebe843aa1491a53daee64eee7018e6e6ec7a3
Andrew Wilkins <axwalk@gmail.com>
2020-02-22 08:31:55 +0800

    script/jenkins: simplify unit-test.sh
    No need to use a separate tool for coverage
    testing, recent versions of the go tool give
    the ability to run tests for multiple packages
    and capture coverage.

--- script/jenkins/unit-test.sh
+++ script/jenkins/unit-test.sh
@@ -8,11 +8,10 @@ jenkins_setup
 export OUT_FILE="build/test-report.out"
 export COV_DIR="build/coverage"
 
-mkdir -p {COV_DIR}
+make update test-deps
 
-make update prepare-tests test-deps
-
-(gotestcover -race -coverprofile=${COV_DIR}/unit.cov -v ./... 2>&1 | tee ${OUT_FILE}) || echo -e "\033[31;49mTests FAILED\033[0m"
+mkdir -p ${COV_DIR}
+(go test -race -covermode=atomic -coverprofile=${COV_DIR}/unit.cov -v ./... 2>&1 | tee ${OUT_FILE}) || echo -e "\033[31;49mTests FAILED\033[0m"
 
 cat ${OUT_FILE} | go-junit-report > build/junit-apm-server-report.xml
 go tool cover -html="${COV_DIR}/unit.cov" -o "${COV_DIR}/coverage-unit-report.html"
