3f27beebc2b18f8f4c3ea94a3b81c9e566413636
Andrew Wilkins <axwalk@gmail.com>
2020-02-22 08:34:16 +0800

    Makefile: rewrite all the things
    Key changes
     - removed reliance on libbeat/scripts/Makefile
     - now using mage in a few more places
     - build tools are installed into the "build" dir,
       to ensure consistency and avoid polluting the environment
     - using proper Make targets/dependencies rather than
       having everything be .PHONY, reducing unnecessary rebuilds
     - renamed "system-tests-environment" to "docker-system-tests",
       and changed it to remove containers after running. The
       "start-environment" and "stop-environment" targets no longer
       exist -- use docker-compose commands directly as needed
     - introduced "config.mk", which can be used for overriding
       variable defaults, e.g. for GOLINT_UPSTREAM

--- script/jenkins/linux-test.sh
+++ script/jenkins/linux-test.sh
@@ -8,9 +8,7 @@ jenkins_setup
 cleanup() {
   rm -rf $TEMP_PYTHON_ENV
   .ci/scripts/docker-get-logs.sh
-  make stop-environment fix-permissions
 }
 trap cleanup EXIT
 
-make update
-make system-tests-environment
+make docker-system-tests
