530a6ab33596825d35e5181ec9b163e598239e50
Andrew Wilkins <axw@elastic.co>
2020-02-25 10:43:08 +0800

    Add system test environment variable defaults to docker-compose.yml (#3369)
    * tests/system: update default auth
    
    * docker-compose: define env vars directly
    
    Rather than defining them in the Makefile, define the
    defaults in the docker-compose.yml file directly. This
    enables the use of "docker-compose" without setting any
    environment variables.

--- tests/system/apmserver.py
+++ tests/system/apmserver.py
@@ -86,7 +86,7 @@ class BaseTest(TestCase):
         host = os.getenv("ES_HOST", "localhost")
 
         if not user:
-            user = os.getenv("ES_USER", "admin")
+            user = os.getenv("ES_USER", "apm_server_user")
         if not password:
             password = os.getenv("ES_PASS", "changeme")
 
@@ -106,7 +106,7 @@ class BaseTest(TestCase):
         host = os.getenv("KIBANA_HOST", "localhost")
 
         if not user:
-            user = os.getenv("KIBANA_USER", "admin")
+            user = os.getenv("KIBANA_USER", "apm_user_ro")
         if not password:
             password = os.getenv("KIBANA_PASS", "changeme")
 
