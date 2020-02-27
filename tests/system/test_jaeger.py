4e1bbcfccd71292497da9bae1c284979542daedc
Andrew Wilkins <axw@elastic.co>
2020-02-27 20:48:12 +0800

    beater/jaeger: introduce "auth_tag" configuration (#3394) (#3402)
    * beater/jaeger: introduce "auth_tag" configuration
    
    We introduce support for configuring the server to
    look for a specified Process tag in Jaeger gRPC batches,
    and use the value for authorization. The value is
    expected to have the same format as HTTP Authorization
    headers that the Elastic APM agents support.
    
    By default Jaeger authorization is disabled, as there
    is no standard auth tag name. This configuration only
    applies to gRPC, and not HTTP, as Jaeger clients do
    not support TLS/HTTPS.

--- tests/system/test_jaeger.py
+++ tests/system/test_jaeger.py
@@ -6,10 +6,9 @@ from apmserver import integration_test, ElasticTest
 from helper import wait_until
 
 
-@integration_test
-class Test(ElasticTest):
+class JaegerBaseTest(ElasticTest):
     def setUp(self):
-        super(Test, self).setUp()
+        super(JaegerBaseTest, self).setUp()
         wait_until(lambda: self.log_contains("Listening for Jaeger HTTP"), name="Jaeger HTTP listener started")
         wait_until(lambda: self.log_contains("Listening for Jaeger gRPC"), name="Jaeger gRPC listener started")
 
@@ -21,16 +20,23 @@ class Test(ElasticTest):
         self.jaeger_grpc_addr = match.group(1)
 
     def config(self):
-        cfg = super(Test, self).config()
+        cfg = super(JaegerBaseTest, self).config()
         cfg.update({
             "jaeger_grpc_enabled": "true",
             "jaeger_http_enabled": "true",
             # Listen on dynamic ports
             "jaeger_grpc_host": "localhost:0",
             "jaeger_http_host": "localhost:0",
+            # jaeger_grpc_auth_tag is set in the base suite so we can
+            # check that the authorization tag is always removed,
+            # even if there's no secret token / API Key auth.
+            "jaeger_grpc_auth_tag": "authorization",
         })
         return cfg
 
+
+@integration_test
+class Test(JaegerBaseTest):
     def test_jaeger_http(self):
         """
         This test sends a Jaeger span in Thrift encoding over HTTP, and verifies that it is indexed.
@@ -61,3 +67,61 @@ class Test(ElasticTest):
         transaction_docs = self.wait_for_events('transaction', 1)
         error_docs = self.wait_for_events('error', 3)
         self.approve_docs('jaeger_batch_0', transaction_docs + error_docs)
+
+    def test_jaeger_auth_tag_removed(self):
+        """
+        This test sends a Jaeger batch over gRPC, with an "authorization" process tag,
+        and verifies that the spans are indexed without that process tag indexed as a label.
+        """
+        jaeger_request_data = self.get_testdata_path('jaeger', 'batch_0_authorization.json')
+
+        client = os.path.join(os.path.dirname(__file__), 'jaegergrpc')
+        subprocess.run(
+            ['go', 'run', client, '-addr', self.jaeger_grpc_addr, '-insecure', jaeger_request_data],
+            check=True,
+        )
+
+        transaction_docs = self.wait_for_events('transaction', 1)
+        error_docs = self.wait_for_events('error', 3)
+        self.approve_docs('jaeger_batch_0_auth_tag_removed', transaction_docs + error_docs)
+
+
+@integration_test
+class TestAuthTag(JaegerBaseTest):
+    def config(self):
+        cfg = super(TestAuthTag, self).config()
+        cfg.update({"secret_token": "1234"})
+        return cfg
+
+    def test_jaeger_unauthorized(self):
+        """
+        This test sends a Jaeger batch over gRPC, without an "authorization" process tag,
+        and verifies that the spans are indexed.
+        """
+        jaeger_request_data = self.get_testdata_path('jaeger', 'batch_0.json')
+
+        client = os.path.join(os.path.dirname(__file__), 'jaegergrpc')
+        proc = subprocess.Popen(
+            ['go', 'run', client, '-addr', self.jaeger_grpc_addr, '-insecure', jaeger_request_data],
+            stderr=subprocess.PIPE,
+        )
+        stdout, stderr = proc.communicate()
+        self.assertNotEqual(proc.returncode, 0)
+        self.assertRegex(stderr.decode("utf-8"), "not authorized")
+
+    def test_jaeger_authorized(self):
+        """
+        This test sends a Jaeger batch over gRPC, with an "authorization" process tag,
+        and verifies that the spans are indexed without that tag indexed as a label.
+        """
+        jaeger_request_data = self.get_testdata_path('jaeger', 'batch_0_authorization.json')
+
+        client = os.path.join(os.path.dirname(__file__), 'jaegergrpc')
+        subprocess.run(
+            ['go', 'run', client, '-addr', self.jaeger_grpc_addr, '-insecure', jaeger_request_data],
+            check=True,
+        )
+
+        transaction_docs = self.wait_for_events('transaction', 1)
+        error_docs = self.wait_for_events('error', 3)
+        self.approve_docs('jaeger_batch_0_authorization', transaction_docs + error_docs)
