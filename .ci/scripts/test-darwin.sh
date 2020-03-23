9173a753c0df0ddb670d76e0cb6b5ea0d3396b3f
Victor Martinez <VictorMartinezRubio@gmail.com>
2020-03-23 17:11:38 +0000

    install pyenv

--- .ci/scripts/test-darwin.sh
+++ .ci/scripts/test-darwin.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 set -euox pipefail
 
+# Setup python3
+curl -L https://github.com/pyenv/pyenv-installer/raw/master/bin/pyenv-installer | bash
+pyenv install 3.7.7
+
 # shellcheck disable=SC1091
 source ./script/common.bash
 
