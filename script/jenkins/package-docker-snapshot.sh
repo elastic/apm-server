b2cae78c4bfa4a952362cd123e437ebdadda9206
Andrew Wilkins <axwalk@gmail.com>
2020-03-23 17:29:28 +0800

    Fix CI script take2

--- script/jenkins/package-docker-snapshot.sh
+++ script/jenkins/package-docker-snapshot.sh
@@ -17,7 +17,7 @@ export SNAPSHOT='true'
 export IMAGE="docker.elastic.co/apm/apm-server"
 
 echo 'INFO: Build docker images'
-./build/linux/mage package
+make release
 
 echo 'INFO: Get the just built docker image'
 TAG=$(docker images ${IMAGE} --format "{{.Tag}}" | head -1)
