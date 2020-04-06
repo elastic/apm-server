6cc7adec6643347672afaccad84b7e68fbbe96a4
Victor Martinez <victormartinezrubio@gmail.com>
2020-04-06 16:40:57 +0100

    packer: cache docker images (#3620)

---
+++ .ci/packer_cache.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+# shellcheck disable=SC1091
+source /usr/local/bin/bash_standard_lib.sh
+
+# shellcheck disable=SC1091
+source ./script/common.bash
+
+get_go_version
+
+DOCKER_IMAGES="
+docker.elastic.co/beats-dev/golang-crossbuild:${GO_VERSION}-arm
+docker.elastic.co/beats-dev/golang-crossbuild:${GO_VERSION}-darwin
+docker.elastic.co/beats-dev/golang-crossbuild:${GO_VERSION}-main
+docker.elastic.co/beats-dev/golang-crossbuild:${GO_VERSION}-main-debian7
+docker.elastic.co/beats-dev/golang-crossbuild:${GO_VERSION}-main-debian8
+docker.elastic.co/beats-dev/golang-crossbuild:${GO_VERSION}-mips
+docker.elastic.co/beats-dev/golang-crossbuild:${GO_VERSION}-ppc
+docker.elastic.co/beats-dev/golang-crossbuild:${GO_VERSION}-s390x
+golang:${GO_VERSION}
+"
+
+for image in ${DOCKER_IMAGES}
+do
+(retry 2 docker pull ${image}) || echo "Error pulling ${image} Docker image, we continue"
+done
