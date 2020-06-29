00935b67452dbe1f115d71b31ec411cec21ad0ac
Andrew Wilkins <axw@elastic.co>
2020-06-29 14:47:54 +0800

    tests/system: add test for flushing aggregations

--- tests/system/test_aggregation.py
+++ tests/system/test_aggregation.py
@@ -45,3 +45,25 @@ class Test(ElasticTest):
             # @timestamp is dynamic, so set it to something known.
             doc['_source']['@timestamp'] = '2020-04-14T08:56:03.100Z'
         self.approve_docs('rum_transaction_histogram_metrics', metric_docs)
+
+
+@integration_test
+class TestShutdown(ElasticTest):
+    def config(self):
+        cfg = super(TestShutdown, self).config()
+        cfg.update({
+            "aggregation_enabled": True,
+            # Set aggregation_interval to something that would cause
+            # a timeout if we were to wait that long. The server
+            # should flush metrics on shutdown without waiting for
+            # the configured interval.
+            "aggregation_interval": "180s",
+        })
+        return cfg
+
+    def test_transaction_metrics_flushed_shutdown(self):
+        self.load_docs_with_template(self.get_payload_path("transactions_spans.ndjson"),
+                                     self.intake_url, 'transaction', 9)
+        self.assert_no_logged_warnings()
+        self.apmserver_proc.kill() # Stop server to ensure metrics are flushed on shutdown
+        self.wait_for_events('metric', 3, index=index_metric)
