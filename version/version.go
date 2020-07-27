0220bdba581c583aa2174e9fb00ccc1571e6f2a5
Gil Raphaelli <gil@elastic.co>
2020-07-27 18:05:24 -0400

    fix package comment

--- version/version.go
+++ version/version.go
@@ -14,6 +14,7 @@
 // KIND, either express or implied.  See the License for the
 // specific language governing permissions and limitations
 // under the License.
+
 package version
 
 // name matches github.com/elastic/beats/v7/dev-tools/mage/settings.go parseBeatVersion
