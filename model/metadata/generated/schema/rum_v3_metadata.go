4c2a6d0b5215b0ce4be5f51a88057f04a47d4675
simitt <silvia.mitter@elastic.co>
2020-09-08 16:44:19 +0200

    Enforce same requirements on v3rum

--- model/metadata/generated/schema/rum_v3_metadata.go
+++ model/metadata/generated/schema/rum_v3_metadata.go
@@ -26,208 +26,143 @@ const RUMV3Schema = `{
     ],
     "properties": {
         "se": {
-                "$id": "docs/spec/rum_v3_service.json",
-    "title": "Service",
-    "type": [
-        "object",
-        "null"
-    ],
-    "properties": {
-        "a": {
-            "description": "Name and version of the Elastic APM agent",
+            "$id": "docs/spec/rum_v3_service.json",
+            "title": "Service",
             "type": [
-                "object",
-                "null"
+                "object"
             ],
             "properties": {
+                "a": {
+                    "description": "Name and version of the Elastic APM agent",
+                    "type": [
+                        "object"
+                    ],
+                    "properties": {
+                        "n": {
+                            "description": "Name of the Elastic APM agent, e.g. \"Python\"",
+                            "type": [
+                                "string"
+                            ],
+                            "maxLength": 1024
+                        },
+                        "ve": {
+                            "description": "Version of the Elastic APM agent, e.g.\"1.0.0\"",
+                            "type": [
+                                "string"
+                            ],
+                            "maxLength": 1024
+                        }
+                    },
+                    "required": [
+                        "n",
+                        "ve"
+                    ]
+                },
+                "fw": {
+                    "description": "Name and version of the web framework used",
+                    "type": [
+                        "object",
+                        "null"
+                    ],
+                    "properties": {
+                        "n": {
+                            "type": [
+                                "string",
+                                "null"
+                            ],
+                            "maxLength": 1024
+                        },
+                        "ve": {
+                            "type": [
+                                "string",
+                                "null"
+                            ],
+                            "maxLength": 1024
+                        }
+                    }
+                },
+                "la": {
+                    "description": "Name and version of the programming language used",
+                    "type": [
+                        "object",
+                        "null"
+                    ],
+                    "properties": {
+                        "n": {
+                            "type": [
+                                "string"
+                            ],
+                            "maxLength": 1024
+                        },
+                        "ve": {
+                            "type": [
+                                "string",
+                                "null"
+                            ],
+                            "maxLength": 1024
+                        }
+                    },
+                    "required": [
+                        "n"
+                    ]
+                },
                 "n": {
-                    "description": "Name of the Elastic APM agent, e.g. \"Python\"",
+                    "description": "Immutable name of the service emitting this event",
+                    "type": [
+                        "string"
+                    ],
+                    "pattern": "^[a-zA-Z0-9 _-]+$",
+                    "maxLength": 1024
+                },
+                "en": {
+                    "description": "Environment name of the service, e.g. \"production\" or \"staging\"",
                     "type": [
                         "string",
                         "null"
                     ],
                     "maxLength": 1024
                 },
-                "ve": {
-                    "description": "Version of the Elastic APM agent, e.g.\"1.0.0\"",
+                "ru": {
+                    "description": "Name and version of the language runtime running this service",
                     "type": [
-                        "string",
+                        "object",
                         "null"
                     ],
-                    "maxLength": 1024
-                }
-            }
-        },
-        "fw": {
-            "description": "Name and version of the web framework used",
-            "type": [
-                "object",
-                "null"
-            ],
-            "properties": {
-                "n": {
-                    "type": [
-                        "string",
-                        "null"
-                    ],
-                    "maxLength": 1024
+                    "properties": {
+                        "n": {
+                            "type": [
+                                "string"
+                            ],
+                            "maxLength": 1024
+                        },
+                        "ve": {
+                            "type": [
+                                "string"
+                            ],
+                            "maxLength": 1024
+                        }
+                    },
+                    "required": [
+                        "n",
+                        "ve"
+                    ]
                 },
                 "ve": {
+                    "description": "Version of the service emitting this event",
                     "type": [
                         "string",
                         "null"
                     ],
                     "maxLength": 1024
                 }
-            }
-        },
-        "la": {
-            "description": "Name and version of the programming language used",
-            "type": [
-                "object",
-                "null"
-            ],
-            "properties": {
-                "n": {
-                    "type": [
-                        "string",
-                        "null"
-                    ],
-                    "maxLength": 1024
-                },
-                "ve": {
-                    "type": [
-                        "string",
-                        "null"
-                    ],
-                    "maxLength": 1024
-                }
-            }
-        },
-        "n": {
-            "description": "Immutable name of the service emitting this event",
-            "type": [
-                "string",
-                "null"
-            ],
-            "pattern": "^[a-zA-Z0-9 _-]+$",
-            "maxLength": 1024
-        },
-        "en": {
-            "description": "Environment name of the service, e.g. \"production\" or \"staging\"",
-            "type": [
-                "string",
-                "null"
-            ],
-            "maxLength": 1024
-        },
-        "ru": {
-            "description": "Name and version of the language runtime running this service",
-            "type": [
-                "object",
-                "null"
-            ],
-            "properties": {
-                "n": {
-                    "type": [
-                        "string",
-                        "null"
-                    ],
-                    "maxLength": 1024
-                },
-                "ve": {
-                    "type": [
-                        "string",
-                        "null"
-                    ],
-                    "maxLength": 1024
-                }
-            }
-        },
-        "ve": {
-            "description": "Version of the service emitting this event",
-            "type": [
-                "string",
-                "null"
-            ],
-            "maxLength": 1024
-        }
-    },
-            "type": "object",
+            },
             "required": [
-                "n",
-                "a"
-            ],
-            "properties.n.type": "string",
-            "properties.a.type": "string",
-            "properties.a.required": [
-                "n",
-                "ve"
-            ],
-            "properties.a.properties.n.type": "string",
-            "properties.a.properties.ve.type": "string",
-            "properties.ru.required": [
-                "n",
-                "ve"
-            ],
-            "properties.ru.properties.n.type": "string",
-            "properties.ru.properties.ve.type": "string",
-            "properties.la.required": [
+                "a",
                 "n"
-            ],
-            "properties.la.properties.n.type": "string"
-        },
-        "u": {
-                "$id": "docs/spec/rum_v3_user.json",
-    "title": "User",
-    "type": [
-        "object",
-        "null"
-    ],
-    "properties": {
-        "id": {
-            "description": "Identifier of the logged in user, e.g. the primary key of the user",
-            "type": [
-                "string",
-                "integer",
-                "null"
-            ],
-            "maxLength": 1024
-        },
-        "em": {
-            "description": "Email of the logged in user",
-            "type": [
-                "string",
-                "null"
-            ],
-            "maxLength": 1024
-        },
-        "un": {
-            "description": "The username of the logged in user",
-            "type": [
-                "string",
-                "null"
-            ],
-            "maxLength": 1024
-        }
-    }
-        },
-        "l": {
-                "$id": "docs/spec/tags.json",
-    "title": "Tags",
-    "type": ["object", "null"],
-    "description": "A flat mapping of user-defined tags with string, boolean or number values.",
-    "patternProperties": {
-        "^[^.*\"]*$": {
-            "type": ["string", "boolean", "number", "null"],
-            "maxLength": 1024
-        }
-    },
-    "additionalProperties": false
+            ]
         }
     },
     "required": [
         "se"
     ]
-}
-`
+}`
