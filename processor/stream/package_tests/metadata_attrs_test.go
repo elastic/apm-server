a0177df5af81a1254a82c8f4ba673901738d627b
Silvia Mitter <silvia.mitter@elastic.co>
2020-09-14 12:52:45 +0200

    Fix metadata.service.* fields requirements in json schema (#4142) (#4194)
    Validations on metadata.service.* fields were not run correctly, changing the schema to properly run validations and
    adding package tests for verification.
    
    fixes #4141

--- processor/stream/package_tests/metadata_attrs_test.go
+++ processor/stream/package_tests/metadata_attrs_test.go
@@ -159,13 +159,36 @@ func TestKeywordLimitationOnMetadataAttrs(t *testing.T) {
 	)
 }
 
+func metadataRequiredKeys() *tests.Set {
+	return tests.NewSet(
+		"metadata",
+		"metadata.cloud.provider",
+		"metadata.service",
+		"metadata.service.name",
+		"metadata.service.agent",
+		"metadata.service.agent.name",
+		"metadata.service.agent.version",
+		"metadata.service.runtime.name",
+		"metadata.service.runtime.version",
+		"metadata.service.language.name",
+		"metadata.system.container.id",
+		"metadata.process.pid",
+	)
+}
+
+func TestAttrsPresenceInMetadata(t *testing.T) {
+	metadataProcSetup().AttrsPresence(t, metadataRequiredKeys(), nil)
+}
+
 func TestInvalidPayloadsForMetadata(t *testing.T) {
 	type val []interface{}
 
 	payloadData := []tests.SchemaTestData{
 		{Key: "metadata.service.name",
-			Valid:   val{"my-service"},
-			Invalid: []tests.Invalid{{Msg: "service/properties/name", Values: val{tests.Str1024Special}}},
+			Valid: val{"m"},
+			Invalid: []tests.Invalid{
+				{Msg: "service/properties/name", Values: val{tests.Str1024Special}},
+				{Msg: "service/properties/name", Values: val{""}}},
 		}}
 	metadataProcSetup().DataValidation(t, payloadData)
 }
