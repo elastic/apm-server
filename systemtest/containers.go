3291f4e7c24f7ccaa6d5472860ac7fd36ebb3a93
Andrew Wilkins <axw@elastic.co>
2020-12-10 10:59:13 +0800

    sampling: add missing license check (#4328) (#4507)
    * Require platinum/trial license for tail-sampling

--- systemtest/containers.go
+++ systemtest/containers.go
@@ -21,14 +21,24 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"net"
+	"net/url"
 	"os"
 	"os/exec"
+	"strings"
 	"time"
 
 	"github.com/docker/docker/api/types"
 	"github.com/docker/docker/api/types/filters"
 	"github.com/docker/docker/client"
+	"github.com/testcontainers/testcontainers-go"
+	"github.com/testcontainers/testcontainers-go/wait"
+
+	"github.com/elastic/apm-server/systemtest/estest"
+	"github.com/elastic/go-elasticsearch/v7"
 )
+
+const startContainersTimeout = 5 * time.Minute
 
 // StartStackContainers starts Docker containers for Elasticsearch and Kibana.
 //
@@ -47,29 +57,71 @@ func StartStackContainers() error {
 
 	// Wait for up to 5 minutes for Kibana to become healthy,
 	// which implies Elasticsearch is healthy too.
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Minute)
+	ctx, cancel := context.WithTimeout(context.Background(), startContainersTimeout)
 	defer cancel()
+	return waitKibanaContainerHealthy(ctx)
+}
 
+// NewUnstartedElasticsearchContainer returns a new ElasticsearchContainer.
+func NewUnstartedElasticsearchContainer() (*ElasticsearchContainer, error) {
+	// Create a testcontainer.ContainerRequest based on the "elasticsearch service
+	// defined in docker-compose.
+
+	docker, err := client.NewClientWithOpts(client.FromEnv)
+	if err != nil {
+		return nil, err
+	}
+	defer docker.Close()
+
+	container, err := stackContainerInfo(context.Background(), docker, "elasticsearch")
+	if err != nil {
+		return nil, err
+	}
+	containerDetails, err := docker.ContainerInspect(context.Background(), container.ID)
+	if err != nil {
+		return nil, err
+	}
+
+	req := testcontainers.ContainerRequest{
+		Image:      container.Image,
+		AutoRemove: true,
+	}
+	waitFor := wait.ForHTTP("/")
+	waitFor.Port = "9200/tcp"
+	req.WaitingFor = waitFor
+
+	for port := range containerDetails.Config.ExposedPorts {
+		req.ExposedPorts = append(req.ExposedPorts, string(port))
+	}
+
+	env := make(map[string]string)
+	for _, kv := range containerDetails.Config.Env {
+		sep := strings.IndexRune(kv, '=')
+		k, v := kv[:sep], kv[sep+1:]
+		env[k] = v
+	}
+	for network := range containerDetails.NetworkSettings.Networks {
+		req.Networks = append(req.Networks, network)
+	}
+
+	// BUG(axw) ElasticsearchContainer currently does not support security.
+	env["xpack.security.enabled"] = "false"
+
+	return &ElasticsearchContainer{request: req, Env: env}, nil
+}
+
+func waitKibanaContainerHealthy(ctx context.Context) error {
 	docker, err := client.NewClientWithOpts(client.FromEnv)
 	if err != nil {
 		return err
 	}
 	defer docker.Close()
 
-	containers, err := docker.ContainerList(ctx, types.ContainerListOptions{
-		Filters: filters.NewArgs(
-			filters.Arg("label", "com.docker.compose.project=apm-server"),
-			filters.Arg("label", "com.docker.compose.service=kibana"),
-		),
-	})
+	container, err := stackContainerInfo(ctx, docker, "kibana")
 	if err != nil {
 		return err
 	}
-	if n := len(containers); n != 1 {
-		return fmt.Errorf("expected 1 kibana container, got %d", n)
-	}
 
-	container := containers[0]
 	first := true
 	for {
 		containerJSON, err := docker.ContainerInspect(ctx, container.ID)
@@ -86,4 +138,99 @@ func StartStackContainers() error {
 		time.Sleep(5 * time.Second)
 	}
 	return nil
+}
+
+func stackContainerInfo(ctx context.Context, docker *client.Client, name string) (*types.Container, error) {
+	containers, err := docker.ContainerList(ctx, types.ContainerListOptions{
+		Filters: filters.NewArgs(
+			filters.Arg("label", "com.docker.compose.project=apm-server"),
+			filters.Arg("label", "com.docker.compose.service="+name),
+		),
+	})
+	if err != nil {
+		return nil, err
+	}
+	if n := len(containers); n != 1 {
+		return nil, fmt.Errorf("expected 1 %s container, got %d", name, n)
+	}
+	return &containers[0], nil
+}
+
+// ElasticsearchContainer represents an ephemeral Elasticsearch container.
+//
+// This can be used when the docker-compose "elasticsearch" service is insufficient.
+type ElasticsearchContainer struct {
+	request   testcontainers.ContainerRequest
+	container testcontainers.Container
+
+	// Env holds the environment variables to pass to the container,
+	// and will be initialised with the values in the docker-compose
+	// "elasticsearch" service definition.
+	//
+	// BUG(axw) ElasticsearchContainer currently does not support security,
+	// and will set "xpack.security.enabled=false" by default.
+	Env map[string]string
+
+	// Addr holds the "host:port" address for Elasticsearch's REST API.
+	// This will be populated by Start.
+	Addr string
+
+	// Client holds a client for interacting with Elasticsearch's REST API.
+	// This will be populated by Start.
+	Client *estest.Client
+}
+
+// Start starts the container.
+//
+// The Addr and Client fields will be updated on successful return.
+//
+// The container will be removed when Close() is called, or otherwise by a
+// reaper process if the test process is aborted.
+func (c *ElasticsearchContainer) Start() error {
+	ctx, cancel := context.WithTimeout(context.Background(), startContainersTimeout)
+	defer cancel()
+
+	// Update request from user-definable fields.
+	c.request.Env = c.Env
+
+	container, err := testcontainers.GenericContainer(ctx, testcontainers.GenericContainerRequest{
+		ContainerRequest: c.request,
+		Started:          true,
+	})
+	if err != nil {
+		return err
+	}
+	defer func() {
+		if c.container == nil {
+			// Something has gone wrong.
+			container.Terminate(ctx)
+		}
+	}()
+
+	ip, err := container.Host(ctx)
+	if err != nil {
+		return err
+	}
+	port, err := container.MappedPort(ctx, "9200")
+	if err != nil {
+		return err
+	}
+	c.Addr = net.JoinHostPort(ip, port.Port())
+
+	esURL := url.URL{Scheme: "http", Host: c.Addr}
+	config := newElasticsearchConfig()
+	config.Addresses[0] = esURL.String()
+	client, err := elasticsearch.NewClient(config)
+	if err != nil {
+		return err
+	}
+	c.Client = &estest.Client{Client: client}
+
+	c.container = container
+	return nil
+}
+
+// Close terminates and removes the container.
+func (c *ElasticsearchContainer) Close() error {
+	return c.container.Terminate(context.Background())
 }
