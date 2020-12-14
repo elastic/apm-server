07ce8d9766cc7ef86bfbd6e30b34977446cbb8c4
Silvia Mitter <silvia.mitter@elastic.co>
2020-12-14 07:22:46 +0100

    Format code, change tests/system to avoid breakage (#4459) (#4518)
    autopep8 will move imports to the top of the file
    if they come after statements. Move the sys.path
    magic to another file so we can preserve import
    ordering.
    
    Co-authored-by: Andrew Wilkins <axw@elastic.co>

--- tests/system/test_tls.py
+++ tests/system/test_tls.py
@@ -6,13 +6,12 @@ import subprocess
 import socket
 import pytest
 from requests.packages.urllib3.exceptions import SubjectAltNameWarning
-requests.packages.urllib3.disable_warnings(SubjectAltNameWarning)
-
 from apmserver import ServerBaseTest
 from apmserver import TimeoutError, integration_test
 
 INTEGRATION_TESTS = os.environ.get('INTEGRATION_TESTS', False)
 
+requests.packages.urllib3.disable_warnings(SubjectAltNameWarning)
 
 @integration_test
 class TestSecureServerBaseTest(ServerBaseTest):
