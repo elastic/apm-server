c9b29e0be6ecbe80d0a1bc8b022d5c18cb42eac7
Andrew Wilkins <axw@elastic.co>
2020-12-14 18:33:23 +0800

    model: sanitize/de-dot keys for labels, custom, and marks (#4465) (#4527)
    * model: sanitize label keys
    
    Replace reserved label key characters with '_', alleviating clients
    from having to do this.
    
    Also, optimise how we merge global and event-specific labels. Instead
    of adding all global labels and then using utility.DeepUpdate, de-dot
    the label keys first and range over the two maps to create a second map.
    Because the labels keys cannot have dots, we do not need to deep merge.
    
    * model: sanitize "custom" attribute keys
    
    Sanitize the keys of "custom" context using the same
    logic applied to labels. Also, drop the model.Custom
    type and just use common.MapStr. This eliminates some
    unnecessary pointer indirection.
    
    * model: sanitize "marks" keys
    
    * model/modeldecoder: drop label key regexp rules
    
    * Add changelog entry

--- model/modeldecoder/rumv3/transaction_test.go
+++ model/modeldecoder/rumv3/transaction_test.go
@@ -154,8 +154,8 @@ func TestDecodeMapToTransactionModel(t *testing.T) {
 		// metadata labels and transaction labels should not be merged
 		mLabels := common.MapStr{"init0": "init", "init1": "init", "init2": "init"}
 		assert.Equal(t, mLabels, out.Metadata.Labels)
-		tLabels := model.Labels{"overwritten0": "overwritten", "overwritten1": "overwritten"}
-		assert.Equal(t, &tLabels, out.Labels)
+		tLabels := common.MapStr{"overwritten0": "overwritten", "overwritten1": "overwritten"}
+		assert.Equal(t, tLabels, out.Labels)
 		// service values should be set
 		modeldecodertest.AssertStructValues(t, &out.Metadata.Service, metadataExceptions("Node", "Agent.EphemeralID"), otherVal)
 		// user values should be set
