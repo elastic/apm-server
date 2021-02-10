4da9e41871fc2dfdb98b4d9d2157832766915f24
Andrew Wilkins <axw@elastic.co>
2021-02-10 18:16:18 +0800

    Add support for consuming OTLP/gRPC metrics
    Add support for consuming basic OTLP/gRPC metrics,
    like we support in the OpenTelemetry Collector exporter.
    This does not cover distributions or summaries; these
    will be added once the Elasticsearch enhancements we
    are dependent on are available.

--- processor/otel/consumer.go
+++ processor/otel/consumer.go
@@ -41,6 +41,7 @@ import (
 	"net/url"
 	"strconv"
 	"strings"
+	"sync/atomic"
 
 	"go.opentelemetry.io/collector/consumer/pdata"
 	"go.opentelemetry.io/collector/translator/conventions"
@@ -65,7 +66,27 @@ const (
 
 // Consumer transforms open-telemetry data to be compatible with elastic APM data
 type Consumer struct {
+	stats consumerStats
+
 	Reporter publish.Reporter
+}
+
+// ConsumerStats holds statistics about data consumption.
+type ConsumerStats struct {
+	// UnsupportedMetricsDropped records the number of unsupported metrics
+	// that have been dropped by the consumer.
+	UnsupportedMetricsDropped int64
+}
+
+type consumerStats struct {
+	unsupportedMetricsDropped int64
+}
+
+// Stats returns statistics about data consumption.
+func (c *Consumer) Stats() ConsumerStats {
+	return ConsumerStats{
+		UnsupportedMetricsDropped: atomic.LoadInt64(&c.stats.unsupportedMetricsDropped),
+	}
 }
 
 // ConsumeTraces consumes OpenTelemetry trace data,
