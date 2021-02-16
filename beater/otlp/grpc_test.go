2b2c05b349bca98974c00154ce786dffaf3d949a
Andrew Wilkins <axw@elastic.co>
2021-02-16 10:56:29 +0800

    Add support for consuming OTLP/gRPC metrics (#4722)
    * Add support for consuming OTLP/gRPC metrics
    
    Add support for consuming basic OTLP/gRPC metrics,
    like we support in the OpenTelemetry Collector exporter.
    This does not cover distributions or summaries; these
    will be added once the Elasticsearch enhancements we
    are dependent on are available.
    
    * processor/otel: add comment about [Cc]onsumerStats
    # Conflicts:
    #	changelogs/head.asciidoc

--- beater/otlp/grpc_test.go
+++ beater/otlp/grpc_test.go
@@ -40,8 +40,10 @@ import (
 )
 
 var (
-	exportTraceServiceRequestType  = proto.MessageType("opentelemetry.proto.collector.trace.v1.ExportTraceServiceRequest")
-	exportTraceServiceResponseType = proto.MessageType("opentelemetry.proto.collector.trace.v1.ExportTraceServiceResponse")
+	exportMetricsServiceRequestType  = proto.MessageType("opentelemetry.proto.collector.metrics.v1.ExportMetricsServiceRequest")
+	exportMetricsServiceResponseType = proto.MessageType("opentelemetry.proto.collector.metrics.v1.ExportMetricsServiceResponse")
+	exportTraceServiceRequestType    = proto.MessageType("opentelemetry.proto.collector.trace.v1.ExportTraceServiceRequest")
+	exportTraceServiceResponseType   = proto.MessageType("opentelemetry.proto.collector.trace.v1.ExportTraceServiceResponse")
 )
 
 func TestConsumeTraces(t *testing.T) {
@@ -93,10 +95,69 @@ func TestConsumeTraces(t *testing.T) {
 	assert.Len(t, events, 2)
 
 	actual := map[string]interface{}{}
-	monitoring.GetRegistry("apm-server.otlp.grpc.consumer").Do(monitoring.Full, func(key string, value interface{}) {
+	monitoring.GetRegistry("apm-server.otlp.grpc.traces").Do(monitoring.Full, func(key string, value interface{}) {
 		actual[key] = value
 	})
 	assert.Equal(t, map[string]interface{}{
+		"request.count":         int64(2),
+		"response.count":        int64(2),
+		"response.errors.count": int64(1),
+		"response.valid.count":  int64(1),
+	}, actual)
+}
+
+func TestConsumeMetrics(t *testing.T) {
+	var reportError error
+	report := func(ctx context.Context, req publish.PendingReq) error {
+		return reportError
+	}
+
+	// Send a minimal metric to verify that everything is connected properly.
+	//
+	// We intentionally do not check the published event contents; those are
+	// tested in processor/otel.
+	cannedRequest := jsonExportMetricsServiceRequest(`{
+"resource_metrics": [
+  {
+    "instrumentation_library_metrics": [
+      {
+        "metrics": [
+	  {
+	    "name": "metric_name"
+	  }
+	]
+      }
+    ]
+  }
+]
+}`)
+
+	conn := newServer(t, report)
+	err := conn.Invoke(
+		context.Background(), "/opentelemetry.proto.collector.metrics.v1.MetricsService/Export",
+		cannedRequest, newExportMetricsServiceResponse(),
+	)
+	assert.NoError(t, err)
+
+	reportError = errors.New("failed to publish events")
+	err = conn.Invoke(
+		context.Background(), "/opentelemetry.proto.collector.metrics.v1.MetricsService/Export",
+		cannedRequest, newExportMetricsServiceResponse(),
+	)
+	assert.Error(t, err)
+	errStatus := status.Convert(err)
+	assert.Equal(t, "failed to publish events", errStatus.Message())
+
+	actual := map[string]interface{}{}
+	monitoring.GetRegistry("apm-server.otlp.grpc.metrics").Do(monitoring.Full, func(key string, value interface{}) {
+		actual[key] = value
+	})
+	assert.Equal(t, map[string]interface{}{
+		// In both of the requests we send above,
+		// the metrics do not have a type and so
+		// we treat them as unsupported metrics.
+		"consumer.unsupported_dropped": int64(2),
+
 		"request.count":         int64(2),
 		"response.count":        int64(2),
 		"response.errors.count": int64(1),
@@ -116,6 +177,20 @@ func jsonExportTraceServiceRequest(j string) interface{} {
 
 func newExportTraceServiceResponse() interface{} {
 	return reflect.New(exportTraceServiceResponseType.Elem()).Interface()
+}
+
+func jsonExportMetricsServiceRequest(j string) interface{} {
+	request := reflect.New(exportMetricsServiceRequestType.Elem()).Interface()
+	decoder := json.NewDecoder(strings.NewReader(j))
+	decoder.DisallowUnknownFields()
+	if err := decoder.Decode(request); err != nil {
+		panic(err)
+	}
+	return request
+}
+
+func newExportMetricsServiceResponse() interface{} {
+	return reflect.New(exportMetricsServiceResponseType.Elem()).Interface()
 }
 
 func newServer(t *testing.T, report publish.Reporter) *grpc.ClientConn {
