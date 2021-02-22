f45bc6adb44382a7ecd7bbb05b68db7e0bdb58b0
Victor Martinez <VictorMartinezRubio@gmail.com>
2021-02-22 10:44:41 +0000

    Enable automation for the go agent

---
+++ .ci/.jenkins-schema.groovy
@@ -0,0 +1,11 @@
+---
+agents:
+  - REPO: "apm-agent-dotnet"
+  - REPO: "apm-agent-go"
+    SPEC_FILEPATH: "internal/apmschema/jsonschema"
+  - REPO: "apm-agent-java"
+  - REPO: "apm-agent-nodejs"
+  - REPO: "apm-agent-php"
+  - REPO: "apm-agent-python"
+  - REPO: "apm-agent-ruby"
+  - REPO: "apm-agent-rum-js"
