2bacefbb0c7963fa53671ad98b03a2d6677c1f1e
stuart nelson <stuartnelson3@gmail.com>
2021-03-22 17:32:19 +0100

    add exponential backoff to es clients

---
+++ elasticsearch/backoff.go
@@ -0,0 +1,55 @@
+// Licensed to Elasticsearch B.V. under one or more contributor
+// license agreements. See the NOTICE file distributed with
+// this work for additional information regarding copyright
+// ownership. Elasticsearch B.V. licenses this file to you under
+// the Apache License, Version 2.0 (the "License"); you may
+// not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+package elasticsearch
+
+import (
+	"time"
+
+	"github.com/cenkalti/backoff"
+
+	"github.com/elastic/beats/v7/libbeat/outputs/elasticsearch"
+)
+
+type backoffFunc func(int) time.Duration
+
+var (
+	// DefaultBackoffConfig is the default backoff configuration used for
+	// es clients.
+	DefaultBackoffConfig = elasticsearch.Backoff{
+		Init: time.Second,
+		Max:  time.Minute,
+	}
+
+	// DefaultBackoff is an exponential backoff configured with default
+	// backoff settings.
+	DefaultBackoff = exponentialBackoff(DefaultBackoffConfig)
+)
+
+func exponentialBackoff(b elasticsearch.Backoff) backoffFunc {
+	retryBackoff := backoff.NewExponentialBackOff()
+	retryBackoff.InitialInterval = b.Init
+	retryBackoff.MaxInterval = b.Max
+	return func(attempts int) time.Duration {
+		// Attempts starts at 1, after there's already been a failure.
+		// https://github.com/elastic/go-elasticsearch/blob/de2391/estransport/estransport.go#L339
+		if attempts == 1 {
+			retryBackoff.Reset()
+		}
+		return retryBackoff.NextBackOff()
+	}
+}
--- elasticsearch/client.go
+++ elasticsearch/client.go
@@ -131,43 +131,66 @@ func NewClient(config *Config) (Client, error) {
 	if err != nil {
 		return nil, err
 	}
-	return NewVersionedClient(config.APIKey, config.Username, config.Password, addresses, headers, transport)
+	backoff := exponentialBackoff(config.Backoff)
+	return NewVersionedClient(config.APIKey, config.Username, config.Password, addresses, headers, transport, config.MaxRetries, backoff)
 }
 
 // NewVersionedClient returns the right elasticsearch client for the current Stack version, as an interface
-func NewVersionedClient(apikey, user, pwd string, addresses []string, headers http.Header, transport http.RoundTripper) (Client, error) {
+func NewVersionedClient(apikey, user, pwd string, addresses []string, headers http.Header, transport http.RoundTripper, maxRetries int, backoff backoffFunc) (Client, error) {
 	if apikey != "" {
 		apikey = base64.StdEncoding.EncodeToString([]byte(apikey))
 	}
 	transport = apmelasticsearch.WrapRoundTripper(transport)
 	version := common.MustNewVersion(version.GetDefaultVersion())
 	if version.IsMajor(8) {
-		c, err := newV8Client(apikey, user, pwd, addresses, headers, transport)
+		c, err := newV8Client(apikey, user, pwd, addresses, headers, transport, maxRetries, backoff)
 		return clientV8{c}, err
 	}
-	c, err := newV7Client(apikey, user, pwd, addresses, headers, transport)
+	c, err := newV7Client(apikey, user, pwd, addresses, headers, transport, maxRetries, backoff)
 	return clientV7{c}, err
 }
 
-func newV7Client(apikey, user, pwd string, addresses []string, headers http.Header, transport http.RoundTripper) (*esv7.Client, error) {
+func newV7Client(
+	apikey, user, pwd string,
+	addresses []string,
+	headers http.Header,
+	transport http.RoundTripper,
+	maxRetries int,
+	fn backoffFunc,
+) (*esv7.Client, error) {
 	return esv7.NewClient(esv7.Config{
-		APIKey:    apikey,
-		Username:  user,
-		Password:  pwd,
-		Addresses: addresses,
-		Transport: transport,
-		Header:    headers,
+		APIKey:               apikey,
+		Username:             user,
+		Password:             pwd,
+		Addresses:            addresses,
+		Transport:            transport,
+		Header:               headers,
+		RetryOnStatus:        []int{502, 503, 504, 429},
+		EnableRetryOnTimeout: true,
+		RetryBackoff:         fn,
+		MaxRetries:           maxRetries,
 	})
 }
 
-func newV8Client(apikey, user, pwd string, addresses []string, headers http.Header, transport http.RoundTripper) (*esv8.Client, error) {
+func newV8Client(
+	apikey, user, pwd string,
+	addresses []string,
+	headers http.Header,
+	transport http.RoundTripper,
+	maxRetries int,
+	fn backoffFunc,
+) (*esv8.Client, error) {
 	return esv8.NewClient(esv8.Config{
-		APIKey:    apikey,
-		Username:  user,
-		Password:  pwd,
-		Addresses: addresses,
-		Transport: transport,
-		Header:    headers,
+		APIKey:               apikey,
+		Username:             user,
+		Password:             pwd,
+		Addresses:            addresses,
+		Transport:            transport,
+		Header:               headers,
+		RetryOnStatus:        []int{502, 503, 504, 429},
+		EnableRetryOnTimeout: true,
+		RetryBackoff:         fn,
+		MaxRetries:           maxRetries,
 	})
 }
 
