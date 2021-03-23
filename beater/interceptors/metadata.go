3f4379a70448434714288633f9f3afd96fc3b038
Andrew Wilkins <axw@elastic.co>
2021-03-23 11:58:52 +0800

    Set client.ip for iOS agent (#4975)
    Introduce a new "ClientMetadata" gRPC interceptor,
    which records information about the gRPC client.
    The logic was extracted from the logging interceptor,
    which now relies on the client metadata interceptor
    being installed before it.
    
    Also, move the gRPC auth interceptor after logging
    and metrics so auth failures are logged and included
    in metrics.

---
+++ beater/interceptors/metadata.go
@@ -0,0 +1,83 @@
+// Licensed to Elasticsearch B.V. under one or more contributor
+// license agreements. See the NOTICE file distributed with
+// this work for additional information regarding copyright
+// ownership. Elasticsearch B.V. licenses this file to you under
+// the Apache License, Version 2.0 (the "License"); you may
+// not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+package interceptors
+
+import (
+	"context"
+	"net"
+	"net/http"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/metadata"
+	"google.golang.org/grpc/peer"
+
+	"github.com/elastic/apm-server/utility"
+)
+
+// ClientMetadata returns an interceptor that intercepts unary gRPC requests,
+// extracts metadata relating to the gRPC client, and adds it to the context.
+//
+// Metadata can be extracted from context using ClientMetadataFromContext.
+func ClientMetadata() grpc.UnaryServerInterceptor {
+	return func(
+		ctx context.Context,
+		req interface{},
+		info *grpc.UnaryServerInfo,
+		handler grpc.UnaryHandler,
+	) (interface{}, error) {
+		var values ClientMetadataValues
+		if p, ok := peer.FromContext(ctx); ok {
+			if addr, ok := p.Addr.(*net.TCPAddr); ok {
+				values.SourceIP = addr.IP
+			}
+		}
+		if md, ok := metadata.FromIncomingContext(ctx); ok {
+			if ua := md["user-agent"]; len(ua) > 0 {
+				values.UserAgent = ua[0]
+			}
+			// Account for `forwarded`, `x-real-ip`, `x-forwarded-for` headers
+			if ip := utility.ExtractIPFromHeader(http.Header(md)); ip != nil {
+				values.SourceIP = ip
+			}
+		}
+		ctx = context.WithValue(ctx, clientMetadataKey{}, values)
+		return handler(ctx, req)
+	}
+}
+
+type clientMetadataKey struct{}
+
+// ContextWithClientMetadata returns a copy of ctx with values.
+func ContextWithClientMetadata(ctx context.Context, values ClientMetadataValues) context.Context {
+	return context.WithValue(ctx, clientMetadataKey{}, values)
+}
+
+// ClientMetadataFromContext returns client metadata extracted by the ClientMetadata interceptor.
+func ClientMetadataFromContext(ctx context.Context) (ClientMetadataValues, bool) {
+	values, ok := ctx.Value(clientMetadataKey{}).(ClientMetadataValues)
+	return values, ok
+}
+
+// ClientMetadataValues holds metadata relating to the gRPC client that initiated the request being handled.
+type ClientMetadataValues struct {
+	// SourceIP net.IP holds the IP address of the gRPC client, if known.
+	SourceIP net.IP
+
+	// UserAgent holds the User-Agent for the gRPC client, if known.
+	UserAgent string
+}
---
+++ beater/interceptors/metadata_test.go
@@ -0,0 +1,78 @@
+// Licensed to Elasticsearch B.V. under one or more contributor
+// license agreements. See the NOTICE file distributed with
+// this work for additional information regarding copyright
+// ownership. Elasticsearch B.V. licenses this file to you under
+// the Apache License, Version 2.0 (the "License"); you may
+// not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+package interceptors
+
+import (
+	"context"
+	"net"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"google.golang.org/grpc/metadata"
+	"google.golang.org/grpc/peer"
+)
+
+func TestClientMetadata(t *testing.T) {
+	tcpAddr := &net.TCPAddr{
+		IP:   net.ParseIP("1.2.3.4"),
+		Port: 56837,
+	}
+	udpAddr := &net.UDPAddr{
+		IP:   net.ParseIP("1.1.1.1"),
+		Port: 1111,
+	}
+
+	interceptor := ClientMetadata()
+
+	for _, test := range []struct {
+		peer     *peer.Peer
+		metadata metadata.MD
+		expected ClientMetadataValues
+	}{{
+		expected: ClientMetadataValues{},
+	}, {
+		peer:     &peer.Peer{Addr: udpAddr},
+		expected: ClientMetadataValues{},
+	}, {
+		peer:     &peer.Peer{Addr: tcpAddr},
+		expected: ClientMetadataValues{SourceIP: tcpAddr.IP},
+	}, {
+		peer:     &peer.Peer{Addr: tcpAddr},
+		metadata: metadata.Pairs("X-Real-Ip", "5.6.7.8"),
+		expected: ClientMetadataValues{SourceIP: net.ParseIP("5.6.7.8")},
+	}, {
+		metadata: metadata.Pairs("User-Agent", "User-Agent"),
+		expected: ClientMetadataValues{UserAgent: "User-Agent"},
+	}} {
+		ctx := context.Background()
+		if test.peer != nil {
+			ctx = peer.NewContext(ctx, test.peer)
+		}
+		if test.metadata != nil {
+			ctx = metadata.NewIncomingContext(ctx, test.metadata)
+		}
+		var got ClientMetadataValues
+		var ok bool
+		interceptor(ctx, nil, nil, func(ctx context.Context, req interface{}) (interface{}, error) {
+			got, ok = ClientMetadataFromContext(ctx)
+			return nil, nil
+		})
+		assert.True(t, ok)
+		assert.Equal(t, test.expected, got)
+	}
+}
