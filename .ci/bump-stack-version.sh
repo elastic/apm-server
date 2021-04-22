e26bf1944a39abbf86804554e09e41c4d25f209c
Victor Martinez <VictorMartinezRubio@gmail.com>
2021-04-22 10:10:28 +0100

    Allow to configure whether to create a new branch or use the existing branch

--- .ci/bump-stack-version.sh
+++ .ci/bump-stack-version.sh
@@ -5,9 +5,16 @@
 # This script is executed by the automation we are putting in place
 # and it requires the git add/commit commands.
 #
+# Parameters:
+#	$1 -> the version to be bumped. Mandatory.
+#	$2 -> whether to create a branch where to commit the changes to.
+#		  this is required when reusing an existing Pull Request.
+#		  Optional. Default true.
+#
 set -euo pipefail
 MSG="parameter missing."
 VERSION=${1:?$MSG}
+CREATE_BRANCH=${2:-true}
 
 OS=$(uname -s| tr '[:upper:]' '[:lower:]')
 
@@ -22,7 +29,11 @@ ${SED} -E -e "s#(image: docker\.elastic\.co/.*):[0-9]+\.[0-9]+\.[0-9]+(-[a-f0-9]
 
 echo "Commit changes"
 git config user.email
-git checkout -b "update-stack-version-$(date "+%Y%m%d%H%M%S")"
+if [ "$CREATE_BRANCH" = "true" ]; then
+	git checkout -b "update-stack-version-$(date "+%Y%m%d%H%M%S")"
+else
+	echo "Branch creation disabled."
+fi
 git add docker-compose.yml
 git commit -m "bump stack version ${VERSION}"
 git --no-pager log -1
