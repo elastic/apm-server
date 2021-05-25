0121dc4f2a8e9f2b8a2bcbf6f5063907a4e30690
Manuel de la Peña <mdelapenya@gmail.com>
2021-05-25 12:58:45 +0200

    fix: force python version

--- script/jenkins/windows-test.ps1
+++ script/jenkins/windows-test.ps1
@@ -28,7 +28,7 @@ $env:PATH = "$env:GOPATH\bin;C:\tools\mingw64\bin;$env:PATH"
 $env:MAGEFILE_CACHE = "$env:WORKSPACE\.magefile"
 
 # Setup Python.
-exec { choco install python2 -y -r --no-progress --version 2.7.17 }
+exec { choco install python2 -y -r --force --no-progress --version 2.7.17 }
 refreshenv
 $env:PATH = "C:\Python27;C:\Python27\Scripts;$env:PATH"
 $env:PYTHON_ENV = "$env:TEMP\python-env"
