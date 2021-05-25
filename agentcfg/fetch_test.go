532cb6d12b2d724dddb5eb2810cd73736c259e4c
Andrew Wilkins <axw@elastic.co>
2021-05-25 12:05:22 +0800

    Move tests.MockKibanaClient to its own package (#5273)
    (cherry picked from commit cb20a97839ec89530838301bf7d73d9210a4719c)
    
    # Conflicts:
    #	agentcfg/fetch_test.go
    #	beater/api/config/agent/handler_test.go
    #	beater/jaeger/grpc_test.go

--- agentcfg/fetch_test.go
+++ agentcfg/fetch_test.go
@@ -30,7 +30,7 @@ import (
 	"github.com/elastic/beats/v7/libbeat/common"
 
 	"github.com/elastic/apm-server/kibana"
-	"github.com/elastic/apm-server/tests"
+	"github.com/elastic/apm-server/kibana/kibanatest"
 )
 
 type m map[string]interface{}
@@ -43,21 +43,31 @@ var (
 func TestFetcher_Fetch(t *testing.T) {
 
 	t.Run("ExpectationFailed", func(t *testing.T) {
+<<<<<<< HEAD
 		kb := tests.MockKibana(http.StatusExpectationFailed, m{"error": "an error"}, mockVersion, true)
 		_, err := NewFetcher(kb, testExpiration).Fetch(context.Background(), query(t.Name()))
+=======
+		kb := kibanatest.MockKibana(http.StatusExpectationFailed, m{"error": "an error"}, mockVersion, true)
+		_, err := NewKibanaFetcher(kb, testExpiration).Fetch(context.Background(), query(t.Name()))
+>>>>>>> cb20a978 (Move tests.MockKibanaClient to its own package (#5273))
 		require.Error(t, err)
 		assert.Equal(t, "{\"error\":\"an error\"}", err.Error())
 	})
 
 	t.Run("NotFound", func(t *testing.T) {
+<<<<<<< HEAD
 		kb := tests.MockKibana(http.StatusNotFound, m{}, mockVersion, true)
 		result, err := NewFetcher(kb, testExpiration).Fetch(context.Background(), query(t.Name()))
+=======
+		kb := kibanatest.MockKibana(http.StatusNotFound, m{}, mockVersion, true)
+		result, err := NewKibanaFetcher(kb, testExpiration).Fetch(context.Background(), query(t.Name()))
+>>>>>>> cb20a978 (Move tests.MockKibanaClient to its own package (#5273))
 		require.NoError(t, err)
 		assert.Equal(t, zeroResult(), result)
 	})
 
 	t.Run("Success", func(t *testing.T) {
-		kb := tests.MockKibana(http.StatusOK, mockDoc(0.5), mockVersion, true)
+		kb := kibanatest.MockKibana(http.StatusOK, mockDoc(0.5), mockVersion, true)
 		b, err := json.Marshal(mockDoc(0.5))
 		expectedResult, err := newResult(b, err)
 		require.NoError(t, err)
@@ -71,7 +81,7 @@ func TestFetcher_Fetch(t *testing.T) {
 		fetch := func(f *Fetcher, kibanaSamplingRate, expectedSamplingRate float64) {
 
 			client := func(samplingRate float64) kibana.Client {
-				return tests.MockKibana(http.StatusOK, mockDoc(samplingRate), mockVersion, true)
+				return kibanatest.MockKibana(http.StatusOK, mockDoc(samplingRate), mockVersion, true)
 			}
 			f.client = client(kibanaSamplingRate)
 
