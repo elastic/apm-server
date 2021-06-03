323456c69b873053e908670f264a1f85f37c0d80
Juan <juan.alvarez@elastic.co>
2021-06-03 12:27:31 +0200

    fix test

--- model/span_test.go
+++ model/span_test.go
@@ -170,6 +170,11 @@ func TestSpanTransform(t *testing.T) {
 				"parent":      common.MapStr{"id": parentID},
 				"destination": common.MapStr{"address": address, "ip": address, "port": port},
 				"event":       common.MapStr{"outcome": "unknown"},
+				"http": common.MapStr{
+					"url":      common.MapStr{"original": url},
+					"response": common.MapStr{"status_code": statusCode},
+					"method":   "get",
+				},
 			},
 		},
 	}
