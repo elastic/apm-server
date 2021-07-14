e45cf3f5c741016ffe9caf8b7538dfbe09334855
Victor Martinez <victormartinezrubio@gmail.com>
2021-07-14 10:31:11 +0100

    [CI] automate update-beats (#5692)
    (cherry picked from commit bd10c37b9d96374411305bb61c5544fe6854bee5)
    
    # Conflicts:
    #	.ci/scripts/update-beats.sh

--- .ci/scripts/update-beats.sh
+++ .ci/scripts/update-beats.sh
@@ -9,11 +9,15 @@ make update-beats
 COMMIT_MESSAGE="Update to elastic/beats@$(go list -m -f {{.Version}} github.com/elastic/beats/... | cut -d- -f3)"
 
 git checkout -b "update-beats-$(date "+%Y%m%d%H%M%S")"
+<<<<<<< HEAD
 git add go.mod go.sum NOTICE.txt \
 	.go-version docs/version.asciidoc \
 	docs/fields.asciidoc include/fields.go x-pack/apm-server/include/fields.go
 find . -maxdepth 2 -name Dockerfile -exec git add {} \;
 
+=======
+git add go.mod go.sum NOTICE.txt
+>>>>>>> bd10c37b ([CI] automate update-beats (#5692))
 git diff --staged --quiet || git commit -m "$COMMIT_MESSAGE"
 git --no-pager log -1
 
