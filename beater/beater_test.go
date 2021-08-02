632a218a520a92dd414b3d4b808503a31f3eca9d
Andrew Wilkins <axw@elastic.co>
2021-08-02 14:20:42 +0800

    Remove sourcemap/test and elasticsearch/estest
    elasticsearch/estest was only used inside the sourcemap
    package's tests. Remove this package and refactor tests
    to use an httptest.Server where feasible.
    
    Change the beater test that was using sourcemap/test
    for test data to use the sourcemap in testdata/sourcemap
    instead, and remove sourcemap/test.

--- beater/beater_test.go
+++ beater/beater_test.go
@@ -22,6 +22,7 @@ import (
 	"context"
 	"errors"
 	"fmt"
+	"io/ioutil"
 	"net"
 	"net/http"
 	"net/http/httptest"
@@ -40,7 +41,6 @@ import (
 	"github.com/elastic/apm-server/beater/config"
 	"github.com/elastic/apm-server/elasticsearch"
 	"github.com/elastic/apm-server/model"
-	"github.com/elastic/apm-server/sourcemap/test"
 	"github.com/elastic/beats/v7/libbeat/beat"
 	"github.com/elastic/beats/v7/libbeat/common"
 	"github.com/elastic/beats/v7/libbeat/instrumentation"
@@ -268,11 +268,13 @@ func TestTransformConfigIndex(t *testing.T) {
 	t.Run("with-observer-version", func(t *testing.T) { test(t, "blah-%{[observer.version]}-blah", "blah-1.2.3-blah") })
 }
 
+var validSourcemap, _ = ioutil.ReadFile("../testdata/sourcemap/bundle.js.map")
+
 func TestStoreUsesRUMElasticsearchConfig(t *testing.T) {
 	var called bool
 	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		called = true
-		w.Write([]byte(test.ValidSourcemap))
+		w.Write(validSourcemap)
 	}))
 	defer ts.Close()
 
@@ -298,7 +300,7 @@ func TestFleetStoreUsed(t *testing.T) {
 		called = true
 		wr := zlib.NewWriter(w)
 		defer wr.Close()
-		wr.Write([]byte(fmt.Sprintf(`{"sourceMap":%s}`, test.ValidSourcemap)))
+		wr.Write([]byte(fmt.Sprintf(`{"sourceMap":%s}`, validSourcemap)))
 	}))
 	defer ts.Close()
 
