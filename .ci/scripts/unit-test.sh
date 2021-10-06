a0815398311f5b1835e0a843110ac5f6f7b2d579
Ivan Fernandez Calvo <kuisathaverat@yahoo.com>
2021-10-06 17:21:15 +0200

    fix: use mage goTestUnit to run the tests and covertura

--- .ci/scripts/unit-test.sh
+++ .ci/scripts/unit-test.sh
@@ -11,11 +11,4 @@ export COV_DIR="build/coverage"
 mkdir -p ${COV_DIR}
 
 make update
-go install -modfile=tools/go.mod github.com/jstemmer/go-junit-report
-go install -modfile=tools/go.mod github.com/t-yuki/gocover-cobertura
-
-(go test -race -covermode=atomic -coverprofile=${COV_DIR}/unit.cov -v ./... 2>&1 | tee ${OUT_FILE}) || echo -e "\033[31;49mTests FAILED\033[0m"
-
-cat ${OUT_FILE} | go-junit-report > build/junit-apm-server-report.xml
-go tool cover -html="${COV_DIR}/unit.cov" -o "${COV_DIR}/coverage-unit-report.html"
-gocover-cobertura < "${COV_DIR}/unit.cov" > "${COV_DIR}/coverage-unit-report.xml"
+mage goTestUnit
