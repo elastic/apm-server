93347abefef341751fb68089324f80a2914964cb
stuart nelson <stuartnelson3@gmail.com>
2021-10-14 14:06:55 +0200

    Otel span mapping (#6308)

--- model/modeldecoder/v2/transaction_test.go
+++ model/modeldecoder/v2/transaction_test.go
@@ -27,6 +27,7 @@ import (
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+	"google.golang.org/grpc/codes"
 
 	"github.com/elastic/apm-server/decoder"
 	"github.com/elastic/apm-server/model"
@@ -243,6 +244,8 @@ func TestDecodeMapToTransactionModel(t *testing.T) {
 			case
 				// Tested separately
 				"RepresentativeCount",
+				// Kind is tested further down
+				"Kind",
 
 				// Not set for transaction events, tested in metricset decoding:
 				"AggregatedDuration",
@@ -265,6 +268,7 @@ func TestDecodeMapToTransactionModel(t *testing.T) {
 		out1.Timestamp = reqTime
 		defaultVal := modeldecodertest.DefaultValues()
 		modeldecodertest.SetStructValues(&input, defaultVal)
+		input.OTel.Reset()
 		mapToTransactionModel(&input, &out1)
 		input.Reset()
 		modeldecodertest.AssertStructValues(t, out1.Transaction, exceptions, defaultVal)
@@ -282,6 +286,7 @@ func TestDecodeMapToTransactionModel(t *testing.T) {
 		out2.Timestamp = reqTime
 		otherVal := modeldecodertest.NonDefaultValues()
 		modeldecodertest.SetStructValues(&input, otherVal)
+		input.OTel.Reset()
 		mapToTransactionModel(&input, &out2)
 		modeldecodertest.AssertStructValues(t, out2.Transaction, exceptions, otherVal)
 		modeldecodertest.AssertStructValues(t, out1.Transaction, exceptions, defaultVal)
@@ -329,6 +334,7 @@ func TestDecodeMapToTransactionModel(t *testing.T) {
 		var input transaction
 		var out model.APMEvent
 		modeldecodertest.SetStructValues(&input, modeldecodertest.DefaultValues())
+		input.OTel.Reset()
 		// sample rate is set to > 0
 		input.SampleRate.Set(0.25)
 		mapToTransactionModel(&input, &out)
@@ -386,5 +392,158 @@ func TestDecodeMapToTransactionModel(t *testing.T) {
 			ID:       "session_id",
 			Sequence: 123,
 		}, out.Session)
+	})
+
+	// OTel bridge tests are mostly testing that the TranslateTransaction
+	// function is being called, with more rigorous testing taking place
+	// with that package.
+	t.Run("otel-bridge", func(t *testing.T) {
+		t.Run("http", func(t *testing.T) {
+			expected := model.URL{
+				Scheme:   "https",
+				Original: "/foo?bar",
+				Full:     "https://testing.invalid:80/foo?bar",
+				Path:     "/foo",
+				Query:    "bar",
+				Domain:   "testing.invalid",
+				Port:     80,
+			}
+			attrs := map[string]interface{}{
+				"http.scheme":   "https",
+				"net.host.name": "testing.invalid",
+				// TODO: will incoming information be properly decoded as int64?
+				"net.host.port": int64(80),
+				"http.target":   "/foo?bar",
+			}
+			var input transaction
+			var event model.APMEvent
+			modeldecodertest.SetStructValues(&input, modeldecodertest.DefaultValues())
+			input.OTel.Attributes = attrs
+			input.OTel.SpanKind.Reset()
+			input.Type.Reset()
+
+			mapToTransactionModel(&input, &event)
+			assert.Equal(t, expected, event.URL)
+			assert.Equal(t, "SERVER", event.Span.Kind)
+		})
+
+		t.Run("net", func(t *testing.T) {
+			expectedDomain := "source.domain"
+			expectedIP := "192.168.0.1"
+			expectedPort := 1234
+			attrs := map[string]interface{}{
+				"net.peer.name": "source.domain",
+				"net.peer.ip":   "192.168.0.1",
+				"net.peer.port": 1234,
+			}
+
+			var input transaction
+			var event model.APMEvent
+			modeldecodertest.SetStructValues(&input, modeldecodertest.DefaultValues())
+			input.OTel.Attributes = attrs
+			input.OTel.SpanKind.Reset()
+			mapToTransactionModel(&input, &event)
+
+			require.NotNil(t, event.HTTP)
+			require.NotNil(t, event.HTTP.Request)
+			parsedIP := net.ParseIP(expectedIP)
+			require.NotNil(t, parsedIP)
+			assert.Equal(t, model.Source{
+				Domain: expectedDomain,
+				IP:     net.ParseIP(expectedIP),
+				Port:   expectedPort,
+			}, event.Source)
+			assert.Equal(t, model.Client(event.Source), event.Client)
+			assert.Equal(t, "INTERNAL", event.Span.Kind)
+		})
+
+		t.Run("rpc", func(t *testing.T) {
+			var input transaction
+			var event model.APMEvent
+			modeldecodertest.SetStructValues(&input, modeldecodertest.DefaultValues())
+
+			attrs := map[string]interface{}{
+				"rpc.system":           "grpc",
+				"rpc.service":          "myservice.EchoService",
+				"rpc.method":           "exampleMethod",
+				"rpc.grpc.status_code": int64(codes.Unavailable),
+				"net.peer.name":        "peer_name",
+				"net.peer.ip":          "10.20.30.40",
+				"net.peer.port":        123,
+			}
+			input.OTel.Attributes = attrs
+			input.OTel.SpanKind.Reset()
+
+			mapToTransactionModel(&input, &event)
+			assert.Equal(t, "request", event.Transaction.Type)
+			assert.Equal(t, "Unavailable", event.Transaction.Result)
+			assert.Equal(t, model.Client{
+				Domain: "peer_name",
+				IP:     net.ParseIP("10.20.30.40"),
+				Port:   123,
+			}, event.Client)
+			assert.Equal(t, "SERVER", event.Span.Kind)
+		})
+
+		t.Run("messaging", func(t *testing.T) {
+			var input transaction
+			var event model.APMEvent
+			modeldecodertest.SetStructValues(&input, modeldecodertest.DefaultValues())
+			input.Type.Reset()
+			attrs := map[string]interface{}{
+				"message_bus.destination": "myQueue",
+			}
+			input.OTel.Attributes = attrs
+			input.OTel.SpanKind.Reset()
+
+			mapToTransactionModel(&input, &event)
+			assert.Equal(t, "messaging", event.Transaction.Type)
+			assert.Equal(t, "CONSUMER", event.Span.Kind)
+			assert.Equal(t, &model.Message{
+				QueueName: "myQueue",
+			}, event.Transaction.Message)
+		})
+
+		t.Run("network", func(t *testing.T) {
+			attrs := map[string]interface{}{
+				"net.host.connection.type":    "cell",
+				"net.host.connection.subtype": "LTE",
+				"net.host.carrier.name":       "Vodafone",
+				"net.host.carrier.mnc":        "01",
+				"net.host.carrier.mcc":        "101",
+				"net.host.carrier.icc":        "UK",
+			}
+			var input transaction
+			var event model.APMEvent
+			modeldecodertest.SetStructValues(&input, modeldecodertest.DefaultValues())
+			input.OTel.Attributes = attrs
+			input.OTel.SpanKind.Reset()
+			mapToTransactionModel(&input, &event)
+
+			expected := model.Network{
+				Connection: model.NetworkConnection{
+					Type:    "cell",
+					Subtype: "LTE",
+				},
+				Carrier: model.NetworkCarrier{
+					Name: "Vodafone",
+					MNC:  "01",
+					MCC:  "101",
+					ICC:  "UK",
+				},
+			}
+			assert.Equal(t, expected, event.Network)
+			assert.Equal(t, "INTERNAL", event.Span.Kind)
+		})
+
+		t.Run("kind", func(t *testing.T) {
+			var input transaction
+			var event model.APMEvent
+			modeldecodertest.SetStructValues(&input, modeldecodertest.DefaultValues())
+			input.OTel.SpanKind.Set("CLIENT")
+
+			mapToTransactionModel(&input, &event)
+			assert.Equal(t, "CLIENT", event.Span.Kind)
+		})
 	})
 }
