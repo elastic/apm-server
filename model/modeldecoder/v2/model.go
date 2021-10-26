e64252a3a64edb4fa95c82b26f71756432e66eb2
Tobias Stadler <ts.stadler@gmx.de>
2021-10-26 06:25:02 +0200

    Fixed copy and paste
    Co-authored-by: Andrew Wilkins <axwalk@gmail.com>

--- model/modeldecoder/v2/model.go
+++ model/modeldecoder/v2/model.go
@@ -679,7 +679,7 @@ type metricsetTransactionRef struct {
 type metricsetServiceRef struct {
 	// Name of the correlated service.
 	Name nullable.String `json:"name" validate:"maxLength=1024"`
-	// Name of the correlated service.
+	// Version of the correlated service.
 	Version nullable.String `json:"version" validate:"maxLength=1024"`
 }
 
