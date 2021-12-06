5b644efec6aa462605033ff12952c20b6111fffc
Andrew Wilkins <axw@elastic.co>
2021-12-06 19:58:56 +0800

    Reintroduce integration package generator (#6802)
    Introduce a new integration package generator/transformer,
    which takes a partial integration package in apmpackage/apm
    and performs APM-specific transformations and validations:
    
     - Inject package version and Kibana version constraint
     - Inject common ingest pipelines
     - Check ilm_policy is present and correct (and fix the
       value in the sampled_traces)
     - Copy fields from traces to rum_traces
    
    We no longer explicitly set `event.ingested` in our ingest
    pipelines; this is done by the common Fleet-installed pipeline,
    which is set as the final pipeline in all Fleet-install templates.
    
    (cherry picked from commit 40631477a092d5e1a72f18915aa12e8512123682)
    
    # Conflicts:
    #	Makefile
    #	apmpackage/apm/changelog.yml
    #	apmpackage/apm/manifest.yml

---
+++ apmpackage/cmd/genpackage/pipelines.go
@@ -0,0 +1,111 @@
+// Licensed to Elasticsearch B.V. under one or more contributor
+// license agreements. See the NOTICE file distributed with
+// this work for additional information regarding copyright
+// ownership. Elasticsearch B.V. licenses this file to you under
+// the Apache License, Version 2.0 (the "License"); you may
+// not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+package main
+
+import (
+	"fmt"
+
+	"github.com/elastic/beats/v7/libbeat/common"
+)
+
+// getCommonPipelines returns pipelines that may be inlined into our data stream ingest pipelines.
+//
+// To use a common pipeline, define a "pipeline" processor with the "name" property set to one of
+// the common pipelines. e.g.
+//
+//   processors:
+//     - ...
+//     - pipeline:
+//         name: observer_version
+//     - ...
+func getCommonPipeline(name string, version *common.Version) []map[string]interface{} {
+	commonPipelines := map[string][]map[string]interface{}{
+		"observer_version": getObserverVersionPipeline(version),
+		"user_agent":       userAgentPipeline,
+		"client_geoip":     clientGeoIPPipeline,
+	}
+	return commonPipelines[name]
+}
+
+// observerVersionPipeline ensures the observer version (i.e. apm-server version) is
+// no greater than the integration package version. The integration package version
+// is always expected to be greater, so this allows us to better reason about and
+// avoid version mismatch bugs.
+func getObserverVersionPipeline(version *common.Version) []map[string]interface{} {
+	observerVersionCheckIf := fmt.Sprintf(
+		"ctx.observer.version_major > %d || ctx.observer.version_minor > %d",
+		version.Major, version.Minor,
+	)
+	observerVersionCheckMessage := fmt.Sprintf(""+
+		"Document produced by APM Server v{{{observer.version}}}, "+
+		"which is newer than the installed APM integration (v%s). "+
+		"The APM integration must be upgraded.",
+		version,
+	)
+
+	return []map[string]interface{}{{
+		// Parse observer.version into observer.version_major, observer.version_minor,
+		// and observer.version_patch fields.
+		"grok": map[string]interface{}{
+			"field":               "observer.version",
+			"pattern_definitions": map[string]string{"DIGITS": "(?:[0-9]+)"},
+			"patterns": []string{"" +
+				"%{DIGITS:observer.version_major:int}." +
+				"%{DIGITS:observer.version_minor:int}." +
+				"%{DIGITS:observer.version_patch:int}(?:[-+].*)?",
+			},
+		},
+	}, {
+		"fail": map[string]interface{}{
+			"if":      observerVersionCheckIf,
+			"message": observerVersionCheckMessage,
+		},
+	}, {
+		// Remove observer.version_minor and observer.version_patch fields introduced above,
+		// leaving only observer.version_major which is used by the UI.
+		"remove": map[string]interface{}{
+			"ignore_missing": true,
+			"field":          []string{"observer.version_minor", "observer.version_patch"},
+		},
+	}}
+}
+
+var userAgentPipeline = []map[string]interface{}{{
+	"user_agent": map[string]interface{}{
+		"field":          "user_agent.original",
+		"target_field":   "user_agent",
+		"ignore_missing": true,
+		"ignore_failure": true,
+	},
+}}
+
+var clientGeoIPPipeline = []map[string]interface{}{{
+	"geoip": map[string]interface{}{
+		"field":          "client.ip",
+		"target_field":   "client.geo",
+		"ignore_missing": true,
+		"database_file":  "GeoLite2-City.mmdb",
+		"on_failure": []map[string]interface{}{{
+			"remove": map[string]interface{}{
+				"field":          "client.ip",
+				"ignore_missing": true,
+				"ignore_failure": true,
+			},
+		}},
+	},
+}}
