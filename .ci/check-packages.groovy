6f61e0f16cc2c190fbe5f94f19c08370b951727a
Silvia Mitter <silvia.mitter@elastic.co>
2021-12-24 09:59:09 +0100

    7.16 packages (#6934)
    * Update version for packaging tests
    
    * Change version from 8.0 to 7.16

--- .ci/check-packages.groovy
+++ .ci/check-packages.groovy
@@ -29,7 +29,7 @@ pipeline {
   }
   parameters {
     string(name: 'APM_URL_BASE', defaultValue: 'https://storage.googleapis.com/apm-ci-artifacts/jobs/snapshots', description: 'The location where the APM packages should be downloaded from')
-    string(name: 'VERSION', defaultValue: '7.11.0-SNAPSHOT', description: 'The package version to test (modify the job configuration to add a new version)')
+    string(name: 'VERSION', defaultValue: '7.16.0-SNAPSHOT', description: 'The package version to test (modify the job configuration to add a new version)')
   }
   stages {
     stage('Checkout') {
@@ -47,11 +47,11 @@ pipeline {
                             selector: upstream(fallbackToLastSuccessful: true))
               def props = readProperties(file: 'beats-tester.properties')
               setEnvVar('APM_URL_BASE', props.get('APM_URL_BASE', ''))
-              setEnvVar('VERSION', props.get('VERSION', '8.0.0-SNAPSHOT'))
+              setEnvVar('VERSION', props.get('VERSION', '7.16.0-SNAPSHOT'))
             } catch(err) {
               log(level: 'WARN', text: "copyArtifacts failed. Fallback to the head of the branch as used to be.")
               setEnvVar('APM_URL_BASE', params.get('APM_URL_BASE', 'https://storage.googleapis.com/apm-ci-artifacts/jobs/snapshots'))
-              setEnvVar('VERSION', params.get('VERSION', '8.0.0-SNAPSHOT'))
+              setEnvVar('VERSION', params.get('VERSION', '7.16.0-SNAPSHOT'))
             }
           } else {
             log(level: 'INFO', text: "No started by upstream pipeline. Fallback to the head of the branch as used to be.")
