666f8b03ec847425d3fc8c910b940081fc90bb89
Marc Lopez Rubio <marc5.12@outlook.com>
2021-12-28 17:54:44 +0800

    Update .ci/scripts/linux-test.sh
    Co-authored-by: Ivan Fernandez Calvo <kuisathaverat@users.noreply.github.com>

--- .ci/scripts/linux-test.sh
+++ .ci/scripts/linux-test.sh
@@ -22,8 +22,7 @@ OUTPUT_JUNIT_FILE="$OUTPUT_DIR/TEST-go-system_tests.xml"
 
 # Download systemtest dependencies soo the download time doesn't count towards
 # the test timeout.
-APM_ROOT=$(pwd)
-cd systemtest && go mod download && cd ${APM_ROOT}
+cd systemtest && go mod download && cd -
 
 export GOTESTFLAGS="-v -json"
 gotestsum --no-color -f standard-quiet --jsonfile "$OUTPUT_JSON_FILE" --junitfile "$OUTPUT_JUNIT_FILE" --raw-command -- make system-test
