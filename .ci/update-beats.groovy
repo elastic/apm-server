d17cb175bd1671e8ef74cfa72593b280b03aa4b0
Victor Martinez <victormartinezrubio@gmail.com>
2022-02-02 13:53:16 +0000

    Update .ci/update-beats.groovy

--- .ci/update-beats.groovy
+++ .ci/update-beats.groovy
@@ -30,9 +30,6 @@ pipeline {
     JOB_GIT_CREDENTIALS = "f6c7695a-671e-4f4f-a331-acdce44ff9ba"
     PIPELINE_LOG_LEVEL = 'INFO'
   }
-  triggers {
-    cron(env.BRANCH_NAME == '7.17' ? 'H H(4-5) * * 1,5' : '')
-  }
   options {
     timeout(time: 1, unit: 'HOURS')
     buildDiscarder(logRotator(numToKeepStr: '5', artifactNumToKeepStr: '5'))
