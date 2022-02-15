a652d3ef283b7174a697124eb3d1753d11c2b50f
Victor Martinez <VictorMartinezRubio@gmail.com>
2022-02-15 09:07:57 +0000

    ci: support for next minor release branch for the 8 major

--- .ci/schedule-daily.groovy
+++ .ci/schedule-daily.groovy
@@ -20,7 +20,7 @@ pipeline {
   stages {
     stage('Nighly update Beats builds') {
       steps {
-        updateBeatsBuilds(branches: ['main', '8.<minor>', '8.<next-patch>', '7.<minor>', '7.<next-minor>'])
+        updateBeatsBuilds(branches: ['main', '8.<minor>', '8.<next-minor>', '8.<next-patch>', '7.<minor>'])
       }
     }
   }
