acf7debb75ae9c7d45c4ad95ce79e13dc166ee80
Victor Martinez <VictorMartinezRubio@gmail.com>
2022-03-17 09:55:55 +0000

    chore: testing purposes

--- .ci/packaging.groovy
+++ .ci/packaging.groovy
@@ -141,7 +141,8 @@ pipeline {
       notifyBuildResult()
     }
     failure {
-      notifyStatus(slackStatus: 'danger', subject: "[${env.REPO}] DRA failed", body: "Build: (<${env.RUN_DISPLAY_URL}|here>)")
+      echo 'disabled'
+      //notifyStatus(slackStatus: 'danger', subject: "[${env.REPO}] DRA failed", body: "Build: (<${env.RUN_DISPLAY_URL}|here>)")
     }
   }
 }
