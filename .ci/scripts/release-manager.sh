e461a46a9a6df6f82d42410fccac49adbbbc9daa
Victor Martinez <VictorMartinezRubio@gmail.com>
2022-03-17 09:53:12 +0000

    chore: testing purposes

--- .ci/scripts/release-manager.sh
+++ .ci/scripts/release-manager.sh
@@ -40,7 +40,7 @@ docker run --rm \
   "$IMAGE" \
     cli collect \
       --project apm-server \
-      --branch "$BRANCH_NAME" \
+      --branch "main" \
       --commit "$(git rev-parse HEAD)" \
       --workflow "snapshot" \
       --artifact-set main
