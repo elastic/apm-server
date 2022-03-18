e166dafc6315ca3b03bf6ad45e622ca93012f18c
Victor Martinez <VictorMartinezRubio@gmail.com>
2022-03-18 17:25:54 +0000

    dra: retry docker pull
    that's the recommended approach while using the docker.elastic.co
    
    Let's rely on the existing cached docker images in the workers

--- .ci/scripts/release-manager.sh
+++ .ci/scripts/release-manager.sh
@@ -9,6 +9,8 @@
 #
 set -uexo pipefail
 
+source /usr/local/bin/bash_standard_lib.sh
+
 # set required permissions on artifacts and directory
 chmod -R a+r build/distributions/*
 chmod -R a+w build/distributions
@@ -33,7 +35,7 @@ done
 
 # ensure the latest image has been pulled
 IMAGE=docker.elastic.co/infra/release-manager:latest
-docker pull --quiet $IMAGE
+(retry 3 docker pull "${IMAGE}") || echo "Error pulling ${IMAGE} Docker image, we continue"
 
 # Generate checksum files and upload to GCS
 docker run --rm \
