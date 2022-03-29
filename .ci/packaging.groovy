935bad4d6e5ec98a6059d645e7c26f3134bfdd14
Victor Martinez <VictorMartinezRubio@gmail.com>
2022-03-29 09:07:24 +0100

    ci: exclude staging when on main

--- .ci/packaging.groovy
+++ .ci/packaging.groovy
@@ -88,6 +88,15 @@ pipeline {
             }
             stages {
               stage('Package') {
+                when {
+                  beforeAgent true
+                  // Exclude running staging for the main branch since
+                  // it's an expensive operation and it's not used for releases
+                  allOf {
+                    not { branch 'main' }
+                    expression { TYPE != 'staging'}
+                  }
+                }
                 environment {
                   PLATFORMS = "${isArm() ? 'linux/arm64' : ''}"
                   PACKAGES = "${isArm() ? 'docker' : ''}"
@@ -97,6 +106,15 @@ pipeline {
                 }
               }
               stage('Publish') {
+                when {
+                  beforeAgent true
+                  // Exclude running staging for the main branch since
+                  // it's an expensive operation and it's not used for releases
+                  allOf {
+                    not { branch 'main' }
+                    expression { TYPE != 'staging'}
+                  }
+                }
                 steps {
                   publishArtifacts(type: env.TYPE)
                 }
