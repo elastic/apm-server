a79386c6b2989321a35a79cabae00f4e5b9c9b4f
Marc Lopez Rubio <marc5.12@outlook.com>
2022-03-29 09:29:11 +0200

    temp: Add debug information to build
    Signed-off-by: Marc Lopez Rubio <marc5.12@outlook.com>

--- .ci/scripts/linux-test.sh
+++ .ci/scripts/linux-test.sh
@@ -13,6 +13,13 @@ trap cleanup EXIT
 
 make update apm-server
 
+
+echo "-> START OF DEBUG INFORMATION"
+docker version
+docker-compose version
+docker info
+echo "-> END OF DEBUG INFORMATION"
+
 # Start docker-compose environment first, so it doesn't count towards the test timeout.
 docker-compose up -d
 
