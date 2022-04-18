43730d3ed1de302b5fda16c12f30aa5e07c96628
Victor Martinez <VictorMartinezRubio@gmail.com>
2022-04-18 17:01:54 +0100

    packaging: enable apmpackage stage when changes in the folder, version or the pipeline itself

--- .ci/packaging.groovy
+++ .ci/packaging.groovy
@@ -68,6 +68,8 @@ pipeline {
             setEnvVar('IS_BRANCH_AVAILABLE', isBranchUnifiedReleaseAvailable(env.BRANCH_NAME))
             dir("${BASE_DIR}"){
               setEnvVar('VERSION', sh(label: 'Get version', script: 'make get-version', returnStdout: true)?.trim())
+              // The apmpackage stage gets triggers as described in https://github.com/elastic/apm-server/issues/6970
+              setEnvVar('IS_APM_PACKAGE', isGitRegionMatch(patterns: [ '(cmd/version.go|apmpackage/.*|.ci/packaging.groovy)' ], comparator: 'regexp'))
             }
           }
         }
@@ -119,6 +121,9 @@ pipeline {
         }
         stage('apmpackage') {
           options { skipDefaultCheckout() }
+          when {
+            expression { return env.IS_APM_PACKAGE == "true" }
+          }
           steps {
             runWithMage(script: "make build-package") {
               echo 'elastic-package check'
