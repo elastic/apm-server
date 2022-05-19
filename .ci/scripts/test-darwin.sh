550a2565f4c28fca4bd3ff814cd222623602c448
Victor Martinez <VictorMartinezRubio@gmail.com>
2022-05-19 17:56:17 +0100

    ci: install pyenv if needed

--- .ci/scripts/test-darwin.sh
+++ .ci/scripts/test-darwin.sh
@@ -4,8 +4,10 @@ set -eox pipefail
 # Setup python3
 pythonVersion=3.7.7
 rm -fr "${HOME}/.pyenv"
-curl -L https://github.com/pyenv/pyenv-installer/raw/master/bin/pyenv-installer | bash
-export PATH="${HOME}/.pyenv/bin:$PATH"
+if ! command -v pyenv 2> /dev/null ; then
+    curl -L https://github.com/pyenv/pyenv-installer/raw/master/bin/pyenv-installer | bash
+    export PATH="${HOME}/.pyenv/bin:$PATH"
+fi
 eval "$(pyenv init -)"
 eval "$(pyenv virtualenv-init -)"
 pyenv install ${pythonVersion}
