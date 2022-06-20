78e39aa0b12fe2712c11815f6aaffce3813e2eb4
stuart nelson <stuartnelson3@gmail.com>
2022-06-20 14:47:31 +0200

    add test for setting namespace from integration
    A regression was introduced in 8.x where the
    namespace couldn't be set by the integration.
    This bug is not present in 7.17, but add a test to
    prevent the regression from occurring.
    
    Test is taken from
    https://github.com/elastic/apm-server/pull/8218,
    but the implementation being backported is
    unnecessary.

--- beater/server_test.go
+++ beater/server_test.go
@@ -607,6 +607,7 @@ func TestServerOutputConfigReload(t *testing.T) {
 	}
 
 	inputConfig := common.MustNewConfigFrom(map[string]interface{}{
+		"data_stream.namespace": "custom",
 		"apm-server": map[string]interface{}{
 			"host": "localhost:0",
 			"sampling.tail": map[string]interface{}{
@@ -622,6 +623,7 @@ func TestServerOutputConfigReload(t *testing.T) {
 
 	runServerArgs := <-runServerCalls
 	assert.Equal(t, "", runServerArgs.Config.Sampling.Tail.ESConfig.Username)
+	assert.Equal(t, "custom", runServerArgs.Namespace)
 
 	// Reloaded output config should be passed into apm-server config.
 	err = apmBeat.OutputConfigReloader.Reload(&reload.ConfigWithMeta{
