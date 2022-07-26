9e7f2c58a4f1a7ee56ac968eb8f916ed5628dc38
mtojek <marcin.tojek@elastic.co>
2022-07-26 11:48:43 +0200

    Archive artifacts

--- .ci/packaging.groovy
+++ .ci/packaging.groovy
@@ -143,7 +143,8 @@ pipeline {
                 //  sh(label: 'create-package-storage-pull-request', script: 'make -C .ci/scripts create-package-storage-pull-request')
                 // }
 
-                sh(script: 'make build-package', label: 'v2: make build-package')
+                sh(script: 'make build-package', label: 'v2: make build-package-snapshot')
+                archiveArtifacts(allowEmptyArchive: false, artifacts: 'build/packages/*.zip')
               }
             }
           }
