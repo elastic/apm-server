71907cfb2ec835aa86cb47fbf3623ffaebbca9f6
Andrew Wilkins <axw@elastic.co>
2022-07-28 11:35:48 +0800

    .ci: don't notify of packaging failure on PRs

--- .ci/packaging.groovy
+++ .ci/packaging.groovy
@@ -118,8 +118,10 @@ pipeline {
           }
           post {
             failure {
-              notifyStatus(subject: "[${env.REPO}@${env.BRANCH_NAME}] package failed.",
-                           body: 'Contact the Productivity team [#observablt-robots] if you need further assistance.')
+              if (!isPR()) {
+                notifyStatus(subject: "[${env.REPO}@${env.BRANCH_NAME}] package failed.",
+                             body: 'Contact the Productivity team [#observablt-robots] if you need further assistance.')
+              }
             }
           }
         }
