7664c2c4672ca42bbae050b22a0deda4203882ae
Andrew Wilkins <axw@elastic.co>
2022-08-02 12:45:09 +0800

    .ci: cache Go modules

--- .ci/packer_cache.sh
+++ .ci/packer_cache.sh
@@ -6,8 +6,9 @@ source /usr/local/bin/bash_standard_lib.sh
 # shellcheck disable=SC1091
 source ./script/common.bash
 
-get_go_version
+jenkins_setup
 
+# Fetch Docker images used for packaging.
 DOCKER_IMAGES="
 docker.elastic.co/infra/release-manager:latest
 golang:${GO_VERSION}
@@ -18,3 +19,8 @@ if [ -x "$(command -v docker)" ]; then
   (retry 2 docker pull "${image}") || echo "Error pulling ${image} Docker image, we continue"
   done
 fi
+
+# Download Go module dependencies.
+go mod download
+go mod download -modfile=tools/go.mod
+go mod download -modfile=systemtest/go.mod
