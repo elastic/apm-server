7fb6414b56d89aa25002dd140c7d6e528059bec4
Andrew Wilkins <axw@elastic.co>
2022-08-15 11:11:17 +0800

    Introduce more support for cloud-first testing (#8856)
    * Introduce more support for cloud-first testing
    
    testing/cloud holds a Makefile for automating
    the building and pushing of custom Kibana and
    Elastic Agent images for running in the Cloud-
    First Testing region, and a Terraform module
    for creating deployments using them.
    
    testing/docker/elastic-agent holds a Dockerfile
    and supporting script for building an Elastic
    Agent image with a local apm-server binary
    injected.
    
    testing/docker/kibana holds a Dockerfile for
    building a Kibana image with the local APM
    integration package injected.
    
    * buildapm: return errors
    
    * testing/cloud: specify --platform for kibana

---
+++ testing/cloud/main.tf
@@ -0,0 +1,43 @@
+terraform {
+  required_version = ">= 1.1.8, < 2.0.0"
+  required_providers {
+    ec = {
+      source  = "elastic/ec"
+      version = ">=0.4.0"
+    }
+  }
+}
+
+provider "ec" {}
+
+locals {
+    docker_image_tag = regex("docker.elastic.co/.*:(.*)",file("${path.module}/../../docker-compose.yml"))[0]
+    match = regex("(?:(.*)(?:-.*)-(?:SNAPSHOT))|(.*)", local.docker_image_tag)
+    stack_version = local.match[0] != null ? format("%s-SNAPSHOT", local.match[0]) : local.match[1]
+}
+
+module "ec_deployment" {
+  source = "../infra/terraform/modules/ec_deployment"
+
+  region        = var.ess_region
+  stack_version = local.stack_version
+
+  deployment_template    = var.deployment_template
+  deployment_name_prefix = "apm-server-testing"
+
+  apm_server_size       = var.apm_server_size
+  apm_server_zone_count = var.apm_server_zone_count
+  apm_server_expvar     = false
+  apm_server_pprof      = false
+  integrations_server   = true
+
+  elasticsearch_size       = var.elasticsearch_size
+  elasticsearch_zone_count = var.elasticsearch_zone_count
+
+  docker_image              = var.docker_image_override
+  docker_image_tag_override = {
+    "elasticsearch": coalesce(var.docker_image_tag_override["elasticsearch"], local.docker_image_tag),
+    "kibana": coalesce(var.docker_image_tag_override["kibana"], local.docker_image_tag),
+    "apm": coalesce(var.docker_image_tag_override["apm"], local.docker_image_tag)
+  }
+}
