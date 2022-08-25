97e29b854bea1fa75b27ad53505712b024d279e5
eyalkoren <41850454+eyalkoren@users.noreply.github.com>
2022-08-25 14:29:00 +0300

    Separate tests into OS-specific files

--- internal/beater/java_attacher/java_attacher_test.go
+++ internal/beater/java_attacher/java_attacher_test.go
@@ -18,14 +18,8 @@
 package javaattacher
 
 import (
-	"context"
-	"fmt"
 	"os"
-	"os/user"
-	"path/filepath"
 	"regexp"
-	"runtime"
-	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -40,107 +34,6 @@ func TestNoAttacherCreatedWithoutDiscoveryRules(t *testing.T) {
 	}
 	_, err := New(cfg)
 	require.Error(t, err)
-}
-
-func TestBuildCommandWithBundledJar(t *testing.T) {
-	if runtime.GOOS != "windows" {
-		return
-	}
-	cfg := createTestConfig()
-	f, err := os.Create(bundledJavaAttacher)
-	require.NoError(t, err)
-	//goland:noinspection GoUnhandledErrorResult
-	defer os.Remove(f.Name())
-
-	attacher, err := New(cfg)
-	require.NoError(t, err)
-	defer attacher.cleanResources()
-
-	// invalid user and group ensures usage of the bundled jar
-	jvm := &jvmDetails{
-		pid:     12345,
-		command: filepath.FromSlash("/home/someuser/java_home/bin/java"),
-	}
-	command := attacher.attachJVMCommand(context.Background(), jvm)
-	want := filepath.FromSlash("/home/someuser/java_home/bin/java -jar java-attacher.jar") +
-		" --log-level debug --include-pid 12345 --download-agent-version 1.27.0 --config server_url=http://myhost:8200"
-
-	cmdArgs := strings.Join(command.Args, " ")
-	assert.Equal(t, want, cmdArgs)
-	assert.Empty(t, attacher.tmpDirs)
-	assert.Empty(t, attacher.uidToAttacherJar)
-}
-
-func TestBuildInvalidCommand(t *testing.T) {
-	if runtime.GOOS == "windows" {
-		return
-	}
-	cfg := createTestConfig()
-	f, err := os.Create(bundledJavaAttacher)
-	require.NoError(t, err)
-	//goland:noinspection GoUnhandledErrorResult
-	defer os.Remove(f.Name())
-
-	attacher, err := New(cfg)
-	require.NoError(t, err)
-	defer attacher.cleanResources()
-
-	// invalid user and group ensures usage of the bundled jar
-	jvm := &jvmDetails{
-		pid:     12345,
-		uid:     "invalid",
-		command: filepath.FromSlash("/home/someuser/java_home/bin/java"),
-	}
-	command := attacher.attachJVMCommand(context.Background(), jvm)
-	assert.Nil(t, command)
-	assert.Empty(t, attacher.tmpDirs)
-	assert.Len(t, attacher.uidToAttacherJar, 1)
-}
-
-func TestBuildCommandWithTempJar(t *testing.T) {
-	if runtime.GOOS == "windows" {
-		return
-	}
-	cfg := createTestConfig()
-	f, err := os.Create(bundledJavaAttacher)
-	require.NoError(t, err)
-	//goland:noinspection GoUnhandledErrorResult
-	defer os.Remove(f.Name())
-
-	attacher, err := New(cfg)
-	require.NoError(t, err)
-	defer attacher.cleanResources()
-
-	currentUser, _ := user.Current()
-	jvm := &jvmDetails{
-		pid:     12345,
-		uid:     currentUser.Uid,
-		gid:     currentUser.Gid,
-		command: filepath.FromSlash("/home/someuser/java_home/bin/java"),
-	}
-	assert.Empty(t, attacher.tmpDirs)
-	assert.Empty(t, attacher.uidToAttacherJar)
-	command := attacher.attachJVMCommand(context.Background(), jvm)
-	assert.Len(t, attacher.tmpDirs, 1)
-	assert.Len(t, attacher.uidToAttacherJar, 1)
-	attacherJar := attacher.uidToAttacherJar[currentUser.Uid]
-	assert.NotEqual(t, attacherJar, "")
-	require.NotEqual(t, bundledJavaAttacher, attacherJar)
-	want := filepath.FromSlash(fmt.Sprintf("/home/someuser/java_home/bin/java -jar %v", attacherJar)) +
-		" --log-level debug --include-pid 12345 --download-agent-version 1.27.0 --config server_url=http://myhost:8200"
-
-	cmdArgs := strings.Join(command.Args, " ")
-	assert.Equal(t, want, cmdArgs)
-
-	cfg.Config["service_name"] = "my-cool-service"
-	attacher, err = New(cfg)
-	require.NoError(t, err)
-	defer attacher.cleanResources()
-
-	command = attacher.attachJVMCommand(context.Background(), jvm)
-	cmdArgs = strings.Join(command.Args, " ")
-	assert.Contains(t, cmdArgs, "--config server_url=http://myhost:8200")
-	assert.Contains(t, cmdArgs, "--config service_name=my-cool-service")
 }
 
 func createTestConfig() config.JavaAttacherConfig {
@@ -255,57 +148,6 @@ func TestConfig(t *testing.T) {
 	require.True(t, match.include())
 	javaAttacher.discoveryRules[4] = &userDiscoveryRule{}
 	require.Nil(t, javaAttacher.findFirstMatch(&jvmDetails))
-}
-
-func TestTempDirCreation(t *testing.T) {
-	if runtime.GOOS == "windows" {
-		return
-	}
-	cfg := createTestConfig()
-	f, err := os.Create(bundledJavaAttacher)
-	require.NoError(t, err)
-	//goland:noinspection GoUnhandledErrorResult
-	defer os.Remove(f.Name())
-	attacher, err := New(cfg)
-	require.NoError(t, err)
-	defer attacher.cleanResources()
-
-	currentUser, _ := user.Current()
-	jvm := &jvmDetails{
-		pid:     12345,
-		uid:     currentUser.Uid,
-		gid:     currentUser.Gid,
-		command: filepath.FromSlash("/home/someuser/java_home/bin/java"),
-	}
-	assert.Empty(t, attacher.tmpDirs)
-	assert.Empty(t, attacher.uidToAttacherJar)
-	// this call creates the temp dir
-	attacher.attachJVMCommand(context.Background(), jvm)
-	assert.Len(t, attacher.uidToAttacherJar, 1)
-	attacherJar := attacher.uidToAttacherJar[currentUser.Uid]
-	assert.NotEqual(t, attacherJar, "")
-	require.NotEqual(t, bundledJavaAttacher, attacherJar)
-
-	assert.Len(t, attacher.tmpDirs, 1)
-	tempAttacherDir := attacher.tmpDirs[0]
-	require.DirExists(t, tempAttacherDir)
-	attacherDirInfo, err := os.Stat(tempAttacherDir)
-	require.NoError(t, err)
-	require.Equal(t, os.FileMode(0700), attacherDirInfo.Mode().Perm())
-	tempDir, err := os.Open(tempAttacherDir)
-	require.NoError(t, err)
-	files, err := tempDir.ReadDir(0)
-	require.NoError(t, err)
-	require.Len(t, files, 1)
-	attacherJarFileInfo, err := files[0].Info()
-	require.NoError(t, err)
-	require.Equal(t, os.FileMode(0600), attacherJarFileInfo.Mode().Perm())
-	require.Equal(t, attacherJar, filepath.Join(tempAttacherDir, attacherJarFileInfo.Name()))
-
-	// verify caching
-	attacher.attachJVMCommand(context.Background(), jvm)
-	assert.Len(t, attacher.tmpDirs, 1)
-	assert.Len(t, attacher.uidToAttacherJar, 1)
 }
 
 func TestJavaCommandNormalization(t *testing.T) {
