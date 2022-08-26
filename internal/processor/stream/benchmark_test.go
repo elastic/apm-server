24c5140e2b641cbd914549976192f9ff80057f95
Marc Lopez Rubio <marc5.12@outlook.com>
2022-08-26 15:15:26 +0800

    fixup benchmark_test.go file
    Signed-off-by: Marc Lopez Rubio <marc5.12@outlook.com>

--- internal/processor/stream/benchmark_test.go
+++ internal/processor/stream/benchmark_test.go
@@ -65,7 +65,7 @@ func benchmarkStreamProcessor(b *testing.B, processor *Processor, files []string
 			b.StartTimer()
 
 			var result Result
-			processor.HandleStream(context.Background(), model.APMEvent{}, r, batchSize, batchProcessor, &result)
+			processor.HandleStream(context.Background(), false, model.APMEvent{}, io.NopCloser(r), batchSize, batchProcessor, &result)
 		}
 	}
 
@@ -103,7 +103,7 @@ func benchmarkStreamProcessorParallel(b *testing.B, processor *Processor, files 
 				r := bytes.NewReader(data)
 				for p.Next() {
 					var result Result
-					processor.HandleStream(context.Background(), model.APMEvent{}, r, batchSize, batchProcessor, &result)
+					processor.HandleStream(context.Background(), false, model.APMEvent{}, io.NopCloser(r), batchSize, batchProcessor, &result)
 					r.Seek(0, io.SeekStart)
 				}
 			})
