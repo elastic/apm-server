5164671c421eeb05c64e5a248f0ea8166b52598e
Victor Martinez <VictorMartinezRubio@gmail.com>
2022-09-28 11:52:33 +0100

    skip if no new artifacts to be downloaded

--- testing/smoke/legacy-managed/test.sh
+++ testing/smoke/legacy-managed/test.sh
@@ -8,6 +8,12 @@ if [[ ${1} != 7.17 ]]; then
 fi
 
 VERSION=7.17
+# Check if the version is available.
+if ! curl --fail https://artifacts-api.elastic.co/v1/versions/${VERSION} ; then
+    echo "-> Warning there are no new artifacts to be downloaded in artifacts-api.elastic.co ..."
+    exit 0
+fi
+
 LATEST_VERSION=$(curl -s --fail https://artifacts-api.elastic.co/v1/versions/${VERSION} | jq -r '.version.builds[0].version')
 
 echo "-> Running ${LATEST_VERSION} standalone to ${LATEST_VERSION} managed upgrade"
--- testing/smoke/legacy-standalone-major-managed/test.sh
+++ testing/smoke/legacy-standalone-major-managed/test.sh
@@ -12,6 +12,11 @@ VERSION=7.17
 # Load all versions except SNAPSHOTS
 VERSIONS=$(curl -s --fail https://artifacts-api.elastic.co/v1/versions | jq -r -c '[.versions[] | select(. | endswith("-SNAPSHOT") | not)] | sort')
 NEXT_MAJOR_LATEST=$(echo ${VERSIONS} | jq -r '[.[] | select(. | startswith("8"))] | last')
+# Check if the version is available.
+if ! curl --fail https://artifacts-api.elastic.co/v1/versions/${VERSION} ; then
+    echo "-> Warning there are no new artifacts to be downloaded in artifacts-api.elastic.co ..."
+    exit 0
+fi
 LATEST_VERSION=$(curl -s --fail https://artifacts-api.elastic.co/v1/versions/${VERSION} | jq -r '.version.builds[0].version')
 
 echo "-> Running ${LATEST_VERSION} standalone to ${NEXT_MAJOR_LATEST} to ${NEXT_MAJOR_LATEST} managed"
