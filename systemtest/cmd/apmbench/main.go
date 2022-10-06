ed947f1ba0c8b0fb5dc45145109c992e392d6042
kruskal <99559985+kruskall@users.noreply.github.com>
2022-10-06 15:01:14 +0200

    test: allow to run otlptraces test on demand with env variable
    Enable otlptraces test with OTLPTRACES_TEST=1 similar to what we are doing with
    integration tests.

--- systemtest/cmd/apmbench/main.go
+++ systemtest/cmd/apmbench/main.go
@@ -21,6 +21,8 @@ import (
 	"context"
 	"flag"
 	"log"
+	"os"
+	"strings"
 	"testing"
 
 	sdktrace "go.opentelemetry.io/otel/sdk/trace"
@@ -49,7 +51,11 @@ func Benchmark1000Transactions(b *testing.B, l *rate.Limiter) {
 }
 
 func BenchmarkOTLPTraces(b *testing.B, l *rate.Limiter) {
-	b.Skipf("Disabled until we've addressed https://github.com/elastic/apm-server/issues/9242")
+	switch strings.ToLower(os.Getenv("OTLPTRACES_TEST")) {
+	case "1", "true":
+	default:
+		b.Skip("Disabled until we've addressed https://github.com/elastic/apm-server/issues/9242, export OTLPTRACES_TEST=1 to run")
+	}
 
 	b.RunParallel(func(pb *testing.PB) {
 		exporter := benchtest.NewOTLPExporter(b)
