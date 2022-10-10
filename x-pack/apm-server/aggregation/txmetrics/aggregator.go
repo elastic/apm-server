27c2a19aefda928c14b78a09d5dd5e56ce625eac
kruskal <99559985+kruskall@users.noreply.github.com>
2022-10-10 04:27:34 +0200

    docs: update transaction metrics limit reached log message
    reference use_path_as_transaction_name config

--- x-pack/apm-server/aggregation/txmetrics/aggregator.go
+++ x-pack/apm-server/aggregation/txmetrics/aggregator.go
@@ -273,7 +273,8 @@ func (a *Aggregator) AggregateTransaction(event model.APMEvent) model.APMEvent {
 	a.tooManyGroupsLogger.Warn(`
 Transaction group limit reached, falling back to sending individual metric documents.
 This is typically caused by ineffective transaction grouping, e.g. by creating many
-unique transaction names.`[1:],
+unique transaction names.
+If you are using an unsupported framework you can enable 'use_path_as_transaction_name'.`[1:],
 	)
 	atomic.AddInt64(&a.metrics.overflowed, 1)
 	counts := []int64{int64(math.Round(count))}
