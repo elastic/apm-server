6e7546304e4d2f0829120e3dfcf7c60c054d5e1a
Marc Lopez Rubio <marc5.12@outlook.com>
2022-10-26 14:18:14 +0100

    modelindexer: Scale active indexers based on load (#9393)
    Modifies the `modelindexer` to scale up and down the number of "active"
    indexers (goroutines pulling out of the internal modelindexer queue),
    based on consecutive number of flushes up to 25% of the `GOMAXPROCS`.
    
    When N number of full flushes (Reached or exceeded `FlushBytes`) occur,
    a new active indexer will be created by the scale action, as long as the
    scaling cooldown has elapsed.
    
    Equally, when N number of timed flushes (due to `FlushInterval`) occur,
    an active indexer will be scaled down, since not enough load is going
    through the server to warrant the current number of active indexers.
    
    Active indexer downscaling can also be triggered due to a change in the
    `GOMAXPROCS`. This is particularly important for containerized or cgroup
    environments where CPU quotas may be updated "live". In this case, the
    downscale cooldown is ignored and active indexers are scaled down until
    until `active <= math.RoundToEven(GOMAXPROCS / 4)`.
    
    When traffic isn't going through an active indexer, a new timer has been
    introduced to allow completely idle indexers to be scaled back. The idle
    check interval can be configured via `IdleInterval`.
    
    Scaling is enabled by default, but can be disabled via the configuration
    option `output.elasticsearch.scaling.enabled: false`.
    
    Last, modifies the default settings for `max_requests` and `flush_bytes`
    to be `50` and `1MB` respectively. This allows smaller payloads to be
    sent to Elasticsearch, more available indexers can be used by instances
    with more processing power, and the indexers are cycled faster, which
    results in better usage and performance.
    
    Signed-off-by: Marc Lopez Rubio <marc5.12@outlook.com>
    Co-authored-by: Silvia Mitter <silvia.mitter@elastic.co>

--- systemtest/fleet_test.go
+++ systemtest/fleet_test.go
@@ -107,9 +107,13 @@ func TestFleetIntegrationMonitoring(t *testing.T) {
 	assert.Equal(t, map[string]interface{}{
 		"elasticsearch": map[string]interface{}{
 			"bulk_requests": map[string]interface{}{
-				"active":    float64(1),
-				"available": float64(25),
+				"available": float64(50),
 				"completed": 1.0,
+			},
+			"indexers": map[string]interface{}{
+				"active":    float64(1),
+				"created":   0.0,
+				"destroyed": 0.0,
 			},
 		},
 	}, metrics.Output)
