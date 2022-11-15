5734521f98f447dd66de2d91583c0a476e315b7b
Victor Martinez <VictorMartinezRubio@gmail.com>
2022-11-15 15:29:56 +0000

    ci: use github actions
    - Windows and macOS unit tests
    - Linting (make check-full)

--- internal/beatcmd/logging_test.go
+++ internal/beatcmd/logging_test.go
@@ -25,7 +25,7 @@ func init() {
 	// Configure tests to log at debug level, and to send
 	// logs to logp.ObserverLogs(). It is important to not
 	// log to files by default in tests, as logp will keep
-	// the files open and prevent tempory directories from
+	// the files open and prevent temporary directories from
 	// being removed on Windows.
 	logOptions = append(logOptions,
 		logp.ToObserverOutput(),
