cb18728c92b24d184df12d1a9ab64981fefee12d
Pavel Zorin <pavel.zorin@elastic.co>
2022-12-01 16:51:56 +0100

    Cloud labeling for benchmark instances

--- testing/infra/terraform/modules/benchmark_executor/instance.tf
+++ testing/infra/terraform/modules/benchmark_executor/instance.tf
@@ -71,5 +71,9 @@ module "ec2_instance" {
     Name       = "${var.user_name}-worker"
     managed-by = "terraform"
     Owner      = var.user_name
+    division   = "engineering"
+    org        = "obs"
+    team       = "observability"
+    project    = "apm-server"
   }
 }
