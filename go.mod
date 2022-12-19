f1f24485295941c3b6605cc93be7ae25ab03b995
Andrew Wilkins <axw@elastic.co>
2022-12-19 13:48:34 +0800

    Update to Go 1.19
    Update the minimum Go version to 1.19 in go.mod,
    and update .go-version with the specific Go patch
    release. We continue to use a specific patch
    release to ensure builds are reproducible.

--- go.mod
+++ go.mod
@@ -1,6 +1,6 @@
 module github.com/elastic/apm-server
 
-go 1.18
+go 1.19
 
 require (
 	github.com/cespare/xxhash/v2 v2.1.2
--- script/common.bash
+++ script/common.bash
@@ -23,89 +23,22 @@ err()
   echo "ERROR: $1" >&2
 }
 
-# get_go_version
-# Read the project's Go version and return it in the GO_VERSION variable.
-# On failure it will exit.
-get_go_version() {
-  GO_VERSION=$(cat "${_sdir}/../.go-version")
-  if [ -z "$GO_VERSION" ]; then
-    err "Failed to detect the project's Go version"
-    exit 1
-  fi
-}
-
-# setup_go_root "version"
-# This configures the Go version being used. It sets GOROOT and adds
-# GOROOT/bin to the PATH. It uses gimme to download the Go version if
-# it does not already exist in the ~/.gimme dir.
-setup_go_root() {
-  local version=${1}
-
-  # Use the current Go installation if the given Go version is already
-  # installed and configured.
-  if command -v go &>/dev/null ; then
-    debug "Found Go. Checking version..."
-    FOUND_GO_VERSION=$(go version|awk '{print $3}'|sed s/go//)
-    if [ "$FOUND_GO_VERSION" == "$version" ] ; then
-      debug "Versions match. No need to install Go. Exiting."
-      FOUND_GO="true"
-    fi
-  fi
-
-  # Install Go with gimme in case the given Go version is not
-  # installed.
-  if [ -z $FOUND_GO ] ; then
-    # Setup GOROOT and add go to the PATH.
-    GIMME=${_sdir}/gimme/gimme
-    debug "Gimme version $(${GIMME} version)"
-    ${GIMME} "${version}" > /dev/null
-    source "${HOME}/.gimme/envs/go${version}.env" 2> /dev/null
-  fi
-
-  debug "$(go version)"
-}
-
-# setup_go_path "gopath"
-# This sets GOPATH and adds GOPATH/bin to the PATH.
-setup_go_path() {
-  local gopath="${1}"
-  if [ -z "$gopath" ]; then return; fi
-
-  # Setup GOPATH.
-  export GOPATH="${gopath}"
-
-  # Add GOPATH to PATH.
-  export PATH="${GOPATH}/bin:${PATH}"
-
-  debug "GOPATH=${GOPATH}"
+# setup_go uses gimme to download Go, if needed, based on the
+# Go version defined in .go-version, and sets up $PATH.
+setup_go() {
+  export GO_VERSION=$(cat $_sdir/../.go-version)
+  OUTPUT=$(eval "$($_sdir/gimme/gimme $GO_VERSION)" 2>&1)
+  debug "$OUTPUT"
 }
 
 jenkins_setup() {
   : "${HOME:?Need to set HOME to a non-empty value.}"
   : "${WORKSPACE:?Need to set WORKSPACE to a non-empty value.}"
 
-  if [ -z ${GO_VERSION:-} ]; then
-    get_go_version
-  fi
-
-  # Setup Go.
-  export GOPATH=${WORKSPACE}
-  export PATH=${GOPATH}/bin:${PATH}
-  eval "$(gvm ${GO_VERSION})"
+  setup_go
 
   # Workaround for Python virtualenv path being too long.
   export TEMP_PYTHON_ENV=$(mktemp -d)
   export PYTHON_ENV="${TEMP_PYTHON_ENV}/python-env"
   export PATH=${PYTHON_ENV}/build/ve/linux/bin:${PATH}
-}
-
-docker_setup() {
-  OS="$(uname)"
-  case $OS in
-    'Darwin')
-      # Start the docker machine VM (ignore error if it's already running).
-      docker-machine start default || true
-      eval $(docker-machine env default)
-      ;;
-  esac
 }
