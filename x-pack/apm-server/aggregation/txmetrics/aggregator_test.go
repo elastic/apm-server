0aeec9e57276e3613ef626311ed3030f979d5cc4
Carson Ip <carson.ip@elastic.co>
2023-01-04 17:11:21 +0000

    Add `transaction.duration.summary` to transaction metrics

--- x-pack/apm-server/aggregation/txmetrics/aggregator_test.go
+++ x-pack/apm-server/aggregation/txmetrics/aggregator_test.go
@@ -594,6 +594,10 @@ func TestAggregationFields(t *testing.T) {
 				Counts: []int64{expectedCount},
 				Values: []float64{0},
 			},
+			DurationSummary: model.SummaryMetric{
+				Count: expectedCount,
+				Sum:   0,
+			},
 		}
 		expected = append(expected, expectedEvent)
 	}
@@ -711,6 +715,10 @@ func createOverflowMetricset(overflowCount, repCount int, txnDuration time.Durat
 			DurationHistogram: model.Histogram{
 				Counts: []int64{int64(overflowCount * repCount)},
 				Values: []float64{float64(txnDuration.Microseconds())},
+			},
+			DurationSummary: model.SummaryMetric{
+				Count: int64(overflowCount * repCount),
+				Sum:   float64(txnDuration.Microseconds()),
 			},
 		},
 		Metricset: &model.Metricset{
