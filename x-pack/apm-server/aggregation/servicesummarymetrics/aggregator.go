334543c42ec33afed161191f8844345b5de480a6
Carson Ip <carson.ip@elastic.co>
2023-01-12 15:02:42 +0000

    Add service summary metrics aggregation

---
+++ x-pack/apm-server/aggregation/servicesummarymetrics/aggregator.go
@@ -0,0 +1,264 @@
+// Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+// or more contributor license agreements. Licensed under the Elastic License 2.0;
+// you may not use this file except in compliance with the Elastic License 2.0.
+
+package servicesummarymetrics
+
+import (
+	"context"
+	"encoding/binary"
+	"sync"
+	"time"
+
+	"github.com/cespare/xxhash/v2"
+	"github.com/pkg/errors"
+
+	"github.com/elastic/elastic-agent-libs/logp"
+
+	"github.com/elastic/apm-data/model"
+	"github.com/elastic/apm-server/internal/logs"
+	"github.com/elastic/apm-server/x-pack/apm-server/aggregation/baseaggregator"
+	"github.com/elastic/apm-server/x-pack/apm-server/aggregation/interval"
+	"github.com/elastic/apm-server/x-pack/apm-server/aggregation/labels"
+)
+
+const (
+	metricsetName = "service_summary"
+)
+
+// AggregatorConfig holds configuration for creating an Aggregator.
+type AggregatorConfig struct {
+	// BatchProcessor is a model.BatchProcessor for asynchronously
+	// processing metrics documents.
+	BatchProcessor model.BatchProcessor
+
+	// Logger is the logger for logging metrics aggregation/publishing.
+	//
+	// If Logger is nil, a new logger will be constructed.
+	Logger *logp.Logger
+
+	// RollUpIntervals are additional MetricsInterval for the aggregator to
+	// compute and publish metrics for. Each additional interval is constrained
+	// to the same rules as MetricsInterval, and will result in additional
+	// memory to be allocated.
+	RollUpIntervals []time.Duration
+
+	// Interval is the interval between publishing of aggregated metrics.
+	// There may be additional metrics reported at arbitrary times if the
+	// aggregation groups fill up.
+	Interval time.Duration
+
+	// MaxGroups is the maximum number of distinct service summary metrics to
+	// store within an aggregation period.
+	// Once this number of groups is reached, any new aggregation keys will cause
+	// individual metrics documents to be immediately published.
+	MaxGroups int
+}
+
+// Validate validates the aggregator config.
+func (config AggregatorConfig) Validate() error {
+	if config.BatchProcessor == nil {
+		return errors.New("BatchProcessor unspecified")
+	}
+	if config.MaxGroups <= 0 {
+		return errors.New("MaxGroups unspecified or negative")
+	}
+	return nil
+}
+
+// Aggregator aggregates all events, periodically publishing service summary metrics.
+type Aggregator struct {
+	*baseaggregator.Aggregator
+
+	config AggregatorConfig
+
+	mu sync.RWMutex
+	// These two metricsBuffer are set to the same size and act as buffers
+	// for caching and then publishing the metrics as batches.
+	active, inactive map[time.Duration]*metricsBuffer
+}
+
+// NewAggregator returns a new Aggregator with the given config.
+func NewAggregator(config AggregatorConfig) (*Aggregator, error) {
+	if err := config.Validate(); err != nil {
+		return nil, errors.Wrap(err, "invalid aggregator config")
+	}
+	if config.Logger == nil {
+		config.Logger = logp.NewLogger(logs.ServiceSummaryMetrics)
+	}
+	aggregator := Aggregator{
+		config:   config,
+		active:   make(map[time.Duration]*metricsBuffer),
+		inactive: make(map[time.Duration]*metricsBuffer),
+	}
+	base, err := baseaggregator.New(baseaggregator.AggregatorConfig{
+		PublishFunc:     aggregator.publish, // inject local publish
+		Logger:          config.Logger,
+		Interval:        config.Interval,
+		RollUpIntervals: config.RollUpIntervals,
+	})
+	if err != nil {
+		return nil, err
+	}
+	aggregator.Aggregator = base
+	for _, interval := range aggregator.Intervals {
+		aggregator.active[interval] = newMetricsBuffer(config.MaxGroups)
+		aggregator.inactive[interval] = newMetricsBuffer(config.MaxGroups)
+	}
+	return &aggregator, nil
+}
+
+func (a *Aggregator) publish(ctx context.Context, period time.Duration) error {
+	// We hold a.mu only long enough to swap the serviceSummaryMetrics. This will
+	// be blocked by serviceSummaryMetrics updates, which is OK, as we prefer not
+	// to block serviceSummaryMetrics updaters. After the lock is released nothing
+	// will be accessing a.inactive.
+	a.mu.Lock()
+
+	// EXPLAIN: We swap active <-> inactive, so that we're only working on the
+	// inactive property while publish is running. `a.active` is the buffer that
+	// receives/stores/updates the metricsets, once swapped, we're working on the
+	// `a.inactive` which we're going to process and publish.
+	current := a.active[period]
+	a.active[period], a.inactive[period] = a.inactive[period], current
+	a.mu.Unlock()
+
+	size := len(current.m)
+	if size == 0 {
+		a.config.Logger.Debugf("no service summary metrics to publish")
+		return nil
+	}
+
+	intervalStr := interval.FormatDuration(period)
+	batch := make(model.Batch, 0, size)
+	for key, aggKey := range current.m {
+		m := makeMetricset(aggKey, intervalStr)
+		batch = append(batch, m)
+		delete(current.m, key)
+	}
+	a.config.Logger.Debugf("publishing %d metricsets", len(batch))
+	return a.config.BatchProcessor.ProcessBatch(ctx, &batch)
+}
+
+// ProcessBatch aggregates all service summary metrics.
+func (a *Aggregator) ProcessBatch(ctx context.Context, b *model.Batch) error {
+	a.mu.RLock()
+	defer a.mu.RUnlock()
+	for _, event := range *b {
+		// Not filtering on Processor.
+		a.processEvent(&event, b)
+	}
+	return nil
+}
+
+func (a *Aggregator) processEvent(event *model.APMEvent, b *model.Batch) {
+	for _, period := range a.Intervals {
+		key := makeAggregationKey(event, period)
+		if !a.active[period].storeOrUpdate(key, a.config.Logger) {
+			intervalStr := interval.FormatDuration(period)
+			*b = append(*b, makeMetricset(key, intervalStr))
+		}
+	}
+}
+
+type metricsBuffer struct {
+	mu sync.RWMutex
+	m  map[uint64]aggregationKey
+
+	maxSize int
+}
+
+func newMetricsBuffer(maxSize int) *metricsBuffer {
+	return &metricsBuffer{
+		maxSize: maxSize,
+		m:       make(map[uint64]aggregationKey),
+	}
+}
+
+func (mb *metricsBuffer) storeOrUpdate(
+	key aggregationKey,
+	logger *logp.Logger,
+) bool {
+	hash := key.hash()
+
+	mb.mu.Lock()
+	defer mb.mu.Unlock()
+
+	_, ok := mb.m[hash]
+	if !ok {
+		n := len(mb.m)
+		half := mb.maxSize / 2
+
+		switch n {
+		case mb.maxSize:
+			return false
+		case half - 1:
+			logger.Warn("service summary groups reached 50% capacity")
+		case mb.maxSize - 1:
+			logger.Warn("service summary groups reached 100% capacity")
+		}
+		mb.m[hash] = key
+	}
+	return true
+}
+
+type aggregationKey struct {
+	labels.AggregatedGlobalLabels
+	comparable
+}
+
+func (k *aggregationKey) hash() uint64 {
+	var h xxhash.Digest
+	var buf [8]byte
+	binary.LittleEndian.PutUint64(buf[:], uint64(k.timestamp.UnixNano()))
+	h.Write(buf[:])
+
+	k.AggregatedGlobalLabels.Write(&h)
+	h.WriteString(k.agentName)
+	h.WriteString(k.serviceEnvironment)
+	h.WriteString(k.serviceName)
+	return h.Sum64()
+}
+
+type comparable struct {
+	timestamp time.Time
+
+	agentName          string
+	serviceName        string
+	serviceEnvironment string
+}
+
+func makeAggregationKey(event *model.APMEvent, interval time.Duration) aggregationKey {
+	key := aggregationKey{
+		comparable: comparable{
+			// Group metrics by time interval.
+			timestamp: event.Timestamp.Truncate(interval),
+
+			agentName:          event.Agent.Name,
+			serviceName:        event.Service.Name,
+			serviceEnvironment: event.Service.Environment,
+		},
+	}
+	key.AggregatedGlobalLabels.Read(event)
+	return key
+}
+
+func makeMetricset(key aggregationKey, interval string) model.APMEvent {
+	return model.APMEvent{
+		Timestamp: key.timestamp,
+		Service: model.Service{
+			Name:        key.serviceName,
+			Environment: key.serviceEnvironment,
+		},
+		Agent: model.Agent{
+			Name: key.agentName,
+		},
+		Labels:        key.Labels,
+		NumericLabels: key.NumericLabels,
+		Processor:     model.MetricsetProcessor,
+		Metricset: &model.Metricset{
+			Name:     metricsetName,
+			Interval: interval,
+		},
+	}
+}
---
+++ x-pack/apm-server/aggregation/servicesummarymetrics/aggregator_test.go
@@ -0,0 +1,318 @@
+// Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+// or more contributor license agreements. Licensed under the Elastic License 2.0;
+// you may not use this file except in compliance with the Elastic License 2.0.
+
+package servicesummarymetrics
+
+import (
+	"context"
+	"fmt"
+	"net/netip"
+	"sort"
+	"sync"
+	"testing"
+	"time"
+
+	"github.com/google/go-cmp/cmp"
+	"github.com/google/go-cmp/cmp/cmpopts"
+
+	"go.opentelemetry.io/collector/pdata/plog"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+
+	"github.com/elastic/apm-data/model"
+)
+
+func TestNewAggregatorConfigInvalid(t *testing.T) {
+	report := makeErrBatchProcessor(nil)
+
+	type test struct {
+		config AggregatorConfig
+		err    string
+	}
+
+	for _, test := range []test{{
+		config: AggregatorConfig{},
+		err:    "BatchProcessor unspecified",
+	}, {
+		config: AggregatorConfig{
+			BatchProcessor: report,
+		},
+		err: "MaxGroups unspecified or negative",
+	}, {
+		config: AggregatorConfig{
+			BatchProcessor: report,
+			MaxGroups:      1,
+		},
+		err: "Interval unspecified or negative",
+	}} {
+		agg, err := NewAggregator(test.config)
+		require.Error(t, err)
+		require.Nil(t, agg)
+		assert.EqualError(t, err, "invalid aggregator config: "+test.err)
+	}
+}
+
+func TestAggregatorRun(t *testing.T) {
+	batches := make(chan model.Batch, 3)
+	config := AggregatorConfig{
+		BatchProcessor:  makeChanBatchProcessor(batches),
+		Interval:        1 * time.Millisecond,
+		RollUpIntervals: []time.Duration{200 * time.Millisecond, time.Second},
+		MaxGroups:       1000,
+	}
+	agg, err := NewAggregator(config)
+	require.NoError(t, err)
+
+	apmEvents := []model.APMEvent{
+		{
+			Agent:   model.Agent{Name: "java"},
+			Service: model.Service{Name: "backend"},
+			Event: model.Event{
+				Outcome:  "success",
+				Duration: time.Millisecond,
+			},
+			Processor: model.TransactionProcessor,
+			Transaction: &model.Transaction{
+				Name:                "transaction_name",
+				Type:                "request",
+				RepresentativeCount: 1,
+			},
+			Labels: model.Labels{
+				"department_name": model.LabelValue{Global: true, Value: "apm"},
+				"organization":    model.LabelValue{Global: true, Value: "observability"},
+				"company":         model.LabelValue{Global: true, Value: "elastic"},
+			},
+			NumericLabels: model.NumericLabels{
+				"user_id":     model.NumericLabelValue{Global: true, Value: 100},
+				"cost_center": model.NumericLabelValue{Global: true, Value: 10},
+			},
+		},
+		{
+			Processor: model.LogProcessor,
+			Agent: model.Agent{
+				Name:    "java",
+				Version: "unknown",
+			},
+			Service: model.Service{
+				Name:     "backend",
+				Language: model.Language{Name: "java"},
+			},
+			Message: "a random log message",
+			Event: model.Event{
+				Severity: int64(plog.SeverityNumberInfo),
+			},
+			Log:   model.Log{Level: "Info"},
+			Span:  &model.Span{ID: "0200000000000000"},
+			Trace: model.Trace{ID: "01000000000000000000000000000000"},
+			Labels: model.Labels{
+				"department_name": model.LabelValue{Global: true, Value: "apm"},
+				"organization":    model.LabelValue{Global: true, Value: "observability"},
+				"company":         model.LabelValue{Global: true, Value: "elastic"},
+			},
+			NumericLabels: model.NumericLabels{
+				"user_id":     model.NumericLabelValue{Global: true, Value: 100},
+				"cost_center": model.NumericLabelValue{Global: true, Value: 10},
+			},
+		},
+		{
+			Processor: model.ErrorProcessor,
+			Agent: model.Agent{
+				Name: "go",
+			},
+			Service: model.Service{
+				Name:     "backend",
+				Language: model.Language{Name: "go"},
+			},
+		},
+		{
+			Processor: model.MetricsetProcessor,
+			Agent: model.Agent{
+				Name: "go",
+			},
+			Service: model.Service{
+				Name:        "backend",
+				Environment: "dev",
+				Language:    model.Language{Name: "go"},
+			},
+		},
+		{
+			Processor: model.SpanProcessor,
+			Agent: model.Agent{
+				Name: "js-base",
+			},
+			Service: model.Service{
+				Name: "frontend",
+			},
+		},
+	}
+
+	var wg sync.WaitGroup
+	for _, in := range apmEvents {
+		wg.Add(1)
+		go func(in model.APMEvent) {
+			defer wg.Done()
+			batch := model.Batch{in}
+			err := agg.ProcessBatch(context.Background(), &batch)
+			require.NoError(t, err)
+			assert.Equal(t, model.Batch{in}, batch)
+		}(in)
+	}
+	wg.Wait()
+
+	// Start the aggregator after processing to ensure metrics are aggregated deterministically.
+	go agg.Run()
+	defer agg.Stop(context.Background())
+	// Stop the aggregator to ensure all metrics are published.
+	assert.NoError(t, agg.Stop(context.Background()))
+
+	for _, interval := range append([]time.Duration{config.Interval}, config.RollUpIntervals...) {
+		batch := expectBatch(t, batches)
+		metricsets := batchMetricsets(t, batch)
+		expected := []model.APMEvent{{
+			Processor: model.MetricsetProcessor,
+			Metricset: &model.Metricset{
+				Name: "service_summary", Interval: fmt.Sprintf("%.0fs", interval.Seconds()),
+			},
+			Service: model.Service{Name: "backend"},
+			Agent:   model.Agent{Name: "java"},
+			Labels: model.Labels{
+				"department_name": model.LabelValue{Value: "apm"},
+				"organization":    model.LabelValue{Value: "observability"},
+				"company":         model.LabelValue{Value: "elastic"},
+			},
+			NumericLabels: model.NumericLabels{
+				"user_id":     model.NumericLabelValue{Value: 100},
+				"cost_center": model.NumericLabelValue{Value: 10},
+			},
+		}, {
+			Processor: model.MetricsetProcessor,
+			Metricset: &model.Metricset{
+				Name: "service_summary", Interval: fmt.Sprintf("%.0fs", interval.Seconds()),
+			},
+			Service: model.Service{Name: "backend"},
+			Agent:   model.Agent{Name: "go"},
+		}, {
+			Processor: model.MetricsetProcessor,
+			Metricset: &model.Metricset{
+				Name: "service_summary", Interval: fmt.Sprintf("%.0fs", interval.Seconds()),
+			},
+			Service: model.Service{Name: "backend", Environment: "dev"},
+			Agent:   model.Agent{Name: "go"},
+		}, {
+			Processor: model.MetricsetProcessor,
+			Metricset: &model.Metricset{
+				Name: "service_summary", Interval: fmt.Sprintf("%.0fs", interval.Seconds()),
+			},
+			Service: model.Service{Name: "frontend"},
+			Agent:   model.Agent{Name: "js-base"},
+		}}
+
+		assert.Equal(t, len(expected), len(metricsets))
+		out := cmp.Diff(expected, metricsets, cmpopts.IgnoreTypes(netip.Addr{}), cmpopts.SortSlices(func(e1 model.APMEvent, e2 model.APMEvent) bool {
+			if e1.Agent.Name != e2.Agent.Name {
+				return e1.Agent.Name < e2.Agent.Name
+			}
+
+			return e1.Service.Environment < e2.Service.Environment
+		}))
+		assert.Empty(t, out)
+	}
+
+	select {
+	case <-batches:
+		t.Fatal("unexpected publish")
+	case <-time.After(100 * time.Millisecond):
+	}
+}
+
+func TestAggregateTimestamp(t *testing.T) {
+	batches := make(chan model.Batch, 1)
+	agg, err := NewAggregator(AggregatorConfig{
+		BatchProcessor: makeChanBatchProcessor(batches),
+		Interval:       30 * time.Second,
+		MaxGroups:      1000,
+	})
+	require.NoError(t, err)
+
+	t0 := time.Unix(0, 0)
+	for _, ts := range []time.Time{t0, t0.Add(15 * time.Second), t0.Add(30 * time.Second)} {
+		transaction := makeTransaction("service_name", "agent_name", "tx_type", "success", 100*time.Millisecond, 1)
+		transaction.Timestamp = ts
+		batch := model.Batch{transaction}
+		err = agg.ProcessBatch(context.Background(), &batch)
+		require.NoError(t, err)
+		assert.Empty(t, batchMetricsets(t, batch))
+	}
+
+	go agg.Run()
+	err = agg.Stop(context.Background()) // stop to flush
+	require.NoError(t, err)
+
+	batch := expectBatch(t, batches)
+	metricsets := batchMetricsets(t, batch)
+	require.Len(t, metricsets, 2)
+	sort.Slice(metricsets, func(i, j int) bool {
+		return metricsets[i].Timestamp.Before(metricsets[j].Timestamp)
+	})
+	assert.Equal(t, t0, metricsets[0].Timestamp)
+	assert.Equal(t, t0.Add(30*time.Second), metricsets[1].Timestamp)
+}
+
+func makeTransaction(
+	serviceName, agentName, transactionType, outcome string,
+	duration time.Duration, count float64,
+) model.APMEvent {
+	return model.APMEvent{
+		Agent:   model.Agent{Name: agentName},
+		Service: model.Service{Name: serviceName},
+		Event: model.Event{
+			Outcome:  outcome,
+			Duration: duration,
+		},
+		Processor: model.TransactionProcessor,
+		Transaction: &model.Transaction{
+			Name:                "transaction_name",
+			Type:                transactionType,
+			RepresentativeCount: count,
+		},
+	}
+}
+
+func makeErrBatchProcessor(err error) model.BatchProcessor {
+	return model.ProcessBatchFunc(func(context.Context, *model.Batch) error { return err })
+}
+
+func makeChanBatchProcessor(ch chan<- model.Batch) model.BatchProcessor {
+	return model.ProcessBatchFunc(func(ctx context.Context, batch *model.Batch) error {
+		select {
+		case <-ctx.Done():
+			return ctx.Err()
+		case ch <- *batch:
+			return nil
+		}
+	})
+}
+
+func expectBatch(t *testing.T, ch <-chan model.Batch) model.Batch {
+	t.Helper()
+	select {
+	case batch := <-ch:
+		return batch
+	case <-time.After(time.Second * 5):
+		t.Fatal("expected publish")
+	}
+	panic("unreachable")
+}
+
+func batchMetricsets(t testing.TB, batch model.Batch) []model.APMEvent {
+	var metricsets []model.APMEvent
+	for _, event := range batch {
+		if event.Metricset == nil {
+			continue
+		}
+		metricsets = append(metricsets, event)
+	}
+	return metricsets
+}
