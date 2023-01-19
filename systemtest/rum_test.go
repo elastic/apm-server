020f869a7146d51b87b6468b839fa8d4a75f271e
kruskal <99559985+kruskall@users.noreply.github.com>
2023-01-19 04:40:26 +0100

    test: update RUM test to use standalone APM server

--- systemtest/rum_test.go
+++ systemtest/rum_test.go
@@ -193,17 +193,23 @@ func TestRUMCORS(t *testing.T) {
 	assert.Equal(t, "stick, door, Content-Type, Content-Encoding, Accept", resp.Header.Get("Access-Control-Allow-Headers"))
 }
 
-func TestRUMRoutingIntegration(t *testing.T) {
+func TestRUMRouting(t *testing.T) {
 	// This test asserts that the events that are coming from the RUM JS agent
 	// are sent to the appropriate datastream.
 	systemtest.CleanupElasticsearch(t)
-	apmIntegration := newAPMIntegration(t, nil)
+
+	srv := apmservertest.NewUnstartedServerTB(t)
+	srv.Config.RUM = &apmservertest.RUMConfig{
+		Enabled:      true,
+	}
+	err := srv.Start()
+	require.NoError(t, err)
 
 	body, err := os.Open(filepath.Join("..", "testdata", "intake-v3", "rum_events.ndjson"))
 	require.NoError(t, err)
 	defer body.Close()
 
-	req, err := http.NewRequest("POST", apmIntegration.URL+"/intake/v3/rum/events", body)
+	req, err := http.NewRequest("POST", srv.URL+"/intake/v3/rum/events", body)
 	require.NoError(t, err)
 	req.Header.Add("Content-Type", "application/x-ndjson")
 
