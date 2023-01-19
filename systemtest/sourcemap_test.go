b881da387a50b6695ef079c4d81825245f01ba42
kruskal <99559985+kruskall@users.noreply.github.com>
2023-01-19 11:07:36 +0100

    test: only run absolute and relative bundle filepath test on standalone

--- systemtest/sourcemap_test.go
+++ systemtest/sourcemap_test.go
@@ -31,39 +31,26 @@ import (
 
 func TestRUMErrorSourcemapping(t *testing.T) {
 	test := func(t *testing.T, bundleFilepath string) {
+		systemtest.CleanupElasticsearch(t)
+
 		sourcemap, err := os.ReadFile("../testdata/sourcemap/bundle.js.map")
 		require.NoError(t, err)
 		systemtest.CreateSourceMap(t, string(sourcemap), "apm-agent-js", "1.0.1", bundleFilepath)
 
-		// Create the integration after uploading the sourcemap, so that it is added
-		// to the integration policy from the start. Otherwise we would have to wait
-		// for the policy to be reloaded.
-		apmIntegration := newAPMIntegration(t, map[string]interface{}{"enable_rum": true})
+		srv := apmservertest.NewUnstartedServerTB(t)
+		srv.Config.RUM = &apmservertest.RUMConfig{Enabled: true}
+		err = srv.Start()
+		require.NoError(t, err)
 
-		test := func(t *testing.T, serverURL string) {
-			systemtest.CleanupElasticsearch(t)
-			systemtest.SendRUMEventsPayload(t, serverURL, "../testdata/intake-v2/errors_rum.ndjson")
-			result := systemtest.Elasticsearch.ExpectDocs(t, "logs-apm.error-*", nil)
-			systemtest.ApproveEvents(
-				t, t.Name(), result.Hits.Hits,
-				// RUM timestamps are set by the server based on the time the payload is received.
-				"@timestamp", "timestamp.us",
-				// RUM events have the source IP and port recorded, which are dynamic in the tests
-				"client.ip", "source.ip", "source.port",
-			)
-		}
-
-		t.Run("standalone", func(t *testing.T) {
-			srv := apmservertest.NewUnstartedServerTB(t)
-			srv.Config.RUM = &apmservertest.RUMConfig{Enabled: true}
-			err := srv.Start()
-			require.NoError(t, err)
-			test(t, srv.URL)
-		})
-
-		t.Run("integration", func(t *testing.T) {
-			test(t, apmIntegration.URL)
-		})
+		systemtest.SendRUMEventsPayload(t, srv.URL, "../testdata/intake-v2/errors_rum.ndjson")
+		result := systemtest.Elasticsearch.ExpectDocs(t, "logs-apm.error-*", nil)
+		systemtest.ApproveEvents(
+			t, t.Name(), result.Hits.Hits,
+			// RUM timestamps are set by the server based on the time the payload is received.
+			"@timestamp", "timestamp.us",
+			// RUM events have the source IP and port recorded, which are dynamic in the tests
+			"client.ip", "source.ip", "source.port",
+		)
 	}
 
 	t.Run("absolute_bundle_filepath", func(t *testing.T) {
