55f95c9945cd563261837d1e5e1327297888dceb
kruskal <99559985+kruskall@users.noreply.github.com>
2023-02-06 14:21:21 +0100

    fix: create a new context when falling back in the chained fetcher

--- internal/sourcemap/chained.go
+++ internal/sourcemap/chained.go
@@ -20,6 +20,7 @@ package sourcemap
 import (
 	"context"
 	"errors"
+	"time"
 
 	"github.com/go-sourcemap/sourcemap"
 )
@@ -41,6 +42,13 @@ func (c ChainedFetcher) Fetch(ctx context.Context, name, version, path string) (
 		// Return the result: error and consumer.
 		if !errors.Is(err, errFetcherUnvailable) {
 			return consumer, err
+		}
+
+		// previous fetcher is unavailable but the deadline expired so we cannot reuse that
+		if t, _ := ctx.Deadline(); t.Before(time.Now()) {
+			var cancel context.CancelFunc
+			ctx, cancel = context.WithTimeout(context.Background(), 500 * time.Millisecond)
+			defer cancel()
 		}
 
 		// err is errFetcherUnvailable
