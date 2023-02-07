039aea49cbd5c6abcc50298e6557e15212e33a04
Jan Calanog <jan.calanog@elastic.co>
2023-02-07 12:47:01 +0100

    Use json source in updatecli

--- .ci/scripts/get-latest-beats-sha.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-set -euo pipefail
-
-BEATS_BRANCH=$(grep "BEATS_VERSION?=" Makefile | awk 'awk BEGIN { FS = "=" } ; { print $2 }')
-
-curl "https://api.github.com/repos/elastic/beats/commits?sha=${BEATS_BRANCH}" | jq --raw-output '.[0].sha' | xargs | cut -c 1-12
