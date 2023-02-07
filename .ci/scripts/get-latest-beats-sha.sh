a68316e6e8c4feae6bd4c9c93d02dd9416c28a62
Jan Calanog <jan.calanog@elastic.co>
2023-02-07 09:41:31 +0100

    test

--- .ci/scripts/get-latest-beats-sha.sh
+++ .ci/scripts/get-latest-beats-sha.sh
@@ -4,4 +4,4 @@ set -euo pipefail
 
 BEATS_BRANCH=$(grep "BEATS_VERSION?=" Makefile | awk 'awk BEGIN { FS = "=" } ; { print $2 }')
 
-curl "https://api.github.com/repos/reakaleek/beats/commits?sha=${BEATS_BRANCH}" | jq --raw-output '.[0].sha' | xargs | cut -c 1-12
+curl "https://api.github.com/repos/elastic/beats/commits?sha=${BEATS_BRANCH}" | jq --raw-output '.[0].sha' | xargs | cut -c 1-12
