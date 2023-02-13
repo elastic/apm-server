fbc78171ecebba32e53fd494613a01f19b9ffc9f
Jan Calanog <jan.calanog@elastic.co>
2023-02-13 10:00:53 +0100

    Resolve conflicts

--- .ci/scripts/update-beats.sh
+++ .ci/scripts/update-beats.sh
@@ -5,21 +5,5 @@
 #
 set -euo pipefail
 
-<<<<<<< HEAD
-make update-beats
-COMMIT_MESSAGE="Update to elastic/beats@$(go list -m -f {{.Version}} github.com/elastic/beats/... | cut -d- -f3)"
-
-git checkout -b "update-beats-$(date "+%Y%m%d%H%M%S")"
-git add --ignore-errors go.mod go.sum NOTICE.txt \
-	.go-version docs/version.asciidoc \
-	include/fields.go x-pack/apm-server/include/fields.go
-
-find . -maxdepth 2 -name Dockerfile -exec git add {} \;
-
-git diff --staged --quiet || git commit -m "$COMMIT_MESSAGE"
-git --no-pager log -1
-
-echo "You can now push and create a Pull Request"
-=======
 make update-beats BEATS_VERSION="${1}"
->>>>>>> 956a4a308 (Migrate update-beats pipeline to updatecli and GH actions workflow (#10179))
+
