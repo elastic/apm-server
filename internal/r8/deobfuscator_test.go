370fa07c438ff7a89322c489824123ac88d8aea0
Cesar Munoz <cesar.munozquintero@elastic.co>
2023-03-20 15:18:10 +0100

    Adding deobfuscator tests

---
+++ internal/r8/deobfuscator_test.go
@@ -0,0 +1,47 @@
+package r8
+
+import (
+    "fmt"
+    "testing"
+    "os"
+    "io"
+    "log"
+    "github.com/stretchr/testify/assert"
+)
+
+func TestDeobfuscation(t *testing.T) {
+    cases := []string {
+        "../../testdata/r8/deobfuscator/1",
+        "../../testdata/r8/deobfuscator/2",
+        "../../testdata/r8/deobfuscator/3",
+    }
+
+    for _, c := range cases {
+        inputPath := c + "/obfuscated-crash.txt"
+        expectedOutputPath := c + "/de-obfuscated-crash.txt"
+        mapFilePath := c + "/mapping.txt"
+        t.Run(fmt.Sprintf("(%s)->(%s)", inputPath, expectedOutputPath), func (t *testing.T) {
+            obfuscated := readFile(inputPath)
+            expected := readFile(expectedOutputPath)
+            deobfuscated := Deobfuscate(&obfuscated, mapFilePath)
+            assert.Equal(t, expected, deobfuscated)
+        })
+    }
+}
+
+func readFile(path string) string {
+    file, err := os.Open(path)
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    defer file.Close()
+
+    bytes, err := io.ReadAll(file)
+
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    return string(bytes)
+}
