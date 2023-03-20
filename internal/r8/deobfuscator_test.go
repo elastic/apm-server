65c71df9ee77ea266099c2cd7b78be6de8215a46
Cesar Munoz <cesar.munozquintero@elastic.co>
2023-03-20 16:44:30 +0100

    Sorting imports and updating formatting

--- internal/r8/deobfuscator_test.go
+++ internal/r8/deobfuscator_test.go
@@ -1,47 +1,47 @@
 package r8
 
 import (
-    "fmt"
-    "testing"
-    "os"
-    "io"
-    "log"
-    "github.com/stretchr/testify/assert"
+	"fmt"
+	"github.com/stretchr/testify/assert"
+	"io"
+	"log"
+	"os"
+	"testing"
 )
 
 func TestDeobfuscation(t *testing.T) {
-    cases := []string {
-        "../../testdata/r8/deobfuscator/1",
-        "../../testdata/r8/deobfuscator/2",
-        "../../testdata/r8/deobfuscator/3",
-    }
+	cases := []string{
+		"../../testdata/r8/deobfuscator/1",
+		"../../testdata/r8/deobfuscator/2",
+		"../../testdata/r8/deobfuscator/3",
+	}
 
-    for _, c := range cases {
-        inputPath := c + "/obfuscated-crash.txt"
-        expectedOutputPath := c + "/de-obfuscated-crash.txt"
-        mapFilePath := c + "/mapping.txt"
-        t.Run(fmt.Sprintf("(%s)->(%s)", inputPath, expectedOutputPath), func (t *testing.T) {
-            obfuscated := readFile(inputPath)
-            expected := readFile(expectedOutputPath)
-            deobfuscated := Deobfuscate(&obfuscated, mapFilePath)
-            assert.Equal(t, expected, deobfuscated)
-        })
-    }
+	for _, c := range cases {
+		inputPath := c + "/obfuscated-crash.txt"
+		expectedOutputPath := c + "/de-obfuscated-crash.txt"
+		mapFilePath := c + "/mapping.txt"
+		t.Run(fmt.Sprintf("(%s)->(%s)", inputPath, expectedOutputPath), func(t *testing.T) {
+			obfuscated := readFile(inputPath)
+			expected := readFile(expectedOutputPath)
+			deobfuscated := Deobfuscate(&obfuscated, mapFilePath)
+			assert.Equal(t, expected, deobfuscated)
+		})
+	}
 }
 
 func readFile(path string) string {
-    file, err := os.Open(path)
-    if err != nil {
-        log.Fatal(err)
-    }
+	file, err := os.Open(path)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-    defer file.Close()
+	defer file.Close()
 
-    bytes, err := io.ReadAll(file)
+	bytes, err := io.ReadAll(file)
 
-    if err != nil {
-        log.Fatal(err)
-    }
+	if err != nil {
+		log.Fatal(err)
+	}
 
-    return string(bytes)
+	return string(bytes)
 }
