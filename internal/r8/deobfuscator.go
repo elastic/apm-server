c6dc62747970ced99632b95ed60300e57b3b0f9b
LikeTheSalad <56847527+LikeTheSalad@users.noreply.github.com>
2023-03-31 16:48:26 +0200

    Adding R8 stacktrace deobfuscator (#10492)
    * Adding deobfuscator.go
    
    * Adding deobfuscator tests
    
    * Sorting imports and updating formatting
    
    * Adding comment for handling methods from the map with different range numbers
    
    * Using map instead of struct for stacktrace methods
    
    * Avoid using string pointer in the Deobfuscate function
    
    * Using raw strings to define regex
    
    * Making regex vars global
    
    * Using map to store unique types
    
    * Propagating errors to the caller
    
    * Ensuring all methods are provided after for loop ends
    
    * Avoiding to pass map pointers
    
    * Avoid reading regex indexes in a separate method from when the match was found
    
    * Using primitives for getKey function
    
    * Removing addMainCall
    
    * Using map for stacktrace types
    
    * Using os.ReadFile for tests
    
    * Removing .txt suffix from R8 test files
    
    * Updating test imports
    
    * Moving changelog update from 8.7.asciidoc to head.asciidoc
    
    * Update internal/r8/deobfuscator.go
    
    Co-authored-by: Carson Ip <carsonip@users.noreply.github.com>
    
    * Removing changelog entry
    
    * Update internal/r8/deobfuscator.go
    
    Co-authored-by: Carson Ip <carsonip@users.noreply.github.com>
    
    * Using os.Reader for map file
    
    * Update internal/r8/deobfuscator.go
    
    Co-authored-by: Carson Ip <carsonip@users.noreply.github.com>
    
    * Update internal/r8/deobfuscator.go
    
    Co-authored-by: Carson Ip <carsonip@users.noreply.github.com>
    
    * Update internal/r8/deobfuscator.go
    
    Co-authored-by: Carson Ip <carsonip@users.noreply.github.com>
    
    * Update internal/r8/deobfuscator.go
    
    Co-authored-by: Carson Ip <carsonip@users.noreply.github.com>
    
    * Update internal/r8/deobfuscator.go
    
    Co-authored-by: Carson Ip <carsonip@users.noreply.github.com>
    
    * Update internal/r8/deobfuscator.go
    
    Co-authored-by: Carson Ip <carsonip@users.noreply.github.com>
    
    * Renaming vars
    
    * Adding comments to Deobfuscate
    
    * Renaming vars
    
    * Renaming function
    
    * Adding comments to upsertMappedMethodCall
    
    * Renaming and commenting on mapLeftoverUnmappedMethods
    
    * Adding comment to method references
    
    * Adding gitattributes to avoid R8 test files to get their newlines turned to crlf on Windows
    
    * Adding license header to deobfuscator.go and deobfuscator_test.go
    
    * Update internal/r8/deobfuscator.go
    
    Co-authored-by: Carson Ip <carsonip@users.noreply.github.com>
    
    * Update internal/r8/deobfuscator.go
    
    Co-authored-by: Carson Ip <carsonip@users.noreply.github.com>
    
    * Update internal/r8/deobfuscator.go
    
    Co-authored-by: Carson Ip <carsonip@users.noreply.github.com>
    
    * Update internal/r8/deobfuscator.go
    
    Co-authored-by: Carson Ip <carsonip@users.noreply.github.com>
    
    * Update internal/r8/deobfuscator_test.go
    
    Co-authored-by: Carson Ip <carsonip@users.noreply.github.com>
    
    * Update internal/r8/deobfuscator_test.go
    
    Co-authored-by: Carson Ip <carsonip@users.noreply.github.com>
    
    * Updating tests based on reviews
    
    * Added comment for MappedMethodCall reference
    
    * Update internal/r8/deobfuscator.go
    
    Co-authored-by: Carson Ip <carsonip@users.noreply.github.com>
    
    * Renaming vars
    
    ---------
    
    Co-authored-by: Carson Ip <carsonip@users.noreply.github.com>

---
+++ internal/r8/deobfuscator.go
@@ -0,0 +1,209 @@
+// Licensed to Elasticsearch B.V. under one or more contributor
+// license agreements. See the NOTICE file distributed with
+// this work for additional information regarding copyright
+// ownership. Elasticsearch B.V. licenses this file to you under
+// the Apache License, Version 2.0 (the "License"); you may
+// not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+package r8
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"regexp"
+	"strings"
+)
+
+type StacktraceType struct {
+	name        string
+	indentation int
+	methods     map[string]string // Maps method references to their stacktrace call site
+}
+
+type MappedType struct {
+	obfuscated StacktraceType
+	realName   string
+}
+
+type MappedMethodCall struct {
+	reference string // For simple obfuscated methods, it's the method name. For multiline method calls, it's methodName:sourceFileNumber. E.g. for a method named "a" with (SourceFile:4) reference = "a:4"
+	key       string
+}
+
+type MethodMatch struct {
+	sourceFileStart      string
+	sourceFileEnd        string
+	methodRealName       string
+	methodObfuscatedName string
+}
+
+var (
+	symbolPattern     = regexp.MustCompile(`^\s*at (.+)\.(.+)\((.+)\)$`)
+	sourceFilePattern = regexp.MustCompile(`SourceFile:(\d+)`)
+	typePattern       = regexp.MustCompile(`^(\S+) -> (\S+):$`)
+	methodPattern     = regexp.MustCompile(`(?:(\d+):(\d+):)*\S+ (\S+)\(.*\)(?:[:\d]+)* -> (\S+)`)
+)
+
+// Deobfuscate parses the stacktrace looking for type names and their methods, then searches for those stacktrace items through the mapFile, looking
+// for their de-obfuscated names to later replace the ones in the original stacktrace by their real names found within the mapFile.
+func Deobfuscate(stacktrace string, mapFile io.Reader) (string, error) {
+	types, err := findUniqueTypes(stacktrace)
+	if err != nil {
+		return "", err
+	}
+	mapping, err := findMappingFor(types, mapFile)
+	if err != nil {
+		return "", err
+	}
+
+	deobfuscated := stacktrace
+	for k, v := range mapping {
+		// Uses ReplaceAll since an obfuscated name may be present several times in a single stacktrace.
+		deobfuscated = strings.ReplaceAll(deobfuscated, k, v)
+	}
+
+	return deobfuscated, nil
+}
+
+func findUniqueTypes(stacktrace string) (map[string]StacktraceType, error) {
+	var symbols = make(map[string]StacktraceType)
+	scanner := bufio.NewScanner(strings.NewReader(stacktrace))
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		indices := symbolPattern.FindStringSubmatchIndex(line)
+		if indices != nil {
+			typeIndex := indices[2]
+			typeName := line[typeIndex:indices[3]]
+			methodName := line[indices[4]:indices[5]]
+			callSite := line[indices[6]:indices[7]]
+			sourceFileMatch := sourceFilePattern.FindStringSubmatch(callSite)
+			if sourceFileMatch != nil {
+				// Sometimes a method call in the stacktrace might end with (SourceFile:N), where N is an int. When this happens,
+				// it means that the de-obfuscated version of this method starts with "N:N" in the map file. So in those cases,
+				// we append the N to the method name M so that M:N becomes a "method reference" that we can later spot
+				// when looping through the map file looking for the de-obfuscated names.
+				methodName = fmt.Sprintf("%s:%s", methodName, sourceFileMatch[1])
+			}
+			symbol, ok := symbols[typeName]
+			if !ok {
+				symbol = StacktraceType{
+					name:        typeName,
+					indentation: typeIndex + 1,
+					methods:     make(map[string]string),
+				}
+				symbols[typeName] = symbol
+			}
+			_, ok = symbol.methods[methodName]
+			if !ok {
+				symbol.methods[methodName] = callSite
+			}
+		}
+	}
+
+	if err := scanner.Err(); err != nil {
+		return nil, err
+	}
+
+	return symbols, nil
+}
+
+func findMappingFor(symbols map[string]StacktraceType, mapReader io.Reader) (map[string]string, error) {
+	mapping := make(map[string]string)
+	scanner := bufio.NewScanner(mapReader)
+	var currentType *MappedType
+	var currentMappedMethodCall *MappedMethodCall
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		typeMatch := typePattern.FindStringSubmatch(line)
+		if typeMatch != nil {
+			currentMappedMethodCall = nil
+			if currentType != nil {
+				mapLeftoverUnmappedMethods(mapping, currentType)
+			}
+			obfuscatedName := typeMatch[2]
+			stacktraceType, ok := symbols[obfuscatedName]
+			if ok {
+				currentType = &MappedType{stacktraceType, typeMatch[1]}
+			} else {
+				currentType = nil
+			}
+		} else if currentType != nil {
+			methodMatch := methodPattern.FindStringSubmatch(line)
+			if methodMatch != nil {
+				currentMappedMethodCall = upsertMappedMethodCall(mapping, MethodMatch{
+					sourceFileStart:      methodMatch[1],
+					sourceFileEnd:        methodMatch[2],
+					methodRealName:       methodMatch[3],
+					methodObfuscatedName: methodMatch[4],
+				}, currentType, currentMappedMethodCall)
+			}
+		}
+	}
+	if currentType != nil {
+		mapLeftoverUnmappedMethods(mapping, currentType)
+	}
+
+	if err := scanner.Err(); err != nil {
+		return nil, err
+	}
+
+	return mapping, nil
+}
+
+// Checks if the found methodMatch from the map file is part of the methods previously parsed for the currentType within the
+// stacktrace.
+// If the method is found, then it's added to the mapping in order to be used for replacing the obfuscated name later, it also returns it as the current MappedMethodCall.
+// If the method is NOT found, but it turns out to be a continuation of a previously found method (currentMappedMethodCall), then it appends it to the existing method replacement in the mapping.
+// If the method is NOT found and is also NOT a continuation for the currentMappedMethodCall, then the methodMatch is ignored.
+func upsertMappedMethodCall(mapping map[string]string, methodMatch MethodMatch, currentType *MappedType, currentMappedMethodCall *MappedMethodCall) *MappedMethodCall {
+	methodNameReference := methodMatch.methodObfuscatedName
+	if methodMatch.sourceFileStart != "" {
+		if methodMatch.sourceFileStart != methodMatch.sourceFileEnd {
+			// This is probably due an edge-case where the mapping line starts with different numbers (e.g 1:2). We don't
+			// have that case in our tests, therefore we are ignoring it.
+			return currentMappedMethodCall
+		}
+		methodNameReference = fmt.Sprintf("%s:%s", methodMatch.methodObfuscatedName, methodMatch.sourceFileStart)
+	}
+	mapReference := currentType.obfuscated.name + ":" + methodNameReference
+	methodCallSite, ok := currentType.obfuscated.methods[methodNameReference]
+	if ok {
+		// Found this method in the list of methods parsed from the stacktrace for the currentType.
+		delete(currentType.obfuscated.methods, methodNameReference)
+		key := getKey(currentType.obfuscated.name, methodMatch.methodObfuscatedName, methodCallSite)
+		mapping[key] = getKey(currentType.realName, methodMatch.methodRealName, methodCallSite)
+		return &MappedMethodCall{reference: mapReference, key: key}
+	} else if currentMappedMethodCall != nil && currentMappedMethodCall.reference == mapReference {
+		// This is a continuation call for the currentMappedMethodCall.
+		mapping[currentMappedMethodCall.key] += "\n" + fmt.Sprintf("%s%s", strings.Repeat(" ", len(currentType.realName)+currentType.obfuscated.indentation), methodMatch.methodRealName)
+	}
+
+	return currentMappedMethodCall
+}
+
+// Sometimes the map file contains a type but not a method name belonging to said type, this might happen because the type name was obfuscated
+// but its method wasn't, in this case we use this function to make sure we add the leftover de-obfuscated type names from the map file,
+// along with the method names as found in the stacktrace (which should be de-obfuscated already).
+func mapLeftoverUnmappedMethods(mapping map[string]string, currentType *MappedType) {
+	for methodName, callSite := range currentType.obfuscated.methods {
+		key := getKey(currentType.obfuscated.name, methodName, callSite)
+		mapping[key] = getKey(currentType.realName, methodName, callSite)
+	}
+}
+
+func getKey(typeName string, methodName string, callSite string) string {
+	return fmt.Sprintf("%s.%s(%s)", typeName, methodName, callSite)
+}
---
+++ internal/r8/deobfuscator_test.go
@@ -0,0 +1,58 @@
+// Licensed to Elasticsearch B.V. under one or more contributor
+// license agreements. See the NOTICE file distributed with
+// this work for additional information regarding copyright
+// ownership. Elasticsearch B.V. licenses this file to you under
+// the Apache License, Version 2.0 (the "License"); you may
+// not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+package r8
+
+import (
+	"fmt"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestDeobfuscation(t *testing.T) {
+	cases := []string{
+		"../../testdata/r8/deobfuscator/1",
+		"../../testdata/r8/deobfuscator/2",
+		"../../testdata/r8/deobfuscator/3",
+	}
+
+	for _, c := range cases {
+		inputPath := c + "/obfuscated-crash"
+		expectedOutputPath := c + "/de-obfuscated-crash"
+		mapFilePath := c + "/mapping"
+
+		t.Run(fmt.Sprintf("(%s)->(%s)", inputPath, expectedOutputPath), func(t *testing.T) {
+			t.Parallel()
+			reader, err := os.Open(mapFilePath)
+			require.Nil(t, err)
+			defer reader.Close()
+			obfuscated := readFile(t, inputPath)
+			expected := readFile(t, expectedOutputPath)
+			deobfuscated, _ := Deobfuscate(obfuscated, reader)
+			assert.Equal(t, expected, deobfuscated)
+		})
+	}
+}
+
+func readFile(t *testing.T, path string) string {
+	bytes, err := os.ReadFile(path)
+	require.Nil(t, err)
+	return string(bytes)
+}
