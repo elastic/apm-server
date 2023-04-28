d58d537a10d41f042ddd4ecb22050ab569e0a3a6
kruskal <99559985+kruskall@users.noreply.github.com>
2023-04-28 17:22:01 +0200

    test: remove duplicate unused files

--- testing/smoke/supported-os/latest_apm_server.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -eo pipefail
-
-LATEST_STACK=$(curl -s "https://artifacts-api.elastic.co/v1/versions" | jq -r '.versions[-1]')
-LATEST_BUILD=$(curl -s "https://artifacts-api.elastic.co/v1/versions/${LATEST_STACK}/builds/" | jq -r '.builds[0]')
-
-curl -s "https://artifacts-api.elastic.co/v1/versions/${LATEST_STACK}/builds/${LATEST_BUILD}/projects/apm-server" | jq -r ".project.packages | {deb: .\"apm-server-${LATEST_STACK}-amd64.deb\".url, rpm: .\"apm-server-${LATEST_STACK}-x86_64.rpm\".url }"
