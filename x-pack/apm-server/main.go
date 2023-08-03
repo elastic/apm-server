78082edcafc40341bcbd89b51ff2d11e66540cb6
Carson Ip <carson.ip@elastic.co>
2023-08-03 13:14:28 +0100

    Disable aggregation

--- x-pack/apm-server/main.go
+++ x-pack/apm-server/main.go
@@ -101,84 +101,86 @@ type processor interface {
 // events in sequential order, prior to the events being published.
 func newProcessors(args beater.ServerParams) ([]namedProcessor, error) {
 	processors := make([]namedProcessor, 0, 5)
-	const txName = "transaction metrics aggregation"
-	args.Logger.Infof("creating %s with config: %+v", txName, args.Config.Aggregation.Transactions)
-	agg, err := txmetrics.NewAggregator(txmetrics.AggregatorConfig{
-		BatchProcessor:                 args.BatchProcessor,
-		MaxTransactionGroups:           args.Config.Aggregation.Transactions.MaxTransactionGroups,
-		MetricsInterval:                metricsInterval,
-		RollUpIntervals:                rollUpMetricsIntervals,
-		MaxTransactionGroupsPerService: int(math.Ceil(0.1 * float64(args.Config.Aggregation.Transactions.MaxTransactionGroups))),
-		MaxServices:                    args.Config.Aggregation.Transactions.MaxServices,
-		HDRHistogramSignificantFigures: args.Config.Aggregation.Transactions.HDRHistogramSignificantFigures,
-	})
-	if err != nil {
-		return nil, errors.Wrapf(err, "error creating %s", txName)
-	}
-	processors = append(processors, namedProcessor{name: txName, processor: agg})
-	aggregationMonitoringRegistry.Remove("txmetrics")
-	monitoring.NewFunc(aggregationMonitoringRegistry, "txmetrics", agg.CollectMonitoring, monitoring.Report)
+	if false {
+		const txName = "transaction metrics aggregation"
+		args.Logger.Infof("creating %s with config: %+v", txName, args.Config.Aggregation.Transactions)
+		agg, err := txmetrics.NewAggregator(txmetrics.AggregatorConfig{
+			BatchProcessor:                 args.BatchProcessor,
+			MaxTransactionGroups:           args.Config.Aggregation.Transactions.MaxTransactionGroups,
+			MetricsInterval:                metricsInterval,
+			RollUpIntervals:                rollUpMetricsIntervals,
+			MaxTransactionGroupsPerService: int(math.Ceil(0.1 * float64(args.Config.Aggregation.Transactions.MaxTransactionGroups))),
+			MaxServices:                    args.Config.Aggregation.Transactions.MaxServices,
+			HDRHistogramSignificantFigures: args.Config.Aggregation.Transactions.HDRHistogramSignificantFigures,
+		})
+		if err != nil {
+			return nil, errors.Wrapf(err, "error creating %s", txName)
+		}
+		processors = append(processors, namedProcessor{name: txName, processor: agg})
+		aggregationMonitoringRegistry.Remove("txmetrics")
+		monitoring.NewFunc(aggregationMonitoringRegistry, "txmetrics", agg.CollectMonitoring, monitoring.Report)
 
-	const spanName = "service destination metrics aggregation"
-	args.Logger.Infof("creating %s with config: %+v", spanName, args.Config.Aggregation.ServiceDestinations)
-	spanAggregator, err := spanmetrics.NewAggregator(spanmetrics.AggregatorConfig{
-		BatchProcessor:  args.BatchProcessor,
-		Interval:        metricsInterval,
-		RollUpIntervals: rollUpMetricsIntervals,
-		MaxGroups:       args.Config.Aggregation.ServiceDestinations.MaxGroups,
-	})
-	if err != nil {
-		return nil, errors.Wrapf(err, "error creating %s", spanName)
-	}
-	processors = append(processors, namedProcessor{name: spanName, processor: spanAggregator})
-	aggregationMonitoringRegistry.Remove("spanmetrics")
-	monitoring.NewFunc(
-		aggregationMonitoringRegistry,
-		"spanmetrics",
-		spanAggregator.CollectMonitoring,
-		monitoring.Report,
-	)
+		const spanName = "service destination metrics aggregation"
+		args.Logger.Infof("creating %s with config: %+v", spanName, args.Config.Aggregation.ServiceDestinations)
+		spanAggregator, err := spanmetrics.NewAggregator(spanmetrics.AggregatorConfig{
+			BatchProcessor:  args.BatchProcessor,
+			Interval:        metricsInterval,
+			RollUpIntervals: rollUpMetricsIntervals,
+			MaxGroups:       args.Config.Aggregation.ServiceDestinations.MaxGroups,
+		})
+		if err != nil {
+			return nil, errors.Wrapf(err, "error creating %s", spanName)
+		}
+		processors = append(processors, namedProcessor{name: spanName, processor: spanAggregator})
+		aggregationMonitoringRegistry.Remove("spanmetrics")
+		monitoring.NewFunc(
+			aggregationMonitoringRegistry,
+			"spanmetrics",
+			spanAggregator.CollectMonitoring,
+			monitoring.Report,
+		)
 
-	const serviceTxName = "service transaction metrics aggregation"
-	args.Logger.Infof("creating %s with config: %+v", serviceTxName, args.Config.Aggregation.ServiceTransactions)
-	serviceTxAggregator, err := servicetxmetrics.NewAggregator(servicetxmetrics.AggregatorConfig{
-		BatchProcessor:                 args.BatchProcessor,
-		Interval:                       metricsInterval,
-		RollUpIntervals:                rollUpMetricsIntervals,
-		MaxGroups:                      args.Config.Aggregation.ServiceTransactions.MaxGroups,
-		HDRHistogramSignificantFigures: args.Config.Aggregation.ServiceTransactions.HDRHistogramSignificantFigures,
-	})
-	if err != nil {
-		return nil, errors.Wrapf(err, "error creating %s", serviceTxName)
-	}
-	processors = append(processors, namedProcessor{name: serviceTxName, processor: serviceTxAggregator})
-	aggregationMonitoringRegistry.Remove("servicetxmetrics")
-	monitoring.NewFunc(
-		aggregationMonitoringRegistry,
-		"servicetxmetrics",
-		serviceTxAggregator.CollectMonitoring,
-		monitoring.Report,
-	)
+		const serviceTxName = "service transaction metrics aggregation"
+		args.Logger.Infof("creating %s with config: %+v", serviceTxName, args.Config.Aggregation.ServiceTransactions)
+		serviceTxAggregator, err := servicetxmetrics.NewAggregator(servicetxmetrics.AggregatorConfig{
+			BatchProcessor:                 args.BatchProcessor,
+			Interval:                       metricsInterval,
+			RollUpIntervals:                rollUpMetricsIntervals,
+			MaxGroups:                      args.Config.Aggregation.ServiceTransactions.MaxGroups,
+			HDRHistogramSignificantFigures: args.Config.Aggregation.ServiceTransactions.HDRHistogramSignificantFigures,
+		})
+		if err != nil {
+			return nil, errors.Wrapf(err, "error creating %s", serviceTxName)
+		}
+		processors = append(processors, namedProcessor{name: serviceTxName, processor: serviceTxAggregator})
+		aggregationMonitoringRegistry.Remove("servicetxmetrics")
+		monitoring.NewFunc(
+			aggregationMonitoringRegistry,
+			"servicetxmetrics",
+			serviceTxAggregator.CollectMonitoring,
+			monitoring.Report,
+		)
 
-	const serviceSummaryName = "service summary metrics aggregation"
-	args.Logger.Infof("creating %s with config: %+v", serviceSummaryName, args.Config.Aggregation.ServiceTransactions)
-	serviceSummaryAggregator, err := servicesummarymetrics.NewAggregator(servicesummarymetrics.AggregatorConfig{
-		BatchProcessor:  args.BatchProcessor,
-		Interval:        metricsInterval,
-		RollUpIntervals: rollUpMetricsIntervals,
-		MaxGroups:       args.Config.Aggregation.ServiceTransactions.MaxGroups,
-	})
-	if err != nil {
-		return nil, errors.Wrapf(err, "error creating %s", serviceSummaryName)
+		const serviceSummaryName = "service summary metrics aggregation"
+		args.Logger.Infof("creating %s with config: %+v", serviceSummaryName, args.Config.Aggregation.ServiceTransactions)
+		serviceSummaryAggregator, err := servicesummarymetrics.NewAggregator(servicesummarymetrics.AggregatorConfig{
+			BatchProcessor:  args.BatchProcessor,
+			Interval:        metricsInterval,
+			RollUpIntervals: rollUpMetricsIntervals,
+			MaxGroups:       args.Config.Aggregation.ServiceTransactions.MaxGroups,
+		})
+		if err != nil {
+			return nil, errors.Wrapf(err, "error creating %s", serviceSummaryName)
+		}
+		processors = append(processors, namedProcessor{name: serviceSummaryName, processor: serviceSummaryAggregator})
+		aggregationMonitoringRegistry.Remove("servicesummarymetrics")
+		monitoring.NewFunc(
+			aggregationMonitoringRegistry,
+			"servicesummarymetrics",
+			serviceSummaryAggregator.CollectMonitoring,
+			monitoring.Report,
+		)
 	}
-	processors = append(processors, namedProcessor{name: serviceSummaryName, processor: serviceSummaryAggregator})
-	aggregationMonitoringRegistry.Remove("servicesummarymetrics")
-	monitoring.NewFunc(
-		aggregationMonitoringRegistry,
-		"servicesummarymetrics",
-		serviceSummaryAggregator.CollectMonitoring,
-		monitoring.Report,
-	)
 
 	if args.Config.Sampling.Tail.Enabled {
 		const name = "tail sampler"
