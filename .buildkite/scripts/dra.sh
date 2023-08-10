e14ace0683cf3b3b498fe4787387d10fb78896cd
Victor Martinez <VictorMartinezRubio@gmail.com>
2023-08-10 15:12:42 +0200

    action: support for DRA

---
+++ .buildkite/scripts/dra.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+##
+##  It relies on the .buildkite/hooks/pre-command so the Vault and other tooling
+##  are prepared automatically by buildkite.
+##
+##  Required environment variables passed when running the Buildkite pipeline:
+##   * BRANCH_NAME
+##   * DRA_WORKFLOW
+##   * GITHUB_SHA
+##
+
+set -eo pipefail
+
+### TODO: retry a few times just in case docker.elastic.co is not accessible
+docker pull --quiet docker.elastic.co/infra/release-manager:latest
+
+## Read current version.
+VERSION=$(make get-version)
+
+### TODO: fetch all the generated artifacts
+# BK artifact API call
+
+### TODO: retry a few times just in case some infra issues, like the vault accessing.
+docker run --rm \
+  --name release-manager \
+  -e VAULT_ADDR \
+  -e VAULT_ROLE_ID_SECRET \
+  -e VAULT_SECRET_ID_SECRET \
+  --mount type=bind,readonly=false,src=$(pwd),target=/artifacts \
+  docker.elastic.co/infra/release-manager:latest \
+    cli collect \
+    --project apm-server \
+    --branch $BRANCH_NAME \
+    --commit $GITHUB_SHA \
+    --workflow $DRA_WORKFLOW \
+    --artifact-set main \
+    --version $VERSION
---
+++ .buildkite/scripts/package.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+##
+##  Builds the distribution packages: tarballs, RPMs, Docker images, etc.
+##  It runs for both linux/amd64 and linux/arm64. On linux/amd64 it builds all packages;
+##  on linux/arm64 it builds only Docker images.
+##
+
+set -eo pipefail
+
+TYPE="$1"
+PLATFORM_TYPE=$(uname -m)
+
+MAKE_GOAL=package
+if [[ ${PLATFORM_TYPE} == "arm" || ${PLATFORM_TYPE} == "aarch64" ]]; then
+  MAKE_GOAL=package-docker
+fi
+
+if [[ ${TYPE} == "snapshot" ]]; then
+  MAKE_GOAL="${MAKE_GOAL}-snapshot"
+fi
+
+make $MAKE_GOAL
+
+# BK artifact API call
