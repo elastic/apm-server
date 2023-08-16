9d8c613a641a601f7afe431101ebb306d2e16fa0
Victor Martinez <VictorMartinezRubio@gmail.com>
2023-08-16 22:53:42 +0200

    Support active branches

--- .buildkite/scripts/dra.sh
+++ .buildkite/scripts/dra.sh
@@ -3,10 +3,9 @@
 ##  It relies on the .buildkite/hooks/pre-command so the Vault and other tooling
 ##  are prepared automatically by buildkite.
 ##
-##  Required environment variables passed when running the Buildkite pipeline:
-##   * BRANCH_NAME
-##   * DRA_WORKFLOW
-##   * GITHUB_SHA
+##  It downloads the generated artifacts and run the DRA only if the branch is an active
+##  branch, based on the Unified Release policy. Otherwise, it won't run the DRA but print
+##  some traces.
 ##
 
 set -eo pipefail
@@ -30,18 +29,35 @@ if [[ "${BUILDKITE_PULL_REQUEST:-false}" == "false" ]]; then
   exit 0
 fi
 
-echo "--- Run release manager"
-docker run --rm \
-  --name release-manager \
-  -e VAULT_ADDR="${VAULT_ADDR_SECRET}" \
-  -e VAULT_ROLE_ID="${VAULT_ROLE_ID_SECRET}" \
-  -e VAULT_SECRET_ID="${VAULT_SECRET}" \
-  --mount type=bind,readonly=false,src=$(pwd),target=/artifacts \
-  docker.elastic.co/infra/release-manager:latest \
-    cli collect \
-    --project apm-server \
-    --branch $BRANCH_NAME \
-    --commit $GITHUB_SHA \
-    --workflow $DRA_WORKFLOW \
-    --artifact-set main \
-    --version $VERSION
+curl -s https://storage.googleapis.com/artifacts-api/snapshots/branches.json > active-branches.json
+if ! grep -q "\"$BUILDKITE_BRANCH\"" active-branches.json ; then
+  echo "Release Manager only support the current active branches, skipping"
+  echo "BUILDKITE_BRANCH=$BUILDKITE_BRANCH"
+  echo "BUILDKITE_COMMIT=$BUILDKITE_COMMIT"
+  echo "VERSION=$VERSION"
+  exit 0
+fi
+
+dra() {
+  local workflow=$1
+  echo "--- Run release manager $workflow"
+  docker run --rm \
+    --name release-manager \
+    -e VAULT_ADDR="${VAULT_ADDR_SECRET}" \
+    -e VAULT_ROLE_ID="${VAULT_ROLE_ID_SECRET}" \
+    -e VAULT_SECRET_ID="${VAULT_SECRET}" \
+    --mount type=bind,readonly=false,src=$(pwd),target=/artifacts \
+    docker.elastic.co/infra/release-manager:latest \
+      cli collect \
+      --project apm-server \
+      --branch $BUILDKITE_BRANCH \
+      --commit $BUILDKITE_COMMIT \
+      --workflow $workflow \
+      --artifact-set main \
+      --version $VERSION
+}
+
+dra "snapshot"
+if [[ "${BUILDKITE_BRANCH}" != "main" ]]; then
+  dra "staging"
+fi
