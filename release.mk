362bfd67d226e1c11f4e70c05ee7b174800d388f
Victor Martinez <VictorMartinezRubio@gmail.com>
2023-12-18 10:29:39 +0100

    ci(release): automate release steps

---
+++ release.mk
@@ -0,0 +1,7 @@
+run-minor:
+	echo 'TODO: prepare-major-minor-release'
+	echo 'TODO: create-branch-major-minor-release'
+
+run-patch:
+	echo 'TODO: prepare-patch-release'
+	echo 'TODO: create-prs-patch-release'
