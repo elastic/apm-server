01a46d9b66821cddab9f2850e897dc5bf7a932a3
Victor Martinez <VictorMartinezRubio@gmail.com>
2023-12-19 14:10:59 +0100

    rename make goals and add some docs

--- release.mk
+++ release.mk
@@ -1,9 +1,19 @@
-run-minor:
+# This is the contract with the GitHub action .github/workflows/run-minor-release.yml.
+# The GitHub action will provide the below environment variables:
+#  - RELEASE_BRANCH
+#  - RELEASE_VERSION
+.PHONY: minor-release
+minor-release:
+	@echo "BRANCH: $${RELEASE_BRANCH}"
 	@echo "VERSION: $${RELEASE_VERSION}"
 	@echo 'TODO: prepare-major-minor-release'
 	@echo 'TODO: create-branch-major-minor-release'
 
-run-patch:
+# This is the contract with the GitHub action .github/workflows/run-patch-release.yml
+# The GitHub action will provide the below environment variables:
+#  - RELEASE_VERSION
+.PHONY: patch-release
+patch-release:
 	@echo "VERSION: $${RELEASE_VERSION}"
 	@echo 'TODO: prepare-patch-release'
 	@echo 'TODO: create-prs-patch-release'
