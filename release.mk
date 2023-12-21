6c4aa1b58d8c9a7306c7078a1619083ac68594be
Victor Martinez <VictorMartinezRubio@gmail.com>
2023-12-21 09:36:44 +0100

    refactor

--- release.mk
+++ release.mk
@@ -57,6 +57,7 @@ PROJECT_OWNER ?= elastic
 RELEASE_TYPE ?= minor
 
 CURRENT_RELEASE ?= $(shell gh api repos/elastic/apm-server/releases/latest | jq -r '.tag_name|sub("v"; ""; "")')
+RELEASE_BRANCH ?= $(PROJECT_MAJOR_VERSION).$(PROJECT_MINOR_VERSION)
 NEXT_PROJECT_MINOR_VERSION ?= $(PROJECT_MAJOR_VERSION).$(shell expr $(PROJECT_MINOR_VERSION) + 1).0
 NEXT_RELEASE ?= $(RELEASE_BRANCH).$(shell expr $(PROJECT_PATCH_VERSION) + 1)
 
@@ -80,23 +81,27 @@ endif
 #  - RELEASE_VERSION
 .PHONY: minor-release
 minor-release:
+	@echo "INFO: Create release branch and update new version $(RELEASE_VERSION)"
 	$(MAKE) create-branch NAME=$(RELEASE_BRANCH) BASE=$(BASE_BRANCH)
 	$(MAKE) update-version VERSION=$(RELEASE_VERSION)
-	$(MAKE) update-version-makefile VERSION=$(RELEASE_VERSION)
+	$(MAKE) update-version-makefile VERSION=$(PROJECT_MAJOR_VERSION)\.$(PROJECT_MINOR_VERSION)
 	$(MAKE) create-commit COMMIT_MESSAGE="[Release] update version $(RELEASE_VERSION)"
 
-	$(MAKE) create-branch NAME=update-$(RELEASE_VERSION) BASE=$(BASE_BRANCH) 
-	$(MAKE) update-mergify VERSION=$(RELEASE_VERSION)
+	@echo "INFO: Create feature branch and update the versions. Target branch $(RELEASE_BRANCH)"
+	$(MAKE) create-branch NAME=changelog-$(RELEASE_BRANCH) BASE=$(RELEASE_BRANCH)
+	$(MAKE) rename-changelog VERSION=$(RELEASE_BRANCH)
+	$(MAKE) create-commit COMMIT_MESSAGE="docs: Update changelogs for $(RELEASE_BRANCH) release"
+
+	@echo "INFO: Create feature branch and update the versions. Target branch $(BASE_BRANCH)"
+	$(MAKE) create-branch NAME=update-$(RELEASE_VERSION) BASE=$(BASE_BRANCH)
 	$(MAKE) update-docs VERSION=$(CURRENT_RELEASE)
+	$(MAKE) update-mergify VERSION=$(RELEASE_VERSION)
 	$(MAKE) update-version VERSION=$(NEXT_PROJECT_MINOR_VERSION)
 	$(MAKE) create-commit COMMIT_MESSAGE="[Release] update version $(NEXT_PROJECT_MINOR_VERSION)"
-	$(MAKE) rename-changelog
-	$(MAKE) create-commit COMMIT_MESSAGE="docs: Update changelogs for $(RELEASE_BRANCH) release"
+	$(MAKE) rename-changelog VERSION=$(RELEASE_BRANCH)
+	$(MAKE) create-commit COMMIT_MESSAGE="[Release] update changelogs for $(RELEASE_BRANCH) release"
 
-	$(MAKE) create-branch NAME=changelog-$(RELEASE_BRANCH) BASE=$(RELEASE_BRANCH)
-	$(MAKE) rename-changelog
-	$(MAKE) create-commit COMMIT_MESSAGE="docs: Update changelogs for $(RELEASE_BRANCH) release"
-
+	@echo "INFO: Push changes to $(PROJECT_OWNER)/apm-server and create the relevant Pull Requests"
 	git push origin $(RELEASE_BRANCH)
 	$(MAKE) create-pull-request BRANCH=update-$(RELEASE_VERSION) TARGET_BRANCH=$(BASE_BRANCH) TITLE="$(RELEASE_BRANCH): update docs, mergify, versions and changelogs"
 	$(MAKE) create-pull-request BRANCH=changelog-$(RELEASE_BRANCH) TARGET_BRANCH=$(RELEASE_BRANCH) TITLE="$(RELEASE_BRANCH): update docs"
@@ -110,87 +115,28 @@ patch-release:
 	@echo 'TODO: prepare-patch-release'
 	@echo 'TODO: create-prs-patch-release'
 
-#######################
-## Internal make goals
-#######################
-
-## Create a new branch
-## It will delete the branch if it already exists before the creation.
-.PHONY: create-branch
-create-branch: NAME=$${NAME} BASE=$${BASE}
-create-branch:
-	@echo "::group::create-branch $(NAME)"
-	git checkout $(BASE)
-	git branch -D $(NAME) &>/dev/null || true
-	git checkout $(BASE) -b $(NAME)
-	@echo "::endgroup::"
+############################################
+## Internal make goals to bump versions
+############################################
 
 # Rename changelog file.
 .PHONY: rename-changelog
 export CHANGELOG_TMPL
+rename-changelog: VERSION=$${VERSION}
 rename-changelog:
-	mv changelogs/head.asciidoc changelogs/$(RELEASE_BRANCH).asciidoc
+	mv changelogs/head.asciidoc changelogs/$(VERSION).asciidoc
     #echo "$${CHANGELOG_TMPL}" > changelogs/head.asciidoc
-	awk "NR==2{print \"include::./changelogs/$(RELEASE_BRANCH).asciidoc[]\"}1" CHANGELOG.asciidoc > CHANGELOG.asciidoc.new
+	awk "NR==2{print \"include::./changelogs/$(VERSION).asciidoc[]\"}1" CHANGELOG.asciidoc > CHANGELOG.asciidoc.new
 	mv CHANGELOG.asciidoc.new CHANGELOG.asciidoc
-	awk "NR==12{print \"* <<release-notes-$(RELEASE_BRANCH)>>\"}1" docs/release-notes.asciidoc > docs/release-notes.asciidoc.new
+	awk "NR==12{print \"* <<release-notes-$(VERSION)>>\"}1" docs/release-notes.asciidoc > docs/release-notes.asciidoc.new
 	mv docs/release-notes.asciidoc.new docs/release-notes.asciidoc
 
-## Update the version in the different files with the hardcoded version.
-.PHONY: update-version
-update-version: VERSION=$${VERSION} PREVIOUS_VERSION=$${PREVIOUS_VERSION}
-update-version:
-	@echo "::group::update-version"
-	if [ -f "cmd/intake-receiver/version.go" ]; then \
-		$(SED) -E -e 's#(version[[:blank:]]*)=[[:blank:]]*"[0-9]+\.[0-9]+\.[0-9]+#\1= "$(VERSION)#g' cmd/intake-receiver/version.go; \
-	fi
-	if [ -f "internal/version/version.go" ]; then \
-		$(SED) -E -e 's#(Version[[:blank:]]*)=[[:blank:]]*"[0-9]+\.[0-9]+\.[0-9]+#\1= "$(VERSION)#g' internal/version/version.go; \
-	fi
-	@echo "::endgroup::"
-
-## Update project version in the Makefile.
-.PHONY: update-version-makefile
-update-version-makefile: VERSION=$${VERSION} PREVIOUS_VERSION=$${PREVIOUS_VERSION}
-update-version-makefile:
-	@echo "::group::update-version"
-	$(SED) -E -e 's#BEATS_VERSION\s*\?=\s*(([0-9]+\.[0-9]+)|main)#BEATS_VERSION\?=$(PROJECT_MAJOR_VERSION)\.$(PROJECT_MINOR_VERSION)#g' Makefile
-	@echo "::endgroup::"
-
-## Update the version in the different files with the hardcoded version. Legacy stuff
-.PHONY: update-version-legacy
-update-version-legacy: VERSION=$${VERSION} PREVIOUS_VERSION=$${PREVIOUS_VERSION}
-update-version-legacy:
-	@echo "::group::update-version-legacy"
-	if [ -f "cmd/version.go" ]; then \
-		$(SED) -E -e 's#(defaultBeatVersion[[:blank:]]*)=[[:blank:]]*"[0-9]+\.[0-9]+\.[0-9]+#\1= "$(VERSION)#g' cmd/version.go; \
-	fi
-	if [ -f "apmpackage/apm/changelog.yml" ]; then \
-		$(SED) -E -e 's#(version[[:blank:]]*):[[:blank:]]*"$(PREVIOUS_VERSION)#\1: "$(VERSION)#g' apmpackage/apm/changelog.yml; \
-	fi
-	if [ -f "apmpackage/apm/manifest.yml" ]; then \
-		$(SED) -E -e 's#(version[[:blank:]]*):[[:blank:]]*$(PREVIOUS_VERSION)#\1: $(VERSION)#g' apmpackage/apm/manifest.yml; \
-	fi
-	@echo "::endgroup::"
-
-## Create a new commit only if there is a diff.
-.PHONY: create-commit
-create-commit:
-	$(MAKE) git-diff
-	@echo "::group::create-commit"
-	if [ ! -z "$$(git status -s)" ]; then \
-		git status -s; \
-		git add --all; \
-		git commit -a -m "$(COMMIT_MESSAGE)"; \
-	fi
-	@echo "::endgroup::"
-
-## Diff output
-.PHONY: git-diff
-git-diff:
-	@echo "::group::git-diff"
-	git --no-pager  diff || true
-	@echo "::endgroup::"
+## Update project documentation.
+.PHONY: update-docs
+update-docs: VERSION=$${VERSION}
+update-docs: setup-yq
+	$(YQ) e --inplace '.[] |= with_entries((select(.value == "generated") | .value) ="$(VERSION)")' ./apmpackage/apm/changelog.yml; \
+	$(YQ) e --inplace '[{"version": "generated", "changes":[{"description": "Placeholder", "type": "enhancement", "link": "https://github.com/elastic/apm-server/pull/123"}]}] + .' ./apmpackage/apm/changelog.yml;
 
 ## Update the references on .mergify.yml with the new minor release.
 .PHONY: update-mergify
@@ -216,21 +162,70 @@ update-mergify:
 		echo "WARN: Mergify already contains backport-$(VERSION)"; \
 	fi
 
-## Update project documentation.
-.PHONY: update-docs
-update-docs: VERSION=$${VERSION}
-update-docs: setup-yq
-	$(YQ) e --inplace '.[] |= with_entries((select(.value == "generated") | .value) ="$(VERSION)")' ./apmpackage/apm/changelog.yml; \
-	$(YQ) e --inplace '[{"version": "generated", "changes":[{"description": "Placeholder", "type": "enhancement", "link": "https://github.com/elastic/apm-server/pull/123"}]}] + .' ./apmpackage/apm/changelog.yml;
-
-## @help:setup-yq:Install yq in CURDIR/bin/yq.
-.PHONY: setup-yq
-setup-yq:
-	if [ ! -x "$$(command -v $(YQ))" ] && [ ! -f "$(CURDIR)/bin/$(YQ)" ]; then \
-		echo "Downloading $(YQ) - $(YQ_VERSION)/$(YQ_BINARY)" ; \
-		curl -sSfL -o $(CURDIR)/bin/yq https://github.com/mikefarah/yq/releases/download/$(YQ_VERSION)/$(YQ_BINARY) ; \
-		chmod +x $(CURDIR)/bin/$(YQ); \
+## Update the version in the different files with the hardcoded version.
+.PHONY: update-version
+update-version: VERSION=$${VERSION}
+update-version:
+	@echo "::group::update-version"
+	if [ -f "cmd/intake-receiver/version.go" ]; then \
+		$(SED) -E -e 's#(version[[:blank:]]*)=[[:blank:]]*"[0-9]+\.[0-9]+\.[0-9]+#\1= "$(VERSION)#g' cmd/intake-receiver/version.go; \
 	fi
+	if [ -f "internal/version/version.go" ]; then \
+		$(SED) -E -e 's#(Version[[:blank:]]*)=[[:blank:]]*"[0-9]+\.[0-9]+\.[0-9]+#\1= "$(VERSION)#g' internal/version/version.go; \
+	fi
+	@echo "::endgroup::"
+
+## Update the version in the different files with the hardcoded version. Legacy stuff
+## @DEPRECATED: likely in the 7.17 branch
+.PHONY: update-version-legacy
+update-version-legacy: VERSION=$${VERSION} PREVIOUS_VERSION=$${PREVIOUS_VERSION}
+update-version-legacy:
+	@echo "::group::update-version-legacy"
+	if [ -f "cmd/version.go" ]; then \
+		$(SED) -E -e 's#(defaultBeatVersion[[:blank:]]*)=[[:blank:]]*"[0-9]+\.[0-9]+\.[0-9]+#\1= "$(VERSION)#g' cmd/version.go; \
+	fi
+	if [ -f "apmpackage/apm/changelog.yml" ]; then \
+		$(SED) -E -e 's#(version[[:blank:]]*):[[:blank:]]*"$(PREVIOUS_VERSION)#\1: "$(VERSION)#g' apmpackage/apm/changelog.yml; \
+	fi
+	if [ -f "apmpackage/apm/manifest.yml" ]; then \
+		$(SED) -E -e 's#(version[[:blank:]]*):[[:blank:]]*$(PREVIOUS_VERSION)#\1: $(VERSION)#g' apmpackage/apm/manifest.yml; \
+	fi
+	@echo "::endgroup::"
+
+## Update project version in the Makefile.
+.PHONY: update-version-makefile
+update-version-makefile: VERSION=$${VERSION}
+update-version-makefile:
+	@echo "::group::update-version-makefile"
+	$(SED) -E -e 's#BEATS_VERSION\s*\?=\s*(([0-9]+\.[0-9]+)|main)#BEATS_VERSION\?=$(VERSION)#g' Makefile
+	@echo "::endgroup::"
+
+############################################
+## Internal make goals to interact with Git
+############################################
+
+## Create a new branch
+## It will delete the branch if it already exists before the creation.
+.PHONY: create-branch
+create-branch: NAME=$${NAME} BASE=$${BASE}
+create-branch:
+	@echo "::group::create-branch $(NAME)"
+	git checkout $(BASE)
+	git branch -D $(NAME) &>/dev/null || true
+	git checkout $(BASE) -b $(NAME)
+	@echo "::endgroup::"
+
+## Create a new commit only if there is a diff.
+.PHONY: create-commit
+create-commit:
+	$(MAKE) git-diff
+	@echo "::group::create-commit"
+	if [ ! -z "$$(git status -s)" ]; then \
+		git status -s; \
+		git add --all; \
+		git commit -a -m "$(COMMIT_MESSAGE)"; \
+	fi
+	@echo "::endgroup::"
 
 ## @help:create-pull-request:Create pull request
 .PHONY: create-pull-request
@@ -245,3 +240,24 @@ create-pull-request:
 		--label 'release' \
 		--reviewer "$(PROJECT_REVIEWERS)" \
 		--repo $(PROJECT_OWNER)/apm-server || echo "There is no changes"
+
+## Diff output
+.PHONY: git-diff
+git-diff:
+	@echo "::group::git-diff"
+	git --no-pager  diff || true
+	@echo "::endgroup::"
+
+############################################
+## Internal make goals to install tools
+############################################
+
+## @help:setup-yq:Install yq in CURDIR/bin/yq.
+.PHONY: setup-yq
+setup-yq:
+	if [ ! -x "$$(command -v $(YQ))" ] && [ ! -f "$(CURDIR)/bin/$(YQ)" ]; then \
+		echo "Downloading $(YQ) - $(YQ_VERSION)/$(YQ_BINARY)" ; \
+		curl -sSfL -o $(CURDIR)/bin/yq https://github.com/mikefarah/yq/releases/download/$(YQ_VERSION)/$(YQ_BINARY) ; \
+		chmod +x $(CURDIR)/bin/$(YQ); \
+	fi
+
