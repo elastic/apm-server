3cf65392fa76bc035c165984ebef222f46e08d2c
Victor Martinez <victormartinezrubio@gmail.com>
2024-04-15 15:43:42 +0200

    package: honour `.go-version` content when creating docker images (#12970)
    (cherry picked from commit a7922d35a181f970a678167833c2f0fef88186e7)
    
    # Conflicts:
    #	.ci/bump-golang.yml
    #	.github/workflows/ci.yml
    #	go.mk
    #	packaging.mk
    #	packaging/docker/Dockerfile

---
+++ go.mk
@@ -0,0 +1,50 @@
+GITDIR ?= $(shell git rev-parse --git-dir)
+GITCOMMIT ?= $(shell git rev-parse HEAD)
+GITCOMMITTIMESTAMP ?= $(shell git log -1 --pretty=%cI)
+GITCOMMITTIMESTAMPUNIX ?= $(shell git log -1 --pretty=%ct)
+GITREFFILE ?= $(GITDIR)/$(shell git rev-parse --symbolic-full-name HEAD)
+GITROOT ?= $(shell git rev-parse --show-toplevel)
+
+# Ensure the Go version in .go-version is installed and used.
+GOROOT?=$(shell $(GITROOT)/script/run_with_go_ver go env GOROOT)
+GOLANG_VERSION=$(shell cat $(GITROOT)/.go-version)
+GO:=$(GOROOT)/bin/go
+GOARCH:=$(shell $(GO) env GOARCH)
+export PATH:=$(GOROOT)/bin:$(PATH)
+
+GOOSBUILD:=$(GITROOT)/build/$(shell $(GO) env GOOS)
+GOIMPORTS=$(GOOSBUILD)/goimports
+GOLICENSER=$(GOOSBUILD)/go-licenser
+STATICCHECK=$(GOOSBUILD)/staticcheck
+TERRAFORMDOCS=$(GOOSBUILD)/terraform-docs
+GOBENCH=$(GOOSBUILD)/gobench
+GOVERSIONINFO=$(GOOSBUILD)/goversioninfo
+NFPM=$(GOOSBUILD)/nfpm
+
+APM_SERVER_VERSION=$(shell grep "const Version" $(GITROOT)/internal/version/version.go | cut -d'=' -f2 | tr -d '" ')
+APM_SERVER_VERSION_MAJORMINOR=$(shell echo $(APM_SERVER_VERSION) | sed 's/\(.*\..*\)\..*/\1/')
+
+##############################################################################
+# Rules for creating and installing build tools.
+##############################################################################
+
+$(GOIMPORTS): $(GITROOT)/go.mod
+	$(GO) build -o $@ golang.org/x/tools/cmd/goimports
+
+$(STATICCHECK): $(GITROOT)/tools/go.mod
+	$(GO) build -o $@ -modfile=$< honnef.co/go/tools/cmd/staticcheck
+
+$(GOLICENSER): $(GITROOT)/tools/go.mod
+	$(GO) build -o $@ -modfile=$< github.com/elastic/go-licenser
+
+$(TERRAFORMDOCS): $(GITROOT)/tools/go.mod
+	$(GO) build -o $@ -modfile=$< github.com/terraform-docs/terraform-docs
+
+$(GOBENCH): $(GITROOT)/tools/go.mod
+	$(GO) build -o $@ -modfile=$< github.com/elastic/gobench
+
+$(GOVERSIONINFO): $(GITROOT)/tools/go.mod
+	$(GO) build -o $@ -modfile=$< github.com/josephspurrier/goversioninfo/cmd/goversioninfo
+
+$(NFPM): $(GITROOT)/tools/go.mod
+	$(GO) build -o $@ -modfile=$< github.com/goreleaser/nfpm/v2/cmd/nfpm
