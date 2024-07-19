93db259950c30d6f33164216cf7e81201ab83b18
Jan Calanog <jan.calanog@elastic.co>
2024-07-19 16:47:23 +0200

    DRA: fix permissions of build artifacts (#13733)
    * DRA: fix permissions of build artifacts
    
    * test
    
    * test
    
    * Add debug
    
    * test
    
    * test

--- .buildkite/scripts/dra.sh
+++ .buildkite/scripts/dra.sh
@@ -20,6 +20,7 @@ buildkite-agent artifact download "build/dependencies*.csv" .
 cp build/dependencies*.csv build/distributions/
 
 echo "--- Changing permissions for the release manager"
+chmod -R a+r build/
 sudo chown -R :1000 build/
 ls -l build/distributions/
 
