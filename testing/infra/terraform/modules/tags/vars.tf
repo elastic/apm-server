bd1e502def45e8ad7864d07eacd904b6e6af4528
Victor Martinez <victormartinezrubio@gmail.com>
2024-08-07 16:19:23 +0200

    terraform: enable build tag (#13841)
    (cherry picked from commit b4c79b3ab0245090f2439b78a04dabf5b6451ae9)
    
    # Conflicts:
    #	testing/infra/terraform/modules/tags/output.tf

--- testing/infra/terraform/modules/tags/vars.tf
+++ testing/infra/terraform/modules/tags/vars.tf
@@ -1,3 +1,8 @@
 variable "project" {
   description = "The value to use for the project tag/label"
 }
+
+variable "build" {
+  description = "The value to use for the build tag/label"
+  default     = "unknown"
+}
