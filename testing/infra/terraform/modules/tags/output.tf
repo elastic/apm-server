b075873b86af8a5e9d537d9b1acd4e89318ab422
Victor Martinez <victormartinezrubio@gmail.com>
2024-09-25 15:16:23 +0200

    ci: add ephemeral tag (#14179)
    this will help the oblt-cli automation to destroy the ECE/ESS/Serverless deployments automatically
    
    (cherry picked from commit 812d7dde316ee5c5dc8cd04d2809a7bab067cf94)
    
    # Conflicts:
    #	testing/infra/terraform/modules/tags/output.tf

---
+++ testing/infra/terraform/modules/tags/output.tf
@@ -0,0 +1,20 @@
+locals {
+  tags = {
+    "division" : "engineering"
+    "org" : "obs"
+    "team" : "apm-server"
+    "project" : var.project
+    "build" : var.build
+    "ephemeral" : "true"
+  }
+}
+
+output "tags" {
+  value       = local.tags
+  description = "Tags for CSP resources"
+}
+
+output "labels" {
+  value       = local.tags
+  description = "Labels for CSP resources"
+}
