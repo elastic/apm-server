527da5c8697f0700572c5d31336c7fde9a2711c0
Carson Ip <carson.ip@elastic.co>
2024-10-10 16:22:40 +0100

    Fix concurrent map write panic in monitoring middleware

--- internal/beater/middleware/monitoring_middleware.go
+++ internal/beater/middleware/monitoring_middleware.go
@@ -20,6 +20,7 @@ package middleware
 import (
 	"context"
 	"net/http"
+	"sync"
 	"time"
 
 	"go.opentelemetry.io/otel"
@@ -36,9 +37,13 @@ const (
 type monitoringMiddleware struct {
 	meter metric.Meter
 
-	ints       map[request.ResultID]*monitoring.Int
-	counters   map[string]metric.Int64Counter
-	histograms map[string]metric.Int64Histogram
+	ints map[request.ResultID]*monitoring.Int
+
+	counters        map[string]metric.Int64Counter
+	countersRWMutex sync.RWMutex
+
+	histograms        map[string]metric.Int64Histogram
+	histogramsRWMutex sync.RWMutex
 }
 
 func (m *monitoringMiddleware) Middleware() Middleware {
@@ -79,10 +84,19 @@ func (m *monitoringMiddleware) inc(id request.ResultID) {
 
 func (m *monitoringMiddleware) getCounter(n string) metric.Int64Counter {
 	name := "http.server." + n
+
+	m.countersRWMutex.RLock()
 	if met, ok := m.counters[name]; ok {
+		m.countersRWMutex.RUnlock()
 		return met
 	}
 
+	m.countersRWMutex.RUnlock()
+	m.countersRWMutex.Lock()
+	defer m.countersRWMutex.Unlock()
+	if met, ok := m.counters[name]; ok {
+		return met
+	}
 	nm, _ := m.meter.Int64Counter(name)
 	m.counters[name] = nm
 	return nm
@@ -90,10 +104,20 @@ func (m *monitoringMiddleware) getCounter(n string) metric.Int64Counter {
 
 func (m *monitoringMiddleware) getHistogram(n string, opts ...metric.Int64HistogramOption) metric.Int64Histogram {
 	name := "http.server." + n
+
+	m.histogramsRWMutex.RLock()
 	if met, ok := m.histograms[name]; ok {
+		m.histogramsRWMutex.RUnlock()
 		return met
 	}
 
+	m.histogramsRWMutex.RUnlock()
+	m.histogramsRWMutex.Lock()
+	defer m.histogramsRWMutex.Unlock()
+
+	if met, ok := m.histograms[name]; ok {
+		return met
+	}
 	nm, _ := m.meter.Int64Histogram(name, opts...)
 	m.histograms[name] = nm
 	return nm
