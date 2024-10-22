31bec95d2b337c5324b36c17c65e6a397d10d5ec
Kostiantyn Masliuk <1pkg@protonmail.com>
2024-10-22 13:24:41 -0700

    monitoring: enable tracing self instrumentation in APM Server (#14231)
    (cherry picked from commit 56228ced66d66e39363e3ea19200b36c1c2834eb)

--- internal/beatcmd/beat_test.go
+++ internal/beatcmd/beat_test.go
@@ -183,7 +183,7 @@ func TestRunManager(t *testing.T) {
 			},
 		},
 		"instrumentation": map[string]interface{}{
-			"enabled":     false,
+			"enabled":     true,
 			"environment": "testenv",
 		},
 	}, m)
