d6bb9de492ab3a45e4e878fe81d65341f6a67e85
kruskal <99559985+kruskall@users.noreply.github.com>
2024-12-03 10:32:02 +0100

    feat: remove API Key CLI functionality
    update systemtest to create apikey using api
    remove unused functions

--- internal/elasticsearch/security_api.go
+++ internal/elasticsearch/security_api.go
@@ -25,35 +25,6 @@ import (
 	"github.com/elastic/go-elasticsearch/v8/esutil"
 )
 
-// CreateAPIKey requires manage_api_key cluster privilege
-func CreateAPIKey(ctx context.Context, client *Client, apikeyReq CreateAPIKeyRequest) (CreateAPIKeyResponse, error) {
-	var apikey CreateAPIKeyResponse
-	req := esapi.SecurityCreateAPIKeyRequest{Body: esutil.NewJSONReader(apikeyReq)}
-	err := doRequest(ctx, client, req, &apikey)
-	return apikey, err
-}
-
-// GetAPIKeys requires manage_api_key cluster privilege
-func GetAPIKeys(ctx context.Context, client *Client, apikeyReq GetAPIKeyRequest) (GetAPIKeyResponse, error) {
-	req := esapi.SecurityGetAPIKeyRequest{}
-	if apikeyReq.ID != nil {
-		req.ID = *apikeyReq.ID
-	} else if apikeyReq.Name != nil {
-		req.Name = *apikeyReq.Name
-	}
-	var apikey GetAPIKeyResponse
-	err := doRequest(ctx, client, req, &apikey)
-	return apikey, err
-}
-
-// InvalidateAPIKey requires manage_api_key cluster privilege
-func InvalidateAPIKey(ctx context.Context, client *Client, apikeyReq InvalidateAPIKeyRequest) (InvalidateAPIKeyResponse, error) {
-	var confirmation InvalidateAPIKeyResponse
-	req := esapi.SecurityInvalidateAPIKeyRequest{Body: esutil.NewJSONReader(apikeyReq)}
-	err := doRequest(ctx, client, req, &confirmation)
-	return confirmation, err
-}
-
 func HasPrivileges(ctx context.Context, client *Client, privileges HasPrivilegesRequest, credentials string) (HasPrivilegesResponse, error) {
 	var info HasPrivilegesResponse
 	req := esapi.SecurityHasPrivilegesRequest{Body: esutil.NewJSONReader(privileges)}
@@ -66,27 +37,6 @@ func HasPrivileges(ctx context.Context, client *Client, privileges HasPrivileges
 	return info, err
 }
 
-type CreateAPIKeyRequest struct {
-	Name            string                 `json:"name"`
-	Expiration      *string                `json:"expiration,omitempty"`
-	RoleDescriptors RoleDescriptor         `json:"role_descriptors"`
-	Metadata        map[string]interface{} `json:"metadata,omitempty"`
-}
-
-type CreateAPIKeyResponse struct {
-	APIKey
-	Key string `json:"api_key"`
-}
-
-type GetAPIKeyRequest struct {
-	APIKeyQuery
-	Owner bool `json:"owner"`
-}
-
-type GetAPIKeyResponse struct {
-	APIKeys []APIKeyResponse `json:"api_keys"`
-}
-
 type HasPrivilegesRequest struct {
 	// can't reuse the `Applications` type because here the JSON attribute must be singular
 	Applications []Application `json:"application"`
@@ -97,47 +47,10 @@ type HasPrivilegesResponse struct {
 	Application map[AppName]PermissionsPerResource `json:"application"`
 }
 
-type InvalidateAPIKeyRequest struct {
-	// normally the Elasticsearch API will require either Ids or Name, but not both
-	IDs  []string `json:"ids,omitempty"`
-	Name *string  `json:"name,omitempty"`
-}
-
-type InvalidateAPIKeyResponse struct {
-	Invalidated []string `json:"invalidated_api_keys"`
-	ErrorCount  int      `json:"error_count"`
-}
-
-type RoleDescriptor map[AppName]Applications
-
-type Applications struct {
-	Applications []Application `json:"applications"`
-}
-
 type Application struct {
 	Name       AppName           `json:"application"`
 	Privileges []PrivilegeAction `json:"privileges"`
 	Resources  []Resource        `json:"resources"`
-}
-
-type APIKeyResponse struct {
-	APIKey
-	Creation    int64                  `json:"creation"`
-	Invalidated bool                   `json:"invalidated"`
-	Username    string                 `json:"username"`
-	Metadata    map[string]interface{} `json:"metadata,omitempty"`
-}
-
-type APIKeyQuery struct {
-	// normally the Elasticsearch API will require either Id or Name, but not both
-	ID   *string `json:"id,omitempty"`
-	Name *string `json:"name,omitempty"`
-}
-
-type APIKey struct {
-	ID           string `json:"id"`
-	Name         string `json:"name"`
-	ExpirationMs *int64 `json:"expiration,omitempty"`
 }
 
 type Permissions map[PrivilegeAction]bool
