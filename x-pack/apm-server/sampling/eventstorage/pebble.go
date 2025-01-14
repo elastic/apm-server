d73809db20cec18950dd513c91ac97d1be5342ea
Carson Ip <carson.ip@elastic.co>
2025-01-14 15:16:37 +0000

    Revert "Try disable WAL"
    This reverts commit adac2a25d7b1a32206b1fceb5112ee6a2407769a.

--- x-pack/apm-server/sampling/eventstorage/pebble.go
+++ x-pack/apm-server/sampling/eventstorage/pebble.go
@@ -3,7 +3,5 @@ package eventstorage
 import "github.com/cockroachdb/pebble"
 
 func OpenPebble(storageDir string) (*pebble.DB, error) {
-	return pebble.Open(storageDir, &pebble.Options{
-		DisableWAL: true,
-	})
+	return pebble.Open(storageDir, &pebble.Options{})
 }
