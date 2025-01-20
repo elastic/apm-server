56f78a9c41bf33563fd86c712a79235a7004873c
Edoardo Tenani <edoardo.tenani@elastic.co>
2025-01-20 16:16:54 +0100

    add plain 8.17 test

---
+++ functionaltests/to_8_17_test.go
@@ -0,0 +1,59 @@
+// Licensed to Elasticsearch B.V. under one or more contributor
+// license agreements. See the NOTICE file distributed with
+// this work for additional information regarding copyright
+// ownership. Elasticsearch B.V. licenses this file to you under
+// the Apache License, Version 2.0 (the "License"); you may
+// not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+package functionaltests
+
+import (
+	"testing"
+)
+
+func TestUpgradeTo8170_plain(t *testing.T) {
+	ecAPICheck(t)
+
+	runESSUpgradeTest(t, essUpgradeTestCase{
+		from: "8.16.1",
+		to:   "8.17.0",
+		beforeUpgradeAfterIngest: checkDatastreamWant{
+			Quantity:     8,
+			PreferIlm:    false,
+			DSManagedBy:  lifecycleDSL,
+			IndicesPerDs: 1,
+			IndicesManagedBy: []checkDatastreamIndex{
+				{ManagedBy: lifecycleDSL},
+			},
+		},
+		afterUpgradeBeforeIngest: checkDatastreamWant{
+			Quantity:     8,
+			PreferIlm:    true,
+			DSManagedBy:  lifecycleILM,
+			IndicesPerDs: 1,
+			IndicesManagedBy: []checkDatastreamIndex{
+				{ManagedBy: lifecycleDSL},
+			},
+		},
+		afterUpgradeAfterIngest: checkDatastreamWant{
+			Quantity:     8,
+			PreferIlm:    true,
+			DSManagedBy:  lifecycleILM,
+			IndicesPerDs: 2,
+			IndicesManagedBy: []checkDatastreamIndex{
+				{ManagedBy: lifecycleDSL, PreferIlm: false},
+				{ManagedBy: lifecycleILM, PreferIlm: true},
+			},
+		},
+	})
+}
