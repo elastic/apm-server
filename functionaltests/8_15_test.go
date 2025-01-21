0769ee6dba31dc080f1389280c388b8e40a2752e
Edoardo Tenani <526307+endorama@users.noreply.github.com>
2025-01-21 09:15:57 +0100

    Update functionaltests/8_15_test.go
    Co-authored-by: Marc Lopez Rubio <marc5.12@outlook.com>

--- functionaltests/8_15_test.go
+++ functionaltests/8_15_test.go
@@ -151,7 +151,7 @@ func TestUpgrade_8_15_4_to_8_16_0(t *testing.T) {
 	if !assert.Zero(t, res.Hits.Total.Value, "expected no error logs, but found some") {
 		t.Log("found error logs:")
 		for _, h := range res.Hits.Hits {
-			t.Log(h.Source_)
+			t.Log(string(h.Source_))
 		}
 	}
 }
