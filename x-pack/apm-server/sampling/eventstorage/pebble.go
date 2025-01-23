55e2de41f9344ec4ec681447159163f210896f6f
Carson Ip <carson.ip@elastic.co>
2025-01-23 00:48:31 +0000

    Reduce memtable size by half

--- x-pack/apm-server/sampling/eventstorage/pebble.go
+++ x-pack/apm-server/sampling/eventstorage/pebble.go
@@ -15,24 +15,6 @@ import (
 	"github.com/elastic/elastic-agent-libs/logp"
 )
 
-const (
-	// pebbleMemTableSize defines the max stead state size of a memtable.
-	// There can be more than 1 memtable in memory at a time as it takes
-	// time for old memtable to flush. The memtable size also defines
-	// the size for large batches. A large batch is a batch which will
-	// take atleast half of the memtable size. Note that the Batch#Len
-	// is not the same as the memtable size that the batch will occupy
-	// as data in batches are encoded differently. In general, the
-	// memtable size of the batch will be higher than the length of the
-	// batch data.
-	//
-	// On commit, data in the large batch maybe kept by pebble and thus
-	// large batches will need to be reallocated. Note that large batch
-	// classification uses the memtable size that a batch will occupy
-	// rather than the length of data slice backing the batch.
-	pebbleMemTableSize = 32 << 20 // 32MB
-)
-
 func eventComparer() *pebble.Comparer {
 	comparer := *pebble.DefaultComparer
 	// Required for prefix bloom filter
@@ -47,7 +29,7 @@ func OpenEventPebble(storageDir string) (*pebble.DB, error) {
 		// FIXME: Specify FormatMajorVersion to use value blocks?
 		FormatMajorVersion: pebble.FormatNewest,
 		Logger:             logp.NewLogger(logs.Sampling),
-		MemTableSize:       pebbleMemTableSize,
+		MemTableSize:       16 << 20,
 		Levels: []pebble.LevelOptions{
 			{
 				BlockSize:    16 << 10,
@@ -65,6 +47,7 @@ func OpenDecisionPebble(storageDir string) (*pebble.DB, error) {
 		// FIXME: Specify FormatMajorVersion to use value blocks?
 		FormatMajorVersion: pebble.FormatNewest,
 		Logger:             logp.NewLogger(logs.Sampling),
+		MemTableSize:       2 << 20,
 		Levels: []pebble.LevelOptions{
 			{
 				BlockSize:    2 << 10,
