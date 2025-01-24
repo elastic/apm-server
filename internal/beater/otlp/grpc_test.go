83bc51bdd15b09dcdc12bcf5bb01fec392a01c9e
kruskal <99559985+kruskall@users.noreply.github.com>
2025-01-24 20:27:20 +0100

    lint: fix linter issues

--- internal/beater/otlp/grpc_test.go
+++ internal/beater/otlp/grpc_test.go
@@ -87,16 +87,11 @@ func TestConsumeTracesGRPC(t *testing.T) {
 	assert.Len(t, batches[0], 1)
 	assert.Len(t, batches[1], 1)
 
-	monitoringtest.ExpectOtelMetrics(t, reader, map[string]any{
-		"grpc.server.request.duration":                      2,
-		"grpc.server.request.count":                         2,
-		"grpc.server.response.count":                        2,
-		"grpc.server.response.errors.count":                 1,
-		"grpc.server.response.valid.count":                  1,
-		"apm-server.otlp.grpc.traces.request.count":         2,
-		"apm-server.otlp.grpc.traces.response.count":        2,
-		"apm-server.otlp.grpc.traces.response.errors.count": 1,
-		"apm-server.otlp.grpc.traces.response.valid.count":  1,
+	monitoringtest.ExpectContainOtelMetrics(t, reader, map[string]any{
+		"grpc.server.request.count":         2,
+		"grpc.server.response.count":        2,
+		"grpc.server.response.errors.count": 1,
+		"grpc.server.response.valid.count":  1,
 	})
 }
 
@@ -138,10 +133,10 @@ func TestConsumeMetricsGRPC(t *testing.T) {
 	assert.Equal(t, "failed to publish events", errStatus.Message())
 
 	monitoringtest.ExpectContainOtelMetrics(t, reader, map[string]any{
-		"grpc.server.request.count":                          2,
-		"grpc.server.response.count":                         2,
-		"grpc.server.response.errors.count":                  1,
-		"grpc.server.response.valid.count":                   1,
+		"grpc.server.request.count":         2,
+		"grpc.server.response.count":        2,
+		"grpc.server.response.errors.count": 1,
+		"grpc.server.response.valid.count":  1,
 	})
 }
 
@@ -185,10 +180,10 @@ func TestConsumeLogsGRPC(t *testing.T) {
 	assert.Len(t, batches[1], 1)
 
 	monitoringtest.ExpectContainOtelMetrics(t, reader, map[string]any{
-		"grpc.server.request.count":                       2,
-		"grpc.server.response.valid.count":                1,
-		"grpc.server.response.count":                      2,
-		"grpc.server.response.errors.count":               1,
+		"grpc.server.request.count":         2,
+		"grpc.server.response.valid.count":  1,
+		"grpc.server.response.count":        2,
+		"grpc.server.response.errors.count": 1,
 	})
 }
 
