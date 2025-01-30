3793ea3f9262abba89974f3b48e195b687d497fd
Edoardo Tenani <edoardo.tenani@elastic.co>
2025-01-30 17:54:42 +0100

    enhance Elasticsearch logs assertion
    We observed error logs unrelated to APM Server. We don't wont such
    logs to fail these tests, so this commit includes filtering logic
    based on the log message content.

--- functionaltests/8_15_test.go
+++ functionaltests/8_15_test.go
@@ -25,6 +25,7 @@ import (
 	"go.uber.org/zap"
 	"go.uber.org/zap/zaptest"
 
+	"github.com/elastic/apm-server/functionaltests/internal/asserts"
 	"github.com/elastic/apm-server/functionaltests/internal/ecclient"
 	"github.com/elastic/apm-server/functionaltests/internal/esclient"
 	"github.com/elastic/apm-server/functionaltests/internal/gen"
@@ -154,12 +155,7 @@ func TestUpgrade_8_15_4_to_8_16_0(t *testing.T) {
 	}, dss2)
 	t.Logf("time elapsed: %s", time.Now().Sub(start))
 
-	res, err := ecc.GetESErrorLogs(ctx)
+	resp, err := ecc.GetESErrorLogs(ctx)
 	require.NoError(t, err)
-	if !assert.Zero(t, res.Hits.Total.Value, "expected no error logs, but found some") {
-		t.Log("found error logs:")
-		for _, h := range res.Hits.Hits {
-			t.Log(string(h.Source_))
-		}
-	}
+	asserts.ZeroESLogs(t, *resp)
 }
