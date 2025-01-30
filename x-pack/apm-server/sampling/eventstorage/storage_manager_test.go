elastic/apm-server

commit 0ca58b8cb6ee63c1de46181669df85b7cc473d19

TBS: Replace badger with pebble (#15235)
This PR replaces badger with pebble as the database for tail-based sampling. Significant performance gains.

The database of choice is Pebble, which does not have TTL handling built-in,
and we implement our own TTL handling on top of the database:
- TTL is divided up into N parts, where N is partitionsPerTTL.
- A database holds N + 1 + 1 partitions.
- Every TTL/N we will discard the oldest partition, so we keep a rolling window of N+1 partitions.
- Writes will go to the most recent partition, and we'll read across N+1 partitions

x-pack/apm-server/main.go
@@ -36,13 +36,9 @@ const (
 )
 
 var (
-	// badgerDB holds the badger database to use when tail-based sampling is configured.
-	badgerMu                   sync.Mutex
-	badgerDB                   *eventstorage.StorageManager
-	badgerDBMetricRegistration metric.Registration
-
-	storageMu sync.Mutex
-	storage   *eventstorage.ManagedReadWriter
+	// db holds the database to use when tail-based sampling is configured.
+	dbMu sync.Mutex
+	db   *eventstorage.StorageManager
 
 	// samplerUUID is a UUID used to identify sampled trace ID documents
 	// published by this process.
@@ -112,11 +108,10 @@ func newTailSamplingProcessor(args beater.ServerParams) (*sampling.Processor, er
 	}
 
 	storageDir := paths.Resolve(paths.Data, tailSamplingStorageDir)
-	badgerDB, err = getBadgerDB(storageDir, args.MeterProvider)
+	db, err := getDB(storageDir, args.MeterProvider)
 	if err != nil {
-		return nil, fmt.Errorf("failed to get Badger database: %w", err)
+		return nil, fmt.Errorf("failed to get tail-sampling database: %w", err)
 	}
-	readWriter := getStorage(badgerDB)
 
 	policies := make([]sampling.Policy, len(tailSamplingConfig.Policies))
 	for i, in := range tailSamplingConfig.Policies {
@@ -151,10 +146,8 @@ func newTailSamplingProcessor(args beater.ServerParams) (*sampling.Processor, er
 			UUID: samplerUUID.String(),
 		},
 		StorageConfig: sampling.StorageConfig{
-			DB:                    badgerDB,
-			Storage:               readWriter,
-			StorageDir:            storageDir,
-			StorageGCInterval:     tailSamplingConfig.StorageGCInterval,
+			DB:                    db,
+			Storage:               db.NewReadWriter(),
 			StorageLimit:          tailSamplingConfig.StorageLimitParsed,
 			TTL:                   tailSamplingConfig.TTL,
 			DiscardOnWriteFailure: tailSamplingConfig.DiscardOnWriteFailure,
@@ -162,37 +155,21 @@ func newTailSamplingProcessor(args beater.ServerParams) (*sampling.Processor, er
 	})
 }
 
-func getBadgerDB(storageDir string, mp metric.MeterProvider) (*eventstorage.StorageManager, error) {
-	badgerMu.Lock()
-	defer badgerMu.Unlock()
-	if badgerDB == nil {
-		sm, err := eventstorage.NewStorageManager(storageDir)
+func getDB(storageDir string, mp metric.MeterProvider) (*eventstorage.StorageManager, error) {
+	dbMu.Lock()
+	defer dbMu.Unlock()
+	if db == nil {
+		var opts []eventstorage.StorageManagerOptions
+		if mp != nil {
+			opts = append(opts, eventstorage.WithMeterProvider(mp))
+		}
+		sm, err := eventstorage.NewStorageManager(storageDir, opts...)
 		if err != nil {
 			return nil, err
 		}
-		badgerDB = sm
-
-		meter := mp.Meter("github.com/elastic/apm-server/x-pack/apm-server")
-		lsmSizeGauge, _ := meter.Int64ObservableGauge("apm-server.sampling.tail.storage.lsm_size")
-		valueLogSizeGauge, _ := meter.Int64ObservableGauge("apm-server.sampling.tail.storage.value_log_size")
-
-		badgerDBMetricRegistration, _ = meter.RegisterCallback(func(ctx context.Context, o metric.Observer) error {
-			lsmSize, valueLogSize := sm.Size()
-			o.ObserveInt64(lsmSizeGauge, lsmSize)
-			o.ObserveInt64(valueLogSizeGauge, valueLogSize)
-			return nil
-		}, lsmSizeGauge, valueLogSizeGauge)
+		db = sm
 	}
-	return badgerDB, nil
-}
-
-func getStorage(sm *eventstorage.StorageManager) *eventstorage.ManagedReadWriter {
-	storageMu.Lock()
-	defer storageMu.Unlock()
-	if storage == nil {
-		storage = sm.NewReadWriter()
-	}
-	return storage
+	return db, nil
 }
 
 // runServerWithProcessors runs the APM Server and the given list of processors.
@@ -256,25 +233,21 @@ func wrapServer(args beater.ServerParams, runServer beater.RunServerFunc) (beate
 	return args, wrappedRunServer, nil
 }
 
-// closeBadger is called at process exit time to close the badger.DB opened
+// closeDB is called at process exit time to close the StorageManager opened
 // by the tail-based sampling processor constructor, if any. This is never
-// called concurrently with opening badger.DB/accessing the badgerDB global,
-// so it does not need to hold badgerMu.
-func closeBadger() error {
-	if badgerDBMetricRegistration != nil {
-		badgerDBMetricRegistration.Unregister()
-		badgerDBMetricRegistration = nil
-	}
-	if badgerDB != nil {
-		db := badgerDB
-		badgerDB = nil
-		return db.Close()
+// called concurrently with opening DB/accessing the db global,
+// so it does not need to hold dbMu.
+func closeDB() error {
+	if db != nil {
+		err := db.Close()
+		db = nil
+		return err
 	}
 	return nil
 }
 
 func cleanup() error {
-	return closeBadger()
+	return closeDB()
 }
 
 func Main() error {

x-pack/apm-server/sampling/config.go
@@ -100,24 +100,15 @@ type DataStreamConfig struct {
 
 // StorageConfig holds Processor configuration related to event storage.
 type StorageConfig struct {
-	// DB holds the badger database in which event storage will be maintained.
+	// DB holds the StorageManager in which event storage will be maintained.
 	//
 	// DB will not be closed when the processor is closed.
 	DB *eventstorage.StorageManager
 
-	// Storage holds the read writers which provide sharded, locked access to storage.
-	//
-	// Storage lives outside processor lifecycle and will not be closed when processor
-	// is closed
-	Storage rw
+	// Storage is the read writer to DB.
+	Storage eventstorage.RW
 
-	// StorageDir holds the directory in which event storage will be maintained.
-	StorageDir string
-
-	// StorageGCInterval holds the amount of time between storage garbage collections.
-	StorageGCInterval time.Duration
-
-	// StorageLimit for the badger database, in bytes.
+	// StorageLimit for the TBS database, in bytes.
 	StorageLimit uint64
 
 	// TTL holds the amount of time before events and sampling decisions
@@ -246,15 +237,6 @@ func (config DataStreamConfig) validate() error {
 func (config StorageConfig) validate() error {
 	if config.DB == nil {
 		return errors.New("DB unspecified")
-	}
-	if config.Storage == nil {
-		return errors.New("Storage unspecified")
-	}
-	if config.StorageDir == "" {
-		return errors.New("StorageDir unspecified")
-	}
-	if config.StorageGCInterval <= 0 {
-		return errors.New("StorageGCInterval unspecified or negative")
 	}
 	if config.TTL <= 0 {
 		return errors.New("TTL unspecified or negative")

x-pack/apm-server/sampling/eventstorage/rw.go
@@ -1,0 +1,120 @@
+// Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+// or more contributor license agreements. Licensed under the Elastic License 2.0;
+// you may not use this file except in compliance with the Elastic License 2.0.
+
+package eventstorage
+
+import (
+	"errors"
+	"fmt"
+
+	"github.com/elastic/apm-data/model/modelpb"
+)
+
+var (
+	// ErrLimitReached is returned by RW methods when storage usage
+	// is greater than configured limit.
+	ErrLimitReached = errors.New("configured storage limit reached")
+)
+
+// RW is a read writer interface that has methods to read and write trace event and sampling decisions.
+type RW interface {
+	ReadTraceEvents(traceID string, out *modelpb.Batch) error
+	WriteTraceEvent(traceID, id string, event *modelpb.APMEvent) error
+	WriteTraceSampled(traceID string, sampled bool) error
+	IsTraceSampled(traceID string) (bool, error)
+	DeleteTraceEvent(traceID, id string) error
+}
+
+// SplitReadWriter is a RW that splits method calls to eventRW and decisionRW.
+// - *TraceEvent* method calls are passed through to eventRW.
+// - *TraceSampled method calls are passed through to decisionRW.
+type SplitReadWriter struct {
+	eventRW, decisionRW RW
+}
+
+func (s SplitReadWriter) ReadTraceEvents(traceID string, out *modelpb.Batch) error {
+	return s.eventRW.ReadTraceEvents(traceID, out)
+}
+
+func (s SplitReadWriter) WriteTraceEvent(traceID, id string, event *modelpb.APMEvent) error {
+	return s.eventRW.WriteTraceEvent(traceID, id, event)
+}
+
+func (s SplitReadWriter) WriteTraceSampled(traceID string, sampled bool) error {
+	return s.decisionRW.WriteTraceSampled(traceID, sampled)
+}
+
+func (s SplitReadWriter) IsTraceSampled(traceID string) (bool, error) {
+	return s.decisionRW.IsTraceSampled(traceID)
+}
+
+func (s SplitReadWriter) DeleteTraceEvent(traceID, id string) error {
+	return s.eventRW.DeleteTraceEvent(traceID, id)
+}
+
+func (s SplitReadWriter) Close() error {
+	return nil
+}
+
+type storageLimitChecker interface {
+	DiskUsage() uint64
+	StorageLimit() uint64
+}
+
+// StorageLimitReadWriter is a RW that forbids Write* method calls based on disk usage and limit from storageLimitChecker.
+// If there is no limit or limit is not reached, method calls are passed through to nextRW.
+type StorageLimitReadWriter struct {
+	checker storageLimitChecker
+	nextRW  RW
+}
+
+func NewStorageLimitReadWriter(checker storageLimitChecker, nextRW RW) StorageLimitReadWriter {
+	return StorageLimitReadWriter{
+		checker: checker,
+		nextRW:  nextRW,
+	}
+}
+
+func (s StorageLimitReadWriter) checkStorageLimit() error {
+	limit := s.checker.StorageLimit()
+	if limit != 0 { // unlimited storage
+		usage := s.checker.DiskUsage()
+		if usage >= limit {
+			return fmt.Errorf("%w (current: %d, limit %d)", ErrLimitReached, usage, limit)
+		}
+	}
+	return nil
+}
+
+// ReadTraceEvents passes through to s.nextRW.ReadTraceEvents.
+func (s StorageLimitReadWriter) ReadTraceEvents(traceID string, out *modelpb.Batch) error {
+	return s.nextRW.ReadTraceEvents(traceID, out)
+}
+
+// WriteTraceEvent passes through to s.nextRW.WriteTraceEvent only if storage limit is not reached.
+func (s StorageLimitReadWriter) WriteTraceEvent(traceID, id string, event *modelpb.APMEvent) error {
+	if err := s.checkStorageLimit(); err != nil {
+		return err
+	}
+	return s.nextRW.WriteTraceEvent(traceID, id, event)
+}
+
+// WriteTraceSampled passes through to s.nextRW.WriteTraceSampled only if storage limit is not reached.
+func (s StorageLimitReadWriter) WriteTraceSampled(traceID string, sampled bool) error {
+	if err := s.checkStorageLimit(); err != nil {
+		return err
+	}
+	return s.nextRW.WriteTraceSampled(traceID, sampled)
+}
+
+// IsTraceSampled passes through to s.nextRW.IsTraceSampled.
+func (s StorageLimitReadWriter) IsTraceSampled(traceID string) (bool, error) {
+	return s.nextRW.IsTraceSampled(traceID)
+}
+
+// DeleteTraceEvent passes through to s.nextRW.DeleteTraceEvent.
+func (s StorageLimitReadWriter) DeleteTraceEvent(traceID, id string) error {
+	// Technically DeleteTraceEvent writes, but it should have a net effect of reducing disk usage
+	return s.nextRW.DeleteTraceEvent(traceID, id)
+}

commit dcb08ac9a356743407ab32c6a44882598b54dfe9

TBS: make storage_limit follow processor lifecycle; update TBS processor config (#15488)
Fix a regression from #15235 where storage_limit does not follow processor lifecycle.
Remove storage limit from processor config.
Add storage to processor config validation.

x-pack/apm-server/main.go
@@ -147,8 +147,7 @@ func newTailSamplingProcessor(args beater.ServerParams) (*sampling.Processor, er
 		},
 		StorageConfig: sampling.StorageConfig{
 			DB:                    db,
-			Storage:               db.NewReadWriter(),
-			StorageLimit:          tailSamplingConfig.StorageLimitParsed,
+			Storage:               db.NewReadWriter(tailSamplingConfig.StorageLimitParsed),
 			TTL:                   tailSamplingConfig.TTL,
 			DiscardOnWriteFailure: tailSamplingConfig.DiscardOnWriteFailure,
 		},

x-pack/apm-server/sampling/config.go
@@ -108,9 +108,6 @@ type StorageConfig struct {
 	// Storage is the read writer to DB.
 	Storage eventstorage.RW
 
-	// StorageLimit for the TBS database, in bytes.
-	StorageLimit uint64
-
 	// TTL holds the amount of time before events and sampling decisions
 	// are expired from local storage.
 	TTL time.Duration
@@ -237,6 +234,9 @@ func (config DataStreamConfig) validate() error {
 func (config StorageConfig) validate() error {
 	if config.DB == nil {
 		return errors.New("DB unspecified")
+	}
+	if config.Storage == nil {
+		return errors.New("Storage unspecified")
 	}
 	if config.TTL <= 0 {
 		return errors.New("TTL unspecified or negative")

x-pack/apm-server/sampling/eventstorage/rw.go
@@ -62,15 +62,36 @@ type storageLimitChecker interface {
 	StorageLimit() uint64
 }
 
+type storageLimitCheckerFunc struct {
+	diskUsage, storageLimit func() uint64
+}
+
+func NewStorageLimitCheckerFunc(diskUsage, storageLimit func() uint64) storageLimitCheckerFunc {
+	return storageLimitCheckerFunc{
+		diskUsage:    diskUsage,
+		storageLimit: storageLimit,
+	}
+}
+
+func (f storageLimitCheckerFunc) DiskUsage() uint64 {
+	return f.diskUsage()
+}
+
+func (f storageLimitCheckerFunc) StorageLimit() uint64 {
+	return f.storageLimit()
+}
+
 // StorageLimitReadWriter is a RW that forbids Write* method calls based on disk usage and limit from storageLimitChecker.
 // If there is no limit or limit is not reached, method calls are passed through to nextRW.
 type StorageLimitReadWriter struct {
+	name    string
 	checker storageLimitChecker
 	nextRW  RW
 }
 
-func NewStorageLimitReadWriter(checker storageLimitChecker, nextRW RW) StorageLimitReadWriter {
+func NewStorageLimitReadWriter(name string, checker storageLimitChecker, nextRW RW) StorageLimitReadWriter {
 	return StorageLimitReadWriter{
+		name:    name,
 		checker: checker,
 		nextRW:  nextRW,
 	}
@@ -81,7 +102,7 @@ func (s StorageLimitReadWriter) checkStorageLimit() error {
 	if limit != 0 { // unlimited storage
 		usage := s.checker.DiskUsage()
 		if usage >= limit {
-			return fmt.Errorf("%w (current: %d, limit %d)", ErrLimitReached, usage, limit)
+			return fmt.Errorf("%s: %w (current: %d, limit %d)", s.name, ErrLimitReached, usage, limit)
 		}
 	}
 	return nil

x-pack/apm-server/sampling/eventstorage/storage_manager.go
@@ -75,15 +75,13 @@ type StorageManager struct {
 
 	partitioner *Partitioner
 
-	storageLimit atomic.Uint64
-
 	codec Codec
 
 	// subscriberPosMu protects the subscriber file from concurrent RW.
 	subscriberPosMu sync.Mutex
 
-	// cachedDiskUsage is a cached result of DiskUsage
-	cachedDiskUsage atomic.Uint64
+	// cachedDBSize is a cached result of db size.
+	cachedDBSize atomic.Uint64
 
 	// runCh acts as a mutex to ensure only 1 Run is actively running per StorageManager.
 	// as it is possible that 2 separate Run are created by 2 TBS processors during a hot reload.
@@ -202,17 +200,17 @@ func (sm *StorageManager) Size() (lsm, vlog int64) {
 	// Also remember to update
 	// - x-pack/apm-server/sampling/processor.go:CollectMonitoring
 	// - systemtest/benchtest/expvar/metrics.go
-	return int64(sm.DiskUsage()), 0
+	return int64(sm.dbSize()), 0
 }
 
-// DiskUsage returns the disk usage of databases in bytes.
-func (sm *StorageManager) DiskUsage() uint64 {
+// dbSize returns the disk usage of databases in bytes.
+func (sm *StorageManager) dbSize() uint64 {
 	// pebble DiskSpaceUsage overhead is not high, but it adds up when performed per-event.
-	return sm.cachedDiskUsage.Load()
+	return sm.cachedDBSize.Load()
 }
 
 func (sm *StorageManager) updateDiskUsage() {
-	sm.cachedDiskUsage.Store(sm.eventDB.Metrics().DiskSpaceUsage() + sm.decisionDB.Metrics().DiskSpaceUsage())
+	sm.cachedDBSize.Store(sm.eventDB.Metrics().DiskSpaceUsage() + sm.decisionDB.Metrics().DiskSpaceUsage())
 }
 
 // runDiskUsageLoop runs a loop that updates cached disk usage regularly.
@@ -227,10 +225,6 @@ func (sm *StorageManager) runDiskUsageLoop(stopping <-chan struct{}) error {
 			sm.updateDiskUsage()
 		}
 	}
-}
-
-func (sm *StorageManager) StorageLimit() uint64 {
-	return sm.storageLimit.Load()
 }
 
 func (sm *StorageManager) Flush() error {
@@ -260,7 +254,7 @@ func (sm *StorageManager) close() error {
 
 // Reload flushes out pending disk writes to disk by reloading the database.
 // For testing only.
-// Read writers created prior to Reload cannot be used and will need to be recreated via NewReadWriter.
+// Read writers created prior to Reload cannot be used and will need to be recreated via NewUnlimitedReadWriter.
 func (sm *StorageManager) Reload() error {
 	if err := sm.close(); err != nil {
 		return err
@@ -269,7 +263,7 @@ func (sm *StorageManager) Reload() error {
 }
 
 // Run has the same lifecycle as the TBS processor as opposed to StorageManager to facilitate EA hot reload.
-func (sm *StorageManager) Run(stopping <-chan struct{}, ttl time.Duration, storageLimit uint64) error {
+func (sm *StorageManager) Run(stopping <-chan struct{}, ttl time.Duration) error {
 	select {
 	case <-stopping:
 		return nil
@@ -278,8 +272,6 @@ func (sm *StorageManager) Run(stopping <-chan struct{}, ttl time.Duration, stora
 	defer func() {
 		<-sm.runCh
 	}()
-
-	sm.storageLimit.Store(storageLimit)
 
 	g := errgroup.Group{}
 	g.Go(func() error {
@@ -346,11 +338,33 @@ func (sm *StorageManager) WriteSubscriberPosition(data []byte) error {
 	return os.WriteFile(filepath.Join(sm.storageDir, subscriberPositionFile), data, 0644)
 }
 
-func (sm *StorageManager) NewReadWriter() StorageLimitReadWriter {
-	return NewStorageLimitReadWriter(sm, SplitReadWriter{
+// NewUnlimitedReadWriter returns a read writer with no storage limit.
+// For testing only.
+func (sm *StorageManager) NewUnlimitedReadWriter() StorageLimitReadWriter {
+	return sm.NewReadWriter(0)
+}
+
+// NewReadWriter returns a read writer with storage limit.
+func (sm *StorageManager) NewReadWriter(storageLimit uint64) StorageLimitReadWriter {
+	splitRW := SplitReadWriter{
 		eventRW:    sm.eventStorage.NewReadWriter(),
 		decisionRW: sm.decisionStorage.NewReadWriter(),
-	})
+	}
+
+	dbStorageLimit := func() uint64 {
+		return storageLimit
+	}
+	if storageLimit == 0 {
+		sm.logger.Infof("setting database storage limit to unlimited")
+	} else {
+		sm.logger.Infof("setting database storage limit to %.1fgb", float64(storageLimit))
+	}
+
+	// To limit db size to storage_limit
+	dbStorageLimitChecker := NewStorageLimitCheckerFunc(sm.dbSize, dbStorageLimit)
+	dbStorageLimitRW := NewStorageLimitReadWriter("database storage limit", dbStorageLimitChecker, splitRW)
+
+	return dbStorageLimitRW
 }
 
 // wrapNonNilErr only wraps an error with format if the error is not nil.

x-pack/apm-server/sampling/processor.go
@@ -345,7 +345,7 @@ func (p *Processor) Run() error {
 		}
 	})
 	g.Go(func() error {
-		return p.config.DB.Run(p.stopping, p.config.TTL, p.config.StorageLimit)
+		return p.config.DB.Run(p.stopping, p.config.TTL)
 	})
 	g.Go(func() error {
 		// Subscribe to remotely sampled trace IDs. This is cancelled immediately when

x-pack/apm-server/sampling/config_test.go
@@ -73,6 +73,9 @@ func TestNewProcessorConfigInvalid(t *testing.T) {
 	assertInvalidConfigError("invalid storage config: DB unspecified")
 	config.DB = &eventstorage.StorageManager{}
 
+	assertInvalidConfigError("invalid storage config: Storage unspecified")
+	config.Storage = &eventstorage.SplitReadWriter{}
+
 	assertInvalidConfigError("invalid storage config: TTL unspecified or negative")
 	config.TTL = 1
 }

x-pack/apm-server/sampling/eventstorage/rw_test.go
@@ -79,7 +79,7 @@ func TestStorageLimitReadWriter(t *testing.T) {
 		t.Run(fmt.Sprintf("limit=%d,usage=%d", tt.limit, tt.usage), func(t *testing.T) {
 			checker := mockChecker{limit: tt.limit, usage: tt.usage}
 			var callCount int
-			rw := eventstorage.NewStorageLimitReadWriter(checker, mockRW{
+			rw := eventstorage.NewStorageLimitReadWriter("foo_storage_limiter", checker, mockRW{
 				callback: func() {
 					callCount++
 				},

x-pack/apm-server/sampling/eventstorage/storage_bench_test.go
@@ -18,7 +18,7 @@ import (
 func BenchmarkWriteTransaction(b *testing.B) {
 	test := func(b *testing.B, codec eventstorage.Codec, bigTX bool) {
 		sm := newStorageManager(b, eventstorage.WithCodec(codec))
-		readWriter := sm.NewReadWriter()
+		readWriter := sm.NewUnlimitedReadWriter()
 
 		traceID := hex.EncodeToString([]byte{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16})
 		transactionID := hex.EncodeToString([]byte{1, 2, 3, 4, 5, 6, 7, 8})
@@ -79,7 +79,7 @@ func BenchmarkReadEvents(b *testing.B) {
 		for _, count := range counts {
 			b.Run(fmt.Sprintf("%d events", count), func(b *testing.B) {
 				sm := newStorageManager(b, eventstorage.WithCodec(codec))
-				readWriter := sm.NewReadWriter()
+				readWriter := sm.NewUnlimitedReadWriter()
 
 				for i := 0; i < count; i++ {
 					transactionID := uuid.Must(uuid.NewV4()).String()
@@ -154,7 +154,7 @@ func BenchmarkReadEventsHit(b *testing.B) {
 		for _, hit := range []bool{false, true} {
 			b.Run(fmt.Sprintf("hit=%v", hit), func(b *testing.B) {
 				sm := newStorageManager(b)
-				readWriter := sm.NewReadWriter()
+				readWriter := sm.NewUnlimitedReadWriter()
 
 				traceIDs := make([]string, b.N)
 
@@ -185,7 +185,7 @@ func BenchmarkReadEventsHit(b *testing.B) {
 					}
 				}
 
-				readWriter = sm.NewReadWriter()
+				readWriter = sm.NewUnlimitedReadWriter()
 
 				b.ResetTimer()
 				var batch modelpb.Batch
@@ -224,7 +224,7 @@ func BenchmarkIsTraceSampled(b *testing.B) {
 
 	// Test with varying numbers of events in the trace.
 	sm := newStorageManager(b)
-	readWriter := sm.NewReadWriter()
+	readWriter := sm.NewUnlimitedReadWriter()
 
 	if err := readWriter.WriteTraceSampled(sampledTraceUUID.String(), true); err != nil {
 		b.Fatal(err)

x-pack/apm-server/sampling/eventstorage/storage_manager_bench_test.go
@@ -12,12 +12,12 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func BenchmarkStorageManager_DiskUsage(b *testing.B) {
+func BenchmarkStorageManager_Size(b *testing.B) {
 	stopping := make(chan struct{})
 	defer close(stopping)
 	sm := newStorageManager(b)
-	go sm.Run(stopping, time.Second, 0)
-	rw := sm.NewReadWriter()
+	go sm.Run(stopping, time.Second)
+	rw := sm.NewUnlimitedReadWriter()
 	for i := 0; i < 1000; i++ {
 		traceID := uuid.Must(uuid.NewV4()).String()
 		txnID := uuid.Must(uuid.NewV4()).String()
@@ -29,7 +29,7 @@ func BenchmarkStorageManager_DiskUsage(b *testing.B) {
 	}
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
-		_ = sm.DiskUsage()
+		_, _ = sm.Size()
 	}
 	b.StopTimer()
 }

x-pack/apm-server/sampling/eventstorage/storage_manager_test.go
@@ -32,7 +32,7 @@ func newStorageManagerNoCleanup(tb testing.TB, path string, opts ...eventstorage
 
 func TestStorageManager_samplingDecisionTTL(t *testing.T) {
 	sm := newStorageManager(t)
-	rw := sm.NewReadWriter()
+	rw := sm.NewUnlimitedReadWriter()
 	traceID := uuid.Must(uuid.NewV4()).String()
 	err := rw.WriteTraceSampled(traceID, true)
 	assert.NoError(t, err)
@@ -65,7 +65,7 @@ func TestStorageManager_samplingDecisionTTL(t *testing.T) {
 
 func TestStorageManager_eventTTL(t *testing.T) {
 	sm := newStorageManager(t)
-	rw := sm.NewReadWriter()
+	rw := sm.NewUnlimitedReadWriter()
 	traceID := uuid.Must(uuid.NewV4()).String()
 	txnID1 := uuid.Must(uuid.NewV4()).String()
 	txn1 := makeTransaction(txnID1, traceID)
@@ -119,7 +119,7 @@ func TestStorageManager_partitionID(t *testing.T) {
 	assert.NoError(t, sm.RotatePartitions())
 
 	// write to partition 1
-	err := sm.NewReadWriter().WriteTraceSampled(traceID, true)
+	err := sm.NewUnlimitedReadWriter().WriteTraceSampled(traceID, true)
 	assert.NoError(t, err)
 
 	assert.NoError(t, sm.Close())
@@ -127,7 +127,7 @@ func TestStorageManager_partitionID(t *testing.T) {
 	// it should read directly from partition 1 on startup instead of 0
 	sm = newStorageManagerNoCleanup(t, tmpDir)
 	defer sm.Close()
-	sampled, err := sm.NewReadWriter().IsTraceSampled(traceID)
+	sampled, err := sm.NewUnlimitedReadWriter().IsTraceSampled(traceID)
 	assert.NoError(t, err)
 	assert.True(t, sampled)
 }
@@ -136,29 +136,36 @@ func TestStorageManager_DiskUsage(t *testing.T) {
 	stopping := make(chan struct{})
 	defer close(stopping)
 	sm := newStorageManager(t)
-	go sm.Run(stopping, time.Second, 0)
-	old := sm.DiskUsage()
+	go sm.Run(stopping, time.Second)
 
-	err := sm.NewReadWriter().WriteTraceSampled("foo", true)
+	lsm, vlog := sm.Size()
+	oldSize := lsm + vlog
+
+	err := sm.NewUnlimitedReadWriter().WriteTraceSampled("foo", true)
 	require.NoError(t, err)
 
 	err = sm.Flush()
 	require.NoError(t, err)
 
 	assert.Eventually(t, func() bool {
-		return sm.DiskUsage() > old
+		lsm, vlog := sm.Size()
+		newSize := lsm + vlog
+		return newSize > oldSize
 	}, 10*time.Second, 100*time.Millisecond)
 
-	old = sm.DiskUsage()
+	lsm, vlog = sm.Size()
+	oldSize = lsm + vlog
 
-	err = sm.NewReadWriter().WriteTraceEvent("foo", "bar", makeTransaction("bar", "foo"))
+	err = sm.NewUnlimitedReadWriter().WriteTraceEvent("foo", "bar", makeTransaction("bar", "foo"))
 	require.NoError(t, err)
 
 	err = sm.Flush()
 	require.NoError(t, err)
 
 	assert.Eventually(t, func() bool {
-		return sm.DiskUsage() > old
+		lsm, vlog := sm.Size()
+		newSize := lsm + vlog
+		return newSize > oldSize
 	}, 10*time.Second, 100*time.Millisecond)
 }
 
@@ -167,9 +174,35 @@ func TestStorageManager_Run(t *testing.T) {
 	stopping := make(chan struct{})
 	sm := newStorageManager(t)
 	go func() {
-		assert.NoError(t, sm.Run(stopping, time.Second, 0))
+		assert.NoError(t, sm.Run(stopping, time.Second))
 		close(done)
 	}()
+	close(stopping)
+	<-done
+}
+
+func TestStorageManager_StorageLimit(t *testing.T) {
+	done := make(chan struct{})
+	stopping := make(chan struct{})
+	sm := newStorageManager(t)
+	go func() {
+		assert.NoError(t, sm.Run(stopping, time.Second))
+		close(done)
+	}()
+	require.NoError(t, sm.Flush())
+	lsm, _ := sm.Size()
+	assert.Greater(t, lsm, int64(1))
+
+	traceID := uuid.Must(uuid.NewV4()).String()
+	txnID := uuid.Must(uuid.NewV4()).String()
+	txn := makeTransaction(txnID, traceID)
+
+	small := sm.NewReadWriter(1)
+	assert.ErrorIs(t, small.WriteTraceEvent(traceID, txnID, txn), eventstorage.ErrLimitReached)
+
+	big := sm.NewReadWriter(10 << 10)
+	assert.NoError(t, big.WriteTraceEvent(traceID, txnID, txn))
+
 	close(stopping)
 	<-done
 }

x-pack/apm-server/sampling/processor_test.go
@@ -61,14 +61,14 @@ func TestProcessAlreadyTailSampled(t *testing.T) {
 	// subsequent events in the trace will be reported immediately.
 	trace1 := modelpb.Trace{Id: "0102030405060708090a0b0c0d0e0f10"}
 	trace2 := modelpb.Trace{Id: "0102030405060708090a0b0c0d0e0f11"}
-	writer := config.DB.NewReadWriter()
+	writer := config.DB.NewUnlimitedReadWriter()
 	assert.NoError(t, writer.WriteTraceSampled(trace2.Id, true))
 
 	// simulate 2 TTL
 	assert.NoError(t, config.DB.RotatePartitions())
 	assert.NoError(t, config.DB.RotatePartitions())
 
-	writer = config.DB.NewReadWriter()
+	writer = config.DB.NewUnlimitedReadWriter()
 	assert.NoError(t, writer.WriteTraceSampled(trace1.Id, true))
 
 	require.NoError(t, config.DB.Flush())
@@ -127,7 +127,7 @@ func TestProcessAlreadyTailSampled(t *testing.T) {
 	// Stop the processor and flush global storage so we can access the database.
 	assert.NoError(t, processor.Stop(context.Background()))
 	assert.NoError(t, config.DB.Flush())
-	reader := config.DB.NewReadWriter()
+	reader := config.DB.NewUnlimitedReadWriter()
 
 	batch = nil
 	err = reader.ReadTraceEvents(trace1.Id, &batch)
@@ -243,7 +243,7 @@ func TestProcessLocalTailSampling(t *testing.T) {
 			// Stop the processor and flush global storage so we can access the database.
 			assert.NoError(t, processor.Stop(context.Background()))
 			assert.NoError(t, config.DB.Flush())
-			reader := config.DB.NewReadWriter()
+			reader := config.DB.NewUnlimitedReadWriter()
 
 			sampled, err := reader.IsTraceSampled(sampledTraceID)
 			assert.NoError(t, err)
@@ -307,7 +307,7 @@ func TestProcessLocalTailSamplingUnsampled(t *testing.T) {
 	// Stop the processor so we can access the database.
 	assert.NoError(t, processor.Stop(context.Background()))
 	assert.NoError(t, config.DB.Flush())
-	reader := config.DB.NewReadWriter()
+	reader := config.DB.NewUnlimitedReadWriter()
 
 	var anyUnsampled bool
 	for _, traceID := range traceIDs {
@@ -470,7 +470,7 @@ func TestProcessRemoteTailSampling(t *testing.T) {
 
 	assert.Empty(t, cmp.Diff(trace1Events, events, protocmp.Transform()))
 
-	reader := config.DB.NewReadWriter()
+	reader := config.DB.NewUnlimitedReadWriter()
 
 	sampled, err := reader.IsTraceSampled(traceID1)
 	assert.NoError(t, err)
@@ -682,12 +682,10 @@ func TestStorageLimit(t *testing.T) {
 	err := config.DB.Reload()
 	assert.NoError(t, err)
 
-	config.Storage = config.DB.NewReadWriter()
-
 	lsm, vlog := config.DB.Size()
 	assert.Greater(t, lsm+vlog, int64(10<<10))
 
-	config.StorageLimit = 10 << 10 // Set the storage limit to smaller than existing storage
+	config.Storage = config.DB.NewReadWriter(10 << 10) // Set the storage limit to smaller than existing storage
 
 	writeBatch(1000, config, func(b modelpb.Batch) {
 		assert.Len(t, b, 1000)
@@ -758,7 +756,7 @@ func TestGracefulShutdown(t *testing.T) {
 	assert.Empty(t, batch)
 	assert.NoError(t, processor.Stop(context.Background()))
 
-	reader := config.DB.NewReadWriter()
+	reader := config.DB.NewUnlimitedReadWriter()
 
 	var count int
 	for i := 0; i < totalTraces; i++ {
@@ -815,10 +813,9 @@ func newTempdirConfig(tb testing.TB) testConfig {
 				UUID: "local-apm-server",
 			},
 			StorageConfig: sampling.StorageConfig{
-				DB:           db,
-				Storage:      db.NewReadWriter(),
-				TTL:          30 * time.Minute,
-				StorageLimit: 0, // No storage limit.
+				DB:      db,
+				Storage: db.NewUnlimitedReadWriter(),
+				TTL:     30 * time.Minute,
 			},
 		},
 	}