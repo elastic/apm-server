06c0c1b3f39aa7875dd463a488cce1df6f9805ef
Eric Yap <eric.yap@elastic.co>
2025-03-04 15:28:54 +0800

    Add test parallel

--- functionaltests/8_16_test.go
+++ functionaltests/8_16_test.go
@@ -28,6 +28,7 @@ import (
 )
 
 func TestUpgrade_8_15_4_to_8_16_0(t *testing.T) {
+	t.Parallel()
 	ecAPICheck(t)
 
 	tt := singleUpgradeTestCase{
@@ -63,6 +64,7 @@ func TestUpgrade_8_15_4_to_8_16_0(t *testing.T) {
 }
 
 func TestUpgrade_8_13_4_to_8_16_0_Reroute(t *testing.T) {
+	t.Parallel()
 	ecAPICheck(t)
 
 	tt := singleUpgradeTestCase{
