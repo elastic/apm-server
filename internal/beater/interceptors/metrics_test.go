d48d5fd11a0784520dcc4c541f848a5978f84800
Eric <22215921+ericywl@users.noreply.github.com>
2025-03-17 13:43:08 +0800

    fix: metrics interceptor concurrent map read write (#16182)
    * Fix metrics interceptor concurrent map read write
    
    * Use sync.Map for counters and histograms
    
    * Modify concurrency test
    
    * Add counter value check in unit test
    
    (cherry picked from commit 2ca9f908fefb31e3e0e6a005c09b60887be92b59)
    
    # Conflicts:
    #	internal/beater/interceptors/metrics.go
    #	internal/beater/interceptors/metrics_test.go

--- internal/beater/interceptors/metrics_test.go
+++ internal/beater/interceptors/metrics_test.go
@@ -20,9 +20,14 @@ package interceptors
 import (
 	"context"
 	"errors"
+	"sync"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+<<<<<<< HEAD
+=======
+	"github.com/stretchr/testify/require"
+>>>>>>> 2ca9f908 (fix: metrics interceptor concurrent map read write (#16182))
 	sdkmetric "go.opentelemetry.io/otel/sdk/metric"
 	"go.opentelemetry.io/otel/sdk/metric/metricdata"
 	"google.golang.org/grpc"
@@ -234,8 +239,71 @@ func assertMonitoring(t *testing.T, expected map[request.ResultID]int64, actual 
 	}
 }
 
+<<<<<<< HEAD
 type requestMetricsFunc func(fullMethod string) map[request.ResultID]*monitoring.Int
 
 func (f requestMetricsFunc) RequestMetrics(fullMethod string) map[request.ResultID]*monitoring.Int {
 	return f(fullMethod)
+=======
+func TestMetrics_ConcurrentSafe(t *testing.T) {
+	reader := sdkmetric.NewManualReader(sdkmetric.WithTemporalitySelector(
+		func(ik sdkmetric.InstrumentKind) metricdata.Temporality {
+			return metricdata.DeltaTemporality
+		},
+	))
+	mp := sdkmetric.NewMeterProvider(sdkmetric.WithReader(reader))
+	logger := logp.NewLogger("interceptor.metrics.test")
+	interceptor := Metrics(logger, mp)
+
+	ctx := context.Background()
+	info := &grpc.UnaryServerInfo{
+		FullMethod: "/opentelemetry.proto.collector.trace.v1.TraceService/Export",
+	}
+
+	doNothing := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return req, nil
+	}
+
+	type respAndErr struct {
+		resp interface{}
+		err  error
+	}
+
+	const numG = 10
+	ch := make(chan respAndErr, numG)
+	var wg sync.WaitGroup
+	for range numG {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			resp, err := interceptor(ctx, "hello", info, doNothing)
+			ch <- respAndErr{resp: resp, err: err}
+		}()
+	}
+
+	wg.Wait()
+	close(ch)
+	for r := range ch {
+		assert.Equal(t, "hello", r.resp, "unexpected response")
+		assert.NoError(t, r.err, "unexpected error")
+	}
+
+	var rm metricdata.ResourceMetrics
+	require.NoError(t, reader.Collect(context.Background(), &rm))
+	require.NotEmpty(t, rm.ScopeMetrics)
+
+	findRequestCountMetrics := func() (metricdata.Metrics, bool) {
+		for _, mt := range rm.ScopeMetrics[0].Metrics {
+			if mt.Name == "grpc.server.request.count" {
+				return mt, true
+			}
+		}
+		return metricdata.Metrics{}, false
+	}
+
+	mt, exist := findRequestCountMetrics()
+	require.True(t, exist)
+	counter := mt.Data.(metricdata.Sum[int64])
+	assert.EqualValues(t, numG, counter.DataPoints[0].Value, "unexpected counter value")
+>>>>>>> 2ca9f908 (fix: metrics interceptor concurrent map read write (#16182))
 }
