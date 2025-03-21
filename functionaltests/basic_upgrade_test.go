14f12c7518d1c98162d6b3c778867f6b41551a2e
Eric Yap <eric.yap@elastic.co>
2025-03-21 18:30:32 +0800

    Add reroute tests

--- functionaltests/basic_upgrade_test.go
+++ functionaltests/basic_upgrade_test.go
@@ -18,8 +18,11 @@
 package functionaltests
 
 import (
+	"context"
 	"testing"
 
+	"github.com/elastic/apm-server/functionaltests/internal/esclient"
+	"github.com/elastic/apm-server/functionaltests/internal/kbclient"
 	"github.com/elastic/go-elasticsearch/v8/typedapi/types"
 
 	"github.com/elastic/apm-server/functionaltests/internal/ecclient"
@@ -64,7 +67,20 @@ func runBasicUpgradeILMTest(
 		apmErrorLogsIgnored: apmErrorLogsIgnored,
 	}
 
-	testCase.Run(t)
+	t.Run("Default", func(t *testing.T) {
+		testCase.Run(t)
+	})
+
+	t.Run("Reroute", func(t *testing.T) {
+		rerouteNamespace := "rerouted"
+		testCase.dataStreamNamespace = rerouteNamespace
+		testCase.setupFn = func(t *testing.T, ctx context.Context, esc *esclient.Client, kbc *kbclient.Client) error {
+			t.Log("create reroute processors")
+			return createRerouteIngestPipeline(t, ctx, esc, rerouteNamespace)
+		}
+
+		testCase.Run(t)
+	})
 }
 
 // runBasicUpgradeLazyRolloverDSLTest performs a basic upgrade test from
@@ -116,5 +132,18 @@ func runBasicUpgradeLazyRolloverDSLTest(
 		apmErrorLogsIgnored: apmErrorLogsIgnored,
 	}
 
-	testCase.Run(t)
+	t.Run("Default", func(t *testing.T) {
+		testCase.Run(t)
+	})
+
+	t.Run("Reroute", func(t *testing.T) {
+		rerouteNamespace := "rerouted"
+		testCase.dataStreamNamespace = rerouteNamespace
+		testCase.setupFn = func(t *testing.T, ctx context.Context, esc *esclient.Client, kbc *kbclient.Client) error {
+			t.Log("create reroute processors")
+			return createRerouteIngestPipeline(t, ctx, esc, rerouteNamespace)
+		}
+
+		testCase.Run(t)
+	})
 }
