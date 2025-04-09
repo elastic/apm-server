e798117b6657721707424845c8484046d178c18d
kruskall <99559985+kruskall@users.noreply.github.com>
2025-04-09 16:31:36 +0200

    test: add basic job for fips system-tests (#16174)
    * test: add basic job for fips system-tests
    
    * test: simplify systemtest apm-server build func
    
    * test: update certificates for fips compliance
    
    * test: generate subject key id for fips compliance
    
    * test: update TLS protocol error message
    
    * Update tls_test.go
    
    * Update command.go
    
    * Update command.go
    
    * test: remove unnecessary files and update tls tests
    
    * Update tls_nofips_test.go
    
    * Update systemtest/apmservertest/server.go
    
    Co-authored-by: Eric <22215921+ericywl@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Eric <22215921+ericywl@users.noreply.github.com>
    (cherry picked from commit 8f580a3109c85efb2b7861da74d1be9e5077fa7c)
    
    # Conflicts:
    #	.github/workflows/ci.yml

--- systemtest/tls_test.go
+++ systemtest/tls_test.go
@@ -48,7 +48,7 @@ func TestDefaultTLSConfig(t *testing.T) {
 	}
 
 	t.Run("compatible_cipher_suite", func(t *testing.T) {
-		err := attemptRequest(t, 0, 0, tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256)
+		err := attemptRequest(t, 0, 0, tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256)
 		require.NoError(t, err)
 	})
 
@@ -61,25 +61,13 @@ func TestDefaultTLSConfig(t *testing.T) {
 		err := attemptRequest(t, tls.VersionTLS13, tls.VersionTLS13)
 		require.NoError(t, err)
 	})
-
-	t.Run("incompatible_cipher_suite", func(t *testing.T) {
-		err := attemptRequest(t, tls.VersionTLS12, tls.VersionTLS12, tls.TLS_RSA_WITH_3DES_EDE_CBC_SHA)
-		require.Error(t, err)
-		assert.Regexp(t, ".*tls: handshake failure", err.Error())
-	})
-
-	t.Run("incompatible_protocol", func(t *testing.T) {
-		err := attemptRequest(t, tls.VersionTLS10, tls.VersionTLS10)
-		require.Error(t, err)
-		assert.Regexp(t, ".*tls: protocol version not supported", err.Error())
-	})
 }
 
 func TestCustomTLSConfig(t *testing.T) {
 	srv := apmservertest.NewUnstartedServerTB(t)
 	srv.Config.TLS = &apmservertest.TLSConfig{
 		SupportedProtocols: []string{"TLSv1.2"},
-		CipherSuites:       []string{"ECDHE-RSA-AES-128-GCM-SHA256"},
+		CipherSuites:       []string{"ECDHE-ECDSA-AES-128-GCM-SHA256"},
 	}
 	require.NoError(t, srv.StartTLS())
 
@@ -98,7 +86,7 @@ func TestCustomTLSConfig(t *testing.T) {
 	}
 
 	t.Run("compatible_cipher_suite", func(t *testing.T) {
-		err := attemptRequest(t, 0, 0, tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256)
+		err := attemptRequest(t, 0, 0, tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256)
 		require.NoError(t, err)
 	})
 
