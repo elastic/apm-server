121bd92275eb7608d1e2cc7a9f89e2fb6e7407dc
Eric Yap <eric.yap@elastic.co>
2025-05-16 11:26:48 +0800

    Add migrate managed in 8.x to standalone tests

--- functionaltests/standalone_test.go
+++ functionaltests/standalone_test.go
@@ -24,154 +24,9 @@ import (
 	"github.com/elastic/apm-server/functionaltests/internal/ecclient"
 )
 
-func TestUpgrade_7_17_to_8_x_Snapshot_Standalone_to_Managed(t *testing.T) {
-	from := getLatestSnapshot(t, "7.17")
-	to := getLatestSnapshot(t, "8")
-	if !from.CanUpgradeTo(to.Version) {
-		t.Skipf("upgrade from %s to %s is not allowed", from.Version, to.Version)
-		return
-	}
-
-	t.Run("UpgradeFirst", func(t *testing.T) {
-		t.Parallel()
-		runner := upgradeThenManaged78Runner(from.Version, to.Version)
-		runner.Run(t)
-	})
-
-	t.Run("ManagedFirst", func(t *testing.T) {
-		t.Parallel()
-		runner := managedThenUpgrade78Runner(from.Version, to.Version)
-		runner.Run(t)
-	})
-}
-
-func TestUpgrade_7_17_to_8_x_BC_Standalone_to_Managed(t *testing.T) {
-	from := getLatestVersionOrSkip(t, "7.17")
-	to := getLatestBCOrSkip(t, "8")
-	if !from.CanUpgradeTo(to.Version) {
-		t.Skipf("upgrade from %s to %s is not allowed", from.Version, to.Version)
-		return
-	}
-
-	t.Run("UpgradeFirst", func(t *testing.T) {
-		t.Parallel()
-		runner := upgradeThenManaged78Runner(from.Version, to.Version)
-		runner.Run(t)
-	})
-
-	t.Run("ManagedFirst", func(t *testing.T) {
-		t.Parallel()
-		runner := managedThenUpgrade78Runner(from.Version, to.Version)
-		runner.Run(t)
-	})
-}
-
-func upgradeThenManaged78Runner(fromVersion, toVersion ecclient.StackVersion) testStepsRunner {
-	// Data streams in 8.x should be all ILM if upgraded to a stack < 8.15 and > 8.16.
-	checkILM := asserts.CheckDataStreamsWant{
-		Quantity:         8,
-		PreferIlm:        true,
-		DSManagedBy:      managedByILM,
-		IndicesPerDS:     1,
-		IndicesManagedBy: []string{managedByILM},
-	}
-
-	return testStepsRunner{
-		Steps: []testStep{
-			createStep{
-				DeployVersion:     fromVersion,
-				APMDeploymentMode: apmStandalone,
-			},
-			ingestV7Step{},
-			upgradeV7Step{NewVersion: toVersion},
-			ingestStep{CheckDataStream: checkILM},
-			migrateManagedStep{},
-			ingestStep{CheckDataStream: checkILM},
-			checkErrorLogsStep{
-				ESErrorLogsIgnored: esErrorLogs{
-					eventLoopShutdown,
-				},
-				APMErrorLogsIgnored: apmErrorLogs{
-					tlsHandshakeError,
-					esReturnedUnknown503,
-					refreshCache503,
-					// TODO: remove once fixed
-					populateSourcemapFetcher403,
-				},
-			},
-		},
-	}
-}
-
-func managedThenUpgrade78Runner(fromVersion, toVersion ecclient.StackVersion) testStepsRunner {
-	checkILM := asserts.CheckDataStreamIndividualWant{
-		PreferIlm:        true,
-		DSManagedBy:      managedByILM,
-		IndicesManagedBy: []string{managedByILM},
-	}
-	checkILMRollover := asserts.CheckDataStreamIndividualWant{
-		PreferIlm:        true,
-		DSManagedBy:      managedByILM,
-		IndicesManagedBy: []string{managedByILM, managedByILM},
-	}
-
-	check := map[string]asserts.CheckDataStreamIndividualWant{
-		// These data streams are created in 7.x as well, so when we ingest again
-		// in 8.x, they will be rolled-over.
-		"traces-apm-%s":                     checkILMRollover,
-		"metrics-apm.app.opbeans_python-%s": checkILMRollover,
-		"metrics-apm.internal-%s":           checkILMRollover,
-		"logs-apm.error-%s":                 checkILMRollover,
-		// These data streams are only created in 8.x, so they will only have 1 index.
-		"metrics-apm.service_destination.1m-%s": checkILM,
-		"metrics-apm.service_transaction.1m-%s": checkILM,
-		"metrics-apm.service_summary.1m-%s":     checkILM,
-		"metrics-apm.transaction.1m-%s":         checkILM,
-	}
-
-	// These data streams are created in 7.x, but not used in 8.x,
-	// so we ignore them to avoid wrong assertions.
-	ignoredDataStreams := []string{
-		"metrics-apm.app.opbeans_node-%s",
-		"metrics-apm.app.opbeans_ruby-%s",
-		"metrics-apm.app.opbeans_go-%s",
-	}
-
-	return testStepsRunner{
-		Steps: []testStep{
-			createStep{
-				DeployVersion:     fromVersion,
-				APMDeploymentMode: apmStandalone,
-			},
-			ingestV7Step{},
-			migrateManagedStep{},
-			ingestV7Step{},
-			upgradeV7Step{NewVersion: toVersion},
-			ingestStep{
-				IgnoreDataStreams:         ignoredDataStreams,
-				CheckIndividualDataStream: check,
-			},
-			checkErrorLogsStep{
-				ESErrorLogsIgnored: esErrorLogs{
-					eventLoopShutdown,
-					addIndexTemplateTracesError,
-				},
-				APMErrorLogsIgnored: apmErrorLogs{
-					tlsHandshakeError,
-					esReturnedUnknown503,
-					refreshCache503,
-					preconditionClusterInfoCtxCanceled,
-					waitServerReadyCtxCanceled,
-					grpcServerStopped,
-					// TODO: remove once fixed
-					populateSourcemapFetcher403,
-				},
-			},
-		},
-	}
-}
-
 func TestUpgrade_7_17_to_8_x_to_9_x_Snapshot_Standalone_to_Managed(t *testing.T) {
+	t.Parallel()
+
 	from7 := getLatestSnapshot(t, "7.17")
 	to8 := getLatestSnapshot(t, "8")
 	to9 := getLatestSnapshot(t, "9")
@@ -184,20 +39,28 @@ func TestUpgrade_7_17_to_8_x_to_9_x_Snapshot_Standalone_to_Managed(t *testing.T)
 		return
 	}
 
-	t.Run("UpgradeFirst", func(t *testing.T) {
+	t.Run("Managed7", func(t *testing.T) {
 		t.Parallel()
-		runner := upgradeThenManaged789Runner(from7.Version, to8.Version, to9.Version)
+		runner := managed7Runner(from7.Version, to8.Version, to9.Version)
 		runner.Run(t)
 	})
 
-	t.Run("ManagedFirst", func(t *testing.T) {
+	t.Run("Managed8", func(t *testing.T) {
 		t.Parallel()
-		runner := managedThenUpgrade789Runner(from7.Version, to8.Version, to9.Version)
+		runner := managed8Runner(from7.Version, to9.Version, to9.Version)
+		runner.Run(t)
+	})
+
+	t.Run("Managed9", func(t *testing.T) {
+		t.Parallel()
+		runner := managed9Runner(from7.Version, to8.Version, to9.Version)
 		runner.Run(t)
 	})
 }
 
 func TestUpgrade_7_17_to_8_x_to_9_x_BC_Standalone_to_Managed(t *testing.T) {
+	t.Parallel()
+
 	from7 := getLatestVersionOrSkip(t, "7.17")
 	to8 := getLatestVersionOrSkip(t, "8")
 	to9 := getLatestBCOrSkip(t, "9")
@@ -210,68 +73,26 @@ func TestUpgrade_7_17_to_8_x_to_9_x_BC_Standalone_to_Managed(t *testing.T) {
 		return
 	}
 
-	t.Run("UpgradeFirst", func(t *testing.T) {
+	t.Run("Managed in 7", func(t *testing.T) {
 		t.Parallel()
-		runner := upgradeThenManaged789Runner(from7.Version, to8.Version, to9.Version)
+		runner := managed7Runner(from7.Version, to8.Version, to9.Version)
 		runner.Run(t)
 	})
 
-	t.Run("ManagedFirst", func(t *testing.T) {
+	t.Run("Managed in 8", func(t *testing.T) {
 		t.Parallel()
-		runner := managedThenUpgrade789Runner(from7.Version, to8.Version, to9.Version)
+		runner := managed8Runner(from7.Version, to9.Version, to9.Version)
+		runner.Run(t)
+	})
+
+	t.Run("Managed in 9", func(t *testing.T) {
+		t.Parallel()
+		runner := managed9Runner(from7.Version, to8.Version, to9.Version)
 		runner.Run(t)
 	})
 }
 
-func upgradeThenManaged789Runner(fromVersion7, toVersion8, toVersion9 ecclient.StackVersion) testStepsRunner {
-	// Data streams in 8.x should be all ILM if upgraded to a stack < 8.15 and > 8.16.
-	checkILM := asserts.CheckDataStreamsWant{
-		Quantity:         8,
-		PreferIlm:        true,
-		DSManagedBy:      managedByILM,
-		IndicesPerDS:     1,
-		IndicesManagedBy: []string{managedByILM},
-	}
-
-	return testStepsRunner{
-		Steps: []testStep{
-			// Start from 7.x.
-			createStep{
-				DeployVersion:     fromVersion7,
-				APMDeploymentMode: apmStandalone,
-			},
-			ingestV7Step{},
-			// Upgrade to 8.x.
-			upgradeV7Step{NewVersion: toVersion8},
-			ingestStep{CheckDataStream: checkILM},
-			// Resolve deprecations and upgrade to 9.x.
-			resolveDeprecationsStep{},
-			upgradeStep{
-				NewVersion:      toVersion9,
-				CheckDataStream: checkILM,
-			},
-			ingestStep{CheckDataStream: checkILM},
-			// Migrate to managed.
-			migrateManagedStep{},
-			ingestStep{CheckDataStream: checkILM},
-			checkErrorLogsStep{
-				ESErrorLogsIgnored: esErrorLogs{
-					eventLoopShutdown,
-				},
-				APMErrorLogsIgnored: apmErrorLogs{
-					tlsHandshakeError,
-					esReturnedUnknown503,
-					refreshCache503,
-					populateSourcemapFetcher403,
-					refreshCache403,
-					refreshCacheESConfigInvalid,
-				},
-			},
-		},
-	}
-}
-
-func managedThenUpgrade789Runner(fromVersion7, toVersion8, toVersion9 ecclient.StackVersion) testStepsRunner {
+func managed7Runner(fromVersion7, toVersion8, toVersion9 ecclient.StackVersion) testStepsRunner {
 	checkILM := asserts.CheckDataStreamIndividualWant{
 		PreferIlm:        true,
 		DSManagedBy:      managedByILM,
@@ -346,6 +167,138 @@ func managedThenUpgrade789Runner(fromVersion7, toVersion8, toVersion9 ecclient.S
 					preconditionClusterInfoCtxCanceled,
 					waitServerReadyCtxCanceled,
 					grpcServerStopped,
+					populateSourcemapFetcher403,
+					refreshCache403,
+					refreshCacheESConfigInvalid,
+				},
+			},
+		},
+	}
+}
+
+func managed8Runner(fromVersion7, toVersion8, toVersion9 ecclient.StackVersion) testStepsRunner {
+	checkILMAll := asserts.CheckDataStreamsWant{
+		Quantity:         8,
+		PreferIlm:        true,
+		DSManagedBy:      managedByILM,
+		IndicesPerDS:     1,
+		IndicesManagedBy: []string{managedByILM},
+	}
+	checkILM := asserts.CheckDataStreamIndividualWant{
+		PreferIlm:        true,
+		DSManagedBy:      managedByILM,
+		IndicesManagedBy: []string{managedByILM},
+	}
+	checkILMRollover := asserts.CheckDataStreamIndividualWant{
+		PreferIlm:        true,
+		DSManagedBy:      managedByILM,
+		IndicesManagedBy: []string{managedByILM, managedByILM},
+	}
+
+	check := map[string]asserts.CheckDataStreamIndividualWant{
+		// These data streams are created in 7.x as well, so when we ingest
+		// again in 8.x, they will be rolled-over.
+		"traces-apm-%s":                     checkILMRollover,
+		"metrics-apm.app.opbeans_python-%s": checkILMRollover,
+		"metrics-apm.internal-%s":           checkILMRollover,
+		"logs-apm.error-%s":                 checkILMRollover,
+		// These data streams are only created in 8.x, so they will only have
+		// 1 index.
+		"metrics-apm.service_destination.1m-%s": checkILM,
+		"metrics-apm.service_transaction.1m-%s": checkILM,
+		"metrics-apm.service_summary.1m-%s":     checkILM,
+		"metrics-apm.transaction.1m-%s":         checkILM,
+	}
+
+	// These data streams are created in 7.x, but not used in 8.x and 9.x,
+	// so we ignore them to avoid wrong assertions.
+	ignoredDataStreams := []string{
+		"metrics-apm.app.opbeans_node-%s",
+		"metrics-apm.app.opbeans_ruby-%s",
+		"metrics-apm.app.opbeans_go-%s",
+	}
+
+	return testStepsRunner{
+		Steps: []testStep{
+			// Start from 7.x.
+			createStep{
+				DeployVersion:     fromVersion7,
+				APMDeploymentMode: apmStandalone,
+			},
+			ingestV7Step{},
+			// Upgrade to 8.x.
+			upgradeV7Step{NewVersion: toVersion8},
+			ingestStep{CheckDataStream: checkILMAll},
+			// Migrate to managed
+			migrateManagedStep{},
+			ingestStep{CheckDataStream: checkILMAll},
+			// Resolve deprecations and upgrade to 9.x.
+			resolveDeprecationsStep{},
+			upgradeStep{
+				NewVersion:                toVersion9,
+				IgnoreDataStreams:         ignoredDataStreams,
+				CheckIndividualDataStream: check,
+			},
+			ingestStep{
+				IgnoreDataStreams:         ignoredDataStreams,
+				CheckIndividualDataStream: check,
+			},
+			checkErrorLogsStep{
+				ESErrorLogsIgnored: esErrorLogs{
+					eventLoopShutdown,
+				},
+				APMErrorLogsIgnored: apmErrorLogs{
+					tlsHandshakeError,
+					esReturnedUnknown503,
+					refreshCache503,
+					populateSourcemapFetcher403,
+					refreshCache403,
+					refreshCacheESConfigInvalid,
+				},
+			},
+		},
+	}
+}
+
+func managed9Runner(fromVersion7, toVersion8, toVersion9 ecclient.StackVersion) testStepsRunner {
+	// Data streams in 8.x should be all ILM if upgraded to a stack < 8.15 and > 8.16.
+	checkILMAll := asserts.CheckDataStreamsWant{
+		Quantity:         8,
+		PreferIlm:        true,
+		DSManagedBy:      managedByILM,
+		IndicesPerDS:     1,
+		IndicesManagedBy: []string{managedByILM},
+	}
+
+	return testStepsRunner{
+		Steps: []testStep{
+			// Start from 7.x.
+			createStep{
+				DeployVersion:     fromVersion7,
+				APMDeploymentMode: apmStandalone,
+			},
+			ingestV7Step{},
+			// Upgrade to 8.x.
+			upgradeV7Step{NewVersion: toVersion8},
+			ingestStep{CheckDataStream: checkILMAll},
+			// Resolve deprecations and upgrade to 9.x.
+			resolveDeprecationsStep{},
+			upgradeStep{
+				NewVersion:      toVersion9,
+				CheckDataStream: checkILMAll,
+			},
+			ingestStep{CheckDataStream: checkILMAll},
+			// Migrate to managed.
+			migrateManagedStep{},
+			ingestStep{CheckDataStream: checkILMAll},
+			checkErrorLogsStep{
+				ESErrorLogsIgnored: esErrorLogs{
+					eventLoopShutdown,
+				},
+				APMErrorLogsIgnored: apmErrorLogs{
+					tlsHandshakeError,
+					esReturnedUnknown503,
+					refreshCache503,
 					populateSourcemapFetcher403,
 					refreshCache403,
 					refreshCacheESConfigInvalid,
