3a7334efc6789a0784ecfdb3d223ac456474f389
Isaac Flores <34590010+isaacaflores2@users.noreply.github.com>
2025-06-10 14:02:52 -0700

    tbs: Update storage metrics to be reported synchronously in the existing `runDiskUsageLoop` method (#17154)
    * tbs: Update storage metrics to be reported synchronously in the existing `runDiskUsageLoop` method
    
    * Update `runDiskUsageLoop` to update disk usage right away before regular intervals
    
    * Update main_test.go to reduce wait time since `runDiskUsageLoop` is run immediately
    
    * Update main_test.go to use assert.Eventually within the runServerFunc
    
    * Fix import grouping
    
    (cherry picked from commit d147b7af60aff2b58d9db41d4f01015386389b54)
    
    # Conflicts:
    #	x-pack/apm-server/main_test.go
    #	x-pack/apm-server/sampling/eventstorage/storage_manager.go
    #	x-pack/apm-server/sampling/processor_test.go

--- x-pack/apm-server/main_test.go
+++ x-pack/apm-server/main_test.go
@@ -57,6 +57,17 @@ func TestMonitoring(t *testing.T) {
 
 	// Wrap & run the server twice, to ensure metric registration does not panic.
 	runServerError := errors.New("runServer")
+	runServerFunc := func(ctx context.Context, args beater.ServerParams) error {
+		// run server for some time until storage metrics are reported by the storage manager
+		assert.EventuallyWithT(t, func(c *assert.CollectT) {
+			monitoringtest.ExpectContainOtelMetricsKeys(t, reader, []string{
+				"apm-server.sampling.tail.storage.lsm_size",
+				"apm-server.sampling.tail.storage.value_log_size",
+			})
+		}, time.Second, 10*time.Millisecond)
+
+		return runServerError
+	}
 	for i := 0; i < 2; i++ {
 		serverParams, runServer, err := wrapServer(beater.ServerParams{
 			Config:                 cfg,
@@ -66,17 +77,18 @@ func TestMonitoring(t *testing.T) {
 			BatchProcessor:         modelpb.ProcessBatchFunc(func(ctx context.Context, b *modelpb.Batch) error { return nil }),
 			Namespace:              "default",
 			NewElasticsearchClient: elasticsearch.NewClient,
-		}, func(ctx context.Context, args beater.ServerParams) error {
-			return runServerError
-		})
+		}, runServerFunc)
 		require.NoError(t, err)
 
 		err = runServer(context.Background(), serverParams)
 		assert.Equal(t, runServerError, err)
+<<<<<<< HEAD
 		monitoringtest.ExpectOtelMetrics(t, reader, map[string]any{
 			"apm-server.sampling.tail.storage.lsm_size":       0,
 			"apm-server.sampling.tail.storage.value_log_size": 0,
 		})
+=======
+>>>>>>> d147b7af (tbs: Update storage metrics to be reported synchronously in the existing `runDiskUsageLoop` method (#17154))
 	}
 }
 
--- x-pack/apm-server/sampling/eventstorage/storage_manager.go
+++ x-pack/apm-server/sampling/eventstorage/storage_manager.go
@@ -26,6 +26,37 @@ const (
 	// subscriberPositionFile holds the file name used for persisting
 	// the subscriber position across server restarts.
 	subscriberPositionFile = "subscriber_position.json"
+<<<<<<< HEAD
+=======
+
+	// partitionsPerTTL holds the number of partitions that events in 1 TTL should be stored over.
+	// Increasing partitionsPerTTL increases read amplification, but decreases storage overhead,
+	// as TTL GC can be performed sooner.
+	//
+	// For example, partitionPerTTL=1 means we need to keep 2 partitions active,
+	// such that the last entry in the previous partition is also kept for a full TTL.
+	// This means storage requirement is 2 * TTL, and it needs to read 2 keys per trace ID read.
+	// If partitionPerTTL=2, storage requirement is 1.5 * TTL at the expense of 3 reads per trace ID read.
+	partitionsPerTTL = 1
+
+	// reservedKeyPrefix is the prefix of internal keys used by StorageManager
+	reservedKeyPrefix byte = '~'
+
+	// partitionerMetaKey is the key used to store partitioner metadata, e.g. last partition ID, in decision DB.
+	partitionerMetaKey = string(reservedKeyPrefix) + "partitioner"
+
+	// diskUsageFetchInterval is how often disk usage is fetched which is equivalent to how long disk usage is cached.
+	diskUsageFetchInterval = 1 * time.Second
+
+	// dbStorageLimitFallback is the default fallback storage limit in bytes
+	// that applies when disk usage threshold cannot be enforced due to an error.
+	dbStorageLimitFallback = 3 << 30
+
+	// defaultValueLogSize default is 0 because pebble does not have a vlog.
+	defaultValueLogSize = 0
+
+	gb = float64(1 << 30)
+>>>>>>> d147b7af (tbs: Update storage metrics to be reported synchronously in the existing `runDiskUsageLoop` method (#17154))
 )
 
 var (
@@ -51,10 +82,28 @@ type StorageManager struct {
 	// runCh acts as a mutex to ensure only 1 Run is actively running per StorageManager.
 	// as it is possible that 2 separate Run are created by 2 TBS processors during a hot reload.
 	runCh chan struct{}
+<<<<<<< HEAD
 }
 
 // NewStorageManager returns a new StorageManager with badger DB at storageDir.
 func NewStorageManager(storageDir string) (*StorageManager, error) {
+=======
+
+	// meterProvider is the OTel meter provider
+	meterProvider  metric.MeterProvider
+	storageMetrics storageMetrics
+
+	metricRegistration metric.Registration
+}
+
+type storageMetrics struct {
+	lsmSizeGauge      metric.Int64Gauge
+	valueLogSizeGauge metric.Int64Gauge
+}
+
+// NewStorageManager returns a new StorageManager with pebble DB at storageDir.
+func NewStorageManager(storageDir string, logger *logp.Logger, opts ...StorageManagerOptions) (*StorageManager, error) {
+>>>>>>> d147b7af (tbs: Update storage metrics to be reported synchronously in the existing `runDiskUsageLoop` method (#17154))
 	sm := &StorageManager{
 		storageDir: storageDir,
 		runCh:      make(chan struct{}, 1),
@@ -64,6 +113,24 @@ func NewStorageManager(storageDir string) (*StorageManager, error) {
 	if err != nil {
 		return nil, err
 	}
+<<<<<<< HEAD
+=======
+	for _, opt := range opts {
+		opt(sm)
+	}
+
+	if sm.meterProvider != nil {
+		meter := sm.meterProvider.Meter("github.com/elastic/apm-server/x-pack/apm-server/sampling/eventstorage")
+
+		sm.storageMetrics.lsmSizeGauge, _ = meter.Int64Gauge("apm-server.sampling.tail.storage.lsm_size")
+		sm.storageMetrics.valueLogSizeGauge, _ = meter.Int64Gauge("apm-server.sampling.tail.storage.value_log_size")
+	}
+
+	if err := sm.reset(); err != nil {
+		return nil, fmt.Errorf("storage manager reset error: %w", err)
+	}
+
+>>>>>>> d147b7af (tbs: Update storage metrics to be reported synchronously in the existing `runDiskUsageLoop` method (#17154))
 	return sm, nil
 }
 
@@ -94,10 +161,127 @@ func (s *StorageManager) Size() (lsm, vlog int64) {
 	return s.db.Size()
 }
 
+<<<<<<< HEAD
 func (s *StorageManager) NewTransaction(update bool) *badger.Txn {
 	s.mu.RLock()
 	defer s.mu.RUnlock()
 	return s.db.NewTransaction(update)
+=======
+// savePartitionID saves the partition ID to database to be loaded by loadPartitionID later.
+func (sm *StorageManager) savePartitionID(pid int) error {
+	b, err := json.Marshal(partitionerMeta{ID: pid})
+	if err != nil {
+		return fmt.Errorf("error marshaling partition ID: %w", err)
+	}
+	return sm.decisionDB.Set([]byte(partitionerMetaKey), b, pebble.NoSync)
+}
+
+func (sm *StorageManager) Size() (lsm, vlog int64) {
+	// This is reporting lsm and vlog for legacy reasons.
+	// vlog is always 0 because pebble does not have a vlog.
+	// Keeping this legacy structure such that the metrics are comparable across versions,
+	// and we don't need to update the tooling, e.g. kibana dashboards.
+	//
+	// TODO(carsonip): Update this to report a more helpful size to monitoring,
+	// maybe broken down into event DB vs decision DB, and LSM tree vs WAL vs misc.
+	// Also remember to update
+	// - x-pack/apm-server/sampling/processor.go:CollectMonitoring
+	// - systemtest/benchtest/expvar/metrics.go
+	return int64(sm.dbSize()), defaultValueLogSize
+}
+
+// dbSize returns the disk usage of databases in bytes.
+func (sm *StorageManager) dbSize() uint64 {
+	// pebble DiskSpaceUsage overhead is not high, but it adds up when performed per-event.
+	return sm.cachedDBSize.Load()
+}
+
+func (sm *StorageManager) updateDiskUsage() {
+	lsmSize := sm.getDBSize()
+	sm.cachedDBSize.Store(lsmSize)
+
+	if sm.storageMetrics.lsmSizeGauge != nil {
+		sm.storageMetrics.lsmSizeGauge.Record(context.Background(), int64(lsmSize))
+	}
+	if sm.storageMetrics.valueLogSizeGauge != nil {
+		sm.storageMetrics.valueLogSizeGauge.Record(context.Background(), int64(defaultValueLogSize))
+	}
+
+	if sm.getDiskUsageFailed.Load() {
+		// Skip GetDiskUsage under the assumption that
+		// it will always get the same error if GetDiskUsage ever returns one,
+		// such that it does not keep logging GetDiskUsage errors.
+		return
+	}
+	usage, err := sm.getDiskUsage()
+	if err != nil {
+		sm.logger.With(logp.Error(err)).Warn("failed to get disk usage")
+		sm.getDiskUsageFailed.Store(true)
+		sm.cachedDiskStat.used.Store(0)
+		sm.cachedDiskStat.total.Store(0) // setting total to 0 to disable any running disk usage threshold checks
+		return
+	}
+	sm.cachedDiskStat.used.Store(usage.UsedBytes)
+	sm.cachedDiskStat.total.Store(usage.TotalBytes)
+}
+
+// diskUsed returns the actual used disk space in bytes.
+// Not to be confused with dbSize which is specific to database.
+func (sm *StorageManager) diskUsed() uint64 {
+	return sm.cachedDiskStat.used.Load()
+}
+
+// runDiskUsageLoop runs a loop that updates cached disk usage regularly and reports usage.
+func (sm *StorageManager) runDiskUsageLoop(stopping <-chan struct{}) error {
+	// initial disk usage update so data is available immediately
+	sm.updateDiskUsage()
+
+	ticker := time.NewTicker(diskUsageFetchInterval)
+	defer ticker.Stop()
+	for {
+		select {
+		case <-stopping:
+			return nil
+		case <-ticker.C:
+			sm.updateDiskUsage()
+		}
+	}
+}
+
+func (sm *StorageManager) Flush() error {
+	return errors.Join(
+		wrapNonNilErr("event db flush error: %w", sm.eventDB.Flush()),
+		wrapNonNilErr("decision db flush error: %w", sm.decisionDB.Flush()),
+	)
+}
+
+func (sm *StorageManager) Close() error {
+	return sm.close()
+}
+
+func (sm *StorageManager) close() error {
+	if sm.metricRegistration != nil {
+		if err := sm.metricRegistration.Unregister(); err != nil {
+			sm.logger.With(logp.Error(err)).Error("failed to unregister metric")
+		}
+	}
+	return errors.Join(
+		wrapNonNilErr("event db flush error: %w", sm.eventDB.Flush()),
+		wrapNonNilErr("decision db flush error: %w", sm.decisionDB.Flush()),
+		wrapNonNilErr("event db close error: %w", sm.eventDB.Close()),
+		wrapNonNilErr("decision db close error: %w", sm.decisionDB.Close()),
+	)
+}
+
+// Reload flushes out pending disk writes to disk by reloading the database.
+// For testing only.
+// Read writers created prior to Reload cannot be used and will need to be recreated via NewUnlimitedReadWriter.
+func (sm *StorageManager) Reload() error {
+	if err := sm.close(); err != nil {
+		return err
+	}
+	return sm.reset()
+>>>>>>> d147b7af (tbs: Update storage metrics to be reported synchronously in the existing `runDiskUsageLoop` method (#17154))
 }
 
 // Run has the same lifecycle as the TBS processor as opposed to StorageManager to facilitate EA hot reload.
