ba54982d60fe91e7df2895e60bbe7f5d6d62961e
kruskal <99559985+kruskall@users.noreply.github.com>
2025-06-25 22:08:18 +0200

    feat: bump beats and drop global beat registries
    replace global registries with instance-scoped registries

--- go.mod
+++ go.mod
@@ -9,7 +9,7 @@ require (
 	github.com/dustin/go-humanize v1.0.1
 	github.com/elastic/apm-aggregation v1.3.0
 	github.com/elastic/apm-data v1.19.2
-	github.com/elastic/beats/v7 v7.0.0-alpha2.0.20250618141650-b9ce1006dc2e
+	github.com/elastic/beats/v7 v7.0.0-alpha2.0.20250625112624-7bfa78e27177
 	github.com/elastic/elastic-agent-client/v7 v7.17.2
 	github.com/elastic/elastic-agent-libs v0.20.1
 	github.com/elastic/elastic-agent-system-metrics v0.11.16
--- internal/beater/beater_test.go
+++ internal/beater/beater_test.go
@@ -120,9 +120,7 @@ func sourcemapSearchResponseBody(name string, version string, bundlePath string)
 }
 
 func TestQueryClusterUUIDRegistriesExist(t *testing.T) {
-	stateRegistry := monitoring.GetNamespace("state").GetRegistry()
-	stateRegistry.Clear()
-	defer stateRegistry.Clear()
+	stateRegistry := monitoring.NewRegistry()
 
 	elasticsearchRegistry := stateRegistry.NewRegistry("outputs.elasticsearch")
 	monitoring.NewString(elasticsearchRegistry, "cluster_uuid")
@@ -130,7 +128,7 @@ func TestQueryClusterUUIDRegistriesExist(t *testing.T) {
 	const clusterUUID = "abc123"
 	client := newMockClusterUUIDClient(t, clusterUUID)
 
-	err := queryClusterUUID(context.Background(), client)
+	err := queryClusterUUID(context.Background(), client, stateRegistry)
 	require.NoError(t, err)
 
 	fs := monitoring.CollectFlatSnapshot(elasticsearchRegistry, monitoring.Full, false)
@@ -138,14 +136,12 @@ func TestQueryClusterUUIDRegistriesExist(t *testing.T) {
 }
 
 func TestQueryClusterUUIDRegistriesDoNotExist(t *testing.T) {
-	stateRegistry := monitoring.GetNamespace("state").GetRegistry()
-	stateRegistry.Clear()
-	defer stateRegistry.Clear()
+	stateRegistry := monitoring.NewRegistry()
 
 	const clusterUUID = "abc123"
 	client := newMockClusterUUIDClient(t, clusterUUID)
 
-	err := queryClusterUUID(context.Background(), client)
+	err := queryClusterUUID(context.Background(), client, stateRegistry)
 	require.NoError(t, err)
 
 	elasticsearchRegistry := stateRegistry.GetRegistry("outputs.elasticsearch")
