774bbdaae4c38a06345608c99f27200c1cc493ce
Isaac Flores <34590010+isaacaflores2@users.noreply.github.com>
2025-06-30 14:03:36 -0700

    smoke: update scripts to use `snapshots.elastic.co` instead `artifacts-api.elastic.co`to get the apm-server and elastic agent download urls (#17377)
    Improve smoke test reliability since `artifacts-api.elastic.co` is not always available
    
    (cherry picked from commit b5e8eb59b146bc67156206af2c8808da0e33d8a1)
    
    # Conflicts:
    #	testing/infra/terraform/modules/standalone_apm_server/latest_apm_server.sh
    #	testing/infra/terraform/modules/standalone_apm_server/latest_elastic_agent.sh

--- testing/infra/terraform/modules/standalone_apm_server/latest_elastic_agent.sh
+++ testing/infra/terraform/modules/standalone_apm_server/latest_elastic_agent.sh
@@ -4,8 +4,19 @@ set -eo pipefail
 
 VERSION=${1}
 if [[ -z ${VERSION} ]] || [[ "${VERSION}" == "latest" ]]; then
+<<<<<<< HEAD
     VERSION=$(curl -s "https://artifacts-api.elastic.co/v1/versions" | jq -r '[.versions[] | select(. | startswith("8"))] | last')
+=======
+  LATEST_VERSION_INFO=$(curl -s "https://snapshots.elastic.co/latest/master.json")
+else
+  LATEST_VERSION_INFO=$(curl -s "https://snapshots.elastic.co/latest/${VERSION}.json")
+>>>>>>> b5e8eb59 (smoke: update scripts to use `snapshots.elastic.co` instead `artifacts-api.elastic.co`to get the apm-server and elastic agent download urls (#17377))
 fi
-LATEST_BUILD=$(curl -s "https://artifacts-api.elastic.co/v1/versions/${VERSION}/builds/" | jq -r '.builds[0]')
 
-curl -s "https://artifacts-api.elastic.co/v1/versions/${VERSION}/builds/${LATEST_BUILD}/projects/elastic-agent-package" | jq -r ".project.packages | {deb_amd: .\"elastic-agent-${VERSION}-amd64.deb\".url, deb_arm: .\"elastic-agent-${VERSION}-arm64.deb\".url, rpm_amd: .\"elastic-agent-${VERSION}-x86_64.rpm\".url, rpm_arm: .\"elastic-agent-${VERSION}-aarch64.rpm\".url }"
+# change snapshot version
+VERSION=$(echo $LATEST_VERSION_INFO | jq -r '.version')
+
+MANIFEST_URL=$(echo $LATEST_VERSION_INFO | jq -r '.manifest_url')
+
+# get the download urls
+curl -s "$MANIFEST_URL" | jq -r ".projects.\"elastic-agent-package\".packages | {deb_amd: .\"elastic-agent-${VERSION}-amd64.deb\".url, deb_arm: .\"elastic-agent-${VERSION}-arm64.deb\".url, rpm_amd: .\"elastic-agent-${VERSION}-x86_64.rpm\".url, rpm_arm: .\"elastic-agent-${VERSION}-aarch64.rpm\".url }"
