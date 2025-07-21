f206d50a0c2af96c7b398d59a8824ebd276fbdf6
Victor Martinez <VictorMartinezRubio@gmail.com>
2025-07-21 22:38:47 +0200

    simplify

--- .ci/scripts/update-beats.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-#
-# This script is executed by the automation we are putting in place
-# and it requires the git add/commit commands.
-#
-set -euo pipefail
-
-make update-beats BEATS_VERSION="${1}"
