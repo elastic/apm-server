8ec684162161262d4867c73afb077c0e311fd7c9
kruskal <99559985+kruskall@users.noreply.github.com>
2025-08-25 19:13:57 +0200

    lint: fix linter issues

--- internal/elasticsearch/security_api_test.go
+++ internal/elasticsearch/security_api_test.go
@@ -25,9 +25,10 @@ import (
 	"net/http/httptest"
 	"testing"
 
-	"github.com/elastic/elastic-agent-libs/logp/logptest"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+
+	"github.com/elastic/elastic-agent-libs/logp/logptest"
 )
 
 func TestHasPrivilegesError(t *testing.T) {
