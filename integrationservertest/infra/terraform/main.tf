3acc4a25b4f95ff8ec8546286a3b9198a75cb6d4
Eric Yap <eric.yap@elastic.co>
2025-09-09 20:24:23 +0800

    Format tf

--- integrationservertest/infra/terraform/main.tf
+++ integrationservertest/infra/terraform/main.tf
@@ -17,7 +17,7 @@ module "ec_deployment" {
   stack_version       = var.stack_version
   integrations_server = var.integrations_server
 
-  docker_image = var.ec_docker_image_override
+  docker_image              = var.ec_docker_image_override
   docker_image_tag_override = var.ec_docker_image_tag_override
 
   tags = merge(local.ci_tags, module.tags.tags)
