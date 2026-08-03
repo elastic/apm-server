eb6b71eb98bbc9fabd0cb49e3a06aa67f4220e48
kruskall <99559985+kruskall@users.noreply.github.com>
2026-08-03 18:19:04 +0200

    Update groups.go

--- x-pack/apm-server/sampling/groups.go
+++ x-pack/apm-server/sampling/groups.go
@@ -238,8 +238,8 @@ func (g *traceGroup) finalizeSampledTraces(traceIDs []string, ingestRateDecayFac
 
 	// Resize the reservoir, so that it can hold the desired fraction of
 	// the observed ingest rate.
-	newReservoirSize := max(math.Ceil(g.samplingFraction*g.ingestRate), minReservoirSize)
+	newReservoirSize := max(int(math.Ceil(g.samplingFraction*g.ingestRate), minReservoirSize))
 	g.reservoir.Reset()
-	g.reservoir.Resize(int(newReservoirSize))
+	g.reservoir.Resize(newReservoirSize)
 	return oldTotal, traceIDs
 }
